aba87657e7f3a7edde571f2b2e93163e48bb905a
Jesse Vincent <jesse@fsck.com>
2008-12-03 04:14:41 +0000

    switch from pages of 25 messages to pages of 100

--- src/com/fsck/k9/k9.java
+++ src/com/fsck/k9/k9.java
@@ -84,12 +84,12 @@ public class k9 extends Application {
      * on each new folder and can be incremented with "Load more messages..." by the
      * VISIBLE_LIMIT_INCREMENT
      */
-    public static final int DEFAULT_VISIBLE_LIMIT = 25;
+    public static final int DEFAULT_VISIBLE_LIMIT = 100;
 
     /**
      * Number of additional messages to load when a user selectes "Load more messages..."
      */
-    public static final int VISIBLE_LIMIT_INCREMENT = 25;
+    public static final int VISIBLE_LIMIT_INCREMENT = 100;
 
     /**
      * The maximum size of an attachment we're willing to download (either View or Save)
