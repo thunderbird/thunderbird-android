d55e335f48db42414ef8761a24f9669907dcbc2c
Jesse Vincent <jesse@fsck.com>
2008-12-19 00:31:17 +0000

    merge extra crap

--- src/com/android/k9/mail/MessageRetrievalListener.java
+++
@@ -1,8 +0,0 @@
-
-package com.fsck.k9.mail;
-
-public interface MessageRetrievalListener {
-    public void messageStarted(String uid, int number, int ofTotal);
-
-    public void messageFinished(Message message, int number, int ofTotal);
-}
