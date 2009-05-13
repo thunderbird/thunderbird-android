82bf8abb11c126c6c246fd660dd90407756b72a5
Jesse Vincent <jesse@fsck.com>
2009-05-13 02:23:51 +0000

    Update Welcome activity to open an ml instead of an fml

--- src/com/android/email/activity/Welcome.java
+++ src/com/android/email/activity/Welcome.java
@@ -14,7 +14,7 @@ import android.os.Bundle;
  * the user should start with.
  * If no accounts are configured the user is taken to the Accounts Activity where they
  * can configure an account.
- * If a single account is configured the user is taken directly to the FolderMessageList for
+ * If a single account is configured the user is taken directly to the MessageList for
  * the INBOX of that account.
  * If more than one account is configuref the user is takaen to the Accounts Activity so they
  * can select an account.
@@ -26,7 +26,7 @@ public class Welcome extends Activity {
 
         Account[] accounts = Preferences.getPreferences(this).getAccounts();
         if (accounts.length == 1) {
-            FolderMessageList.actionHandleAccount(this, accounts[0], Email.INBOX);
+            MessageList.actionHandleFolder(this, accounts[0], Email.INBOX);
         } else {
             startActivity(new Intent(this, Accounts.class));
         }
