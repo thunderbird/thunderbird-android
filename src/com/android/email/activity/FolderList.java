931cc4c243ed87f84f1325db4783a3dbe133fbb6
Bao-Long Nguyen-Trong <baolongnt@gmail.com>
2009-09-25 21:02:25 +0000

    Minor changes to help with issue 636

--- src/com/android/email/activity/FolderList.java
+++ src/com/android/email/activity/FolderList.java
@@ -395,6 +395,7 @@ public class FolderList extends K9ListActivity {
         }
         else {
             mInitialFolder = null;
+            mStartup = false;
         }
 
         Log.v(Email.LOG_TAG, "mInitialFolder: " + mInitialFolder);
@@ -537,7 +538,9 @@ public class FolderList extends K9ListActivity {
         // we won't have a parent activity and we'll need to get back to it
         if (mStartup
             || isTaskRoot()) {
-            startActivity(new Intent(this, Accounts.class));
+            Intent intent = new Intent(this, Accounts.class);
+            intent.putExtra(Accounts.EXTRA_STARTUP, false);
+            startActivity(intent);
         }
         finish();
     }
