f318fc159b82393746b130446e68ec2a6062bf39
Daniel Applebaum <danapple@danapple.com>
2010-05-20 11:08:40 +0000

    Fixes Issue 1641
    Populate text and subject when handling ACTION_SENDTO

--- src/com/fsck/k9/activity/MessageCompose.java
+++ src/com/fsck/k9/activity/MessageCompose.java
@@ -395,7 +395,9 @@ public class MessageCompose extends K9Activity implements OnClickListener, OnFoc
             }
         }
         //TODO: Use constant Intent.ACTION_SEND_MULTIPLE once we drop Android 1.5 support
-        else if (Intent.ACTION_SEND.equals(action) || "android.intent.action.SEND_MULTIPLE".equals(action))
+        else if (Intent.ACTION_SEND.equals(action)
+                || Intent.ACTION_SENDTO.equals(action)
+                || "android.intent.action.SEND_MULTIPLE".equals(action))
         {
             /*
              * Someone is trying to compose an email with an attachment, probably Pictures.
