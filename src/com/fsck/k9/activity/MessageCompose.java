5e30c3dab1f22f833d1e09d5e540492b589ce68d
Thialfihar <thi@thialfihar.org>
2010-07-28 12:10:08 +0000

    removed two extra lines from the merge in r2055

--- src/com/fsck/k9/activity/MessageCompose.java
+++ src/com/fsck/k9/activity/MessageCompose.java
@@ -1080,8 +1080,6 @@ public class MessageCompose extends K9Activity implements OnClickListener, OnFoc
         {
             Toast.makeText(this, R.string.send_aborted, Toast.LENGTH_SHORT).show();
         }
-        mDraftNeedsSaving = false;
-        saveMessage();
     }
 
     private void onSend()
