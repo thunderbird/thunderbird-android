9f7b4471812a975930cffb692a9e19b81faaff8f
cketti <cketti@googlemail.com>
2010-12-01 18:14:12 +0000

    POP3: Ignore messages without unique-id when parsing UIDL response
    Fixes issue 2731

--- src/com/fsck/k9/mail/store/Pop3Store.java
+++ src/com/fsck/k9/mail/store/Pop3Store.java
@@ -596,21 +596,26 @@ public class Pop3Store extends Store
                     break;
                 }
                 String[] uidParts = response.split(" ");
-                Integer msgNum = Integer.valueOf(uidParts[0]);
-                String msgUid = uidParts[1];
-                if (unindexedUids.contains(msgUid))
-                {
-                    if (K9.DEBUG && K9.DEBUG_PROTOCOL_POP3)
-                    {
-                        Log.d(K9.LOG_TAG, "Got msgNum " + msgNum + " for UID " + msgUid);
-                    }
 
-                    Pop3Message message = mUidToMsgMap.get(msgUid);
-                    if (message == null)
-                    {
-                        message = new Pop3Message(msgUid, this);
-                    }
-                    indexMessage(msgNum, message);
+                // Ignore messages without a unique-id
+                if (uidParts.length >= 2)
+                {
+	                Integer msgNum = Integer.valueOf(uidParts[0]);
+	                String msgUid = uidParts[1];
+	                if (unindexedUids.contains(msgUid))
+	                {
+	                    if (K9.DEBUG && K9.DEBUG_PROTOCOL_POP3)
+	                    {
+	                        Log.d(K9.LOG_TAG, "Got msgNum " + msgNum + " for UID " + msgUid);
+	                    }
+
+	                    Pop3Message message = mUidToMsgMap.get(msgUid);
+	                    if (message == null)
+	                    {
+	                        message = new Pop3Message(msgUid, this);
+	                    }
+	                    indexMessage(msgNum, message);
+	                }
                 }
             }
         }
