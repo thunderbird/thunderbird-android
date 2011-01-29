68fae6c320c8139391e004cd900a01cb3d565fb4
Jesse Vincent <jesse@fsck.com>
2011-01-29 19:01:03 +0000

    Fix range check for ISO-2022-JP stream.
    Signed-off-by: HIRANO Takahito <hiranotaka@zng.info>

--- src/com/fsck/k9/mail/internet/Iso2022JpToShiftJisInputStream.java
+++ src/com/fsck/k9/mail/internet/Iso2022JpToShiftJisInputStream.java
@@ -60,7 +60,7 @@ class Iso2022JpToShiftJisInputStream extends InputStream
         if (in1 == '\n' || in1 == '\r')
             charset = Charset.ASCII;
 
-        if (in1 < 0x21 || in1 >= 0x7e)
+        if (in1 < 0x21 || in1 >= 0x7f)
             return in1;
 
         switch (charset)
@@ -71,7 +71,7 @@ class Iso2022JpToShiftJisInputStream extends InputStream
                 return in1 + 0x80;
             case JISX0208:
                 int in2 = mIn.read();
-                if (in2 < 0x21 || in2 >= 0x7e)
+                if (in2 < 0x21 || in2 >= 0x7f)
                     throw new MalformedInputException(0);
 
                 int out1 = (in1 + 1) / 2 + (in1 < 0x5f ? 0x70 : 0xb0);
