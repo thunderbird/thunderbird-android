d634bcb38136ae8e968f9f4ce2231b15da912b47
Jesse Vincent <jesse@bestpractical.com>
2011-02-10 19:29:46 -0500

    Switch the Message view to our new SingleMessageView

---
+++ src/com/fsck/k9/view/SingleMessageView.java
@@ -0,0 +1,13 @@
+package com.fsck.k9.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ */
+public class SingleMessageView extends LinearLayout{
+    public SingleMessageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
