7d5bb99dbdf2d42f77551c10d3851d336f63137f
danapple <danapple@danapple.com>
2011-03-27 12:33:07 -0500

    Add progress indicator during account move.

--- src/com/fsck/k9/activity/Accounts.java
+++ src/com/fsck/k9/activity/Accounts.java
@@ -656,6 +656,7 @@ public class Accounts extends K9ListActivity implements OnItemClickListener, OnC
         showDialog(DIALOG_RECREATE_ACCOUNT);
     }
     private void onMove(final Account account, final boolean up) {
+        mHandler.progress(true);
         AsyncUIProcessor.getInstance(getApplication()).execute(
                 new Runnable()
                 {
@@ -666,6 +667,7 @@ public class Accounts extends K9ListActivity implements OnItemClickListener, OnC
                             @Override
                             public void run() {
                                 refresh();
+                                mHandler.progress(false);
                             }
                         });
                     }
