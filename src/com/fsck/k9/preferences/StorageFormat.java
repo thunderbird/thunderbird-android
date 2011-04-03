6b5ac11855dd2cc24daafc9d0e876fa46eaa38fe
danapple <danapple@danapple.com>
2011-04-03 15:10:04 -0500

    Reflectively query the exporter for whether it needs a key.

--- src/com/fsck/k9/preferences/StorageFormat.java
+++ src/com/fsck/k9/preferences/StorageFormat.java
@@ -1,10 +1,15 @@
 package com.fsck.k9.preferences;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import android.util.Log;
+
+import com.fsck.k9.K9;
 import com.fsck.k9.R;
 
 
@@ -17,9 +22,9 @@ public class StorageFormat {
     public static Map<String, StorageFormat> storageFormatMap = new HashMap<String, StorageFormat>();
     public static List<String> presentableVersions = new LinkedList<String>();
     static {
-        storageFormatMap.put(ENCRYPTED_KEY_VALUE, new StorageFormat(StorageImporterEncryptedKeyValue.class, StorageExporterObsolete.class, false, null));
-        storageFormatMap.put(ENCRYPTED_BLOB, new StorageFormat(StorageImporterEncryptedBlob.class, StorageExporterEncryptedBlob.class, true, R.string.settings_format_encrypted));
-        storageFormatMap.put(ENCRYPTED_URL_ENCODED, new StorageFormat(StorageImporterUrlEncoded.class, StorageExporterUrlEncoded.class, false, null /*R.string.settings_format_unencrypted */));  // Uncomment resource id when made release-ready
+        storageFormatMap.put(ENCRYPTED_KEY_VALUE, new StorageFormat(StorageImporterEncryptedKeyValue.class, StorageExporterObsolete.class, null));
+        storageFormatMap.put(ENCRYPTED_BLOB, new StorageFormat(StorageImporterEncryptedBlob.class, StorageExporterEncryptedBlob.class, R.string.settings_format_encrypted));
+        storageFormatMap.put(ENCRYPTED_URL_ENCODED, new StorageFormat(StorageImporterUrlEncoded.class, StorageExporterUrlEncoded.class, null /*R.string.settings_format_unencrypted */));  // Uncomment resource id when made release-ready
         
         for (Map.Entry<String, StorageFormat> entry : storageFormatMap.entrySet()) {
             if (entry.getValue().presentableResource != null) {
@@ -57,24 +62,36 @@ public class StorageFormat {
         return storageVersion.exporterClass.newInstance();
     }
 
-    public static Boolean needsKey(String storageFormat) {
+    public static Boolean exportNeedsKey(String storageFormat) {
         StorageFormat storageVersion = storageFormatMap.get(storageFormat);
         if (storageVersion == null) {
             return null;
         }
-        return storageVersion.needsKey;
+        try {
+            Method method = storageVersion.exporterClass.getMethod("needsKey");
+            if (method == null) {
+                return null;
+            }
+            Object obj = method.invoke(null);
+            if (obj != null && obj instanceof Boolean) {
+                return (Boolean)obj;
+            }
+        }
+        catch (Exception e) {
+            Log.e(K9.LOG_TAG, "Unable to reflectively invoke needsKey on exporter for format " + storageFormat);
+            return null;
+        }
+        return null;
     }
 
 
     private final Class <? extends IStorageImporter > importerClass;
     private final Class <? extends IStorageExporter > exporterClass;
-    private final boolean needsKey;
     private final Integer presentableResource;
 
-    private StorageFormat(Class <? extends IStorageImporter > imclass, Class <? extends IStorageExporter > exclass, boolean nk, Integer present) {
+    private StorageFormat(Class <? extends IStorageImporter > imclass, Class <? extends IStorageExporter > exclass, Integer present) {
         importerClass = imclass;
         exporterClass = exclass;
-        needsKey = nk;
         presentableResource = present;
     }
 }
