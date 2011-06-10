thunderbird/thunderbird-android

commit 79a96952f2b8c95c059afee3e564721a09ed98c1

Correct encoding and decoding for the filename parameter in the Content-Disposition.

src/com/fsck/k9/activity/MessageCompose.java
@@ -1121,7 +1121,9 @@ public class MessageCompose extends K9Activity implements OnClickListener, OnFoc
              */
             bp.addHeader(MimeHeader.HEADER_CONTENT_DISPOSITION, String.format(
                              "attachment;\n filename=\"%s\";\n size=%d",
-                             attachment.name, attachment.size));
+                             EncoderUtil.encodeIfNecessary(attachment.name, 
+                                     EncoderUtil.Usage.WORD_ENTITY, 7),
+                             attachment.size));
 
             mp.addBodyPart(bp);
         }

src/com/fsck/k9/mail/store/LocalStore.java
@@ -16,6 +16,7 @@ import java.util.UUID;
 
 import com.fsck.k9.helper.HtmlConverter;
 import org.apache.commons.io.IOUtils;
+import org.apache.james.mime4j.codec.EncoderUtil;
 
 import android.app.Application;
 import android.content.ContentValues;
@@ -1671,16 +1672,20 @@ public class LocalStore extends Store implements Serializable {
                                             if (contentUri != null) {
                                                 body = new LocalAttachmentBody(Uri.parse(contentUri), mApplication);
                                             }
+
+                                            String encoded_name = EncoderUtil.encodeIfNecessary(name,
+                                                    EncoderUtil.Usage.WORD_ENTITY, 7);
+
                                             MimeBodyPart bp = new LocalAttachmentBodyPart(body, id);
                                             bp.setHeader(MimeHeader.HEADER_CONTENT_TYPE,
                                                          String.format("%s;\n name=\"%s\"",
                                                                        type,
-                                                                       name));
+                                                                       encoded_name));
                                             bp.setHeader(MimeHeader.HEADER_CONTENT_TRANSFER_ENCODING, "base64");
                                             bp.setHeader(MimeHeader.HEADER_CONTENT_DISPOSITION,
                                                          String.format("%s;\n filename=\"%s\";\n size=%d",
                                                                        contentDisposition,
-                                                                       name,
+                                                                       encoded_name, // TODO: Should use encoded word defined in RFC 2231.
                                                                        size));
 
                                             bp.setHeader(MimeHeader.HEADER_CONTENT_ID, contentId);
@@ -2364,7 +2369,7 @@ public class LocalStore extends Store implements Serializable {
                                 Utility.combine(attachment.getHeader(
                                                     MimeHeader.HEADER_ANDROID_ATTACHMENT_STORE_DATA), ',');
 
-                            String name = MimeUtility.getHeaderParameter(attachment.getContentType(), "name");
+                            String name = MimeUtility.unfoldAndDecode(MimeUtility.getHeaderParameter(attachment.getContentType(), "name"));
                             String contentId = MimeUtility.getHeaderParameter(attachment.getContentId(), null);
 
                             String contentDisposition = MimeUtility.unfoldAndDecode(attachment.getDisposition());
@@ -2377,7 +2382,7 @@ public class LocalStore extends Store implements Serializable {
                             }
 
                             if (name == null && contentDisposition != null) {
-                                name = MimeUtility.getHeaderParameter(contentDisposition, "filename");
+                                name = MimeUtility.unfoldAndDecode(MimeUtility.getHeaderParameter(contentDisposition, "filename"));
                             }
                             if (attachmentId == -1) {
                                 ContentValues cv = new ContentValues();

commit 379a8dfa9ec462b7f9046d1b23af4363d0ab2652

It turns out that 79a96952f2b8c95c059afee3e564721a09ed98c1 has caused a lot of messages to be unreadable in K-9 3.8.  (see mailing list threads)

src/com/fsck/k9/activity/MessageCompose.java
@@ -1139,9 +1139,7 @@ public class MessageCompose extends K9Activity implements OnClickListener, OnFoc
              */
             bp.addHeader(MimeHeader.HEADER_CONTENT_DISPOSITION, String.format(
                              "attachment;\n filename=\"%s\";\n size=%d",
-                             EncoderUtil.encodeIfNecessary(attachment.name,
-                                     EncoderUtil.Usage.WORD_ENTITY, 7),
-                             attachment.size));
+                             attachment.name, attachment.size));
 
             mp.addBodyPart(bp);
         }

src/com/fsck/k9/mail/store/LocalStore.java
@@ -18,7 +18,6 @@ import java.util.regex.Pattern;
 
 import com.fsck.k9.helper.HtmlConverter;
 import org.apache.commons.io.IOUtils;
-import org.apache.james.mime4j.codec.EncoderUtil;
 
 import android.app.Application;
 import android.content.ContentValues;
@@ -1670,17 +1669,14 @@ public class LocalStore extends Store implements Serializable {
                                             MimeBodyPart bp = new LocalAttachmentBodyPart(body, id);
                                             bp.setHeader(MimeHeader.HEADER_CONTENT_TRANSFER_ENCODING, "base64");
                                             if (name != null) {
-                                                String encoded_name = EncoderUtil.encodeIfNecessary(name,
-                                                                      EncoderUtil.Usage.WORD_ENTITY, 7);
-
                                                 bp.setHeader(MimeHeader.HEADER_CONTENT_TYPE,
                                                              String.format("%s;\n name=\"%s\"",
                                                                            type,
-                                                                           encoded_name));
+                                                                           name));
                                                 bp.setHeader(MimeHeader.HEADER_CONTENT_DISPOSITION,
                                                              String.format("%s;\n filename=\"%s\";\n size=%d",
                                                                            contentDisposition,
-                                                                           encoded_name, // TODO: Should use encoded word defined in RFC 2231.
+                                                                           name, // TODO: Should use encoded word defined in RFC 2231.
                                                                            size));
                                             }
 
@@ -2383,7 +2379,7 @@ public class LocalStore extends Store implements Serializable {
                                 Utility.combine(attachment.getHeader(
                                                     MimeHeader.HEADER_ANDROID_ATTACHMENT_STORE_DATA), ',');
 
-                            String name = MimeUtility.unfoldAndDecode(MimeUtility.getHeaderParameter(attachment.getContentType(), "name"));
+                            String name = MimeUtility.getHeaderParameter(attachment.getContentType(), "name");
                             String contentId = MimeUtility.getHeaderParameter(attachment.getContentId(), null);
 
                             String contentDisposition = MimeUtility.unfoldAndDecode(attachment.getDisposition());
@@ -2398,7 +2394,7 @@ public class LocalStore extends Store implements Serializable {
                             }
 
                             if (name == null && contentDisposition != null) {
-                                name = MimeUtility.unfoldAndDecode(MimeUtility.getHeaderParameter(contentDisposition, "filename"));
+                                name = MimeUtility.getHeaderParameter(contentDisposition, "filename");
                             }
                             if (attachmentId == -1) {
                                 ContentValues cv = new ContentValues();