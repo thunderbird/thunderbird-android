fd44b58f0ba7c8c874930c7285732becfaab3fef
Bernhard Redl <bernhardredl@inode.at>
2011-09-19 01:46:40 +0200

    change Reply All to be like the thunderbird implementation (CC,everybody except sender)

--- src/com/fsck/k9/activity/MessageCompose.java
+++ src/com/fsck/k9/activity/MessageCompose.java
@@ -2012,7 +2012,7 @@ public class MessageCompose extends K9Activity implements OnClickListener, OnFoc
                 if (ACTION_REPLY_ALL.equals(action)) {
                     for (Address address : message.getRecipients(RecipientType.TO)) {
                         if (!mAccount.isAnIdentity(address)) {
-                            addAddress(mToView, address);
+                            addAddress(mCcView, address);
                         }
 
                     }
