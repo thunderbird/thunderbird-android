8c8b3eea9ed117998079d38824c892356d703514
cketti <cketti@gmail.com>
2011-11-10 05:44:30 +0100

    Use Intent.ACTION_SEND_MULTIPLE instead of string constant

--- src/com/fsck/k9/activity/MessageCompose.java
+++ src/com/fsck/k9/activity/MessageCompose.java
@@ -662,9 +662,7 @@ public class MessageCompose extends K9Activity implements OnClickListener, OnFoc
              * it wrong! So go fix your program or get AOSP to change the documentation.
              */
         }
-        //TODO: Use constant Intent.ACTION_SEND_MULTIPLE once we drop Android 1.5 support
-        else if (Intent.ACTION_SEND.equals(action) ||
-                 "android.intent.action.SEND_MULTIPLE".equals(action)) {
+        else if (Intent.ACTION_SEND.equals(action) || Intent.ACTION_SEND_MULTIPLE.equals(action)) {
             /*
              * Note: Here we allow a slight deviation from the documentated behavior.
              * EXTRA_TEXT is used as message body (if available) regardless of the MIME
