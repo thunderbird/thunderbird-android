85f556e6c9d72b1f73f296fc7a51aa633de19c4a
cketti <cketti@gmail.com>
2011-11-10 05:09:54 +0100

    Removed helper function to create AbsoluteSizeSpan objects

--- src/com/fsck/k9/activity/MessageList.java
+++ src/com/fsck/k9/activity/MessageList.java
@@ -18,6 +18,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
+import android.text.style.AbsoluteSizeSpan;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
 import android.util.Log;
@@ -2181,7 +2182,7 @@ public class MessageList
                                 0,
                                 noSender.length(),
                                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-                    str.setSpan(K9.createAbsoluteSizeSpan(mFontSizes.getMessageListSender()),
+                    str.setSpan(new AbsoluteSizeSpan(mFontSizes.getMessageListSender(), true),
                                 0,
                                 noSender.length(),
                                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
@@ -2270,7 +2271,7 @@ public class MessageList
                             0,
                             message.sender.length() + 1,
                             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-                str.setSpan(K9.createAbsoluteSizeSpan(mFontSizes.getMessageListSender()),
+                str.setSpan(new AbsoluteSizeSpan(mFontSizes.getMessageListSender(), true),
                             0,
                             message.sender.length() + 1,
                             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
