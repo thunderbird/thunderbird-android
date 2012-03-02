8d12244a9c23da0b5146afa418f75a4b7e49747d
m0viefreak <m0viefreak.cm@googlemail.com>
2012-03-02 05:41:01 +0100

    Message header changes
    * remove expand/collapse arrows, instead use the background area of the header to toggle
    * allow expanding of To: and Cc: texts when too long and cut off by clicking on them

--- src/com/fsck/k9/view/MessageHeader.java
+++ src/com/fsck/k9/view/MessageHeader.java
@@ -49,7 +49,6 @@ public class MessageHeader extends ScrollView implements OnClickListener {
     private DateFormat mTimeFormat;
 
     private View mChip;
-    private View mChip2;
     private CheckBox mFlagged;
     private int defaultSubjectColor;
     private LinearLayout mToContainerView;
@@ -60,7 +59,6 @@ public class MessageHeader extends ScrollView implements OnClickListener {
     private Account mAccount;
     private FontSizes mFontSizes = K9.getFontSizes();
     private Contacts mContacts;
-    private ImageView mShowAdditionalHeadersIcon;
     private SavedState mSavedState;
 
     private OnLayoutChangedListener mOnLayoutChangedListener;
@@ -97,12 +95,9 @@ public class MessageHeader extends ScrollView implements OnClickListener {
         mSubjectView = (TextView) findViewById(R.id.subject);
         mAdditionalHeadersView = (TextView) findViewById(R.id.additional_headers_view);
         mChip = findViewById(R.id.chip);
-        mChip2 = findViewById(R.id.chip2);
         mDateView = (TextView) findViewById(R.id.date);
         mTimeView = (TextView) findViewById(R.id.time);
         mFlagged = (CheckBox) findViewById(R.id.flagged);
-        mShowAdditionalHeadersIcon = (ImageView) findViewById(R.id.show_additional_headers_icon);
-
 
         defaultSubjectColor = mSubjectView.getCurrentTextColor();
         mSubjectView.setTextSize(TypedValue.COMPLEX_UNIT_SP, mFontSizes.getMessageViewSubject());
@@ -118,20 +113,27 @@ public class MessageHeader extends ScrollView implements OnClickListener {
         ((TextView) findViewById(R.id.to_label)).setTextSize(TypedValue.COMPLEX_UNIT_SP, mFontSizes.getMessageViewTo());
         ((TextView) findViewById(R.id.cc_label)).setTextSize(TypedValue.COMPLEX_UNIT_SP, mFontSizes.getMessageViewCC());
 
-        findViewById(R.id.show_additional_headers_area).setOnClickListener(this);
+        mToView.setOnClickListener(this);
+        mCcView.setOnClickListener(this);
         mFromView.setOnClickListener(this);
+        findViewById(R.id.top_container).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.show_additional_headers_area: {
+            case R.id.top_container: {
                 onShowAdditionalHeaders();
                 break;
             }
             case R.id.from: {
                 onAddSenderToContacts();
                 break;
+            }
+            case R.id.to:
+            case R.id.cc: {
+                expand((TextView)view, ((TextView)view).getEllipsize() != null);
+                layoutChanged();
             }
         }
     }
@@ -166,7 +168,6 @@ public class MessageHeader extends ScrollView implements OnClickListener {
     private void hideAdditionalHeaders() {
         mAdditionalHeadersView.setVisibility(View.GONE);
         mAdditionalHeadersView.setText("");
-        mShowAdditionalHeadersIcon.setImageResource(R.drawable.show_more);
     }
 
 
@@ -185,7 +186,6 @@ public class MessageHeader extends ScrollView implements OnClickListener {
                 // Show the additional headers that we have got.
                 populateAdditionalHeadersView(additionalHeaders);
                 mAdditionalHeadersView.setVisibility(View.VISIBLE);
-                mShowAdditionalHeadersIcon.setImageResource(R.drawable.show_less);
             }
             if (!allHeadersDownloaded) {
                 /*
@@ -252,8 +252,6 @@ public class MessageHeader extends ScrollView implements OnClickListener {
         int chipColorAlpha = (!message.isSet(Flag.SEEN)) ? 255 : 127;
         mChip.setBackgroundColor(chipColor);
         mChip.getBackground().setAlpha(chipColorAlpha);
-        mChip2.setBackgroundColor(chipColor);
-        mChip2.getBackground().setAlpha(chipColorAlpha);
 
         setVisibility(View.VISIBLE);
 
@@ -271,10 +269,27 @@ public class MessageHeader extends ScrollView implements OnClickListener {
         int currentVisibility = mAdditionalHeadersView.getVisibility();
         if (currentVisibility == View.VISIBLE) {
             hideAdditionalHeaders();
+            expand(mToView, false);
+            expand(mCcView, false);
         } else {
             showAdditionalHeaders();
+            expand(mToView, true);
+            expand(mCcView, true);
         }
         layoutChanged();
+    }
+
+    /**
+     * Expand or collapse a TextView by removing or adding the 2 lines limitation
+     */
+    private void expand(TextView v, boolean expand) {
+       if (expand) {
+           v.setMaxLines(Integer.MAX_VALUE);
+           v.setEllipsize(null);
+       } else {
+           v.setMaxLines(2);
+           v.setEllipsize(android.text.TextUtils.TruncateAt.END);
+       }
     }
 
     private List<HeaderEntry> getAdditionalHeaders(final Message message)
