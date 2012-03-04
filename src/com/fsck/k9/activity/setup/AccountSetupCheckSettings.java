5800ab0c87e5673868e900fee978c85f66a443f3
Sander Bogaert <sander.bogaert@gmail.com>
2012-03-04 11:02:12 +0100

    Added dialog to reenter credentials when wrong at the settings check step in the wizard. This prevents users from having to rerun the whole wizard as it is now.

--- src/com/fsck/k9/activity/setup/AccountSetupCheckSettings.java
+++ src/com/fsck/k9/activity/setup/AccountSetupCheckSettings.java
@@ -3,15 +3,19 @@ package com.fsck.k9.activity.setup;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Process;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.fsck.k9.*;
@@ -24,6 +28,10 @@ import com.fsck.k9.mail.Transport;
 import com.fsck.k9.mail.store.TrustManagerFactory;
 import com.fsck.k9.mail.store.WebDavStore;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
@@ -44,6 +52,10 @@ public class AccountSetupCheckSettings extends K9Activity implements OnClickList
 
     private static final String EXTRA_CHECK_OUTGOING = "checkOutgoing";
 
+	protected static final int REENTER_CREDENTIALS_DIALOG = 1;
+
+	private static final String EXCEPTION_MSG = "AUTH_EXCEPTION";
+
     private Handler mHandler = new Handler();
 
     private ProgressBar mProgressBar;
@@ -60,6 +72,11 @@ public class AccountSetupCheckSettings extends K9Activity implements OnClickList
 
     private boolean mDestroyed;
 
+	private Intent mStartIntent;
+
+    private boolean mIsIncoming = true;
+    private boolean mRetry = true;
+    
     public static void actionCheckSettings(Activity context, Account account,
                                            boolean checkIncoming, boolean checkOutgoing) {
         Intent i = new Intent(context, AccountSetupCheckSettings.class);
@@ -80,6 +97,7 @@ public class AccountSetupCheckSettings extends K9Activity implements OnClickList
         setMessage(R.string.account_setup_check_settings_retr_info_msg);
         mProgressBar.setIndeterminate(true);
 
+        mStartIntent = getIntent();
         String accountUuid = getIntent().getStringExtra(EXTRA_ACCOUNT);
         mAccount = Preferences.getPreferences(this).getAccount(accountUuid);
         mCheckIncoming = getIntent().getBooleanExtra(EXTRA_CHECK_INCOMING, false);
@@ -90,71 +108,82 @@ public class AccountSetupCheckSettings extends K9Activity implements OnClickList
             public void run() {
                 Store store = null;
                 Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
-                try {
-                    if (mDestroyed) {
-                        return;
-                    }
-                    if (mCanceled) {
-                        finish();
-                        return;
-                    }
-                    if (mCheckIncoming) {
-                        store = mAccount.getRemoteStore();
-
-                        if (store instanceof WebDavStore) {
-                            setMessage(R.string.account_setup_check_settings_authenticate);
-                        } else {
-                            setMessage(R.string.account_setup_check_settings_check_incoming_msg);
-                        }
-                        store.checkSettings();
-
-                        if (store instanceof WebDavStore) {
-                            setMessage(R.string.account_setup_check_settings_fetch);
-                        }
-                        MessagingController.getInstance(getApplication()).listFoldersSynchronous(mAccount, true, null);
-                        MessagingController.getInstance(getApplication()).synchronizeMailbox(mAccount, K9.INBOX , null, null);
-                    }
-                    if (mDestroyed) {
-                        return;
-                    }
-                    if (mCanceled) {
-                        finish();
-                        return;
-                    }
-                    if (mCheckOutgoing) {
-                        if (!(mAccount.getRemoteStore() instanceof WebDavStore)) {
-                            setMessage(R.string.account_setup_check_settings_check_outgoing_msg);
-                        }
-                        Transport transport = Transport.getInstance(mAccount);
-                        transport.close();
-                        transport.open();
-                        transport.close();
-                    }
-                    if (mDestroyed) {
-                        return;
-                    }
-                    if (mCanceled) {
-                        finish();
-                        return;
-                    }
-                    setResult(RESULT_OK);
-                    finish();
-                } catch (final AuthenticationFailedException afe) {
-                    Log.e(K9.LOG_TAG, "Error while testing settings", afe);
-                    showErrorDialog(
-                        R.string.account_setup_failed_dlg_auth_message_fmt,
-                        afe.getMessage() == null ? "" : afe.getMessage());
-                } catch (final CertificateValidationException cve) {
-                    Log.e(K9.LOG_TAG, "Error while testing settings", cve);
-                    acceptKeyDialog(
-                        R.string.account_setup_failed_dlg_certificate_message_fmt,
-                        cve);
-                } catch (final Throwable t) {
-                    Log.e(K9.LOG_TAG, "Error while testing settings", t);
-                    showErrorDialog(
-                        R.string.account_setup_failed_dlg_server_message_fmt,
-                        (t.getMessage() == null ? "" : t.getMessage()));
-
+                while(mRetry)
+                {
+	                try {
+	                    if (mDestroyed) {
+	                        return;
+	                    }
+	                    if (mCanceled) {
+	                        finish();
+	                        return;
+	                    }
+	                    if (mCheckIncoming) {
+	                    	mIsIncoming = true;
+	                        store = mAccount.getRemoteStore();
+	
+	                        if (store instanceof WebDavStore) {
+	                            setMessage(R.string.account_setup_check_settings_authenticate);
+	                        } else {
+	                            setMessage(R.string.account_setup_check_settings_check_incoming_msg);
+	                        }
+	                        store.checkSettings();
+	
+	                        if (store instanceof WebDavStore) {
+	                            setMessage(R.string.account_setup_check_settings_fetch);
+	                        }
+	                        MessagingController.getInstance(getApplication()).listFoldersSynchronous(mAccount, true, null);
+	                        MessagingController.getInstance(getApplication()).synchronizeMailbox(mAccount, K9.INBOX , null, null);
+	                    }
+	                    if (mDestroyed) {
+	                        return;
+	                    }
+	                    if (mCanceled) {
+	                        finish();
+	                        return;
+	                    }
+	                    if (mCheckOutgoing) {
+	                    	mIsIncoming = false;
+	                        if (!(mAccount.getRemoteStore() instanceof WebDavStore)) {
+	                            setMessage(R.string.account_setup_check_settings_check_outgoing_msg);
+	                        }
+	                        Transport transport = Transport.getInstance(mAccount);
+	                        transport.close();
+	                        transport.open();
+	                        transport.close();
+	                    }
+	                    if (mDestroyed) {
+	                        return;
+	                    }
+	                    if (mCanceled) {
+	                        finish();
+	                        return;
+	                    }
+	                    setResult(RESULT_OK);
+	                    finish();
+	                } catch (final AuthenticationFailedException afe) {
+	                    Log.e(K9.LOG_TAG, "Error while testing settings", afe);
+	                    
+	                    runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								Bundle tmpBundle = new Bundle();
+								tmpBundle.putString(EXCEPTION_MSG, mIsIncoming ? getString(R.string.wrong_credentials_incoming) : getString(R.string.wrong_credentials_outgoing));
+								showDialog(REENTER_CREDENTIALS_DIALOG, tmpBundle);
+							}
+						});
+	                } catch (final CertificateValidationException cve) {
+	                    Log.e(K9.LOG_TAG, "Error while testing settings", cve);
+	                    acceptKeyDialog(
+	                        R.string.account_setup_failed_dlg_certificate_message_fmt,
+	                        cve);
+	                } catch (final Throwable t) {
+	                    Log.e(K9.LOG_TAG, "Error while testing settings", t);
+	                    showErrorDialog(
+	                        R.string.account_setup_failed_dlg_server_message_fmt,
+	                        (t.getMessage() == null ? "" : t.getMessage()));
+	
+	                }
                 }
             }
 
@@ -179,8 +208,86 @@ public class AccountSetupCheckSettings extends K9Activity implements OnClickList
             }
         });
     }
+    
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog = new Dialog(this);
 
-    private void showErrorDialog(final int msgResId, final Object... args) {
+		if (id == REENTER_CREDENTIALS_DIALOG) {
+			dialog.setContentView(R.layout.alert_dialog_text_entry);
+			dialog.setTitle(getString(R.string.account_setup_failed_dlg_title));
+
+			final EditText mAlertUsernameField = ((EditText) dialog
+					.findViewById(R.id.error_dialog_username_field));
+			final EditText mAlertPasswordField = ((EditText) dialog
+					.findViewById(R.id.error_dialog_password_field));
+			
+
+			// Try new credentials
+			((Button) dialog.findViewById(R.id.error_dialog_ok))
+					.setOnClickListener(new OnClickListener() {
+						public void onClick(View view) {
+							changeCredentials(mAlertUsernameField.getText().toString(),
+									mAlertPasswordField.getText().toString());
+							dismissDialog(REENTER_CREDENTIALS_DIALOG);
+						}
+					});
+
+			// Exit
+			((Button) dialog.findViewById(R.id.error_dialog_quit))
+					.setOnClickListener(new OnClickListener() {
+						public void onClick(View view) {
+							finish();
+						}
+					});
+
+		} else {
+			dialog = null;
+		}
+		return dialog;
+	}
+    
+	@Override
+	protected void onPrepareDialog(int id, Dialog dialog, final Bundle args){
+		if (id == REENTER_CREDENTIALS_DIALOG) {
+			TextView mErrorMsg = ((TextView) dialog.findViewById(R.id.error_msg));
+			mErrorMsg.setText(args.getString(EXCEPTION_MSG));
+		}
+	}
+	
+	
+	// changes the credentials with the new ones
+    protected void changeCredentials(String user, String pasw) {
+    	try {
+    		String userInfo = URLEncoder.encode(user, "UTF-8") + ":" + URLEncoder.encode(pasw, "UTF-8");
+    		
+    		// incoming
+			URI tmpStoreURI = new URI(mAccount.getStoreUri());
+			URI storeUri = new URI(
+                    tmpStoreURI.getScheme(),
+                    userInfo,
+                    tmpStoreURI.getHost(),
+                    tmpStoreURI.getPort(),
+                    null,null,null);
+            mAccount.setStoreUri(storeUri.toString());
+            
+			// outgoing
+			URI tmpTransportURI = new URI(mAccount.getTransportUri());
+			URI transportUri = new URI(
+                    tmpTransportURI.getScheme(),
+                    (tmpTransportURI.getUserInfo() == null || tmpTransportURI.getUserInfo().isEmpty()) ? null : userInfo,
+                    tmpTransportURI.getHost(),
+                    tmpTransportURI.getPort(),
+                    null,null,null);
+			mAccount.setTransportUri(transportUri.toString());
+			
+		} catch (URISyntaxException e) {} 
+		catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}		
+	}
+
+	private void showErrorDialog(final int msgResId, final Object... args) {
         mHandler.post(new Runnable() {
             public void run() {
                 if (mDestroyed) {
