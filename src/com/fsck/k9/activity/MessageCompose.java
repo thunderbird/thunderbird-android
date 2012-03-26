4e21f049d304c21a8caf6bdf655d0f0925365f0a
cketti <cketti@gmail.com>
2012-03-26 21:36:26 +0200

    Validate "original message" field of identity header when loading drafts

--- src/com/fsck/k9/activity/MessageCompose.java
+++ src/com/fsck/k9/activity/MessageCompose.java
@@ -2384,7 +2384,15 @@ public class MessageCompose extends K9Activity implements OnClickListener, OnFoc
                 if (k9identity.containsKey(IdentityField.ORIGINAL_MESSAGE)) {
                     mMessageReference = null;
                     try {
-                        mMessageReference = new MessageReference(k9identity.get(IdentityField.ORIGINAL_MESSAGE));
+                        String originalMessage = k9identity.get(IdentityField.ORIGINAL_MESSAGE);
+                        MessageReference messageReference = new MessageReference(originalMessage);
+
+                        // Check if this is a valid account in our database
+                        Preferences prefs = Preferences.getPreferences(getApplicationContext());
+                        Account account = prefs.getAccount(messageReference.accountUuid);
+                        if (account != null) {
+                            mMessageReference = messageReference;
+                        }
                     } catch (MessagingException e) {
                         Log.e(K9.LOG_TAG, "Could not decode message reference in identity.", e);
                     }
