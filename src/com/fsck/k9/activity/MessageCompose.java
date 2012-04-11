thunderbird/thunderbird-android

commit 6f525f0e31d43271654a06561aa422d7faac782e

Converted hardcoded strings to constants

src/com/fsck/k9/activity/MessageCompose.java
@@ -1804,7 +1804,7 @@ public class MessageCompose extends K9Activity implements OnClickListener, OnFoc
             }
             if (contact.emailAddresses.size() > 1) {
                 Intent i = new Intent(this, EmailAddressList.class);
-                i.putExtra("contact", contact);
+                i.putExtra(EmailAddressList.EXTRA_CONTACT_ITEM, contact);
 
                 if (requestCode == CONTACT_PICKER_TO) {
                     startActivityForResult(i, CONTACT_PICKER_TO2);

src/com/fsck/k9/activity/EmailAddressList.java
@@ -13,6 +13,10 @@ import com.fsck.k9.R;
 import com.fsck.k9.helper.ContactItem;
 
 public class EmailAddressList extends K9ListActivity implements OnItemClickListener {
+    public static final String EXTRA_CONTACT_ITEM = "contact";
+    public static final String EXTRA_EMAIL_ADDRESS = "emailAddress";
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +24,9 @@ public class EmailAddressList extends K9ListActivity implements OnItemClickListe
         setContentView(R.layout.email_address_list);
 
         Intent i = getIntent();
-        ContactItem contact = (ContactItem) i.getSerializableExtra("contact");
+        ContactItem contact = (ContactItem) i.getSerializableExtra(EXTRA_CONTACT_ITEM);
         if (contact == null) {
+            finish();
             return;
         }
 
@@ -41,7 +46,7 @@ public class EmailAddressList extends K9ListActivity implements OnItemClickListe
         Toast.makeText(EmailAddressList.this, item, Toast.LENGTH_LONG).show();
 
         Intent intent = new Intent();
-        intent.putExtra("EMAIL_ADDRESS", item);
+        intent.putExtra(EXTRA_EMAIL_ADDRESS, item);
         setResult(RESULT_OK, intent);
         finish();
     }

commit ff4d6a66c8628246daa567d606858a3f0890759c

Fixed bug introduced with 6f525f0e31d43271654a06561aa422d7faac782e
Intent extra was renamed but the code in MessageCompose still used the
(hardcoded) old name.

src/com/fsck/k9/activity/MessageCompose.java
@@ -1837,16 +1837,15 @@ public class MessageCompose extends K9Activity implements OnClickListener, OnFoc
         case CONTACT_PICKER_TO2:
         case CONTACT_PICKER_CC2:
         case CONTACT_PICKER_BCC2:
-            String emailAddr = data.getStringExtra("EMAIL_ADDRESS");
+            String emailAddr = data.getStringExtra(EmailAddressList.EXTRA_EMAIL_ADDRESS);
             if (requestCode == CONTACT_PICKER_TO2) {
                 addAddress(mToView, new Address(emailAddr, ""));
             } else if (requestCode == CONTACT_PICKER_CC2) {
                 addAddress(mCcView, new Address(emailAddr, ""));
             } else if (requestCode == CONTACT_PICKER_BCC2) {
                 addAddress(mBccView, new Address(emailAddr, ""));
-            } else {
-                return;
             }
+            break;
         }
     }
 