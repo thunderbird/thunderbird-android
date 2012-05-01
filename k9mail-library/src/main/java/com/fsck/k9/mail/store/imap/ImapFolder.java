b246b3c11a23deefe1293be31ebac56c81cba6d5
Sander Bogaert <Sander.Bogaert@gmail.com>
2012-05-01 16:00:05 +0200

    Fix: IMAP clients can't add the RECENT flag.

--- k9mail-library/src/main/java/com/fsck/k9/mail/store/imap/ImapFolder.java
+++ k9mail-library/src/main/java/com/fsck/k9/mail/store/imap/ImapFolder.java
@@ -1262,7 +1262,8 @@ class ImapFolder extends Folder<ImapMessage> {
     private String combineFlags(Iterable<Flag> flags) {
         List<String> flagNames = new ArrayList<String>();
         for (Flag flag : flags) {
-            flagNames.add(flag.realName());
+            if(!flag.equals(Flag.RECENT))
+                flagNames.add(flag.realName());
         }
 
         return combine(flagNames.toArray(new String[flagNames.size()]), ' ');
