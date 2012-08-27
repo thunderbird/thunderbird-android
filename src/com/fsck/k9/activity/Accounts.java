ed03f19ea55f7a65539b11a770cde7839c0c80e4
Sander Bogaert <Sander.Bogaert@tworoadssoftware.com>
2012-08-27 14:44:40 -0400

    Accounts actionbar split. Now contains unread number for consistency and shows status as subtitle if available. Allowed separate method to get the current operation in ActivityListener.

--- src/com/fsck/k9/activity/Accounts.java
+++ src/com/fsck/k9/activity/Accounts.java
@@ -29,6 +29,8 @@ import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.util.TypedValue;
 import android.view.ContextMenu;
+
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import android.view.View;
@@ -129,6 +131,12 @@ public class Accounts extends K9ListActivity implements OnItemClickListener {
     private FontSizes mFontSizes = K9.getFontSizes();
 
     private MenuItem mRefreshMenuItem;
+    private ActionBar mActionBar;
+
+    private TextView mActionBarTitle;
+    private TextView mActionBarSubTitle;
+    private TextView mActionBarUnread;
+
     /**
      * Contains information about objects that need to be retained on configuration changes.
      *
@@ -141,9 +149,17 @@ public class Accounts extends K9ListActivity implements OnItemClickListener {
 
     class AccountsHandler extends Handler {
         private void setViewTitle() {
-            String dispString = mListener.formatHeader(Accounts.this, getString(R.string.accounts_title), mUnreadMessageCount, getTimeFormat());
+		mActionBarTitle.setText(" "+getString(R.string.accounts_title));
+            mActionBarUnread.setText(String.valueOf(mUnreadMessageCount));
 
-            setTitle(dispString);
+            String operation = mListener.getOperation(Accounts.this, getTimeFormat());
+            operation.trim();
+            if (operation.length() < 1) {
+		mActionBarSubTitle.setVisibility(View.GONE);
+            } else {
+		mActionBarSubTitle.setVisibility(View.VISIBLE);
+		mActionBarSubTitle.setText(operation);
+            }
         }
         public void refreshTitle() {
             runOnUiThread(new Runnable() {
@@ -373,7 +389,8 @@ public class Accounts extends K9ListActivity implements OnItemClickListener {
         }
 
         requestWindowFeature(Window.FEATURE_PROGRESS);
-        getSupportActionBar().setHomeButtonEnabled(false);
+        mActionBar = getSupportActionBar();
+        initializeActionBar();
         setContentView(R.layout.accounts);
         ListView listView = getListView();
         listView.setOnItemClickListener(this);
@@ -393,6 +410,18 @@ public class Accounts extends K9ListActivity implements OnItemClickListener {
         if (mNonConfigurationInstance != null) {
             mNonConfigurationInstance.restore(this);
         }
+    }
+
+    private void initializeActionBar() {
+	mActionBar.setDisplayShowCustomEnabled(true);
+	    mActionBar.setCustomView(R.layout.actionbar_custom);
+
+	    View customView = mActionBar.getCustomView();
+	    mActionBarTitle = (TextView) customView.findViewById(R.id.actionbar_title_first);
+	    mActionBarSubTitle = (TextView) customView.findViewById(R.id.actionbar_title_sub);
+	    mActionBarUnread = (TextView) customView.findViewById(R.id.actionbar_unread_count);
+
+        mActionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     /**
