ee0e558f4d9d5813f9c212f744e11ae301872427
Bernhard Redl <bernhardredl@inode.at>
2012-09-10 01:05:10 +0200

    Fixed Issue 4543: Msg UnRead after "Download Complete" pressed

--- src/com/fsck/k9/controller/MessagingController.java
+++ src/com/fsck/k9/controller/MessagingController.java
@@ -2766,6 +2766,7 @@ public class MessagingController implements Runnable {
                         localFolder.fetch(new Message[] { message }, fp, null);
 
                         // Mark that this message is now fully synched
+                        message.setFlag(Flag.SEEN, true);
                         message.setFlag(Flag.X_DOWNLOADED_FULL, true);
                     }
 
