b96e7bb0585eba74a8507f11dee659b3a8f0ec08
cketti <cketti@gmail.com>
2012-11-22 22:50:13 +0100

    Fix NPE related to changed initialization order in Android 4.2

--- src/com/fsck/k9/fragment/MessageViewFragment.java
+++ src/com/fsck/k9/fragment/MessageViewFragment.java
@@ -539,8 +539,10 @@ public class MessageViewFragment extends SherlockFragment implements OnClickList
     }
 
     private void configureMenu(Menu menu) {
-        // first run displayMessage() gets called before onCreateOptionMenu()
-        if (menu == null) {
+        // In Android versions prior to 4.2 onCreateOptionsMenu() (which calls us) is called before
+        // onActivityCreated() when mAccount isn't initialized yet. In that case we do nothing and
+        // wait for displayMessage() to call us again.
+        if (menu == null || mAccount == null) {
             return;
         }
 
