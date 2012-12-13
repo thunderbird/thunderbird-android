bcd52a67072c55c1a4f1457e2985fd523a49965c
cketti <cketti@gmail.com>
2012-12-13 17:03:35 +0100

    Fetch in-reply-to header when downloading envelope on IMAP accounts

--- src/com/fsck/k9/mail/store/ImapStore.java
+++ src/com/fsck/k9/mail/store/ImapStore.java
@@ -1492,7 +1492,7 @@ public class ImapStore extends Store {
                 fetchFields.add("INTERNALDATE");
                 fetchFields.add("RFC822.SIZE");
                 fetchFields.add("BODY.PEEK[HEADER.FIELDS (date subject from content-type to cc " +
-                        "reply-to message-id references " + K9.IDENTITY_HEADER + ")]");
+                        "reply-to message-id references in-reply-to " + K9.IDENTITY_HEADER + ")]");
             }
             if (fp.contains(FetchProfile.Item.STRUCTURE)) {
                 fetchFields.add("BODYSTRUCTURE");
