3f84bb54f22593b0f1e629dae1fe91320b49b0cb
cketti <cketti@gmail.com>
2012-12-18 21:12:40 +0100

    Fix crash when deleting all messages in a thread
    Fixes issue 4772

--- src/com/fsck/k9/fragment/MessageListFragment.java
+++ src/com/fsck/k9/fragment/MessageListFragment.java
@@ -2979,13 +2979,16 @@ public class MessageListFragment extends SherlockFragment implements OnItemClick
         }
 
         if (mIsThreadDisplay) {
-            cursor.moveToFirst();
-            mTitle = cursor.getString(SUBJECT_COLUMN);
-            mTitle = Utility.stripSubject(mTitle);
-            if (StringUtils.isNullOrEmpty(mTitle)) {
-               mTitle = getString(R.string.general_no_subject);
+            if (cursor.moveToFirst()) {
+                mTitle = cursor.getString(SUBJECT_COLUMN);
+                mTitle = Utility.stripSubject(mTitle);
+                if (StringUtils.isNullOrEmpty(mTitle)) {
+                   mTitle = getString(R.string.general_no_subject);
+                }
+                refreshTitle();
+            } else {
+                //TODO: empty thread view -> return to full message list
             }
-            refreshTitle();
         }
 
         cleanupSelected(cursor);
