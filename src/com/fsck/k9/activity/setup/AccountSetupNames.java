b315cef8d7fb59c8272f0bcb4fe199c690d15466
Uni- <administrator@korea.ac.kr>
2013-03-11 00:34:45 +0900

    Add account chip color setting to AccountSetupNames

--- src/com/fsck/k9/activity/setup/AccountSetupNames.java
+++ src/com/fsck/k9/activity/setup/AccountSetupNames.java
@@ -3,11 +3,13 @@ package com.fsck.k9.activity.setup;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.method.TextKeyListener;
 import android.text.method.TextKeyListener.Capitalize;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -18,25 +20,28 @@ import android.widget.Spinner;
 
 import com.fsck.k9.*;
 import com.fsck.k9.activity.Accounts;
+import com.fsck.k9.activity.ColorPickerDialog;
 import com.fsck.k9.activity.K9Activity;
 import com.fsck.k9.helper.Utility;
 
-public class AccountSetupNames extends K9Activity implements OnClickListener {
+public class AccountSetupNames extends K9Activity implements OnClickListener, ColorPickerDialog.OnColorChangedListener {
     private static final String EXTRA_ACCOUNT = "account";
 
     private EditText mDescription;
 
     private EditText mName;
-    
+
     private Spinner mDisplayCountView;
 
     private CheckBox mNotifyView;
     private CheckBox mNotifySyncView;
+    private Button mColorChip;
+    private int mChipColor;
 
     private Account mAccount;
 
     private Button mDoneButton;
-    
+
     public static void actionSetNames(Context context, Account account) {
         Intent i = new Intent(context, AccountSetupNames.class);
         i.putExtra(EXTRA_ACCOUNT, account.getUuid());
@@ -53,7 +58,8 @@ public class AccountSetupNames extends K9Activity implements OnClickListener {
         mNotifyView = (CheckBox)findViewById(R.id.account_notify);
         mNotifySyncView = (CheckBox)findViewById(R.id.account_notify_sync);
         mDoneButton = (Button)findViewById(R.id.done);
-        
+        mColorChip = (Button)findViewById(R.id.account_chip);
+
         mDoneButton.setOnClickListener(this);
 
         TextWatcher validationTextWatcher = new TextWatcher() {
@@ -92,8 +98,11 @@ public class AccountSetupNames extends K9Activity implements OnClickListener {
         mNotifyView.setChecked(mAccount.isNotifyNewMail());
         mNotifySyncView.setChecked(mAccount.isShowOngoing());
         SpinnerOption.setSpinnerOptionValue(mDisplayCountView, mAccount
-                .getDisplayCount());
-        
+                                            .getDisplayCount());
+
+        mChipColor = mAccount.getChipColor();
+        onRefreshColorChip();
+
         /*
          * Since this field is considered optional, we don't set this here. If
          * the user fills in a value we'll reset the current value, otherwise we
@@ -113,6 +122,10 @@ public class AccountSetupNames extends K9Activity implements OnClickListener {
         Utility.setCompoundDrawablesAlpha(mDoneButton, mDoneButton.isEnabled() ? 255 : 128);
     }
 
+    protected void onPickColor() {
+        new ColorPickerDialog(this, this, mAccount.getChipColor()).show();
+    }
+
     protected void onNext() {
         if (Utility.requiredFieldValid(mDescription)) {
             mAccount.setDescription(mDescription.getText().toString());
@@ -122,6 +135,7 @@ public class AccountSetupNames extends K9Activity implements OnClickListener {
         mAccount.setShowOngoing(mNotifySyncView.isChecked());
         mAccount.setDisplayCount((Integer)((SpinnerOption)mDisplayCountView
                                            .getSelectedItem()).value);
+        mAccount.setChipColor(mChipColor);
         mAccount.save(Preferences.getPreferences(this));
         Accounts.listAccounts(this);
         finish();
@@ -129,9 +143,32 @@ public class AccountSetupNames extends K9Activity implements OnClickListener {
 
     public void onClick(View v) {
         switch (v.getId()) {
+        case R.id.account_chip:
+            onPickColor();
+            break;
         case R.id.done:
             onNext();
             break;
         }
+    }
+
+    public void colorChanged(int color) {
+        /*
+         * Temporarily set account chip color to picked one to generate color chip drawable.
+         */
+        mChipColor = mAccount.getChipColor();
+        mAccount.setChipColor(color);
+        onRefreshColorChip();
+        mAccount.setChipColor(mChipColor);
+        mChipColor = color;
+    }
+
+    private void onRefreshColorChip() {
+        Drawable chipDrawable;
+        int size = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 32, getResources().getDisplayMetrics());
+        chipDrawable = mAccount.generateColorChip().drawable();
+        chipDrawable.setBounds(0, 0, size, size);
+        mColorChip.setCompoundDrawables(null, null, chipDrawable, null);
+        mColorChip.setOnClickListener(this);
     }
 }
