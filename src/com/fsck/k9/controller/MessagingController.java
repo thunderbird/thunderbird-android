a364d6262d007c2143386022d7ccc0911d0cdbb4
cketti <cketti@gmail.com>
2013-03-18 17:44:10 +0100

    Fix cache cleanup

--- src/com/fsck/k9/controller/MessagingController.java
+++ src/com/fsck/k9/controller/MessagingController.java
@@ -2803,10 +2803,10 @@ public class MessagingController implements Runnable {
         try {
             if (threadedList) {
                 localStore.setFlagForThreads(ids, flag, newState);
-                removeFlagFromCache(account, ids, flag);
+                removeFlagForThreadsFromCache(account, ids, flag);
             } else {
                 localStore.setFlag(ids, flag, newState);
-                removeFlagForThreadsFromCache(account, ids, flag);
+                removeFlagFromCache(account, ids, flag);
             }
         } catch (MessagingException e) {
             Log.e(K9.LOG_TAG, "Couldn't set flags in local database", e);
