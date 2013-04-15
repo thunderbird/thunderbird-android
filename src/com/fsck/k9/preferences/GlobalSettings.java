d29825fe573aca908ae9c25f66a1e0ba27ad2c41
cketti <cketti@gmail.com>
2013-04-16 01:21:26 +0200

    Fix settings file versioning

--- src/com/fsck/k9/preferences/GlobalSettings.java
+++ src/com/fsck/k9/preferences/GlobalSettings.java
@@ -136,7 +136,8 @@ public class GlobalSettings {
                 new V(1, new BooleanSetting(true))
             ));
         s.put("messageListCheckboxes", Settings.versions(
-                new V(1, new BooleanSetting(true))
+                new V(1, new BooleanSetting(false)),
+                new V(27, new BooleanSetting(true))
             ));
         s.put("messageListPreviewLines", Settings.versions(
                 new V(1, new IntegerRangeSetting(1, 100, 2))
