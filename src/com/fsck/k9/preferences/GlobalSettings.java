acfc3e0f6341aa52e6af05cc797d2459cf7d56c5
Joe Steele <joe@madewell.net>
2013-05-08 23:10:01 -0400

    Fix settings versioning after merging autofitWidth branch.
    Versioning on master was updated subsequent to the creation
    of the branch.

--- src/com/fsck/k9/preferences/GlobalSettings.java
+++ src/com/fsck/k9/preferences/GlobalSettings.java
@@ -223,7 +223,7 @@ public class GlobalSettings {
                 new V(25, new BooleanSetting(true))
             ));
         s.put("autofitWidth", Settings.versions(
-                new V(27, new BooleanSetting(true))
+                new V(28, new BooleanSetting(true))
             ));
 
         SETTINGS = Collections.unmodifiableMap(s);
