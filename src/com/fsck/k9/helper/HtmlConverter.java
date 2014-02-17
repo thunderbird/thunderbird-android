fab31983abac1404f21f20d797275981d65514e3
cketti <cketti@gmail.com>
2014-02-17 18:30:36 +0100

    Fix linkifyText() in the presence of bitcoin URIs

--- src/com/fsck/k9/helper/HtmlConverter.java
+++ src/com/fsck/k9/helper/HtmlConverter.java
@@ -397,7 +397,7 @@ public class HtmlConverter {
         Matcher m = Regex.WEB_URL_PATTERN.matcher(prepared);
         while (m.find()) {
             int start = m.start();
-            if (start == 0 || (start != 0 && text.charAt(start - 1) != '@')) {
+            if (start == 0 || (start != 0 && prepared.charAt(start - 1) != '@')) {
                 if (m.group().indexOf(':') > 0) { // With no URI-schema we may get "http:/" links with the second / missing
                     m.appendReplacement(outputBuffer, "<a href=\"$0\">$0</a>");
                 } else {
