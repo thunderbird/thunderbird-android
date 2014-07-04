cf718780f6bcb0afa8166921219c54923b17d8d3
Joe Steele <joe@madewell.net>
2014-07-04 19:23:43 -0400

    Fixes needed after merging in master
    Also, fix unit tests.

--- src/com/fsck/k9/activity/setup/AccountSetupCheckSettings.java
+++ src/com/fsck/k9/activity/setup/AccountSetupCheckSettings.java
@@ -3,14 +3,14 @@ package com.fsck.k9.activity.setup;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Process;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -524,7 +524,7 @@ public class AccountSetupCheckSettings extends K9Activity implements OnClickList
             }
         }
 
-        FragmentTransaction ta = getSupportFragmentManager().beginTransaction();
+        FragmentTransaction ta = getFragmentManager().beginTransaction();
         ta.add(fragment, getDialogTag(dialogId));
         ta.commitAllowingStateLoss();
 
