3c4ad9161471be7d60872496d2dc03035ee3a99e
cketti <cketti@gmail.com>
2014-10-20 21:24:18 -0400

    Fix method names/visibility

--- src/com/fsck/k9/view/AttachmentView.java
+++ src/com/fsck/k9/view/AttachmentView.java
@@ -236,13 +236,13 @@ public class AttachmentView extends FrameLayout implements OnClickListener, OnLo
             out.flush();
             out.close();
             in.close();
-            attachmentSaved(file.toString());
+            displayAttachmentSavedMessage(file.toString());
             new MediaScannerNotifier(context, file);
         } catch (IOException ioe) {
             if (K9.DEBUG) {
                 Log.e(K9.LOG_TAG, "Error saving attachment", ioe);
             }
-            attachmentNotSaved();
+            displayAttachmentNotSavedMessage();
         }
     }
 
@@ -264,12 +264,12 @@ public class AttachmentView extends FrameLayout implements OnClickListener, OnLo
         }
     }
 
-    public void attachmentSaved(final String filename) {
+    private void displayAttachmentSavedMessage(final String filename) {
         String message = context.getString(R.string.message_view_status_attachment_saved, filename);
         displayMessageToUser(message);
     }
 
-    public void attachmentNotSaved() {
+    private void displayAttachmentNotSavedMessage() {
         String message = context.getString(R.string.message_view_status_attachment_not_saved);
         displayMessageToUser(message);
     }
