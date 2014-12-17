densyong/k-9

Stop using K9.app where easily possible

src/com/fsck/k9/provider/AttachmentProvider.java
@@ -215,7 +215,7 @@ public class AttachmentProvider extends ContentProvider {
         final AttachmentInfo attachmentInfo;
         try {
             final Account account = Preferences.getPreferences(getContext()).getAccount(dbName);
-            attachmentInfo = LocalStore.getLocalInstance(account, K9.app).getAttachmentInfo(id);
+            attachmentInfo = LocalStore.getLocalInstance(account, getContext()).getAttachmentInfo(id);
         } catch (MessagingException e) {
             Log.e(K9.LOG_TAG, "Unable to retrieve attachment info from local store for ID: " + id, e);
             return null;
@@ -269,7 +269,7 @@ public class AttachmentProvider extends ContentProvider {
             final Account account = Preferences.getPreferences(getContext()).getAccount(dbName);
 
             try {
-                final LocalStore localStore = LocalStore.getLocalInstance(account, K9.app);
+                final LocalStore localStore = LocalStore.getLocalInstance(account, getContext());
 
                 AttachmentInfo attachmentInfo = localStore.getAttachmentInfo(id);
                 if (FORMAT_VIEW.equals(format) && mimeType != null) {
@@ -289,7 +289,7 @@ public class AttachmentProvider extends ContentProvider {
     private File getFile(String dbName, String id) throws FileNotFoundException {
         Account account = Preferences.getPreferences(getContext()).getAccount(dbName);
 
-        File attachmentsDir = StorageManager.getInstance(K9.app).getAttachmentDirectory(dbName,
+        File attachmentsDir = StorageManager.getInstance(getContext()).getAttachmentDirectory(dbName,
                 account.getLocalStorageProviderId());
 
         File file = new File(attachmentsDir, id);

src/com/fsck/k9/provider/MessageProvider.java
@@ -302,7 +302,7 @@ public class MessageProvider extends ContentProvider {
 
             // new code for integrated inbox, only execute this once as it will be processed afterwards via the listener
             final SearchAccount integratedInboxAccount = SearchAccount.createUnifiedInboxAccount(getContext());
-            final MessagingController msgController = MessagingController.getInstance(K9.app);
+            final MessagingController msgController = MessagingController.getInstance(getContext());
 
             msgController.searchLocalMessages(integratedInboxAccount.getRelatedSearch(),
                                               new MesssageInfoHolderRetrieverListener(queue));
@@ -1040,7 +1040,7 @@ public class MessageProvider extends ContentProvider {
         // get localstore parameter
         Message msg = null;
         try {
-            Folder lf = LocalStore.getLocalInstance(myAccount, K9.app).getFolder(folderName);
+            Folder lf = LocalStore.getLocalInstance(myAccount, getContext()).getFolder(folderName);
             int msgCount = lf.getMessageCount();
             if (K9.DEBUG) {
                 Log.d(K9.LOG_TAG, "folder msg count = " + msgCount);
@@ -1052,7 +1052,7 @@ public class MessageProvider extends ContentProvider {
 
         // launch command to delete the message
         if ((myAccount != null) && (msg != null)) {
-            MessagingController controller = MessagingController.getInstance(K9.app);
+            MessagingController controller = MessagingController.getInstance(getContext());
             controller.deleteMessages(Collections.singletonList(msg), null);
         }
 

thunderbird/thunderbird-android

src/com/fsck/k9/provider/AttachmentProvider.java
@@ -215,7 +215,7 @@ public class AttachmentProvider extends ContentProvider {
         final AttachmentInfo attachmentInfo;
         try {
             final Account account = Preferences.getPreferences(getContext()).getAccount(dbName);
-            attachmentInfo = LocalStore.getInstance(account, K9.app).getAttachmentInfo(id);
+            attachmentInfo = LocalStore.getInstance(account, getContext()).getAttachmentInfo(id);
         } catch (MessagingException e) {
             Log.e(K9.LOG_TAG, "Unable to retrieve attachment info from local store for ID: " + id, e);
             return null;
@@ -269,7 +269,7 @@ public class AttachmentProvider extends ContentProvider {
             final Account account = Preferences.getPreferences(getContext()).getAccount(dbName);
 
             try {
-                final LocalStore localStore = LocalStore.getInstance(account, K9.app);
+                final LocalStore localStore = LocalStore.getInstance(account, getContext());
 
                 AttachmentInfo attachmentInfo = localStore.getAttachmentInfo(id);
                 if (FORMAT_VIEW.equals(format) && mimeType != null) {
@@ -289,7 +289,7 @@ public class AttachmentProvider extends ContentProvider {
     private File getFile(String dbName, String id) throws FileNotFoundException {
         Account account = Preferences.getPreferences(getContext()).getAccount(dbName);
 
-        File attachmentsDir = StorageManager.getInstance(K9.app).getAttachmentDirectory(dbName,
+        File attachmentsDir = StorageManager.getInstance(getContext()).getAttachmentDirectory(dbName,
                 account.getLocalStorageProviderId());
 
         File file = new File(attachmentsDir, id);

src/com/fsck/k9/provider/MessageProvider.java
@@ -303,7 +303,7 @@ public class MessageProvider extends ContentProvider {
 
             // new code for integrated inbox, only execute this once as it will be processed afterwards via the listener
             final SearchAccount integratedInboxAccount = SearchAccount.createUnifiedInboxAccount(getContext());
-            final MessagingController msgController = MessagingController.getInstance(K9.app);
+            final MessagingController msgController = MessagingController.getInstance(getContext());
 
             msgController.searchLocalMessages(integratedInboxAccount.getRelatedSearch(),
                                               new MesssageInfoHolderRetrieverListener(queue));
@@ -1041,7 +1041,7 @@ public class MessageProvider extends ContentProvider {
         // get localstore parameter
         LocalMessage msg = null;
         try {
-            LocalFolder lf = LocalStore.getInstance(myAccount, K9.app).getFolder(folderName);
+            LocalFolder lf = LocalStore.getInstance(myAccount, getContext()).getFolder(folderName);
             int msgCount = lf.getMessageCount();
             if (K9.DEBUG) {
                 Log.d(K9.LOG_TAG, "folder msg count = " + msgCount);
@@ -1053,7 +1053,7 @@ public class MessageProvider extends ContentProvider {
 
         // launch command to delete the message
         if ((myAccount != null) && (msg != null)) {
-            MessagingController controller = MessagingController.getInstance(K9.app);
+            MessagingController controller = MessagingController.getInstance(getContext());
             controller.deleteMessages(Collections.singletonList(msg), null);
         }
 