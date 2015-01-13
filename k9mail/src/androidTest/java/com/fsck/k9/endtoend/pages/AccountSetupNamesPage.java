055d4104b76600e46c42baea75166c1a442df182
Jan Berkel <jan.berkel@gmail.com>
2015-01-13 16:12:39 +0100

    log ChangeLog click failure

--- k9mail/src/androidTest/java/com/fsck/k9/endtoend/pages/AccountSetupNamesPage.java
+++ k9mail/src/androidTest/java/com/fsck/k9/endtoend/pages/AccountSetupNamesPage.java
@@ -1,8 +1,10 @@
 package com.fsck.k9.endtoend.pages;
 
+import com.fsck.k9.K9;
 import com.fsck.k9.R;
 import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.util.Log;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.clearText;
@@ -41,6 +43,7 @@ public class AccountSetupNamesPage extends AbstractPage {
         try {
             onView(ViewMatchers.withText("OK")).perform(click());
         } catch (NoMatchingViewException ex) {
+            Log.w(K9.LOG_TAG, "did not find Changelog OK button - ignored");
             // Ignored. Not the best way of doing this, but Espresso rightly makes
             // conditional flow difficult.
         }
