12cf19b63eb45fd0f285539733942ebd9386a3be
Dominik Schürmann <dominik@dominikschuermann.de>
2015-01-28 15:15:08 +0100

    SplitUserID into lib

--- plugins/openpgp-api-library/src/org/openintents/openpgp/util/OpenPgpUtils.java
+++ plugins/openpgp-api-library/src/org/openintents/openpgp/util/OpenPgpUtils.java
@@ -73,4 +73,39 @@ public class OpenPgpUtils {
         }
         return hexString;
     }
+
+    private static final Pattern USER_ID_PATTERN = Pattern.compile("^(.*?)(?: \\((.*)\\))?(?: <(.*)>)?$");
+
+    /**
+     * Splits userId string into naming part, email part, and comment part
+     *
+     * @param userId
+     * @return array with naming (0), email (1), comment (2)
+     */
+    public static String[] splitUserId(String userId) {
+        String[] result = new String[]{null, null, null};
+
+        if (userId == null || userId.equals("")) {
+            return result;
+        }
+
+        /*
+         * User ID matching:
+         * http://fiddle.re/t4p6f
+         *
+         * test cases:
+         * "Max Mustermann (this is a comment) <max@example.com>"
+         * "Max Mustermann <max@example.com>"
+         * "Max Mustermann (this is a comment)"
+         * "Max Mustermann [this is nothing]"
+         */
+        Matcher matcher = USER_ID_PATTERN.matcher(userId);
+        if (matcher.matches()) {
+            result[0] = matcher.group(1);
+            result[1] = matcher.group(3);
+            result[2] = matcher.group(2);
+        }
+
+        return result;
+    }
 }
