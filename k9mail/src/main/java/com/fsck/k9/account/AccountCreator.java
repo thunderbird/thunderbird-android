855da35f3adf106acc97a18a47036bd210d03310
cketti <cketti@gmail.com>
2015-03-16 13:06:40 +0100

    Code style fixes

--- k9mail/src/main/java/com/fsck/k9/account/AccountCreator.java
+++ k9mail/src/main/java/com/fsck/k9/account/AccountCreator.java
@@ -1,11 +1,13 @@
 package com.fsck.k9.account;
 
+
 import com.fsck.k9.Account.DeletePolicy;
 import com.fsck.k9.mail.ConnectionSecurity;
 import com.fsck.k9.mail.ServerSettings.Type;
 
 import java.util.HashMap;
 import java.util.Map;
+
 
 /**
  * Deals with logic surrounding account creation.
@@ -28,14 +30,16 @@ public class AccountCreator {
 
     public static int getDefaultPort(ConnectionSecurity securityType, Type storeType) {
         switch (securityType) {
-        case NONE:
-        case STARTTLS_REQUIRED:
-            return storeType.defaultPort;
-        case SSL_TLS_REQUIRED:
-            return storeType.defaultTlsPort;
-        default:
-            throw new AssertionError("Unhandled ConnectionSecurity type encountered: " + securityType);
+            case NONE:
+            case STARTTLS_REQUIRED: {
+                return storeType.defaultPort;
+            }
+            case SSL_TLS_REQUIRED: {
+                return storeType.defaultTlsPort;
+            }
+            default: {
+                throw new AssertionError("Unhandled ConnectionSecurity type encountered: " + securityType);
+            }
         }
     }
-
 }
