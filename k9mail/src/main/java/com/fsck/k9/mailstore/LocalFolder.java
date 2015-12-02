Valodim/k-9

Display *Encrypted* for encrypted messages in message preview #575

k9mail/src/main/java/com/fsck/k9/mailstore/LocalFolder.java
@@ -1262,6 +1262,7 @@ public class LocalFolder extends Folder<LocalMessage> implements Serializable {
                     ? System.currentTimeMillis() : message.getInternalDate().getTime());
             cv.put("mime_type", message.getMimeType());
             cv.put("empty", 0);
+            cv.put("encryption_type", message.getEncryptionType().name());
 
             String messageId = message.getMessageId();
             if (messageId != null) {

k9mail/src/main/java/com/fsck/k9/mailstore/StoreSchemaDefinition.java
@@ -104,7 +104,8 @@ class StoreSchemaDefinition implements LockableDatabase.SchemaDefinition {
                         "flagged INTEGER default 0, " +
                         "answered INTEGER default 0, " +
                         "forwarded INTEGER default 0, " +
-                        "message_part_id INTEGER" +
+                        "message_part_id INTEGER, " +
+                        "encryption_type TEXT" +
                         ")");
 
                 db.execSQL("CREATE TABLE message_parts (" +
@@ -576,6 +577,12 @@ class StoreSchemaDefinition implements LockableDatabase.SchemaDefinition {
                 }
                 if (db.getVersion() < 52) {
                     addMoreMessagesColumnToFoldersTable(db);
+                }
+                if (db.getVersion() < 53) {
+                    /*
+                     * All messages are initialized to "no encrypted" even if encrypted.
+                     */
+                    db.execSQL("ALTER TABLE messages ADD COLUMN encryption_type TEXT default 'NONE'");
                 }
             }
 

thunderbird/thunderbird-android

k9mail/src/main/java/com/fsck/k9/mailstore/LocalFolder.java
@@ -1262,6 +1262,7 @@ public class LocalFolder extends Folder<LocalMessage> implements Serializable {
                     ? System.currentTimeMillis() : message.getInternalDate().getTime());
             cv.put("mime_type", message.getMimeType());
             cv.put("empty", 0);
+            cv.put("encryption_type", message.getEncryptionType().ordinal());
 
             String messageId = message.getMessageId();
             if (messageId != null) {

k9mail/src/main/java/com/fsck/k9/mailstore/StoreSchemaDefinition.java
@@ -104,7 +104,8 @@ class StoreSchemaDefinition implements LockableDatabase.SchemaDefinition {
                         "flagged INTEGER default 0, " +
                         "answered INTEGER default 0, " +
                         "forwarded INTEGER default 0, " +
-                        "message_part_id INTEGER" +
+                        "message_part_id INTEGER, " +
+                        "encryption_type INTEGER" +
                         ")");
 
                 db.execSQL("CREATE TABLE message_parts (" +
@@ -576,6 +577,12 @@ class StoreSchemaDefinition implements LockableDatabase.SchemaDefinition {
                 }
                 if (db.getVersion() < 52) {
                     addMoreMessagesColumnToFoldersTable(db);
+                }
+                if (db.getVersion() < 53) {
+                    /*
+                     * All messages are initialized to "no encrypted" even if encrypted.
+                     */
+                    db.execSQL("ALTER TABLE messages ADD COLUMN encryption_type INTEGER default 0");
                 }
             }
 