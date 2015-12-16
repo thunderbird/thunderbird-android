fb10b66371e66f44fb42efbae9e9bd119f7b6fbc
cketti <cketti@gmail.com>
2015-12-16 22:49:03 +0100

    Check protocol parameter to exclude non-PGP/MIME encrypted/signed messages
    Fixes #571

--- k9mail/src/test/java/com/fsck/k9/crypto/MessageDecryptVerifierTest.java
+++ k9mail/src/test/java/com/fsck/k9/crypto/MessageDecryptVerifierTest.java
@@ -3,8 +3,10 @@ package com.fsck.k9.crypto;
 
 import java.util.List;
 
+import com.fsck.k9.mail.MessagingException;
 import com.fsck.k9.mail.Part;
 import com.fsck.k9.mail.internet.MimeBodyPart;
+import com.fsck.k9.mail.internet.MimeHeader;
 import com.fsck.k9.mail.internet.MimeMessage;
 import com.fsck.k9.mail.internet.MimeMessageHelper;
 import com.fsck.k9.mail.internet.MimeMultipart;
@@ -27,6 +29,7 @@ public class MessageDecryptVerifierTest {
         MimeMessage emptyMessage = new MimeMessage();
 
         List<Part> encryptedParts = MessageDecryptVerifier.findEncryptedParts(emptyMessage);
+
         assertEquals(0, encryptedParts.size());
     }
 
@@ -36,17 +39,20 @@ public class MessageDecryptVerifierTest {
         message.setBody(new TextBody("message text"));
 
         List<Part> encryptedParts = MessageDecryptVerifier.findEncryptedParts(message);
+
         assertEquals(0, encryptedParts.size());
     }
 
     @Test
     public void findEncryptedPartsShouldReturnEmptyEncryptedPart() throws Exception {
         MimeMessage message = new MimeMessage();
-        MimeMultipart mulitpartEncrypted = new MimeMultipart();
-        mulitpartEncrypted.setSubType("encrypted");
-        MimeMessageHelper.setBody(message, mulitpartEncrypted);
+        MimeMultipart multipartEncrypted = new MimeMultipart();
+        multipartEncrypted.setSubType("encrypted");
+        MimeMessageHelper.setBody(message, multipartEncrypted);
+        addProtocolParameter(message);
 
         List<Part> encryptedParts = MessageDecryptVerifier.findEncryptedParts(message);
+
         assertEquals(1, encryptedParts.size());
         assertSame(message, encryptedParts.get(0));
     }
@@ -58,22 +64,31 @@ public class MessageDecryptVerifierTest {
         multipartMixed.setSubType("mixed");
         MimeMessageHelper.setBody(message, multipartMixed);
 
-        MimeMultipart mulitpartEncryptedOne = new MimeMultipart();
-        mulitpartEncryptedOne.setSubType("encrypted");
-        MimeBodyPart bodyPartOne = new MimeBodyPart(mulitpartEncryptedOne);
+        MimeMultipart multipartEncryptedOne = new MimeMultipart();
+        multipartEncryptedOne.setSubType("encrypted");
+        MimeBodyPart bodyPartOne = new MimeBodyPart(multipartEncryptedOne);
+        addProtocolParameter(bodyPartOne);
         multipartMixed.addBodyPart(bodyPartOne);
 
         MimeBodyPart bodyPartTwo = new MimeBodyPart(null, "text/plain");
         multipartMixed.addBodyPart(bodyPartTwo);
 
-        MimeMultipart mulitpartEncryptedThree = new MimeMultipart();
-        mulitpartEncryptedThree.setSubType("encrypted");
-        MimeBodyPart bodyPartThree = new MimeBodyPart(mulitpartEncryptedThree);
+        MimeMultipart multipartEncryptedThree = new MimeMultipart();
+        multipartEncryptedThree.setSubType("encrypted");
+        MimeBodyPart bodyPartThree = new MimeBodyPart(multipartEncryptedThree);
+        addProtocolParameter(bodyPartThree);
         multipartMixed.addBodyPart(bodyPartThree);
 
         List<Part> encryptedParts = MessageDecryptVerifier.findEncryptedParts(message);
+
         assertEquals(2, encryptedParts.size());
         assertSame(bodyPartOne, encryptedParts.get(0));
         assertSame(bodyPartThree, encryptedParts.get(1));
+    }
+
+    //TODO: Find a cleaner way to do this
+    private void addProtocolParameter(Part part) throws MessagingException {
+        String contentType = part.getContentType();
+        part.setHeader(MimeHeader.HEADER_CONTENT_TYPE, contentType + "; protocol=\"application/pgp-encrypted\"");
     }
 }
