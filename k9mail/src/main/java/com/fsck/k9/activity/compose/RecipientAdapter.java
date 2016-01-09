67541a60cbf5eb321f0eb700e53f1ccfec18eb72
Vincent Breitmoser <valodim@mugenguild.com>
2016-01-09 01:12:15 +0100

    compose: add workaround for glide sometimes not loading contact pictures

--- k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientAdapter.java
+++ k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientAdapter.java
@@ -126,11 +126,13 @@ public class RecipientAdapter extends BaseAdapter implements Filterable {
     }
 
     public static void setContactPhotoOrPlaceholder(Context context, ImageView imageView, Recipient recipient) {
-        imageView.setImageDrawable(null);
-
         // TODO don't use two different mechanisms for loading!
         if (recipient.photoThumbnailUri != null) {
-            Glide.with(context).load(recipient.photoThumbnailUri).into(imageView);
+            Glide.with(context).load(recipient.photoThumbnailUri)
+                    // for some reason, this fixes loading issues.
+                    .placeholder(null)
+                    .dontAnimate()
+                    .into(imageView);
         } else {
             ContactPicture.getContactPictureLoader(context).loadContactPicture(recipient.address, imageView);
         }
