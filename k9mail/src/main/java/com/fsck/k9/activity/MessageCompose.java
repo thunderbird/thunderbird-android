73f1bca8a624ba6718b32b59e3576729d1cfe5c3
cketti <cketti@gmail.com>
2016-01-13 11:00:46 +0100

    Remove unused class

--- k9mail/src/main/java/com/fsck/k9/activity/MessageCompose.java
+++ k9mail/src/main/java/com/fsck/k9/activity/MessageCompose.java
@@ -2812,29 +2812,8 @@ public class MessageCompose extends K9Activity implements OnClickListener,
         if (body != null && !subject.isEmpty()) {
             mMessageContentView.setCharacters(body);
         }
-
-
     }
 
-    static class CaseInsensitiveParamWrapper {
-        private final Uri uri;
-        private Set<String> mParamNames;
-
-        public CaseInsensitiveParamWrapper(Uri uri) {
-            this.uri = uri;
-        }
-
-        public List<String> getQueryParameters(String key) {
-            final List<String> params = new ArrayList<String>();
-            for (String paramName : uri.getQueryParameterNames()) {
-                if (paramName.equalsIgnoreCase(key)) {
-                    params.addAll(uri.getQueryParameters(paramName));
-                }
-            }
-            return params;
-        }
-
-    }
 
     private class SendMessageTask extends AsyncTask<Void, Void, Void> {
         @Override
