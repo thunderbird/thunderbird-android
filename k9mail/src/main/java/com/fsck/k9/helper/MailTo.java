042346301f751f1ea6bd9bb5eeb259e592c0e416
cketti <cketti@gmail.com>
2016-01-13 10:40:09 +0100

    Rewrite MailTo to improve readability

--- k9mail/src/main/java/com/fsck/k9/helper/MailTo.java
+++ k9mail/src/main/java/com/fsck/k9/helper/MailTo.java
@@ -9,13 +9,15 @@ import android.net.Uri;
 import com.fsck.k9.mail.Address;
 
 
-public class MailTo {
-    public static final String MAILTO_SCHEME = "mailto";
+public final class MailTo {
+    private static final String MAILTO_SCHEME = "mailto";
     private static final String TO = "to";
     private static final String BODY = "body";
     private static final String CC = "cc";
     private static final String BCC = "bcc";
     private static final String SUBJECT = "subject";
+
+    private static final Address[] EMPTY_ADDRESS_LIST = new Address[0];
 
 
     private final Address[] toAddresses;
@@ -26,11 +28,7 @@ public class MailTo {
 
 
     public static boolean isMailTo(Uri uri) {
-        if (uri == null) {
-            return false;
-        }
-
-        return MAILTO_SCHEME.equals(uri.getScheme());
+        return uri != null && MAILTO_SCHEME.equals(uri.getScheme());
     }
 
     public static MailTo parse(Uri uri) throws NullPointerException, IllegalArgumentException {
@@ -48,53 +46,59 @@ public class MailTo {
             end = schemaSpecific.length();
         }
 
-        CaseInsensitiveParamWrapper caseInsensitiveParamWrapper = new CaseInsensitiveParamWrapper(
-                Uri.parse("foo://bar?" + uri.getEncodedQuery()));
+        CaseInsensitiveParamWrapper params =
+                new CaseInsensitiveParamWrapper(Uri.parse("foo://bar?" + uri.getEncodedQuery()));
 
         // Extract the recipient's email address from the mailto URI if there's one.
         String recipient = Uri.decode(schemaSpecific.substring(0, end));
 
-        // Read additional recipients from the "to" parameter.
-        List<String> toList = caseInsensitiveParamWrapper.getQueryParameters(TO);
+        List<String> toList = params.getQueryParameters(TO);
         if (recipient.length() != 0) {
             toList.add(0, recipient);
         }
 
-        List<String> ccList = caseInsensitiveParamWrapper.getQueryParameters(CC);
+        List<String> ccList = params.getQueryParameters(CC);
+        List<String> bccList = params.getQueryParameters(BCC);
 
-        List<String> bccList = caseInsensitiveParamWrapper.getQueryParameters(BCC);
+        Address[] toAddresses = toAddressArray(toList);
+        Address[] ccAddresses = toAddressArray(ccList);
+        Address[] bccAddresses = toAddressArray(bccList);
 
-        List<String> subjectList = caseInsensitiveParamWrapper.getQueryParameters(SUBJECT);
-        String subject = null;
-        if (!subjectList.isEmpty()) {
-            subject = subjectList.get(0);
-        }
+        String subject = getFirstParameterValue(params, SUBJECT);
+        String body = getFirstParameterValue(params, BODY);
 
-        List<String> bodyList = caseInsensitiveParamWrapper.getQueryParameters(BODY);
-        String body = null;
-        if (!bodyList.isEmpty()) {
-            body = bodyList.get(0);
-        }
-
-        return new MailTo(listToCommaSeperatedString(toList), listToCommaSeperatedString(ccList),
-                listToCommaSeperatedString(bccList), subject, body);
+        return new MailTo(toAddresses, ccAddresses, bccAddresses, subject, body);
     }
 
-    private static String listToCommaSeperatedString(List<String> listStr) {
-        StringBuilder strBuilder = new StringBuilder();
-        String sep = "";
-        for (String str : listStr) {
-            strBuilder.append(sep).append(str);
-            sep = ",";
-        }
+    private static String getFirstParameterValue(CaseInsensitiveParamWrapper params, String paramName) {
+        List<String> paramValues = params.getQueryParameters(paramName);
 
-        return strBuilder.toString();
+        return (paramValues.isEmpty()) ? null : paramValues.get(0);
     }
 
-    private MailTo(String toAddresses, String ccAddresses, String bccAddresses, String subject, String body) {
-        this.toAddresses = Address.parseUnencoded(toAddresses);
-        this.ccAddresses = Address.parseUnencoded(ccAddresses);
-        this.bccAddresses = Address.parseUnencoded(bccAddresses);
+    private static Address[] toAddressArray(List<String> recipients) {
+        if (recipients.isEmpty()) {
+            return EMPTY_ADDRESS_LIST;
+        }
+
+        String addressList = toCommaSeparatedString(recipients);
+        return Address.parse(addressList);
+    }
+
+    private static String toCommaSeparatedString(List<String> list) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String item : list) {
+            stringBuilder.append(item).append(',');
+        }
+
+        stringBuilder.setLength(stringBuilder.length() - 1);
+        return stringBuilder.toString();
+    }
+
+    private MailTo(Address[] toAddresses, Address[] ccAddresses, Address[] bccAddresses, String subject, String body) {
+        this.toAddresses = toAddresses;
+        this.ccAddresses = ccAddresses;
+        this.bccAddresses = bccAddresses;
         this.subject = subject;
         this.body = body;
     }
