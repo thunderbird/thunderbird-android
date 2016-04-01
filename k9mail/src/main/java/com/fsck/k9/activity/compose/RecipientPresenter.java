41d5098c45b0df682cac6eb221946286ac8fc67f
Vincent Breitmoser <valodim@mugenguild.com>
2016-04-01 16:03:10 +0200

    compose: automatically use pgp/inline if replied-to msg is pgp/inline

--- k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientPresenter.java
+++ k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientPresenter.java
@@ -35,6 +35,7 @@ import com.fsck.k9.mail.Message.RecipientType;
 import com.fsck.k9.mail.MessagingException;
 import com.fsck.k9.mailstore.LocalMessage;
 import com.fsck.k9.message.PgpMessageBuilder;
+import com.fsck.k9.message.ComposePgpInlineDecider;
 import com.fsck.k9.view.RecipientSelectView.Recipient;
 import org.openintents.openpgp.IOpenPgpService2;
 import org.openintents.openpgp.util.OpenPgpApi;
@@ -165,6 +166,12 @@ public class RecipientPresenter implements PermissionPingCallback {
 
                 }
             }
+
+            boolean shouldSendAsPgpInline = ComposePgpInlineDecider.getInstance().shouldReplyInline(message);
+            if (shouldSendAsPgpInline) {
+                cryptoEnablePgpInline = true;
+            }
+
         } catch (MessagingException e) {
             // can't happen, we know the recipient types exist
             throw new AssertionError(e);
