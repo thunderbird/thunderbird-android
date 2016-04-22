68abb46a88a5d7224582bcc77cd9536a7ddeb4ef
cketti <cketti@gmail.com>
2016-04-22 03:05:56 +0200

    Fix some checkstyle violations

--- k9mail/src/test/java/com/fsck/k9/SputnikTest.java
+++ k9mail/src/test/java/com/fsck/k9/SputnikTest.java
@@ -3,11 +3,7 @@ package com.fsck.k9;
 import java.util.*;
 
 public class SputnikTest {
-    public   void sputnikTest(){
-        int x= 2+1;
-
-        if(x == 2) {}
-
+    public void sputnikTest() {
         return;
     }
 }
