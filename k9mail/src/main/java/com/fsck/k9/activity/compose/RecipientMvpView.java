jeffreyj100/k-9

compose: highlight pgp/inline icon in background with showcase lib

k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientMvpView.java
@@ -7,12 +7,10 @@ import java.util.List;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 import android.widget.ViewAnimator;
 
@@ -404,25 +402,12 @@ public class RecipientMvpView implements OnFocusChangeListener, OnClickListener 
     }
 
     public void showOpenPgpInlineDialog(boolean firstTime) {
-        hideKeyboard();
-
-        OpenPgpInlineDialog dialog = OpenPgpInlineDialog.newInstance(firstTime);
+        OpenPgpInlineDialog dialog = OpenPgpInlineDialog.newInstance(firstTime, R.id.openpgp_compat_indicator);
         dialog.show(activity.getFragmentManager(), "openpgp_inline");
     }
 
     public void launchUserInteractionPendingIntent(PendingIntent pendingIntent, int requestCode) {
         activity.launchUserInteractionPendingIntent(pendingIntent, requestCode);
-    }
-
-    private void hideKeyboard() {
-        InputMethodManager inputManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
-
-        // check if no view has focus
-        View v = activity.getCurrentFocus();
-        if (v == null)
-            return;
-
-        inputManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
 
     public enum CryptoStatusDisplayType {

thunderbird/thunderbird-android

k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientMvpView.java
@@ -5,12 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import android.app.PendingIntent;
-import android.content.Context;
 import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 import android.widget.ViewAnimator;
 
@@ -369,25 +367,12 @@ public class RecipientMvpView implements OnFocusChangeListener, OnClickListener 
     }
 
     public void showOpenPgpInlineDialog(boolean firstTime) {
-        hideKeyboard();
-
-        PgpInlineDialog dialog = PgpInlineDialog.newInstance(firstTime);
+        PgpInlineDialog dialog = PgpInlineDialog.newInstance(firstTime, R.id.pgp_inline_indicator);
         dialog.show(activity.getFragmentManager(), "openpgp_inline");
     }
 
     public void launchUserInteractionPendingIntent(PendingIntent pendingIntent, int requestCode) {
         activity.launchUserInteractionPendingIntent(pendingIntent, requestCode);
-    }
-
-    private void hideKeyboard() {
-        InputMethodManager inputManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
-
-        // check if no view has focus
-        View v = activity.getCurrentFocus();
-        if (v == null)
-            return;
-
-        inputManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
 
     public enum CryptoStatusDisplayType {