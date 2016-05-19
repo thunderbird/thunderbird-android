simonelberts/K-9

messageview: refactor OpenPgpResultAnnotation into immutable CryptoResultAnnotation

k9mail/src/main/java/com/fsck/k9/mailstore/MessageViewInfo.java
@@ -12,15 +12,15 @@ public class MessageViewInfo {
     public final Part rootPart;
     public final String text;
     public final List<AttachmentViewInfo> attachments;
-    public final OpenPgpResultAnnotation openPgpResultAnnotation;
+    public final CryptoResultAnnotation cryptoResultAnnotation;
 
 
     public MessageViewInfo(Message message, Part rootPart, String text, List<AttachmentViewInfo> attachments,
-            OpenPgpResultAnnotation openPgpResultAnnotation) {
+            CryptoResultAnnotation cryptoResultAnnotation) {
         this.message = message;
         this.rootPart = rootPart;
         this.text = text;
         this.attachments = attachments;
-        this.openPgpResultAnnotation = openPgpResultAnnotation;
+        this.cryptoResultAnnotation = cryptoResultAnnotation;
     }
 }

thunderbird/thunderbird-android

k9mail/src/main/java/com/fsck/k9/mailstore/MessageViewInfo.java
@@ -22,11 +22,11 @@ public class MessageViewInfo {
         public final String text;
         public final Part rootPart;
         public final List<AttachmentViewInfo> attachments;
-        public final OpenPgpResultAnnotation cryptoAnnotation;
+        public final CryptoResultAnnotation cryptoAnnotation;
 
 
         MessageViewContainer(String text, Part rootPart, List<AttachmentViewInfo> attachments,
-                OpenPgpResultAnnotation cryptoAnnotation) {
+                CryptoResultAnnotation cryptoAnnotation) {
             this.text = text;
             this.rootPart = rootPart;
             this.attachments = attachments;