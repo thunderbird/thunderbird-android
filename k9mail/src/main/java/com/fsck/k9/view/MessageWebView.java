24409b773224a319abafb57265b2b0007c095b07
Philip Whitehouse <philip@whiuk.com>
2017-01-08 13:19:14 +0000

    Rework where we wrap message content

--- k9mail/src/main/java/com/fsck/k9/view/MessageWebView.java
+++ k9mail/src/main/java/com/fsck/k9/view/MessageWebView.java
@@ -118,7 +118,7 @@ public class MessageWebView extends RigidWebView {
     public void displayHtmlContentWithInlineAttachments(@NonNull String htmlText,
             @Nullable AttachmentResolver attachmentResolver, @Nullable OnPageFinishedListener onPageFinishedListener) {
         setWebViewClient(attachmentResolver, onPageFinishedListener);
-        setHtmlContent(HtmlConverter.wrapMessageContent(htmlText));
+        setHtmlContent(htmlText);
     }
 
     private void setWebViewClient(@Nullable AttachmentResolver attachmentResolver,
