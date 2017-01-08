f3e55347970bbe6929556492494218f7fae9ee3d
cketti <cketti@gmail.com>
2017-01-08 08:50:10 +0100

    Clean up FTS table on update

---
+++ k9mail/src/main/java/com/fsck/k9/mailstore/migrations/MigrationTo56.java
@@ -0,0 +1,11 @@
+package com.fsck.k9.mailstore.migrations;
+
+
+import android.database.sqlite.SQLiteDatabase;
+
+
+class MigrationTo56 {
+    static void cleanUpFtsTable(SQLiteDatabase db) {
+        db.execSQL("DELETE FROM messages_fulltext WHERE docid NOT IN (SELECT id FROM messages WHERE deleted = 0)");
+    }
+}
