garyjl/k-9

fix broken referencing of recipient token in RecipientSelectView
Passing through the actual recipient object was broken in
a2674efff1155228b31d3a7b966624b2ebed19f7. I changed the code to no
longer rely on the actual object being passed through correctly, but
just one that fulfills `equals`.

k9mail/src/main/java/com/fsck/k9/view/RecipientSelectView.java
@@ -401,8 +401,16 @@ public class RecipientSelectView extends TokenCompleteTextView<Recipient> implem
     }
 
     @Override
-    public void onRecipientChange(Recipient currentRecipient, Recipient alternateAddress) {
+    public void onRecipientChange(Recipient recipientToReplace, Recipient alternateAddress) {
         alternatesPopup.dismiss();
+
+        List<Recipient> currentRecipients = getObjects();
+        int indexOfRecipient = currentRecipients.indexOf(alternateAddress);
+        if (indexOfRecipient == -1) {
+            Log.e(K9.LOG_TAG, "Tried to refresh invalid view token!");
+            return;
+        }
+        Recipient currentRecipient = currentRecipients.get(indexOfRecipient);
 
         currentRecipient.address = alternateAddress.address;
         currentRecipient.addressLabel = alternateAddress.addressLabel;
@@ -630,5 +638,6 @@ public class RecipientSelectView extends TokenCompleteTextView<Recipient> implem
                 photoThumbnailUri = Uri.parse(uriString);
             }
         }
+
     }
 }

thunderbird/thunderbird-android

k9mail/src/main/java/com/fsck/k9/view/RecipientSelectView.java
@@ -396,8 +396,16 @@ public class RecipientSelectView extends TokenCompleteTextView<Recipient> implem
     }
 
     @Override
-    public void onRecipientChange(Recipient currentRecipient, Recipient alternateAddress) {
+    public void onRecipientChange(Recipient recipientToReplace, Recipient alternateAddress) {
         alternatesPopup.dismiss();
+
+        List<Recipient> currentRecipients = getObjects();
+        int indexOfRecipient = currentRecipients.indexOf(recipientToReplace);
+        if (indexOfRecipient == -1) {
+            Log.e(K9.LOG_TAG, "Tried to refresh invalid view token!");
+            return;
+        }
+        Recipient currentRecipient = currentRecipients.get(indexOfRecipient);
 
         currentRecipient.address = alternateAddress.address;
         currentRecipient.addressLabel = alternateAddress.addressLabel;