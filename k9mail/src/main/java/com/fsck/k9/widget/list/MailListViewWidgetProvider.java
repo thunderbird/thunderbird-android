27ec98b1d0b6fabb6c4ec25b579b323bea4e82de
cketti <cketti@gmail.com>
2017-02-14 05:13:16 +0100

    Remove configuration for mail list widget
    Right now MessageProvider only supports the Unified Inbox.

--- k9mail/src/main/java/com/fsck/k9/widget/list/MailListViewWidgetProvider.java
+++ k9mail/src/main/java/com/fsck/k9/widget/list/MailListViewWidgetProvider.java
@@ -10,8 +10,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.widget.RemoteViews;
 
-import com.fsck.k9.Account;
-import com.fsck.k9.Preferences;
 import com.fsck.k9.R;
 import com.fsck.k9.activity.MessageCompose;
 import com.fsck.k9.activity.MessageList;
@@ -21,6 +19,7 @@ public class MailListViewWidgetProvider extends AppWidgetProvider {
     public static final String PACKAGE_NAME = "com.fsck.k9";
     public static String ACTION_VIEW_MAIL_ITEM = PACKAGE_NAME + ".provider.ACTION_VIEW_MAIL_ITEM";
     public static String ACTION_COMPOSE_EMAIL = PACKAGE_NAME + ".provider.ACTION_COMPOSE_EMAIL";
+    public static String ACTION_UPDATE_MESSAGE_LIST = PACKAGE_NAME + ".provider.ACTION_UPDATE_MESSAGE_LIST";
 
 
     public static void updateMailViewList(Context context) {
@@ -30,23 +29,18 @@ public class MailListViewWidgetProvider extends AppWidgetProvider {
         int[] widgetIds = widgetManager.getAppWidgetIds(widget);
 
         Intent intent = new Intent(context, MailListViewWidgetProvider.class);
-        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intent.setAction(ACTION_UPDATE_MESSAGE_LIST);
         intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, widgetIds);
         context.sendBroadcast(intent);
     }
 
-    public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId,
-            String acc) {
-
-        CharSequence widgetText = context.getString(R.string.mail_list_widget_text);
-        Account account = Preferences.getPreferences(context).getAccount(acc);
-        // Construct the RemoteViews object
+    public static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.mail_list_view_widget_layout);
-        views.setRemoteAdapter(R.id.listView, new Intent(context, MailListViewWidgetService.class));
-        views.setTextViewText(R.id.folder, account.getInboxFolderName());
-        views.setTextViewText(R.id.mail_address, account.getEmail());
-        Intent intent = new Intent(context, MailListViewWidgetService.class)
-                .putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+
+        views.setTextViewText(R.id.folder, context.getString(R.string.integrated_inbox_title));
+
+        Intent intent = new Intent(context, MailListViewWidgetService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
         intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
         views.setRemoteAdapter(R.id.listView, intent);
 
@@ -60,49 +54,40 @@ public class MailListViewWidgetProvider extends AppWidgetProvider {
         Intent composeIntent = new Intent(context, MailListViewWidgetProvider.class);
         composeIntent.setPackage(PACKAGE_NAME);
         composeIntent.setAction(ACTION_COMPOSE_EMAIL);
-        composeIntent.putExtra(MessageCompose.EXTRA_ACCOUNT, acc);
 
         PendingIntent newMailIntent = PendingIntent.getBroadcast(context, 0, composeIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
-        // Add intent for composing a new message
         views.setOnClickPendingIntent(R.id.new_message, newMailIntent);
+
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        String acc;
-        for (int widgId : appWidgetIds) {
-            acc = MailListViewWidgetConfiguration.getAccountUuid(context, widgId);
-            updateAppWidget(context, appWidgetManager, widgId, acc);
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // an intent from an item on the listview. get the uri and launch the MessageList activity
+        super.onReceive(context, intent);
+
         if (intent.getAction().equals(ACTION_VIEW_MAIL_ITEM)) {
             Intent viewMailIntent = new Intent(context, MessageList.class);
             viewMailIntent.setAction(Intent.ACTION_VIEW);
             viewMailIntent.setData(Uri.parse(intent.getStringExtra(AppWidgetManager.EXTRA_CUSTOM_INFO)));
             viewMailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(viewMailIntent);
-        } else if (intent.getAction().equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
-            AppWidgetManager.getInstance(context).notifyAppWidgetViewDataChanged(
-                    intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS), R.id.listView);
+        } else if (intent.getAction().equals(ACTION_UPDATE_MESSAGE_LIST)) {
+            AppWidgetManager.getInstance(context)
+                    .notifyAppWidgetViewDataChanged(intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS),
+                            R.id.listView);
         } else if (intent.getAction().equals(ACTION_COMPOSE_EMAIL)) {
             Intent newMessage = new Intent(context, MessageCompose.class)
-                    .putExtra(MessageCompose.EXTRA_ACCOUNT, intent.getStringExtra(MessageCompose.EXTRA_ACCOUNT))
-                    .setAction(Intent.ACTION_VIEW)
+                    .setAction(MessageCompose.ACTION_COMPOSE)
                     .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(newMessage);
-        }
-    }
-
-    @Override
-    public void onDeleted(Context context, int[] appWidgetIds) {
-        for (int widgId : appWidgetIds) {
-            MailListViewWidgetConfiguration.deleteWidgetConfiguration(context, widgId);
         }
     }
 }
