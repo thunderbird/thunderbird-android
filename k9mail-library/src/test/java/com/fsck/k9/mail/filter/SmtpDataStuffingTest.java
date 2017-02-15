d367d74d5fb63829f0dfd2c7a69f45a658b8626a
shonramamurthy <shonramamurthy@users.noreply.github.com>
2017-02-15 19:02:30 +0530

    Issue#2210 : Unit Tests for SmtpDataStuffing. Changes requested from #2231

--- k9mail-library/src/test/java/com/fsck/k9/mail/filter/SmtpDataStuffingTest.java
+++ k9mail-library/src/test/java/com/fsck/k9/mail/filter/SmtpDataStuffingTest.java
@@ -5,74 +5,52 @@ import java.io.ByteArrayOutputStream;
 
 import okio.ByteString;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
 public class SmtpDataStuffingTest {
-    private String expectedMessageAfterStuffing;
-    private String actualMessageAfterStuffing;
-
-    @Before
-    public void setUp(){
-        expectedMessageAfterStuffing = "";
-        actualMessageAfterStuffing = "";
-    }
-
     @Test
-    public void smtpDotStuffing_OnlyStuffingDotTest() throws IOException {
+    public void smtpDotStuffing_stuffingOnlyDotTest() throws IOException {
+        String expectedMessageAfterStuffing = "Hello dot\r\n..";
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         SmtpDataStuffing smtpDataStuffingStream = new SmtpDataStuffing(byteArrayOutputStream);
         byte[] data = ByteString.encodeUtf8("Hello dot\r\n.").toByteArray();
 
-        expectedMessageAfterStuffing = "Hello dot\r\n..";
-        try {
-            smtpDataStuffingStream.write(data);
-        }
-        finally {
-            smtpDataStuffingStream.close();
-            byteArrayOutputStream.close();
-        }
-        actualMessageAfterStuffing = ByteString.of(byteArrayOutputStream.toByteArray()).utf8();
+        smtpDataStuffingStream.write(data);
+        smtpDataStuffingStream.close();
+        byteArrayOutputStream.close();
+        String actualMessageAfterStuffing = ByteString.of(byteArrayOutputStream.toByteArray()).utf8();
 
         assertEquals(expectedMessageAfterStuffing, actualMessageAfterStuffing);
     }
 
     @Test
-    public void smtpDotStuffing_NoStuffingDotTest() throws IOException {
+    public void smtpDotStuffing_noStuffingDotTest() throws IOException {
+        String expectedMessageAfterStuffing = "...Hello .. dots.";
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         SmtpDataStuffing smtpDataStuffingStream = new SmtpDataStuffing(byteArrayOutputStream);
         byte[] data = ByteString.encodeUtf8("...Hello .. dots.").toByteArray();
 
-        expectedMessageAfterStuffing = "...Hello .. dots.";
-        try {
-            smtpDataStuffingStream.write(data);
-        }
-        finally {
-            smtpDataStuffingStream.close();
-            byteArrayOutputStream.close();
-        }
-        actualMessageAfterStuffing = ByteString.of(byteArrayOutputStream.toByteArray()).utf8();
+        smtpDataStuffingStream.write(data);
+        smtpDataStuffingStream.close();
+        byteArrayOutputStream.close();
+        String actualMessageAfterStuffing = ByteString.of(byteArrayOutputStream.toByteArray()).utf8();
 
         assertEquals(expectedMessageAfterStuffing, actualMessageAfterStuffing);
     }
 
     @Test
-    public void smtpDotStuffing_StuffingAndNoStuffingDotsTest() throws IOException {
+    public void smtpDotStuffing_stuffingAndNoStuffingDotTest() throws IOException {
+        String expectedMessageAfterStuffing = "\r\n..Hello . dots.\r\n...\r\n..\r\n....";
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         SmtpDataStuffing smtpDataStuffingStream = new SmtpDataStuffing(byteArrayOutputStream);
         byte[] data = ByteString.encodeUtf8("\r\n.Hello . dots.\r\n..\r\n.\r\n...").toByteArray();
 
-        expectedMessageAfterStuffing = "\r\n..Hello . dots.\r\n...\r\n..\r\n....";
-        try {
-            smtpDataStuffingStream.write(data);
-        }
-        finally {
-            smtpDataStuffingStream.close();
-            byteArrayOutputStream.close();
-        }
-        actualMessageAfterStuffing = ByteString.of(byteArrayOutputStream.toByteArray()).utf8();
+        smtpDataStuffingStream.write(data);
+        smtpDataStuffingStream.close();
+        byteArrayOutputStream.close();
+        String actualMessageAfterStuffing = ByteString.of(byteArrayOutputStream.toByteArray()).utf8();
 
         assertEquals(expectedMessageAfterStuffing, actualMessageAfterStuffing);
     }
