c30e4b40a78db7765ab3a6877c81ffd7c409f12c
ᔕᗩYᗩᑎ GOᔕᗯᗩᗰI <goswami.sayan47@gmail.com>
2017-03-01 07:42:38 +0530

    Add edge test cases, bug fixes
    Fixes : non static method cannot be referenced from a static context

--- k9mail/src/test/java/com/fsck/k9/helper/HorizontalRuleHelperTest.java
+++ k9mail/src/test/java/com/fsck/k9/helper/HorizontalRuleHelperTest.java
@@ -9,58 +9,93 @@ public class HorizontalRuleHelperTest {
 
 	@Test
 	public void replaceHorizontalRule_withDashes_shouldReturnReplacementWithHR() {
-		String result = HorizontalRuleHelper.replaceHorizontalRule("--------------");
+		String result = new HorizontalRuleHelper().replaceHorizontalRule("--------------");
 
 		assertEquals("<hr />", result);
 	}
+
+    @Test
+    public void replaceHorizontalRule_withLessNumberOfDashes_shouldReturnWithoutReplacement() {
+        String result = new HorizontalRuleHelper().replaceHorizontalRule("---");
+
+        assertEquals("---", result);
+    }
 
 	@Test
 	public void replaceHorizontalRule_withEquals_shouldReturnReplacementWithHR() {
-		String result = HorizontalRuleHelper.replaceHorizontalRule("==============");
+		String result = new HorizontalRuleHelper().replaceHorizontalRule("==============");
 
 		assertEquals("<hr />", result);
 	}
+
+    @Test
+    public void replaceHorizontalRule_withLessNumberOfEquals_shouldReturnWithoutReplacement() {
+        String result = new HorizontalRuleHelper().replaceHorizontalRule("===");
+
+        assertEquals("===", result);
+    }
 
 	@Test
 	public void replaceHorizontalRule_withFancyDashEquals_shouldReturnReplacementWithHR() {
-		String result = HorizontalRuleHelper.replaceHorizontalRule("-=-=-=-=-=-=-=-");
+		String result = new HorizontalRuleHelper().replaceHorizontalRule("-=-=-=-=-=-=-=-");
 
 		assertEquals("<hr />", result);
 	}
 
+    @Test
+    public void replaceHorizontalRule_withLessNumberOfFancyDashEquals_shouldReturnWithoutReplacement() {
+        String result = new HorizontalRuleHelper().replaceHorizontalRule("-=-");
+
+        assertEquals("-=-", result);
+    }
+
 	@Test
 	public void replaceHorizontalRule_withUnderscores_shouldReturnReplacementWithHR() {
-		String result = HorizontalRuleHelper.replaceHorizontalRule("_______________");
+		String result = new HorizontalRuleHelper().replaceHorizontalRule("_______________");
 
 		assertEquals("<hr />", result);
 	}
 
 	@Test
 	public void replaceHorizontalRule_withScissorsLeft_shouldReturnReplacementWithHR() {
-		String result = HorizontalRuleHelper.replaceHorizontalRule("- - - >8 - - -");
+		String result = new HorizontalRuleHelper().replaceHorizontalRule("- - - >8 - - -");
 
 		assertEquals("<hr />", result);
 	}
 
 	@Test
 	public void replaceHorizontalRule_withScissorsLeft2_shouldReturnReplacementWithHR() {
-		String result = HorizontalRuleHelper.replaceHorizontalRule("--- >8 ---");
+		String result = new HorizontalRuleHelper().replaceHorizontalRule("--- >8 ---");
 
 		assertEquals("<hr />", result);
 	}
 
+    @Test
+    public void replaceHorizontalRule_withScissorsLeft3_shouldReturnReplacementWithHR() {
+        String result = new HorizontalRuleHelper().replaceHorizontalRule(" >8 ");
+
+        assertEquals("<hr />", result);
+    }
+
 	@Test
 	public void replaceHorizontalRule_withScissorsRight_shouldReturnReplacementWithHR() {
-		String result = HorizontalRuleHelper.replaceHorizontalRule("- - - 8< - - -");
+		String result = new HorizontalRuleHelper().replaceHorizontalRule("- - - 8< - - -");
 
 		assertEquals("<hr />", result);
 	}
 
 	@Test
 	public void replaceHorizontalRule_withScissorsRight2_shouldReturnReplacementWithHR() {
-		String result = HorizontalRuleHelper.replaceHorizontalRule("--- 8< ---");
+		String result = new HorizontalRuleHelper().replaceHorizontalRule("--- 8< ---");
 
 		assertEquals("<hr />", result);
 	}
+
+    @Test
+    public void replaceHorizontalRule_withScissorsRight3_shouldReturnReplacementWithHR() {
+        String result = new HorizontalRuleHelper().replaceHorizontalRule(" 8< ");
+
+        assertEquals("<hr />", result);
+    }
 
 }
