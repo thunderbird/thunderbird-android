eccb1bdf29d850f76b9a34014b3681be0bc7ab30
Daniel Wainwright <dan.wainwright2@gmail.com>
2017-03-02 13:05:05 +0000

    Update Line Wrap tester
    This tester checks if a new line is put in where not needed

--- k9mail-library/src/test/java/com/fsck/k9/mail/filter/LineWrapOutputStreamTest.java
+++ k9mail-library/src/test/java/com/fsck/k9/mail/filter/LineWrapOutputStreamTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class LineWrapOutputStreamTest {
-    private static final int maxLineLength = 28;
     private static final String INPUT_STRING =
             "This is going to be a long test string with some longer words."
                     + " This allows testing for splitting words when needed."
@@ -16,19 +15,35 @@ public class LineWrapOutputStreamTest {
                     + "ParticularlyLongWordToBeSplit.\r\n"
                     + "This will also test for some end of lines.";
 
-    private static final String EXPECTED_OUTPUT =
+    private static final String EXPECTED_OUTPUT_28 =
             "This is going to be a long\r\ntest string with some longer\r\nwords."
                     + " This allows testing\r\nfor splitting words when\r\nneeded."
                     + " This will also mean\r\na shortened maxLineLength.\r\n"
                     + "ParticularlyLongWordToBeSpli\r\nt.\r\n"
                     + "This will also test for some\r\nend of lines.";
 
+    private static final String EXPECTED_OUTPUT_30 =
+            "This is going to be a long\r\ntest string with some longer\r\nwords."
+                    + " This allows testing for\r\nsplitting words when needed."
+                    + "\r\nThis will also mean a\r\nshortened maxLineLength.\r\n"
+                    + "ParticularlyLongWordToBeSplit.\r\n"
+                    + "This will also test for some\r\nend of lines.";
+
     @Test
-    public void testLineWrap() throws IOException{
+    public void testLineWrapLength28() throws IOException{
         OutputStream outputStream = new ByteArrayOutputStream();
-        LineWrapOutputStream lineWrapOutputStream = new LineWrapOutputStream(outputStream, maxLineLength+2);
+        LineWrapOutputStream lineWrapOutputStream = new LineWrapOutputStream(outputStream, 28+2);
         lineWrapOutputStream.write(INPUT_STRING.getBytes("US-ASCII"));
         lineWrapOutputStream.flush();
-        assertEquals(EXPECTED_OUTPUT,outputStream.toString());
+        assertEquals("Failed on length 28",EXPECTED_OUTPUT_28,outputStream.toString());
+    }
+
+    @Test
+    public void testLineWrapLength30() throws IOException{
+        OutputStream outputStream = new ByteArrayOutputStream();
+        LineWrapOutputStream lineWrapOutputStream = new LineWrapOutputStream(outputStream, 30+2);
+        lineWrapOutputStream.write(INPUT_STRING.getBytes("US-ASCII"));
+        lineWrapOutputStream.flush();
+        assertEquals("Failed on length 30",EXPECTED_OUTPUT_30,outputStream.toString());
     }
 }
