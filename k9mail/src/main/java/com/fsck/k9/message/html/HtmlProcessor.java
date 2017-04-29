268189c1b097a058cf8e5da621f760871fa58d58
cketti <cketti@gmail.com>
2017-04-29 02:28:33 +0200

    Use jsoup in HtmlProcessor and HtmlSanitizer

--- k9mail/src/main/java/com/fsck/k9/message/html/HtmlProcessor.java
+++ k9mail/src/main/java/com/fsck/k9/message/html/HtmlProcessor.java
@@ -1,12 +1,15 @@
 package com.fsck.k9.message.html;
 
 
+import org.jsoup.nodes.Document;
+
+
 public class HtmlProcessor {
     private final HtmlSanitizer htmlSanitizer;
 
 
     public static HtmlProcessor newInstance() {
-        HtmlSanitizer htmlSanitizer = HtmlSanitizer.getInstance();
+        HtmlSanitizer htmlSanitizer = new HtmlSanitizer();
         return new HtmlProcessor(htmlSanitizer);
     }
 
@@ -15,7 +18,23 @@ public class HtmlProcessor {
     }
 
     public String processForDisplay(String html) {
-        String wrappedHtml = HtmlConverter.wrapMessageContent(html);
-        return htmlSanitizer.sanitize(wrappedHtml);
+        Document document = htmlSanitizer.sanitize(html);
+        addCustomHeadContents(document);
+
+        return toCompactString(document);
+    }
+
+    private void addCustomHeadContents(Document document) {
+        document.head().append("<meta name=\"viewport\" content=\"width=device-width\"/>" +
+                HtmlConverter.cssStyleTheme() +
+                HtmlConverter.cssStylePre());
+    }
+
+    static String toCompactString(Document document) {
+        document.outputSettings()
+                .prettyPrint(false)
+                .indentAmount(0);
+
+        return document.html();
     }
 }
--- k9mail/src/main/java/com/fsck/k9/message/html/HtmlSanitizer.java
+++ k9mail/src/main/java/com/fsck/k9/message/html/HtmlSanitizer.java
@@ -1,63 +1,31 @@
 package com.fsck.k9.message.html;
 
 
-import android.support.annotation.VisibleForTesting;
-
-import org.htmlcleaner.CleanerProperties;
-import org.htmlcleaner.HtmlCleaner;
-import org.htmlcleaner.HtmlSerializer;
-import org.htmlcleaner.SimpleHtmlSerializer;
-import org.htmlcleaner.TagNode;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Cleaner;
+import org.jsoup.safety.Whitelist;
 
 
 public class HtmlSanitizer {
-    private static final HtmlCleaner HTML_CLEANER;
-    private static final HtmlSerializer HTML_SERIALIZER;
+    private final HeadCleaner headCleaner;
+    private final Cleaner cleaner;
 
-    static {
-        CleanerProperties properties = createCleanerProperties();
-        HTML_CLEANER = new HtmlCleaner(properties);
-        HTML_SERIALIZER = new SimpleHtmlSerializer(properties);
+    HtmlSanitizer() {
+        Whitelist whitelist = Whitelist.relaxed()
+                .addTags("font")
+                .addAttributes("table", "align", "bgcolor", "border", "cellpadding", "cellspacing", "width")
+                .addAttributes(":all", "class", "style", "id")
+                .addProtocols("img", "src", "http", "https", "cid", "data");
+
+        cleaner = new Cleaner(whitelist);
+        headCleaner = new HeadCleaner();
     }
 
-
-    public static HtmlSanitizer getInstance() {
-        return new HtmlSanitizer();
-    }
-
-    @VisibleForTesting
-    HtmlSanitizer() {}
-
-
-    public String sanitize(String html) {
-        TagNode rootNode = HTML_CLEANER.clean(html);
-
-        removeMetaRefresh(rootNode);
-
-        return HTML_SERIALIZER.getAsString(rootNode, "UTF8");
-    }
-
-    private static CleanerProperties createCleanerProperties() {
-        CleanerProperties properties = new CleanerProperties();
-
-        // See http://htmlcleaner.sourceforge.net/parameters.php for descriptions
-        properties.setNamespacesAware(false);
-        properties.setAdvancedXmlEscape(false);
-        properties.setOmitXmlDeclaration(true);
-        properties.setOmitDoctypeDeclaration(false);
-        properties.setTranslateSpecialEntities(false);
-        properties.setRecognizeUnicodeChars(false);
-        properties.setIgnoreQuestAndExclam(false);
-
-        return properties;
-    }
-
-    private void removeMetaRefresh(TagNode rootNode) {
-        for (TagNode element : rootNode.getElementListByName("meta", true)) {
-            String httpEquiv = element.getAttributeByName("http-equiv");
-            if (httpEquiv != null && httpEquiv.trim().equalsIgnoreCase("refresh")) {
-                element.removeFromTree();
-            }
-        }
+    public Document sanitize(String html) {
+        Document dirtyDocument = Jsoup.parse(html);
+        Document cleanedDocument = cleaner.clean(dirtyDocument);
+        headCleaner.clean(dirtyDocument, cleanedDocument);
+        return cleanedDocument;
     }
 }
