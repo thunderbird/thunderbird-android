63206634defbf5361ea23721aaee74a6a918540c
daquexian <daquexian566@gmail.com>
2017-07-22 23:02:17 +0800

    Update presenter of incoming, and fix bug in outgoing's presenter

--- k9mail/src/main/java/com/fsck/k9/activity/setup/incoming/IncomingContract.java
+++ k9mail/src/main/java/com/fsck/k9/activity/setup/incoming/IncomingContract.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import com.fsck.k9.Account;
 import com.fsck.k9.BasePresenter;
 import com.fsck.k9.BaseView;
+import com.fsck.k9.activity.setup.IncomingAndOutgoingState;
 import com.fsck.k9.mail.AuthType;
 import com.fsck.k9.mail.ConnectionSecurity;
 
@@ -13,24 +14,65 @@ import com.fsck.k9.mail.ConnectionSecurity;
 public interface IncomingContract {
     interface View extends BaseView<Presenter> {
         void goToOutgoingSettings(Account account);
-        void resetPreviousSettings();
-        void saveSettings();
-        void enableNext(boolean enabled);
+        void setNextEnabled(boolean enabled);
         Context getContext();
+        void setAuthType(AuthType authType);
+        void setSecurityType(ConnectionSecurity security);
+
+        void setUsername(String username);
+        void setPassword(String password);
+        void setCertificateAlias(String alias);
+        void setServer(String server);
+        void setPort(String port);
+
+        void setServerLabel(String label);
+
+        void hideViewsWhenPop3();
+        void hideViewsWhenImap();
+        void hideViewsWhenImapAndNotEdit();
+        void hideViewsWhenWebDav();
+
+        void setImapAutoDetectNamespace(boolean autoDetectNamespace);
+        void setImapPathPrefix(String prefix);
+
+        void setWebDavPathPrefix(String prefix);
+        void setWebDavAuthPath(String authPath);
+        void setWebDavMailboxPath(String mailboxPath);
+
+        void setSecurityChoices(ConnectionSecurity[] choices);
+
+        void setAuthTypeInsecureText(boolean insecure);
+
+        void onAuthTypeIsNotExternal();
+        void onAuthTypeIsExternal();
+
+        void onAccountLoadFailure(Exception use);
+
+        void setCompressionMobile(boolean compressionMobile);
+        void setCompressionWifi(boolean compressionWifi);
+        void setCompressionOther(boolean compressionOther);
+
+        void setSubscribedFoldersOnly(boolean subscribedFoldersOnly);
+
+        void showInvalidSettingsToast();
+
     }
 
     interface Presenter extends BasePresenter {
+        void onInputChanged(String certificateAlias, String server, String port,
+                            String username, String password, AuthType authType,
+                            ConnectionSecurity connectionSecurity);
+
         void modifyAccount(String username, String password, String clientCertificateAlias,
                 boolean autoDetectNamespace, String imapPathPrefix, String webdavPathPrefix, String webdavAuthPath,
                 String webdavMailboxPath, String host, int port, ConnectionSecurity connectionSecurity,
                 AuthType authType, boolean compressMobile, boolean compressWifi, boolean compressOther,
                 boolean subscribedFoldersOnly);
 
-        void revokeInvalidSettings(AuthType authType, ConnectionSecurity connectionSecurity);
-        void validateFields(String certificateAlias, String server, String port,
-                String username, String password, AuthType authType, ConnectionSecurity connectionSecurity);
         void prepareForOutgoing(String username, String password, String certificateAlias, AuthType authType);
         void updateAccount();
         Account getAccount();
+        IncomingAndOutgoingState getState();
+        void setState(IncomingAndOutgoingState state);
     }
 }
--- k9mail/src/main/java/com/fsck/k9/activity/setup/incoming/IncomingPresenter.java
+++ k9mail/src/main/java/com/fsck/k9/activity/setup/incoming/IncomingPresenter.java
@@ -10,6 +10,9 @@ import com.fsck.k9.Account;
 import com.fsck.k9.Account.FolderMode;
 import com.fsck.k9.K9;
 import com.fsck.k9.Preferences;
+import com.fsck.k9.R;
+import com.fsck.k9.account.AccountCreator;
+import com.fsck.k9.activity.setup.IncomingAndOutgoingState;
 import com.fsck.k9.activity.setup.checksettings.CheckSettingsPresenter.CheckDirection;
 import com.fsck.k9.activity.setup.incoming.IncomingContract.View;
 import com.fsck.k9.helper.Utility;
@@ -31,18 +34,161 @@ class IncomingPresenter implements IncomingContract.Presenter {
     private View view;
     private Account account;
     private ServerSettings settings;
+    private Type storeType;
 
-    IncomingPresenter(View view, Account account) {
+    private AuthType currentAuthType;
+    private ConnectionSecurity currentSecurityType;
+    private String currentPort;
+
+    IncomingPresenter(View view, String accountUuid, boolean editSettings) {
         this.view = view;
-        this.account = account;
+        this.account = Preferences.getPreferences(K9.app).getAccount(accountUuid);
         view.setPresenter(this);
 
-        settings = RemoteStore.decodeStoreUri(account.getStoreUri());
+        ConnectionSecurity[] connectionSecurityChoices = ConnectionSecurity.values();
+        try {
+            settings = RemoteStore.decodeStoreUri(account.getStoreUri());
+
+            /* if (savedInstanceState == null) {
+                // The first item is selected if settings.authenticationType is null or is not in authTypeAdapter
+                currentAuthTypeViewPosition = authTypeAdapter.getAuthPosition(settings.authenticationType);
+            } else {
+                currentAuthTypeViewPosition = savedInstanceState.getInt(STATE_AUTH_TYPE_POSITION);
+            } */
+
+            currentAuthType = settings.authenticationType;
+
+            view.setAuthType(currentAuthType);
+
+            updateViewFromAuthType(currentAuthType);
+
+            currentSecurityType = settings.connectionSecurity;
+
+            if (settings.username != null) {
+                view.setUsername(settings.username);
+            }
+
+            if (settings.password != null) {
+                view.setPassword(settings.password);
+            }
+
+            if (settings.clientCertificateAlias != null) {
+                view.setCertificateAlias(settings.clientCertificateAlias);
+            }
+
+            storeType = settings.type;
+            if (Type.POP3 == settings.type) {
+                view.setServerLabel(getString(R.string.account_setup_incoming_pop_server_label));
+
+                view.hideViewsWhenPop3();
+            } else if (Type.IMAP == settings.type) {
+                view.setServerLabel(getString(R.string.account_setup_incoming_imap_server_label));
+
+                ImapStoreSettings imapSettings = (ImapStoreSettings) settings;
+
+                view.setImapAutoDetectNamespace(imapSettings.autoDetectNamespace);
+                if (imapSettings.pathPrefix != null) {
+                    view.setImapPathPrefix(imapSettings.pathPrefix);
+                }
+
+                view.hideViewsWhenImap();
+
+                if (!editSettings) {
+                    view.hideViewsWhenImapAndNotEdit();
+                }
+            } else if (Type.WebDAV == settings.type) {
+                view.setServerLabel(getString(R.string.account_setup_incoming_webdav_server_label));
+                connectionSecurityChoices = new ConnectionSecurity[] {
+                        ConnectionSecurity.NONE,
+                        ConnectionSecurity.SSL_TLS_REQUIRED };
+
+                // Hide the unnecessary fields
+                view.hideViewsWhenWebDav();
+                WebDavStoreSettings webDavSettings = (WebDavStoreSettings) settings;
+
+                if (webDavSettings.path != null) {
+                    view.setWebDavPathPrefix(webDavSettings.path);
+                }
+
+                if (webDavSettings.authPath != null) {
+                    view.setWebDavAuthPath(webDavSettings.authPath);
+                }
+
+                if (webDavSettings.mailboxPath != null) {
+                    view.setWebDavMailboxPath(webDavSettings.mailboxPath);
+                }
+            } else {
+                throw new IllegalArgumentException("Unknown account type: " + account.getStoreUri());
+            }
+
+            if (!editSettings) {
+                account.setDeletePolicy(AccountCreator.getDefaultDeletePolicy(settings.type));
+            }
+
+            view.setSecurityChoices(connectionSecurityChoices);
+
+            updateAuthPlainTextFromSecurityType(currentSecurityType);
+
+            if (settings.host != null) {
+                view.setServer(settings.host);
+            }
+
+            if (settings.port != -1) {
+                String port = String.valueOf(settings.port);
+                view.setPort(port);
+                currentPort = port;
+            } else {
+                updatePortFromSecurityType(currentSecurityType);
+            }
+
+            view.setCompressionMobile(account.useCompression(NetworkType.MOBILE));
+            view.setCompressionWifi(account.useCompression(NetworkType.WIFI));
+            view.setCompressionOther(account.useCompression(NetworkType.OTHER));
+
+            view.setSubscribedFoldersOnly(account.subscribedFoldersOnly());
+
+        } catch (IllegalArgumentException e) {
+            view.onAccountLoadFailure(e);
+        }
+    }
+
+    private void updatePortFromSecurityType(ConnectionSecurity securityType) {
+        String port = String.valueOf(AccountCreator.getDefaultPort(securityType, storeType));
+        view.setPort(port);
+        currentPort = port;
+    }
+
+    private void updateAuthPlainTextFromSecurityType(ConnectionSecurity securityType) {
+        view.setAuthTypeInsecureText(securityType == ConnectionSecurity.NONE);
     }
 
     @Override
     public Account getAccount() {
         return account;
+    }
+
+    @Override
+    public IncomingAndOutgoingState getState() {
+        return new IncomingAndOutgoingState(currentAuthType, currentSecurityType);
+    }
+
+    @Override
+    public void setState(IncomingAndOutgoingState state) {
+        view.setAuthType(state.getAuthType());
+        view.setSecurityType(state.getConnectionSecurity());
+
+        currentAuthType = state.getAuthType();
+        currentSecurityType = state.getConnectionSecurity();
+    }
+
+    @Override
+    public void onInputChanged(String certificateAlias, String server, String port,
+                               String username, String password, AuthType authType,
+                               ConnectionSecurity connectionSecurity) {
+
+        revokeInvalidSettingsAndUpdateView(authType, connectionSecurity, port);
+        validateFields(certificateAlias, server, currentPort, username, password, currentAuthType,
+                currentSecurityType);
     }
 
     @Override
@@ -62,13 +208,13 @@ class IncomingPresenter implements IncomingContract.Presenter {
 
         Map<String, String> extra = null;
         if (Type.IMAP == storeType) {
-            extra = new HashMap<String, String>();
+            extra = new HashMap<>();
             extra.put(ImapStoreSettings.AUTODETECT_NAMESPACE_KEY,
                     Boolean.toString(autoDetectNamespace));
             extra.put(ImapStoreSettings.PATH_PREFIX_KEY,
                     imapPathPrefix);
         } else if (Type.WebDAV == storeType) {
-            extra = new HashMap<String, String>();
+            extra = new HashMap<>();
             extra.put(WebDavStoreSettings.PATH_KEY,
                     webdavPathPrefix);
             extra.put(WebDavStoreSettings.AUTH_PATH_KEY,
@@ -89,21 +235,54 @@ class IncomingPresenter implements IncomingContract.Presenter {
         account.setSubscribedFoldersOnly(subscribedFoldersOnly);
     }
 
-    @Override
-    public void revokeInvalidSettings(AuthType authType, ConnectionSecurity connectionSecurity) {
+    private void revokeInvalidSettingsAndUpdateView(AuthType authType,
+                                                    ConnectionSecurity connectionSecurity,
+                                                    String port) {
         boolean isAuthTypeExternal = (AuthType.EXTERNAL == authType);
 
         boolean hasConnectionSecurity = (connectionSecurity != ConnectionSecurity.NONE);
 
         if (isAuthTypeExternal && !hasConnectionSecurity) {
-            view.resetPreviousSettings();
+            view.showInvalidSettingsToast();
+            view.setAuthType(currentAuthType);
+            view.setSecurityType(currentSecurityType);
+            view.setPort(currentPort);
         } else {
-            view.saveSettings();
+            onAuthTypeSelected(authType);
+            onSecuritySelected(connectionSecurity);
+            currentAuthType = authType;
+            currentSecurityType = connectionSecurity;
+            currentPort = port;
         }
     }
 
-    @Override
-    public void validateFields(String certificateAlias, String server, String port,
+    private void onSecuritySelected(ConnectionSecurity securityType) {
+        if (securityType != currentSecurityType) {
+            setSecurityType(securityType);
+        }
+    }
+
+    private void onAuthTypeSelected(AuthType authType) {
+        if (authType != currentAuthType) {
+            setAuthType(authType);
+        }
+    }
+
+    private void setSecurityType(ConnectionSecurity securityType) {
+        view.setSecurityType(securityType);
+
+        updatePortFromSecurityType(securityType);
+        updateAuthPlainTextFromSecurityType(securityType);
+    }
+
+    private void setAuthType(AuthType authType) {
+        view.setAuthType(authType);
+
+        updateViewFromAuthType(authType);
+    }
+
+
+    private void validateFields(String certificateAlias, String server, String port,
             String username, String password, AuthType authType, ConnectionSecurity connectionSecurity) {
         boolean isAuthTypeExternal = (AuthType.EXTERNAL == authType);
         boolean hasConnectionSecurity = (connectionSecurity != ConnectionSecurity.NONE);
@@ -124,7 +303,7 @@ class IncomingPresenter implements IncomingContract.Presenter {
                 && Utility.requiredFieldValid(port)
                 && (hasValidPasswordSettings || hasValidExternalAuthSettings);
 
-        view.enableNext(enabled);
+        view.setNextEnabled(enabled);
     }
 
     @Override
@@ -161,5 +340,16 @@ class IncomingPresenter implements IncomingContract.Presenter {
             MailService.actionRestartPushers(view.getContext(), null);
         }
         account.save(Preferences.getPreferences(K9.app));
+    }
+
+    private void updateViewFromAuthType(AuthType authType) {
+        if (authType == AuthType.EXTERNAL) {
+            view.onAuthTypeIsExternal();
+        } else {
+            view.onAuthTypeIsNotExternal();
+        }
+    }
+    private String getString(int id) {
+        return K9.app.getString(id);
     }
 }
