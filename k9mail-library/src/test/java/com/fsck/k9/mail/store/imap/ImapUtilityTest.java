15d2ea6fb7c0ead47ec5568411528c9f59f8329c
Hari <rhari991@gmail.com>
2017-07-27 14:08:36 +0530

    Add tests for ImapUtilityTest

--- k9mail-library/src/test/java/com/fsck/k9/mail/store/imap/ImapUtilityTest.java
+++ k9mail-library/src/test/java/com/fsck/k9/mail/store/imap/ImapUtilityTest.java
@@ -17,14 +17,27 @@
 
 package com.fsck.k9.mail.store.imap;
 
-import com.fsck.k9.mail.K9LibRobolectricTestRunner;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.fsck.k9.mail.Flag;
+import com.fsck.k9.mail.K9LibRobolectricTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.List;
-
+import static com.fsck.k9.mail.store.imap.ImapResponseHelper.createImapResponse;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(K9LibRobolectricTestRunner.class)
@@ -145,5 +158,66 @@ public class ImapUtilityTest  {
         expected = new String[0];
         actual = ImapUtility.getImapRangeValues("1:*");
         assertArrayEquals(expected, actual.toArray());
+    }
+
+    @Test
+    public void setMessageFlags() throws Exception {
+        ImapList fetchList = createImapResponse("(UID 4 MODSEQ (65402) FLAGS (\\Seen $Forwarded))");
+        ImapMessage message = mock(ImapMessage.class);
+        ImapStore store = mock(ImapStore.class);
+        Set<Flag> permanentFlagsIndex = new HashSet<>(0);
+        when(store.getPermanentFlagsIndex()).thenReturn(permanentFlagsIndex);
+
+        ImapUtility.setMessageFlags(fetchList, message, store);
+
+        verify(message).setFlagInternal(Flag.SEEN, true);
+        verify(message).setFlagInternal(Flag.FORWARDED, true);
+        assertEquals(permanentFlagsIndex, Collections.singleton(Flag.FORWARDED));
+    }
+
+    @Test
+    public void extractHighestModSeq_withHighestModSeqResponse_shouldReturnRespectiveValue() throws Exception {
+        ImapResponse response = createImapResponse("* OK [HIGHESTMODSEQ 715194045007]");
+
+        Long highestModSeq = ImapUtility.extractHighestModSeq(response);
+
+        assertNotNull(highestModSeq);
+        assertEquals(highestModSeq.longValue(), 715194045007L);
+    }
+
+    @Test
+    public void extractHighestModSeq_withNoModSeqResponse_shouldReturnNull() throws Exception {
+        ImapResponse response = createImapResponse("* OK [NOMODSEQ] Sorry, this mailbox doesn't support modsequences");
+
+        Long highestModSeq = ImapUtility.extractHighestModSeq(response);
+
+        assertNull(highestModSeq);
+    }
+
+    @Test
+    public void extractHighestModSeq_withoutModSeqResponse_shouldReturnNull() throws Exception {
+        ImapResponse response = createImapResponse("* OK [UIDNEXT 4392] Predicted next UID");
+
+        Long highestModSeq = ImapUtility.extractHighestModSeq(response);
+
+        assertNull(highestModSeq);
+    }
+
+    @Test
+    public void extractVanishedUids_withVanishedResponse_shouldReturnRespectiveValues() throws Exception {
+        List<ImapResponse> response = singletonList(createImapResponse("* VANISHED 505,507,510,625"));
+
+        List<String> expungedUids = ImapUtility.extractVanishedUids(response);
+
+        assertEquals(expungedUids, asList("505", "507", "510", "625"));
+    }
+
+    @Test
+    public void extractVanishedUids_withVanishedEarlierResponse_shouldReturnRespectiveValues() throws Exception {
+        List<ImapResponse> response = singletonList(createImapResponse("* VANISHED (EARLIER) 505,507,510,625"));
+
+        List<String> expungedUids = ImapUtility.extractVanishedUids(response);
+
+        assertEquals(expungedUids, asList("505", "507", "510", "625"));
     }
 }
