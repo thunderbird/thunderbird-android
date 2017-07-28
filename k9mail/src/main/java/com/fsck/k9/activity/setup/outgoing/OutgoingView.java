4f16f9f8c17606adf9b21f3fda25f66dc574b5b9
daquexian <daquexian566@gmail.com>
2017-07-28 14:36:15 +0800

    bugfix

--- k9mail/src/main/java/com/fsck/k9/activity/setup/outgoing/OutgoingView.java
+++ k9mail/src/main/java/com/fsck/k9/activity/setup/outgoing/OutgoingView.java
@@ -170,7 +170,7 @@ public class OutgoingView extends AccountSetupView implements OnClickListener,
 
     @Override
     public void next() {
-        activity.goToAccountNames();
+        activity.goToOutgoingChecking();
     }
 
     /*
