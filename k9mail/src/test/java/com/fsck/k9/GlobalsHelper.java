32b80395dc5fa45e1d8788df3737d32be87a8bb1
daquexian <daquexian566@gmail.com>
2017-08-16 22:01:19 +0800

    Fix tests

--- k9mail/src/test/java/com/fsck/k9/GlobalsHelper.java
+++ k9mail/src/test/java/com/fsck/k9/GlobalsHelper.java
@@ -3,9 +3,15 @@ package com.fsck.k9;
 
 import android.content.Context;
 
+import com.fsck.k9.mail.oauth.OAuth2TokenProvider;
+
 
 public class GlobalsHelper {
     public static void setContext(Context context) {
         Globals.setContext(context);
+    }
+
+    public static void setOAuth2TokenProvider(OAuth2TokenProvider provider) {
+        Globals.setOAuth2TokenProvider(provider);
     }
 }
