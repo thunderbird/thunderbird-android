2d8c711c802d4cd8b14577314de4e0bb2142a524
daquexian <daquexian566@gmail.com>
2017-08-28 04:37:13 +0800

    Delete invalid refresh token when it is revoked during setting checking. Updated settings dialog

--- k9mail-library/src/main/java/com/fsck/k9/mail/AuthenticationFailedException.java
+++ k9mail-library/src/main/java/com/fsck/k9/mail/AuthenticationFailedException.java
@@ -3,6 +3,8 @@ package com.fsck.k9.mail;
 
 public class AuthenticationFailedException extends MessagingException {
     public static final long serialVersionUID = -1;
+    public static final String OAUTH2_ERROR_INVALID_REFRESH_TOKEN = "oauth2-invalid refresh token";
+    public static final String OAUTH2_ERROR_UNKNOWN = "oauth2-unknown";
 
     public AuthenticationFailedException(String message) {
         super(message);
