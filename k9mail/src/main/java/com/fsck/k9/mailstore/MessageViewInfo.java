d9ee8b5cf0e8f51c3be2e4578ac2f2b9aa60e738
Vincent Breitmoser <look@my.amazin.horse>
2017-09-18 01:41:21 +0200

    Simplify message extraction logic, fix #2776

--- k9mail/src/main/java/com/fsck/k9/mailstore/MessageViewInfo.java
+++ k9mail/src/main/java/com/fsck/k9/mailstore/MessageViewInfo.java
@@ -1,6 +1,7 @@
 package com.fsck.k9.mailstore;
 
 
+import java.util.Collections;
 import java.util.List;
 
 import com.fsck.k9.mail.Message;
@@ -36,24 +37,24 @@ public class MessageViewInfo {
         this.extraAttachments = extraAttachments;
     }
 
-    public static MessageViewInfo createWithExtractedContent(
-            Message message, boolean isMessageIncomplete, Part rootPart,
-            String text, List<AttachmentViewInfo> attachments,
-            CryptoResultAnnotation cryptoResultAnnotation,
-            AttachmentResolver attachmentResolver,
-            String extraText, List<AttachmentViewInfo> extraAttachments
-    ) {
+    static MessageViewInfo createWithExtractedContent(Message message, boolean isMessageIncomplete,
+            String text, List<AttachmentViewInfo> attachments, AttachmentResolver attachmentResolver) {
         return new MessageViewInfo(
-                message, isMessageIncomplete, rootPart,
-                text, attachments,
-                cryptoResultAnnotation,
-                attachmentResolver,
-                extraText, extraAttachments
-        );
+                message, isMessageIncomplete, message, text, attachments, null, attachmentResolver, null,
+                Collections.<AttachmentViewInfo>emptyList());
     }
 
     public static MessageViewInfo createWithErrorState(Message message, boolean isMessageIncomplete) {
         return new MessageViewInfo(message, isMessageIncomplete, null, null, null, null, null, null, null);
     }
 
+    MessageViewInfo withCryptoData(CryptoResultAnnotation rootPartAnnotation, String extraViewableText,
+            List<AttachmentViewInfo> extraAttachmentInfos) {
+        return new MessageViewInfo(
+                message, isMessageIncomplete, rootPart, text, attachments,
+                rootPartAnnotation,
+                attachmentResolver,
+                extraViewableText, extraAttachmentInfos
+        );
+    }
 }
