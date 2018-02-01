d85d4c59ed467173fa057093d0d963959d638b5b
fattire <f4ttire@gmail.com>
2018-01-31 18:10:47 -0800

    Support Run-time permissions (for Contacts only, see below)
    The approach was to make it so that any K9Activity can easily request
    whatever permission in the future.
    
    The Contacts permission is now requested in two locations:
    
    1.  When a list of Messages is displayed
    2.  When a new message is first started to be composed.
    
    The permission request is displayed ONCE per onCreate(). Any more than this
    and it got really annoying.  A typical user who reads or writes emails
    WILL see the request, trust me.  Once they see the message 2x, they
    also have the option to block the requests from appearing.
    
    If they DECLINE the request (or decline + DENY any further attempts),
    the app should continues to work, albeit without incorporating contact data
    (thumbnails, autocomplete, etc.). Contacts may still be added to the
    Contacts app, as this uses an Intent and does not require any permission.
    
    Once the Read Contacts permission is enabled, the app immediately begins
    to use it.
    
    To add other permissions in the future (such as External Storage access),
    the request can be made in a similar way and the permission request result
    handled appropriately by just adding it to K9Activity (or overriding in
    a particular Activity).
    
    I did *not* implement this for external storage access, as 1. there is a PR
    waiting (#3118) and 2. I think that using the Storage Access Framework
    is a much better way to approach that as suggested in Issue #2844.
    
    I noticed there are 3 additional custom permissions in the manifest having
    to do with other apps controlling K9, but I don't think they're required by
    k9 itself.  If so, they can be requested from any K9Activity.

--- k9mail/src/main/java/com/fsck/k9/activity/K9Activity.java
+++ k9mail/src/main/java/com/fsck/k9/activity/K9Activity.java
@@ -1,9 +1,12 @@
 package com.fsck.k9.activity;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.widget.Toast;
 
+import com.fsck.k9.R;
 import com.fsck.k9.activity.K9ActivityCommon.K9ActivityMagic;
 import com.fsck.k9.activity.misc.SwipeGestureDetector.OnSwipeGestureListener;
 
@@ -12,6 +15,8 @@ public abstract class K9Activity extends Activity implements K9ActivityMagic {
 
     private K9ActivityCommon mBase;
 
+    public static final int PERMISSIONS_REQUEST_READ_CONTACTS  = 1;
+    public static final int PERMISSIONS_REQUEST_WRITE_CONTACTS = 2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,5 +33,25 @@ public abstract class K9Activity extends Activity implements K9ActivityMagic {
     @Override
     public void setupGestureDetector(OnSwipeGestureListener listener) {
         mBase.setupGestureDetector(listener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_CONTACTS:
+            case PERMISSIONS_REQUEST_WRITE_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, R.string.contact_permission_thanks,
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, R.string.contact_permission_request,
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
