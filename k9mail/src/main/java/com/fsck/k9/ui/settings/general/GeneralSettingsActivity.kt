83fc6f8014b75d83af66ed6816b90ec2aec57489
ByteHamster <info@bytehamster.com>
2018-05-17 17:21:32 +0200

    Search function for general settings

--- k9mail/src/main/java/com/fsck/k9/ui/settings/general/GeneralSettingsActivity.kt
+++ k9mail/src/main/java/com/fsck/k9/ui/settings/general/GeneralSettingsActivity.kt
@@ -6,13 +6,23 @@ import android.os.Bundle
 import android.support.v7.preference.PreferenceFragmentCompat
 import android.support.v7.preference.PreferenceFragmentCompat.OnPreferenceStartScreenCallback
 import android.support.v7.preference.PreferenceScreen
+import android.view.Menu
 import android.view.MenuItem
+import com.bytehamster.lib.preferencesearch.SearchPreferenceResult
+import com.bytehamster.lib.preferencesearch.SearchPreferenceResultListener
 import com.fsck.k9.R
 import com.fsck.k9.activity.K9Activity
+import com.fsck.k9.activity.setup.FontSizeSettings
 import com.fsck.k9.ui.fragmentTransaction
 import com.fsck.k9.ui.fragmentTransactionWithBackStack
+import com.bytehamster.lib.preferencesearch.SearchPreferenceActionView
 
-class GeneralSettingsActivity : K9Activity(), OnPreferenceStartScreenCallback {
+
+
+class GeneralSettingsActivity : K9Activity(), OnPreferenceStartScreenCallback, SearchPreferenceResultListener {
+    private lateinit var searchPreferenceActionView: SearchPreferenceActionView
+    private lateinit var searchPreferenceMenuItem: MenuItem
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +37,43 @@ class GeneralSettingsActivity : K9Activity(), OnPreferenceStartScreenCallback {
         }
     }
 
+    override fun onSearchResultClicked(result: SearchPreferenceResult) {
+        searchPreferenceActionView.close()
+
+        if (result.resourceFile == R.xml.font_preferences) {
+            startActivity(Intent(this, FontSizeSettings::class.java))
+        } else {
+            val fragment = GeneralSettingsFragment.create(result.screen)
+            fragmentTransaction {
+                addToBackStack("Search result")
+                replace(R.id.generalSettingsContainer, fragment)
+            }
+
+            result.highlight(fragment as PreferenceFragmentCompat?, 0x009688) // Default material accent color
+        }
+    }
+
     private fun initializeActionBar() {
         val actionBar = supportActionBar ?: throw RuntimeException("getSupportActionBar() == null")
         actionBar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.general_settings_option, menu)
+        searchPreferenceMenuItem = menu.findItem(R.id.search)
+        searchPreferenceActionView = searchPreferenceMenuItem.actionView as SearchPreferenceActionView
+        searchPreferenceActionView.setActivity(this)
+        val config = searchPreferenceActionView.searchConfiguration
+        config.setFragmentContainerViewId(R.id.generalSettingsContainer)
+        config.setBreadcrumbsEnabled(true)
+        config.setFuzzySearchEnabled(true)
+        config.index().addFile(R.xml.general_settings)
+        config.index().addBreadcrumb(R.string.general_settings_title)
+                .addBreadcrumb(R.string.display_preferences)
+                .addBreadcrumb(R.string.global_preferences)
+                .addBreadcrumb(R.string.font_size_settings_title)
+                .addFile(R.xml.font_preferences)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -39,6 +83,12 @@ class GeneralSettingsActivity : K9Activity(), OnPreferenceStartScreenCallback {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if (!searchPreferenceActionView.onBackPressed()) {
+            super.onBackPressed()
+        }
     }
 
     override fun onPreferenceStartScreen(
