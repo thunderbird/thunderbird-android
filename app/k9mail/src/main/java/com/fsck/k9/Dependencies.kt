9319d36d247ef972c1d1cc20cfeedc09e0168dae
cketti <cketti@gmail.com>
2018-06-24 01:21:57 +0200

    Move message list widget code to :app:k9mail

--- app/k9mail/src/main/java/com/fsck/k9/Dependencies.kt
+++ app/k9mail/src/main/java/com/fsck/k9/Dependencies.kt
@@ -3,6 +3,7 @@ package com.fsck.k9
 import com.fsck.k9.external.BroadcastSenderListener
 import com.fsck.k9.external.externalModule
 import com.fsck.k9.widget.list.MessageListWidgetUpdateListener
+import com.fsck.k9.widget.list.messageListWidgetModule
 import com.fsck.k9.widget.unread.UnreadWidgetUpdateListener
 import org.koin.dsl.module.applicationContext
 
@@ -18,5 +19,6 @@ private val mainAppModule = applicationContext {
 
 val appModules = listOf(
         mainAppModule,
-        externalModule
+        externalModule,
+        messageListWidgetModule
 )
