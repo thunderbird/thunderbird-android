95465ea6d7b6d6eea7736463dbbe663465346fe8
cketti <cketti@gmail.com>
2018-07-13 04:08:22 +0200

    Add proper parser for header parameters (RFC 2045 + RFC 2231)

---
+++ mail/common/src/main/java/com/fsck/k9/mail/internet/MimeValue.kt
@@ -0,0 +1,8 @@
+package com.fsck.k9.mail.internet
+
+data class MimeValue(
+        val value: String,
+        val parameters: Map<String, String> = emptyMap(),
+        val ignoredParameters: List<Pair<String, String>> = emptyList(),
+        val parserErrorIndex: Int? = null
+)
