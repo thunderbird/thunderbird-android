510478046854891793a2353674bcfc2b93beb458
cketti <cketti@gmail.com>
2018-07-15 15:48:39 +0200

    Update BackendStorage to allow getting/setting extra values

--- backend/api/src/main/java/com/fsck/k9/backend/api/BackendStorage.kt
+++ backend/api/src/main/java/com/fsck/k9/backend/api/BackendStorage.kt
@@ -2,4 +2,9 @@ package com.fsck.k9.backend.api
 
 interface BackendStorage {
     fun getFolder(folderServerId: String): BackendFolder
+
+    fun getExtraString(name: String): String?
+    fun setExtraString(name: String, value: String)
+    fun getExtraNumber(name: String): Long?
+    fun setExtraNumber(name: String, value: Long)
 }
