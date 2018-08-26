5d98c15bce7a29c48c6c4f69e118bf0faeec9db4
cketti <cketti@gmail.com>
2018-08-26 17:38:30 +0200

    Use MimeParameterEncoder and MimeParameterDecoder in MimeMessageHelper

--- mail/common/src/main/java/com/fsck/k9/mail/internet/MimeMessageHelper.java
+++ mail/common/src/main/java/com/fsck/k9/mail/internet/MimeMessageHelper.java
@@ -23,23 +23,21 @@ public class MimeMessageHelper {
         if (body instanceof Multipart) {
             Multipart multipart = ((Multipart) body);
             multipart.setParent(part);
-            String mimeType = multipart.getMimeType();
-            String contentType = String.format("%s; boundary=\"%s\"", mimeType, multipart.getBoundary());
+            String contentType = Headers.contentTypeForMultipart(multipart.getMimeType(), multipart.getBoundary());
             part.setHeader(MimeHeader.HEADER_CONTENT_TYPE, contentType);
             // note: if this is ever changed to 8bit, multipart/signed parts must always be 7bit!
             setEncoding(part, MimeUtil.ENC_7BIT);
         } else if (body instanceof TextBody) {
-            String contentType;
-            if (MimeUtility.mimeTypeMatches(part.getMimeType(), "text/*")) {
-                contentType = String.format("%s;\r\n charset=utf-8", part.getMimeType());
-                String name = MimeUtility.getHeaderParameter(part.getContentType(), "name");
-                if (name != null) {
-                    contentType += String.format(";\r\n name=\"%s\"", name);
-                }
+            MimeValue contentTypeHeader = MimeParameterDecoder.decode(part.getContentType());
+            String mimeType = contentTypeHeader.getValue();
+            if (MimeUtility.mimeTypeMatches(mimeType, "text/*")) {
+                String name = contentTypeHeader.getParameters().get("name");
+
+                String contentType = Headers.contentType(mimeType, "utf-8", name);
+                part.setHeader(MimeHeader.HEADER_CONTENT_TYPE, contentType);
             } else {
-                contentType = part.getMimeType();
+                part.setHeader(MimeHeader.HEADER_CONTENT_TYPE, mimeType);
             }
-            part.setHeader(MimeHeader.HEADER_CONTENT_TYPE, contentType);
 
             setEncoding(part, MimeUtil.ENC_QUOTED_PRINTABLE);
         } else if (body instanceof RawDataBody) {
