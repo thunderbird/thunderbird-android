86dde4dabf59156d185778ae6fb756c834258d3a
BO41 <lukasw41@gmail.com>
2018-09-02 20:41:44 +0200

    Remove unused imports

--- mail/common/src/test/java/com/fsck/k9/mail/internet/ListHeadersTest.java
+++ mail/common/src/test/java/com/fsck/k9/mail/internet/ListHeadersTest.java
@@ -4,7 +4,6 @@ package com.fsck.k9.mail.internet;
 import com.fsck.k9.mail.Address;
 import com.fsck.k9.mail.K9LibRobolectricTestRunner;
 import com.fsck.k9.mail.Message;
-import com.fsck.k9.mail.MessagingException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
