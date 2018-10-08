455228f182c60f1c1137d7c65e2d36cd9afade08
Henry <blauherr@yahoo.de>
2018-10-08 20:49:38 +0200

    Added mime4j-dom dependency in app:ui to make sure the package is available. Without it DefaultAddressParser is not found in the RecipentLoaderTest even though the dependecy is declared in common. Looks like a gradle bug to me.

--- app/ui/src/test/java/com/fsck/k9/activity/compose/RecipientLoaderTest.java
+++ app/ui/src/test/java/com/fsck/k9/activity/compose/RecipientLoaderTest.java
@@ -3,6 +3,7 @@ package com.fsck.k9.activity.compose;
 
 import java.util.List;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.MatrixCursor;
@@ -14,9 +15,11 @@ import com.fsck.k9.RobolectricTest;
 import com.fsck.k9.mail.Address;
 import com.fsck.k9.view.RecipientSelectView.Recipient;
 import com.fsck.k9.view.RecipientSelectView.RecipientCryptoStatus;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.robolectric.RuntimeEnvironment;
+import org.robolectric.shadows.ShadowApplication;
 
 import static android.provider.ContactsContract.CommonDataKinds.Email.TYPE_HOME;
 import static org.junit.Assert.assertEquals;
@@ -47,36 +50,39 @@ public class RecipientLoaderTest extends RobolectricTest {
             ContactsContract.Data.CONTACT_ID,
             ContactsContract.CommonDataKinds.Nickname.NAME
     };
-    static final String[] PROJECTION_CRYPTO_ADDRESSES = { "address", "uid_address" };
-    static final String[] PROJECTION_CRYPTO_STATUS = { "address", "uid_key_status", "autocrypt_key_status" };
+    static final String[] PROJECTION_CRYPTO_ADDRESSES = {"address", "uid_address"};
+    static final String[] PROJECTION_CRYPTO_STATUS = {"address", "uid_key_status", "autocrypt_key_status"};
     static final Address CONTACT_ADDRESS_1 = Address.parse("Contact Name <address@example.org>")[0];
     static final Address CONTACT_ADDRESS_2 = Address.parse("Other Contact Name <address_two@example.org>")[0];
     static final String TYPE = "" + TYPE_HOME;
     static final String[] CONTACT_1 =
-            new String[] { "0", "Bob", "bob", "bob@host.com", TYPE, null, "1", null, "100", "Bob" };
+            new String[]{"0", "Bob", "bob", "bob@host.com", TYPE, null, "1", null, "100", "Bob"};
     static final String[] CONTACT_2 =
-            new String[] { "2", "Bob2", "bob2", "bob2@host.com", TYPE, null, "2", null, "99", "Bob2" };
+            new String[]{"2", "Bob2", "bob2", "bob2@host.com", TYPE, null, "2", null, "99", "Bob2"};
     static final String[] CONTACT_NO_EMAIL =
-            new String[] { "0", "Bob", "bob", null, TYPE, null, "1", null, "10", "Bob_noMail" };
+            new String[]{"0", "Bob", "bob", null, TYPE, null, "1", null, "10", "Bob_noMail"};
     static final String[] CONTACT_WITH_NICKNAME_NOT_CONTACTED =
-            new String[] { "0", "Eve_notContacted", "eve_notContacted", "eve_notContacted@host.com", TYPE, null, "2",
-                    null, "0", "Eve" };
-    static final String[] NICKNAME_NOT_CONTACTED = new String[] { "2", "Eves_Nickname_Bob" };
+            new String[]{"0", "Eve_notContacted", "eve_notContacted", "eve_notContacted@host.com", TYPE, null, "2",
+                    null, "0", "Eve"};
+    static final String[] NICKNAME_NOT_CONTACTED = new String[]{"2", "Eves_Nickname_Bob"};
 
     static final String QUERYSTRING = "querystring";
 
-
+    ShadowApplication shadowApp;
     Context context;
     ContentResolver contentResolver;
 
 
     @Before
     public void setUp() throws Exception {
+        shadowApp = ShadowApplication.getInstance();
+        shadowApp.grantPermissions(Manifest.permission.READ_CONTACTS);
+        shadowApp.grantPermissions(Manifest.permission.WRITE_CONTACTS);
+
         context = mock(Context.class);
         contentResolver = mock(ContentResolver.class);
 
-        when(context.getApplicationContext()).thenReturn(RuntimeEnvironment.application);
-
+        when(context.getApplicationContext()).thenReturn(shadowApp.getApplicationContext());
         when(context.getContentResolver()).thenReturn(contentResolver);
     }
 
@@ -153,24 +159,24 @@ public class RecipientLoaderTest extends RobolectricTest {
     private void setupQueryCryptoProvider(String queriedAddress, Address... contactAddresses) {
         MatrixCursor cursor = new MatrixCursor(PROJECTION_CRYPTO_ADDRESSES);
         for (Address contactAddress : contactAddresses) {
-            cursor.addRow(new String[] { queriedAddress, contactAddress.toString() });
+            cursor.addRow(new String[]{queriedAddress, contactAddress.toString()});
         }
 
         when(contentResolver
                 .query(eq(Uri.parse("content://" + CRYPTO_PROVIDER + ".provider.exported/autocrypt_status")),
                         aryEq(PROJECTION_CRYPTO_ADDRESSES), nullable(String.class),
-                        aryEq(new String[] { queriedAddress }),
+                        aryEq(new String[]{queriedAddress}),
                         nullable(String.class))).thenReturn(cursor);
     }
 
     private void setupCryptoProviderStatus(Address address, String uidStatus, String autocryptStatus) {
         MatrixCursor cursorCryptoStatus = new MatrixCursor(PROJECTION_CRYPTO_STATUS);
-        cursorCryptoStatus.addRow(new String[] { address.getAddress(), uidStatus, autocryptStatus });
+        cursorCryptoStatus.addRow(new String[]{address.getAddress(), uidStatus, autocryptStatus});
 
         when(contentResolver
                 .query(eq(Uri.parse("content://" + CRYPTO_PROVIDER + ".provider.exported/autocrypt_status")),
                         aryEq(PROJECTION_CRYPTO_STATUS), nullable(String.class),
-                        aryEq(new String[] { address.getAddress() }),
+                        aryEq(new String[]{address.getAddress()}),
                         nullable(String.class))).thenReturn(cursorCryptoStatus);
     }
 
@@ -183,7 +189,7 @@ public class RecipientLoaderTest extends RobolectricTest {
                 .query(eq(Email.CONTENT_URI),
                         aryEq(PROJECTION),
                         nullable(String.class),
-                        aryEq(new String[] { queriedAddress, queriedAddress }),
+                        aryEq(new String[]{queriedAddress, queriedAddress}),
                         nullable(String.class))).thenReturn(cursor);
     }
 
@@ -209,9 +215,10 @@ public class RecipientLoaderTest extends RobolectricTest {
                 .query(eq(Email.CONTENT_URI),
                         aryEq(PROJECTION),
                         nullable(String.class),
-                        aryEq(new String[] { id }),
+                        aryEq(new String[]{id}),
                         nullable(String.class))).thenReturn(cursor);
     }
+
 
     @Test
     public void queryContactProvider() throws Exception {
@@ -223,6 +230,20 @@ public class RecipientLoaderTest extends RobolectricTest {
         assertEquals(1, recipients.size());
         assertEquals("bob@host.com", recipients.get(0).address.getAddress());
         assertEquals(RecipientCryptoStatus.UNAVAILABLE, recipients.get(0).getCryptoStatus());
+    }
+
+
+    @Test
+    public void queryContactProviderWithoutPermission() throws Exception {
+        shadowApp.denyPermissions(Manifest.permission.READ_CONTACTS);
+        shadowApp.denyPermissions(Manifest.permission.WRITE_CONTACTS);
+
+        RecipientLoader recipientLoader = new RecipientLoader(context, CRYPTO_PROVIDER, QUERYSTRING);
+        setupContactProvider("%" + QUERYSTRING + "%", CONTACT_1);
+
+        List<Recipient> recipients = recipientLoader.loadInBackground();
+
+        assertEquals(0, recipients.size());
     }
 
     @Test
