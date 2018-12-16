a1195f57331fababdc69b499112ffb1fef2496e4
williamvds <william@williamvds.me>
2018-12-16 15:59:28 +0000

    Move libraries in About to main layout
    About's layout has been wrapped in a NestedScrollView
    Added a Library data class to store library information
    Added fragment for libraries, listing their name and licenses, links to
    their homepages
    Added string about_libraries for the section title

--- app/ui/src/main/java/com/fsck/k9/ui/settings/AboutActivity.kt
+++ app/ui/src/main/java/com/fsck/k9/ui/settings/AboutActivity.kt
@@ -7,9 +7,14 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
 import android.webkit.WebView
 import android.widget.TextView
 import com.fsck.k9.activity.K9Activity
@@ -18,9 +23,9 @@ import de.cketti.library.changelog.ChangeLog
 
 import timber.log.Timber
 
-class AboutActivity : K9Activity() {
-    private lateinit var webView: WebView
+private data class Library(val name: String, val URL: String, val license: String = "")
 
+class AboutActivity : K9Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setLayout(R.layout.about)
@@ -52,10 +57,14 @@ class AboutActivity : K9Activity() {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.app_revision_url))))
         }
 
-        webView = findViewById(R.id.about_view)
-
-        val aboutHtml = buildHtml()
-        webView.loadDataWithBaseURL("file:///android_res/drawable/", aboutHtml, "text/html", "utf-8", null)
+        val manager = LinearLayoutManager(this)
+        val libraries = findViewById<RecyclerView>(R.id.libraries)
+        libraries.apply {
+            layoutManager = manager
+            adapter = LibrariesAdapter(USED_LIBRARIES)
+            setNestedScrollingEnabled(false)
+            setFocusable(false)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -78,24 +87,6 @@ class AboutActivity : K9Activity() {
         ChangeLog(this).fullLogDialog.show()
     }
 
-    private fun buildHtml(): String {
-        val html = StringBuilder()
-                .append("<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />")
-
-        val libs = StringBuilder().append("<ul>")
-        for ((library, url) in USED_LIBRARIES) {
-            libs.append("<li><a href=\"").append(url).append("\">")
-                    .append(library)
-                    .append("</a></li>")
-        }
-        libs.append("</ul>")
-
-        html.append(getString(R.string.app_libraries, libs.toString()))
-                .append("</p>")
-
-        return html.toString()
-    }
-
     private fun getVersionNumber(): String {
         return try {
             val packageInfo = packageManager.getPackageInfo(packageName, 0)
@@ -106,29 +97,51 @@ class AboutActivity : K9Activity() {
         }
     }
 
-
     companion object {
-        private val USED_LIBRARIES = mapOf(
-                "Android Support Library" to "https://developer.android.com/topic/libraries/support-library/index.html",
-                "Android-Support-Preference-V7-Fix" to "https://github.com/Gericop/Android-Support-Preference-V7-Fix",
-                "ckChangeLog" to "https://github.com/cketti/ckChangeLog",
-                "Commons IO" to "http://commons.apache.org/io/",
-                "Glide" to "https://github.com/bumptech/glide",
-                "HoloColorPicker" to "https://github.com/LarsWerkman/HoloColorPicker",
-                "jsoup" to "https://jsoup.org/",
-                "jutf7" to "http://jutf7.sourceforge.net/",
-                "JZlib" to "http://www.jcraft.com/jzlib/",
-                "Mime4j" to "http://james.apache.org/mime4j/",
-                "Moshi" to "https://github.com/square/moshi",
-                "Okio" to "https://github.com/square/okio",
-                "SafeContentResolver" to "https://github.com/cketti/SafeContentResolver",
-                "ShowcaseView" to "https://github.com/amlcurran/ShowcaseView",
-                "Timber" to "https://github.com/JakeWharton/timber",
-                "TokenAutoComplete" to "https://github.com/splitwise/TokenAutoComplete/")
+        private val USED_LIBRARIES = arrayOf(
+                Library("Android Support Library", "https://developer.android.com/topic/libraries/support-library/index.html", "Apache License, Version 2.0"),
+                Library("Android-Support-Preference-V7-Fix", "https://github.com/Gericop/Android-Support-Preference-V7-Fix", "Unlicense/Apache License, Version 2.0"),
+                Library("ckChangeLog", "https://github.com/cketti/ckChangeLog", "Apache License, Version 2.0"),
+                Library("Commons IO", "https://commons.apache.org/io/", "Apache License, Version 2.0"),
+                Library("Glide", "https://github.com/bumptech/glide", "Mixed License"),
+                Library("HoloColorPicker", "https://github.com/LarsWerkman/HoloColorPicker", "Apache License, Version 2.0"),
+                Library("jsoup", "https://jsoup.org/", "MIT License"),
+                Library("jutf7", "http://jutf7.sourceforge.net/", "MIT License"),
+                Library("JZlib", "http://www.jcraft.com/jzlib/", "BSD-style License"),
+                Library("Mime4j", "http://james.apache.org/mime4j/", "Apache License, Version 2.0"),
+                Library("Moshi", "https://github.com/square/moshi", "Apache License, Version 2.0"),
+                Library("Okio", "https://github.com/square/okio", "Apache License, Version 2.0"),
+                Library("SafeContentResolver", "https://github.com/cketti/SafeContentResolver", "Apache License, Version 2.0"),
+                Library("ShowcaseView", "https://github.com/amlcurran/ShowcaseView", "Apache License, Version 2.0"),
+                Library("Timber", "https://github.com/JakeWharton/timber", "Apache License, Version 2.0"),
+                Library("TokenAutoComplete", "https://github.com/splitwise/TokenAutoComplete/", "Apache License, Version 2.0"))
 
         fun start(context: Context) {
             val intent = Intent(context, AboutActivity::class.java)
             context.startActivity(intent)
         }
     }
+}
+
+private class LibrariesAdapter(private val dataset: Array<Library>)
+        : RecyclerView.Adapter<LibrariesAdapter.ViewHolder>() {
+
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.about_library, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, index: Int) {
+        val library = dataset[index]
+        holder.view.findViewById<TextView>(R.id.name).text = library.name
+        holder.view.findViewById<TextView>(R.id.license).text = library.license
+        holder.view.setOnClickListener {
+            holder.view.getContext()
+                .startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(library.URL)))
+        }
+    }
+
+    override fun getItemCount() = dataset.size
 }
