26182e432ebac9c4bcb830697e5d4377b3f10bc3
Oji Ofong <ojiofong@gmail.com>
2019-01-14 17:18:50 -0500

    Avoid K9JobManager during robolectric unitTests

--- app/core/src/main/java/com/fsck/k9/job/KoinModule.kt
+++ app/core/src/main/java/com/fsck/k9/job/KoinModule.kt
@@ -4,9 +4,24 @@ import com.evernote.android.job.JobManager
 import org.koin.dsl.module.applicationContext
 
 val jobModule = applicationContext {
-    bean { JobManager.create(get()) as JobManager }
-    bean { K9JobManager(get(), get(), get(), get(), get()) }
-    bean { K9JobCreator(get(), get()) }
-    factory { MailSyncJobManager(get(), get()) }
-    factory { PusherRefreshJobManager(get(), get(), get()) }
+
+    // Creating android-job JobManager fails with Robolectric tests
+    // Let's avoid accessing JobManager during Robolectric tests only
+    // See: https://github.com/evernote/android-job/issues/220
+    if (!isRobolectricUnitTest()) {
+        bean { JobManager.create(get()) as JobManager }
+        bean { K9JobManager(get(), get(), get(), get(), get()) }
+        bean { K9JobCreator(get(), get()) }
+        factory { MailSyncJobManager(get(), get()) }
+        factory { PusherRefreshJobManager(get(), get(), get()) }
+    }
+}
+
+fun isRobolectricUnitTest(): Boolean {
+    return try {
+        Class.forName("org.robolectric.RobolectricTestRunner")
+        true
+    } catch (e: Exception) {
+        false
+    }
 }
