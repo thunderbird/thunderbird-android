5471f942a6eaff3e8706e0c893171995217512b7
Mir Saman Tajbakhsh <ms.tajbakhsh@urmia.ac.ir>
2019-01-17 21:10:20 +0330

    Errors fixed.

--- mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/ImapConnection.java
+++ mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/ImapConnection.java
@@ -222,6 +222,8 @@ class ImapConnection {
         } catch (UnknownHostException e) {
             if (settings.getHost().toLowerCase().endsWith("onion")) { //TOR Onion address
                 return connectToOnionAddress();
+            } else {
+                throw new MessagingException("Cannot connect to host", connectException);
             }
         }
 
