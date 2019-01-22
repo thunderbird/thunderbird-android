932f5f2ddce3eef75ab116e4877a7ae0ac33967c
cketti <cketti@gmail.com>
2019-01-22 16:28:30 +0100

    Add 'isDeleteMoveToTrash' flag to Backend

--- backend/api/src/main/java/com/fsck/k9/backend/api/Backend.kt
+++ backend/api/src/main/java/com/fsck/k9/backend/api/Backend.kt
@@ -21,6 +21,7 @@ interface Backend {
     val supportsTrashFolder: Boolean
     val supportsSearchByDate: Boolean
     val isPushCapable: Boolean
+    val isDeleteMoveToTrash: Boolean
 
     @Throws(MessagingException::class)
     fun refreshFolderList()
