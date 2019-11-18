b7127168a0bd0a7cbcdc3a19f997ac8481d25810
cketti <cketti@gmail.com>
2019-11-18 02:46:28 +0100

    Remove unused ColorChip(Provider)

--- app/ui/src/main/java/com/fsck/k9/activity/KoinModule.kt
+++ app/ui/src/main/java/com/fsck/k9/activity/KoinModule.kt
@@ -3,6 +3,5 @@ package com.fsck.k9.activity
 import org.koin.dsl.module.applicationContext
 
 val activityModule = applicationContext {
-    bean { ColorChipProvider() }
     bean { MessageLoaderHelperFactory(get(), get()) }
 }
