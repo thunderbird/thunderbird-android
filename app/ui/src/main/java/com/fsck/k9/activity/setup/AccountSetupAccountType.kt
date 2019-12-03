59dc5415916d44fefc5364bf14b18c1f08a5e6ea
cketti <cketti@gmail.com>
2019-12-03 20:10:41 +0100

    Convert 'AccountSetupAccountType' to Kotlin

--- app/ui/src/main/java/com/fsck/k9/activity/setup/AccountSetupAccountType.kt
+++ app/ui/src/main/java/com/fsck/k9/activity/setup/AccountSetupAccountType.kt
@@ -1,95 +1,93 @@
-package com.fsck.k9.activity.setup;
+package com.fsck.k9.activity.setup
 
-
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-
-import com.fsck.k9.preferences.Protocols;
-import timber.log.Timber;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Toast;
-import com.fsck.k9.Account;
-import com.fsck.k9.Preferences;
-import com.fsck.k9.ui.R;
-import com.fsck.k9.activity.K9Activity;
-import com.fsck.k9.helper.EmailHelper;
-import com.fsck.k9.setup.ServerNameSuggester;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
+import com.fsck.k9.Account
+import com.fsck.k9.Preferences
+import com.fsck.k9.activity.K9Activity
+import com.fsck.k9.helper.EmailHelper.getDomainFromEmailAddress
+import com.fsck.k9.preferences.Protocols
+import com.fsck.k9.setup.ServerNameSuggester
+import com.fsck.k9.ui.R
+import timber.log.Timber
+import java.net.URI
+import java.net.URISyntaxException
 
 /**
  * Prompts the user to select an account type. The account type, along with the
  * passed in email address, password and makeDefault are then passed on to the
  * AccountSetupIncoming activity.
  */
-public class AccountSetupAccountType extends K9Activity implements OnClickListener {
-    private static final String EXTRA_ACCOUNT = "account";
-    private static final String EXTRA_MAKE_DEFAULT = "makeDefault";
+class AccountSetupAccountType : K9Activity(), OnClickListener {
+    private val serverNameSuggester = ServerNameSuggester()
+    private var account: Account? = null
+    private var makeDefault = false
 
-    private final ServerNameSuggester serverNameSuggester = new ServerNameSuggester();
-    private Account mAccount;
-    private boolean mMakeDefault;
-
-    public static void actionSelectAccountType(Context context, Account account, boolean makeDefault) {
-        Intent i = new Intent(context, AccountSetupAccountType.class);
-        i.putExtra(EXTRA_ACCOUNT, account.getUuid());
-        i.putExtra(EXTRA_MAKE_DEFAULT, makeDefault);
-        context.startActivity(i);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setLayout(R.layout.account_setup_account_type)
+        findViewById<View>(R.id.pop).setOnClickListener(this)
+        findViewById<View>(R.id.imap).setOnClickListener(this)
+        val accountUuid = intent.getStringExtra(EXTRA_ACCOUNT)
+        account = Preferences.getPreferences(this).getAccount(accountUuid)
+        makeDefault = intent.getBooleanExtra(EXTRA_MAKE_DEFAULT, false)
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setLayout(R.layout.account_setup_account_type);
-        findViewById(R.id.pop).setOnClickListener(this);
-        findViewById(R.id.imap).setOnClickListener(this);
-
-        String accountUuid = getIntent().getStringExtra(EXTRA_ACCOUNT);
-        mAccount = Preferences.getPreferences(this).getAccount(accountUuid);
-        mMakeDefault = getIntent().getBooleanExtra(EXTRA_MAKE_DEFAULT, false);
+    @Throws(URISyntaxException::class)
+    private fun setupStoreAndSmtpTransport(serverType: String, schemePrefix: String) {
+        val domainPart = getDomainFromEmailAddress(account!!.email)
+        val suggestedStoreServerName = serverNameSuggester.suggestServerName(serverType, domainPart!!)
+        val storeUriForDecode = URI(account!!.storeUri)
+        val storeUri = URI(
+            schemePrefix, storeUriForDecode.userInfo, suggestedStoreServerName,
+            storeUriForDecode.port, null, null, null
+        )
+        account!!.storeUri = storeUri.toString()
+        val suggestedTransportServerName = serverNameSuggester.suggestServerName(Protocols.SMTP, domainPart)
+        val transportUriForDecode = URI(account!!.transportUri)
+        val transportUri = URI(
+            "smtp+tls+", transportUriForDecode.userInfo, suggestedTransportServerName,
+            transportUriForDecode.port, null, null, null
+        )
+        account!!.transportUri = transportUri.toString()
     }
 
-    private void setupStoreAndSmtpTransport(String serverType, String schemePrefix) throws URISyntaxException {
-        String domainPart = EmailHelper.getDomainFromEmailAddress(mAccount.getEmail());
-
-        String suggestedStoreServerName = serverNameSuggester.suggestServerName(serverType, domainPart);
-        URI storeUriForDecode = new URI(mAccount.getStoreUri());
-        URI storeUri = new URI(schemePrefix, storeUriForDecode.getUserInfo(), suggestedStoreServerName,
-                storeUriForDecode.getPort(), null, null, null);
-        mAccount.setStoreUri(storeUri.toString());
-
-        String suggestedTransportServerName = serverNameSuggester.suggestServerName(Protocols.SMTP, domainPart);
-        URI transportUriForDecode = new URI(mAccount.getTransportUri());
-        URI transportUri = new URI("smtp+tls+", transportUriForDecode.getUserInfo(), suggestedTransportServerName,
-                transportUriForDecode.getPort(), null, null, null);
-        mAccount.setTransportUri(transportUri.toString());
-    }
-
-    public void onClick(View v) {
+    override fun onClick(v: View) {
         try {
-            int id = v.getId();
+            val id = v.id
             if (id == R.id.pop) {
-                setupStoreAndSmtpTransport(Protocols.POP3, "pop3+ssl+");
+                setupStoreAndSmtpTransport(Protocols.POP3, "pop3+ssl+")
             } else if (id == R.id.imap) {
-                setupStoreAndSmtpTransport(Protocols.IMAP, "imap+ssl+");
+                setupStoreAndSmtpTransport(Protocols.IMAP, "imap+ssl+")
             }
-        } catch (Exception ex) {
-            failure(ex);
+        } catch (ex: Exception) {
+            failure(ex)
         }
-
-        AccountSetupIncoming.actionIncomingSettings(this, mAccount, mMakeDefault);
-        finish();
+        AccountSetupIncoming.actionIncomingSettings(this, account, makeDefault)
+        finish()
     }
 
-    private void failure(Exception use) {
-        Timber.e(use, "Failure");
-        String toastText = getString(R.string.account_setup_bad_uri, use.getMessage());
+    private fun failure(use: Exception) {
+        Timber.e(use, "Failure")
+        val toastText = getString(R.string.account_setup_bad_uri, use.message)
+        val toast = Toast.makeText(application, toastText, Toast.LENGTH_LONG)
+        toast.show()
+    }
 
-        Toast toast = Toast.makeText(getApplication(), toastText, Toast.LENGTH_LONG);
-        toast.show();
+    companion object {
+        private const val EXTRA_ACCOUNT = "account"
+        private const val EXTRA_MAKE_DEFAULT = "makeDefault"
+
+        @JvmStatic
+        fun actionSelectAccountType(context: Context, account: Account, makeDefault: Boolean) {
+            val i = Intent(context, AccountSetupAccountType::class.java)
+            i.putExtra(EXTRA_ACCOUNT, account.uuid)
+            i.putExtra(EXTRA_MAKE_DEFAULT, makeDefault)
+            context.startActivity(i)
+        }
     }
 }
