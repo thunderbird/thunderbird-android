21231bc9640df97452b395eff917c482f45bf403
cketti <cketti@gmail.com>
2019-12-03 20:29:18 +0100

    Clean up code in 'AccountSetupAccountType'

--- app/ui/src/main/java/com/fsck/k9/activity/setup/AccountSetupAccountType.kt
+++ app/ui/src/main/java/com/fsck/k9/activity/setup/AccountSetupAccountType.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.view.View.OnClickListener
-import android.widget.Toast
 import com.fsck.k9.Account
 import com.fsck.k9.Preferences
 import com.fsck.k9.activity.K9Activity
@@ -13,69 +11,80 @@ import com.fsck.k9.helper.EmailHelper.getDomainFromEmailAddress
 import com.fsck.k9.preferences.Protocols
 import com.fsck.k9.setup.ServerNameSuggester
 import com.fsck.k9.ui.R
-import timber.log.Timber
 import java.net.URI
-import java.net.URISyntaxException
+import org.koin.android.ext.android.inject
 
 /**
  * Prompts the user to select an account type. The account type, along with the
  * passed in email address, password and makeDefault are then passed on to the
  * AccountSetupIncoming activity.
  */
-class AccountSetupAccountType : K9Activity(), OnClickListener {
-    private val serverNameSuggester = ServerNameSuggester()
-    private var account: Account? = null
+class AccountSetupAccountType : K9Activity() {
+    private val preferences: Preferences by inject()
+    private val serverNameSuggester: ServerNameSuggester by inject()
+
+    private lateinit var account: Account
     private var makeDefault = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setLayout(R.layout.account_setup_account_type)
-        findViewById<View>(R.id.pop).setOnClickListener(this)
-        findViewById<View>(R.id.imap).setOnClickListener(this)
-        val accountUuid = intent.getStringExtra(EXTRA_ACCOUNT)
-        account = Preferences.getPreferences(this).getAccount(accountUuid)
+
+        decodeArguments()
+
+        findViewById<View>(R.id.pop).setOnClickListener { setupPop3Account() }
+        findViewById<View>(R.id.imap).setOnClickListener { setupImapAccount() }
+    }
+
+    private fun decodeArguments() {
+        val accountUuid = intent.getStringExtra(EXTRA_ACCOUNT) ?: error("No account UUID provided")
+        account = preferences.getAccount(accountUuid) ?: error("No account with given UUID found")
         makeDefault = intent.getBooleanExtra(EXTRA_MAKE_DEFAULT, false)
     }
 
-    @Throws(URISyntaxException::class)
+    private fun setupPop3Account() {
+        setupAccount(Protocols.POP3, "pop3+ssl+")
+    }
+
+    private fun setupImapAccount() {
+        setupAccount(Protocols.IMAP, "imap+ssl+")
+    }
+
+    private fun setupAccount(serverType: String, schemePrefix: String) {
+        setupStoreAndSmtpTransport(serverType, schemePrefix)
+        returnAccountTypeSelectionResult()
+    }
+
     private fun setupStoreAndSmtpTransport(serverType: String, schemePrefix: String) {
-        val domainPart = getDomainFromEmailAddress(account!!.email)
-        val suggestedStoreServerName = serverNameSuggester.suggestServerName(serverType, domainPart!!)
-        val storeUriForDecode = URI(account!!.storeUri)
+        val domainPart = getDomainFromEmailAddress(account.email) ?: error("Couldn't get domain from email address")
+
+        setupStoreUri(serverType, domainPart, schemePrefix)
+        setupTransportUri(domainPart)
+    }
+
+    private fun setupStoreUri(serverType: String, domainPart: String, schemePrefix: String) {
+        val suggestedStoreServerName = serverNameSuggester.suggestServerName(serverType, domainPart)
+        val storeUriForDecode = URI(account.storeUri)
         val storeUri = URI(
             schemePrefix, storeUriForDecode.userInfo, suggestedStoreServerName,
             storeUriForDecode.port, null, null, null
         )
-        account!!.storeUri = storeUri.toString()
+        account.storeUri = storeUri.toString()
+    }
+
+    private fun setupTransportUri(domainPart: String) {
         val suggestedTransportServerName = serverNameSuggester.suggestServerName(Protocols.SMTP, domainPart)
-        val transportUriForDecode = URI(account!!.transportUri)
+        val transportUriForDecode = URI(account.transportUri)
         val transportUri = URI(
             "smtp+tls+", transportUriForDecode.userInfo, suggestedTransportServerName,
             transportUriForDecode.port, null, null, null
         )
-        account!!.transportUri = transportUri.toString()
+        account.transportUri = transportUri.toString()
     }
 
-    override fun onClick(v: View) {
-        try {
-            val id = v.id
-            if (id == R.id.pop) {
-                setupStoreAndSmtpTransport(Protocols.POP3, "pop3+ssl+")
-            } else if (id == R.id.imap) {
-                setupStoreAndSmtpTransport(Protocols.IMAP, "imap+ssl+")
-            }
-        } catch (ex: Exception) {
-            failure(ex)
-        }
+    private fun returnAccountTypeSelectionResult() {
         AccountSetupIncoming.actionIncomingSettings(this, account, makeDefault)
         finish()
-    }
-
-    private fun failure(use: Exception) {
-        Timber.e(use, "Failure")
-        val toastText = getString(R.string.account_setup_bad_uri, use.message)
-        val toast = Toast.makeText(application, toastText, Toast.LENGTH_LONG)
-        toast.show()
     }
 
     companion object {
@@ -84,10 +93,11 @@ class AccountSetupAccountType : K9Activity(), OnClickListener {
 
         @JvmStatic
         fun actionSelectAccountType(context: Context, account: Account, makeDefault: Boolean) {
-            val i = Intent(context, AccountSetupAccountType::class.java)
-            i.putExtra(EXTRA_ACCOUNT, account.uuid)
-            i.putExtra(EXTRA_MAKE_DEFAULT, makeDefault)
-            context.startActivity(i)
+            val intent = Intent(context, AccountSetupAccountType::class.java).apply {
+                putExtra(EXTRA_ACCOUNT, account.uuid)
+                putExtra(EXTRA_MAKE_DEFAULT, makeDefault)
+            }
+            context.startActivity(intent)
         }
     }
 }
