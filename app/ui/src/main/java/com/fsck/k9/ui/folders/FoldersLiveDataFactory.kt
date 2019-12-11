5a6fb46343090bb10bbf2ab428a983c0b315dfbb
cketti <cketti@gmail.com>
2019-12-11 21:14:13 +0100

    Update 'FoldersLiveData' when accounts change
    This is so we update folder lists when the display mode is changed.

--- app/ui/src/main/java/com/fsck/k9/ui/folders/FoldersLiveDataFactory.kt
+++ app/ui/src/main/java/com/fsck/k9/ui/folders/FoldersLiveDataFactory.kt
@@ -1,15 +1,17 @@
 package com.fsck.k9.ui.folders
 
 import com.fsck.k9.Account
+import com.fsck.k9.Preferences
 import com.fsck.k9.controller.MessagingController
 import com.fsck.k9.mailstore.FolderRepositoryManager
 
 class FoldersLiveDataFactory(
     private val folderRepositoryManager: FolderRepositoryManager,
-    private val messagingController: MessagingController
+    private val messagingController: MessagingController,
+    private val preferences: Preferences
 ) {
     fun create(account: Account): FoldersLiveData {
         val folderRepository = folderRepositoryManager.getFolderRepository(account)
-        return FoldersLiveData(folderRepository, messagingController, account.uuid)
+        return FoldersLiveData(folderRepository, messagingController, preferences, account.uuid)
     }
 }
