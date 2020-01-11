848b87c996f6b2dd71a63ddc1a95f01679fae9cb
cketti <cketti@gmail.com>
2020-01-11 01:26:00 +0100

    Make MessageListViewModel only expose one LiveData for the folder list

--- app/ui/src/main/java/com/fsck/k9/ui/messagelist/KoinModule.kt
+++ app/ui/src/main/java/com/fsck/k9/ui/messagelist/KoinModule.kt
@@ -1,7 +1,9 @@
 package com.fsck.k9.ui.messagelist
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val messageListUiModule = module {
+    viewModel { MessageListViewModel(get()) }
     factory { DefaultFolderProvider() }
 }
--- app/ui/src/main/java/com/fsck/k9/ui/messagelist/MessageListViewModel.kt
+++ app/ui/src/main/java/com/fsck/k9/ui/messagelist/MessageListViewModel.kt
@@ -1,21 +1,43 @@
 package com.fsck.k9.ui.messagelist
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import com.fsck.k9.Account
+import com.fsck.k9.mailstore.DisplayFolder
 import com.fsck.k9.ui.folders.FoldersLiveData
 import com.fsck.k9.ui.folders.FoldersLiveDataFactory
 
 class MessageListViewModel(private val foldersLiveDataFactory: FoldersLiveDataFactory) : ViewModel() {
-    private var foldersLiveData: FoldersLiveData? = null
+    private var currentFoldersLiveData: FoldersLiveData? = null
+    private val foldersLiveData = MediatorLiveData<List<DisplayFolder>>()
 
-    fun getFolders(account: Account): FoldersLiveData {
-        val liveData = foldersLiveData
-        if (liveData != null && liveData.accountUuid == account.uuid) {
-            return liveData
+    fun getFolderListLiveData(): LiveData<List<DisplayFolder>> {
+        return foldersLiveData
+    }
+
+    fun loadFolders(account: Account) {
+        if (currentFoldersLiveData?.accountUuid == account.uuid) return
+
+        removeCurrentFoldersLiveData()
+
+        val liveData = foldersLiveDataFactory.create(account)
+        currentFoldersLiveData = liveData
+
+        foldersLiveData.addSource(liveData) { items ->
+            foldersLiveData.value = items
         }
+    }
 
-        return foldersLiveDataFactory.create(account).also {
-            foldersLiveData = it
+    fun stopLoadingFolders() {
+        removeCurrentFoldersLiveData()
+        foldersLiveData.value = null
+    }
+
+    private fun removeCurrentFoldersLiveData() {
+        currentFoldersLiveData?.let {
+            currentFoldersLiveData = null
+            foldersLiveData.removeSource(it)
         }
     }
 }
