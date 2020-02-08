cabb4ef47e6bde2e8e5128e289b7d4d7f483c0f6
Meteor0id <34976212+Meteor0id@users.noreply.github.com>
2020-02-08 22:17:25 +0100

    calculate byte size as base 10

--- app/ui/src/main/java/com/fsck/k9/ui/helper/SizeFormatter.java
+++ app/ui/src/main/java/com/fsck/k9/ui/helper/SizeFormatter.java
@@ -8,17 +8,17 @@ import com.fsck.k9.ui.R;
 public class SizeFormatter {
     /*
      * Formats the given size as a String in bytes, kB, MB or GB with a single digit
-     * of precision. Ex: 12,315,000 = 12.3 MB
+     * of precision. Ex: 12,315,000 B = 12.3 MB
      */
     public static String formatSize(Context context, long size) {
-        if (size > 1024000000) {
-            return ((float)(size / 102400000) / 10) + context.getString(R.string.abbrev_gigabytes);
+        if (size > 1000000000) {
+            return ((float)(size / 100000000) / 10) + context.getString(R.string.abbrev_gigabytes);
         }
-        if (size > 1024000) {
-            return ((float)(size / 102400) / 10) + context.getString(R.string.abbrev_megabytes);
+        if (size > 1000000) {
+            return ((float)(size / 100000) / 10) + context.getString(R.string.abbrev_megabytes);
         }
-        if (size > 1024) {
-            return ((float)(size / 102) / 10) + context.getString(R.string.abbrev_kilobytes);
+        if (size > 1000) {
+            return ((float)(size / 100) / 10) + context.getString(R.string.abbrev_kilobytes);
         }
         return size + context.getString(R.string.abbrev_bytes);
     }
