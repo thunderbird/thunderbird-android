bfbb744bbcf829b919ad67723136cc4eeed6cccb
cketti <cketti@gmail.com>
2020-02-14 02:12:02 +0100

    Don't create a new OpenPgpServiceConnection when there already is one
    Doing so would leak the ServiceConnection created by the previous
    OpenPgpServiceConnection.

--- plugins/openpgp-api-lib/openpgp-api/src/main/java/org/openintents/openpgp/OpenPgpApiManager.java
+++ plugins/openpgp-api-lib/openpgp-api/src/main/java/org/openintents/openpgp/OpenPgpApiManager.java
@@ -76,6 +76,12 @@ public class OpenPgpApiManager implements LifecycleObserver {
             return;
         }
 
+        if (openPgpServiceConnection != null) {
+            // An OpenPgpServiceConnection has already been created, but hasn't been bound yet.
+            // We'll just wait for OnBound.onBound() to be called.
+            return;
+        }
+
         setOpenPgpProviderState(OpenPgpProviderState.UNINITIALIZED);
         openPgpServiceConnection = new OpenPgpServiceConnection(context, openPgpProvider, new OnBound() {
             @Override
