0ee6592b0b60633b45e56a4725b15c62480d364d
cketti <cketti@gmail.com>
2020-03-10 21:57:34 +0100

    Change code to resolve theme attributes
    This way doesn't produce Lint errors.

--- app/ui/src/main/java/com/fsck/k9/ui/ThemeExtensions.kt
+++ app/ui/src/main/java/com/fsck/k9/ui/ThemeExtensions.kt
@@ -1,15 +1,27 @@
 package com.fsck.k9.ui
 
 import android.content.res.Resources.Theme
+import android.graphics.drawable.Drawable
 import android.util.TypedValue
 
-fun Theme.resolveAttribute(resId: Int): Int {
+fun Theme.resolveColorAttribute(attrId: Int): Int {
     val typedValue = TypedValue()
 
-    val found = resolveAttribute(resId, typedValue, true)
+    val found = resolveAttribute(attrId, typedValue, true)
     if (!found) {
-        throw IllegalStateException("Couldn't resolve attribute ($resId)")
+        throw IllegalStateException("Couldn't resolve attribute ($attrId)")
     }
 
     return typedValue.data
+}
+
+fun Theme.resolveDrawableAttribute(attrId: Int): Drawable {
+    val typedValue = TypedValue()
+
+    val found = resolveAttribute(attrId, typedValue, true)
+    if (!found) {
+        throw IllegalStateException("Couldn't resolve attribute ($attrId)")
+    }
+
+    return getDrawable(typedValue.resourceId)
 }
