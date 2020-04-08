a44732ee3840b659d6100e2c7c7d660edf4430f0
cketti <cketti@gmail.com>
2020-04-08 19:23:16 +0200

    Remove FolderClass.NONE
    Rewrite entries in the database to use FolderClass.NO_CLASS

---
+++ app/storage/src/main/java/com/fsck/k9/storage/migrations/MigrationTo71.kt
@@ -0,0 +1,16 @@
+package com.fsck.k9.storage.migrations
+
+import android.database.sqlite.SQLiteDatabase
+
+internal class MigrationTo71(private val db: SQLiteDatabase) {
+    fun cleanUpFolderClass() {
+        db.execSQL("UPDATE folders SET poll_class = 'NO_CLASS' " +
+            "WHERE poll_class NOT IN ('NO_CLASS', 'INHERITED', 'FIRST_CLASS', 'SECOND_CLASS')")
+        db.execSQL("UPDATE folders SET push_class = 'NO_CLASS' " +
+            "WHERE push_class NOT IN ('NO_CLASS', 'INHERITED', 'FIRST_CLASS', 'SECOND_CLASS')")
+        db.execSQL("UPDATE folders SET display_class = 'NO_CLASS' " +
+            "WHERE display_class NOT IN ('NO_CLASS', 'INHERITED', 'FIRST_CLASS', 'SECOND_CLASS')")
+        db.execSQL("UPDATE folders SET notify_class = 'NO_CLASS' " +
+            "WHERE notify_class NOT IN ('NO_CLASS', 'INHERITED', 'FIRST_CLASS', 'SECOND_CLASS')")
+    }
+}
