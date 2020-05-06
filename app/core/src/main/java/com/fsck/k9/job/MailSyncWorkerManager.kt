76f1d37bc1f8b3bc67a467221d3ac15f625b7a1b
cketti <cketti@gmail.com>
2020-05-06 17:03:24 +0200

    Use last sync time to calculate initial delay for periodic syncs

--- app/core/src/main/java/com/fsck/k9/job/MailSyncWorkerManager.kt
+++ app/core/src/main/java/com/fsck/k9/job/MailSyncWorkerManager.kt
@@ -7,11 +7,12 @@ import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.workDataOf
 import com.fsck.k9.Account
+import com.fsck.k9.Clock
 import com.fsck.k9.K9
-import java.util.concurrent.TimeUnit
+import java.time.Duration
 import timber.log.Timber
 
-class MailSyncWorkerManager(private val workManager: WorkManager) {
+class MailSyncWorkerManager(private val workManager: WorkManager, val clock: Clock) {
 
     fun cancelMailSync(account: Account) {
         Timber.v("Canceling mail sync worker for %s", account.description)
@@ -22,21 +23,29 @@ class MailSyncWorkerManager(private val workManager: WorkManager) {
     fun scheduleMailSync(account: Account) {
         if (isNeverSyncInBackground()) return
 
-        getSyncIntervalInMinutesIfEnabled(account)?.let { syncInterval ->
+        getSyncIntervalIfEnabled(account)?.let { syncInterval ->
             Timber.v("Scheduling mail sync worker for %s", account.description)
+            Timber.v("  sync interval: %d minutes", syncInterval.toMinutes())
 
             val constraints = Constraints.Builder()
-                    .setRequiredNetworkType(NetworkType.CONNECTED)
-                    .setRequiresStorageNotLow(true)
-                    .build()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .setRequiresStorageNotLow(true)
+                .build()
+
+            val lastSyncTime = account.lastSyncTime
+            Timber.v("  last sync time: %tc", lastSyncTime)
+
+            val initialDelay = calculateInitialDelay(lastSyncTime, syncInterval)
+            Timber.v("  initial delay: %d minutes", initialDelay.toMinutes())
 
             val data = workDataOf(MailSyncWorker.EXTRA_ACCOUNT_UUID to account.uuid)
 
-            val mailSyncRequest = PeriodicWorkRequestBuilder<MailSyncWorker>(syncInterval, TimeUnit.MINUTES)
-                    .setConstraints(constraints)
-                    .setInputData(data)
-                    .addTag(MAIL_SYNC_TAG)
-                    .build()
+            val mailSyncRequest = PeriodicWorkRequestBuilder<MailSyncWorker>(syncInterval)
+                .setInitialDelay(initialDelay)
+                .setConstraints(constraints)
+                .setInputData(data)
+                .addTag(MAIL_SYNC_TAG)
+                .build()
 
             val uniqueWorkName = createUniqueWorkName(account.uuid)
             workManager.enqueueUniquePeriodicWork(uniqueWorkName, ExistingPeriodicWorkPolicy.REPLACE, mailSyncRequest)
@@ -45,13 +54,24 @@ class MailSyncWorkerManager(private val workManager: WorkManager) {
 
     private fun isNeverSyncInBackground() = K9.backgroundOps == K9.BACKGROUND_OPS.NEVER
 
-    private fun getSyncIntervalInMinutesIfEnabled(account: Account): Long? {
+    private fun getSyncIntervalIfEnabled(account: Account): Duration? {
         val intervalMinutes = account.automaticCheckIntervalMinutes
         if (intervalMinutes <= Account.INTERVAL_MINUTES_NEVER) {
             return null
         }
 
-        return intervalMinutes.toLong()
+        return Duration.ofMinutes(intervalMinutes.toLong())
+    }
+
+    private fun calculateInitialDelay(lastSyncTime: Long, syncInterval: Duration): Duration {
+        val now = clock.time
+        val nextSyncTime = lastSyncTime + syncInterval.toMillis()
+
+        return if (lastSyncTime > now || nextSyncTime <= now) {
+            Duration.ZERO
+        } else {
+            Duration.ofMillis(nextSyncTime - now)
+        }
     }
 
     private fun createUniqueWorkName(accountUuid: String): String {
