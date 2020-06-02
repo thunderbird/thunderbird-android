8b043840c7ac267e9477e340f1e9c721db116a9c
cketti <cketti@gmail.com>
2020-06-02 17:08:11 +0200

    Convert 'HtmlHelper' to Kotlin

--- app/core/src/test/java/com/fsck/k9/message/html/HtmlHelper.kt
+++ app/core/src/test/java/com/fsck/k9/message/html/HtmlHelper.kt
@@ -1,12 +1,11 @@
-package com.fsck.k9.message.html;
+package com.fsck.k9.message.html
 
+import org.jsoup.Jsoup
+import org.jsoup.safety.Whitelist
 
-import org.jsoup.Jsoup;
-import org.jsoup.safety.Whitelist;
-
-
-public class HtmlHelper {
-    public static String extractText(String html) {
-        return Jsoup.clean(html, Whitelist.none());
+object HtmlHelper {
+    @JvmStatic
+    fun extractText(html: String): String {
+        return Jsoup.clean(html, Whitelist.none())
     }
 }
