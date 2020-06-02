02b5feacadbf98ce426a17edefc547eaf10e0c38
cketti <cketti@gmail.com>
2020-06-02 17:08:33 +0200

    Replace 'Whitelist' with 'AllowList' in HtmlHelper

--- app/core/src/test/java/com/fsck/k9/message/html/HtmlHelper.kt
+++ app/core/src/test/java/com/fsck/k9/message/html/HtmlHelper.kt
@@ -1,11 +1,11 @@
 package com.fsck.k9.message.html
 
 import org.jsoup.Jsoup
-import org.jsoup.safety.Whitelist
+import org.jsoup.safety.Whitelist as AllowList
 
 object HtmlHelper {
     @JvmStatic
     fun extractText(html: String): String {
-        return Jsoup.clean(html, Whitelist.none())
+        return Jsoup.clean(html, AllowList.none())
     }
 }
