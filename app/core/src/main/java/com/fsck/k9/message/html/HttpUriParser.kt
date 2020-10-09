b1d21dc27c75e976665c582f4023f545977c10f6
cketti <cketti@gmail.com>
2020-10-09 02:33:11 +0200

    Clean up 'HttpUriParser'
    There shouldn't be any functional changes.

--- app/core/src/main/java/com/fsck/k9/message/html/HttpUriParser.kt
+++ app/core/src/main/java/com/fsck/k9/message/html/HttpUriParser.kt
@@ -1,7 +1,5 @@
 package com.fsck.k9.message.html
 
-import java.util.regex.Pattern
-import kotlin.math.max
 import kotlin.math.min
 
 /**
@@ -13,10 +11,10 @@ import kotlin.math.min
  */
 internal class HttpUriParser : UriParser {
     override fun parseUri(text: CharSequence, startPos: Int): UriMatch? {
-        val schemeMatcher = SCHEME_PATTERN.matcher(text)
-        if (!schemeMatcher.find(startPos) || schemeMatcher.start() != startPos) return null
+        val matchResult = SCHEME_REGEX.find(text, startPos) ?: return null
+        if (matchResult.range.first != startPos) return null
 
-        var currentPos = schemeMatcher.end()
+        var currentPos = matchResult.range.last + 1
 
         // Authority
         val matchedAuthorityEnd = tryMatchAuthority(text, currentPos)
@@ -43,98 +41,74 @@ internal class HttpUriParser : UriParser {
     }
 
     private fun tryMatchAuthority(text: CharSequence, startPos: Int): Int {
-        var authorityLimit = indexOf(text, '/', startPos)
+        var authorityLimit = text.indexOf('/', startPos)
         if (authorityLimit == -1) {
             authorityLimit = text.length
         }
         val authorityStart = tryMatchUserInfo(text, startPos, authorityLimit)
 
         var authorityEnd = tryMatchDomainName(text, authorityStart)
-        if (authorityEnd != authorityStart) {
-            return authorityEnd
-        }
+        if (authorityEnd != authorityStart) return authorityEnd
 
         authorityEnd = tryMatchIpv4Address(text, authorityStart, true)
-        if (authorityEnd != authorityStart) {
-            return authorityEnd
-        }
+        if (authorityEnd != authorityStart) return authorityEnd
 
         authorityEnd = tryMatchIpv6Address(text, authorityStart)
-        return if (authorityEnd != authorityStart) authorityEnd else startPos
+        if (authorityEnd != authorityStart) return authorityEnd
+
+        return startPos
     }
 
     private fun tryMatchUserInfo(text: CharSequence, startPos: Int, limit: Int): Int {
-        val userInfoEnd = indexOf(text, '@', startPos)
-        return if (userInfoEnd != -1 && userInfoEnd < limit) {
-            if (matchUnreservedPCTEncodedSubDelimClassesGreedy(text, startPos, ":") != userInfoEnd) {
-                // Illegal character in user info
-                startPos
-            } else {
-                userInfoEnd + 1
-            }
-        } else {
+        val userInfoEnd = text.indexOf('@', startPos)
+        if (userInfoEnd == -1 || userInfoEnd >= limit) return startPos
+
+        return if (matchUnreservedPCTEncodedSubDelimClassesGreedy(text, startPos, ":") != userInfoEnd) {
+            // Illegal character in user info
             startPos
+        } else {
+            userInfoEnd + 1
         }
     }
 
     private fun tryMatchDomainName(text: CharSequence, startPos: Int): Int {
-        return try {
-            val matcher = DOMAIN_PATTERN.matcher(text)
-            if (!matcher.find(startPos) || matcher.start() != startPos) {
-                return startPos
-            }
+        val matchResult = DOMAIN_REGEX.find(text, startPos) ?: return startPos
+        if (matchResult.range.first != startPos) return startPos
 
-            val portString = matcher.group(1)
-            if (portString != null && portString.isNotEmpty()) {
-                val port = portString.toInt()
-                if (port > 65535) {
-                    return startPos
-                }
-            }
-
-            matcher.end()
-        } catch (e: IllegalArgumentException) {
-            startPos
+        val portString = matchResult.groupValues[1]
+        if (portString.isNotEmpty()) {
+            val port = portString.toInt()
+            if (port > 65535) return startPos
         }
+
+        return matchResult.range.last + 1
     }
 
     private fun tryMatchIpv4Address(text: CharSequence, startPos: Int, portAllowed: Boolean): Int {
-        val matcher = IPv4_PATTERN.matcher(text)
-        if (!matcher.find(startPos) || matcher.start() != startPos) {
-            return startPos
-        }
+        val matchResult = IPv4_REGEX.find(text, startPos) ?: return startPos
+        if (matchResult.range.first != startPos) return startPos
 
         for (i in 1..4) {
-            val segment = matcher.group(1).toInt()
-            if (segment > 255) {
-                return startPos
-            }
+            val segment = matchResult.groupValues[1].toInt()
+            if (segment > 255) return startPos
         }
 
-        if (!portAllowed && matcher.group(5) != null) {
-            return startPos
-        }
+        if (!portAllowed && matchResult.groupValues[5].isNotEmpty()) return startPos
 
-        val portString = matcher.group(6)
-        if (portString != null && portString.isNotEmpty()) {
+        val portString = matchResult.groupValues[6]
+        if (portString.isNotEmpty()) {
             val port = portString.toInt()
-            if (port > 65535) {
-                return startPos
-            }
+            if (port > 65535) return startPos
         }
 
-        return matcher.end()
+        return matchResult.range.last + 1
     }
 
     private fun tryMatchIpv6Address(text: CharSequence, startPos: Int): Int {
-        if (startPos == text.length || text[startPos] != '[') {
-            return startPos
-        }
+        if (startPos == text.length || text[startPos] != '[') return startPos
 
-        val addressEnd = indexOf(text, ']', startPos)
-        if (addressEnd == -1) {
-            return startPos
-        }
+        val addressEnd = text.indexOf(']', startPos)
+        if (addressEnd == -1) return startPos
 
         // Actual parsing
         var currentPos = startPos + 1
@@ -142,26 +116,21 @@ internal class HttpUriParser : UriParser {
         var endSegmentsCount = 0
 
         // Handle :: separator and segments in front of it
-        val compressionPos = indexOf(text, "::", currentPos)
+        val compressionPos = text.indexOf("::", currentPos)
         val compressionEnabled = compressionPos != -1 && compressionPos < addressEnd
         if (compressionEnabled) {
             while (currentPos < compressionPos) {
                 // Check segment separator
                 if (beginSegmentsCount > 0) {
-                    if (text[currentPos] != ':') {
-                        return startPos
-                    } else {
-                        ++currentPos
-                    }
+                    if (text[currentPos] != ':') return startPos
+                    currentPos++
                 }
 
                 // Parse segment
                 val possibleSegmentEnd = parse16BitHexSegment(text, currentPos, min(currentPos + 4, compressionPos))
-                if (possibleSegmentEnd == currentPos) {
-                    return startPos
-                }
+                if (possibleSegmentEnd == currentPos) return startPos
                 currentPos = possibleSegmentEnd
-                ++beginSegmentsCount
+                beginSegmentsCount++
             }
 
             currentPos += 2 // Skip :: separator
@@ -171,26 +140,19 @@ internal class HttpUriParser : UriParser {
         while (currentPos < addressEnd && beginSegmentsCount + endSegmentsCount < 8) {
             // Check segment separator
             if (endSegmentsCount > 0) {
-                if (text[currentPos] != ':') {
-                    return startPos
-                } else {
-                    ++currentPos
-                }
+                if (text[currentPos] != ':') return startPos
+                currentPos++
             }
 
             // Small look ahead, do not run into IPv4 tail (7 is IPv4 minimum length)
-            val nextColon = indexOf(text, ':', currentPos)
-            if ((nextColon == -1 || nextColon > addressEnd) && addressEnd - currentPos >= 7) {
-                break
-            }
+            val nextColon = text.indexOf(':', currentPos)
+            if ((nextColon == -1 || nextColon > addressEnd) && addressEnd - currentPos >= 7) break
 
             // Parse segment
-            val possibleSegmentEnd = parse16BitHexSegment(text, currentPos, Math.min(currentPos + 4, addressEnd))
-            if (possibleSegmentEnd == currentPos) {
-                return startPos
-            }
+            val possibleSegmentEnd = parse16BitHexSegment(text, currentPos, min(currentPos + 4, addressEnd))
+            if (possibleSegmentEnd == currentPos) return startPos
             currentPos = possibleSegmentEnd
-            ++endSegmentsCount
+            endSegmentsCount++
         }
 
         // We have 3 valid cases here
@@ -200,60 +162,59 @@ internal class HttpUriParser : UriParser {
             if (!compressionEnabled && beginSegmentsCount + endSegmentsCount == 8 ||
                 compressionEnabled && beginSegmentsCount + endSegmentsCount < 8) {
                 // Only optional port left, skip address bracket
-                ++currentPos
+                currentPos++
             } else {
                 return startPos
             }
         } else {
             // 3) Still some stuff missing, check for IPv4 as tail necessary
-            if (tryMatchIpv4Address(text, currentPos, false) != addressEnd) {
-                return startPos
-            }
+            if (tryMatchIpv4Address(text, currentPos, false) != addressEnd) return startPos
             currentPos = addressEnd + 1
         }
 
         // Check optional port
-        if (currentPos == text.length || text[currentPos] != ':') {
-            return currentPos
-        }
-        ++currentPos
+        if (currentPos == text.length || text[currentPos] != ':') return currentPos
+        currentPos++
 
         var port = 0
         while (currentPos < text.length) {
-            val c = text[currentPos].toInt()
-            if (c < '0'.toInt() || c > '9'.toInt()) {
+            val c = text[currentPos]
+            if (c !in '0'..'9') {
                 break
             }
-            port = port * 10 + c - '0'.toInt()
+            port = port * 10 + (c - '0')
             currentPos++
         }
+
         return if (port <= 65535) currentPos else startPos
     }
 
     private fun parse16BitHexSegment(text: CharSequence, startPos: Int, endPos: Int): Int {
         var currentPos = startPos
-        while (isHexDigit(text[currentPos].toInt()) && currentPos < endPos) {
-            ++currentPos
+        while (isHexDigit(text[currentPos]) && currentPos < endPos) {
+            currentPos++
         }
 
         return currentPos
     }
 
+    @Suppress("ConvertToStringTemplate")
     private fun matchUnreservedPCTEncodedSubDelimClassesGreedy(
-        text: CharSequence, startPos: Int,
+        text: CharSequence,
+        startPos: Int,
         additionalCharacters: String
     ): Int {
         val allowedCharacters = SUB_DELIM + "-._~" + additionalCharacters
         var shouldBeHex = 0
         var currentPos = startPos
         while (currentPos < text.length) {
-            val c = text[currentPos].toInt()
+            val c = text[currentPos]
             if (isHexDigit(c)) {
-                shouldBeHex = max(shouldBeHex - 1, 0)
+                shouldBeHex = (shouldBeHex - 1).coerceAtLeast(0)
             } else if (shouldBeHex == 0) {
-                if (allowedCharacters.indexOf(c.toChar()) != -1) {
+                if (c in allowedCharacters) {
                     // Everything ok here :)
-                } else if (c == '%'.toInt()) {
+                } else if (c == '%') {
                     shouldBeHex = 2
                 } else {
                     break
@@ -267,58 +228,17 @@ internal class HttpUriParser : UriParser {
         return currentPos
     }
 
-    private fun isHexDigit(c: Int): Boolean {
-        return c >= 'a'.toInt() && c <= 'z'.toInt() || c >= 'A'.toInt() && c <= 'Z'.toInt() || c >= '0'.toInt() && c <= '9'.toInt()
-    }
-
-    private fun indexOf(text: CharSequence, ch: Char, fromIndex: Int): Int {
-        var i = fromIndex
-        val end = text.length
-        while (i < end) {
-            if (text[i] == ch) {
-                return i
-            }
-            i++
-        }
-
-        return -1
-    }
-
-    private fun indexOf(text: CharSequence, str: String, fromIndex: Int): Int {
-        val ch = str[0]
-        var i = fromIndex
-        val end = text.length
-        while (i < end) {
-            if (text[i] == ch) {
-                var found = true
-                var j = 1
-                val strLen = str.length
-                while (j < strLen) {
-                    if (text[i + j] != str[j]) {
-                        found = false
-                        break
-                    }
-                    j++
-                }
-
-                if (found) {
-                    return i
-                }
-            }
-            i++
-        }
-
-        return -1
+    private fun isHexDigit(c: Char): Boolean {
+        return c in 'a'..'z' || c in 'A'..'Z' || c in '0'..'9'
     }
 
     companion object {
         // This string represent character group sub-delim as described in RFC 3986
         private const val SUB_DELIM = "!$&'()*+,;="
-        private val SCHEME_PATTERN = Pattern.compile("(https?|rtsp)://", Pattern.CASE_INSENSITIVE)
-        private val DOMAIN_PATTERN = Pattern.compile(
-            "[\\da-z](?:[\\da-z-]*[\\da-z])*(?:\\.[\\da-z](?:[\\da-z-]*[\\da-z])*)*(?::(\\d{0,5}))?",
-            Pattern.CASE_INSENSITIVE
-        )
-        private val IPv4_PATTERN = Pattern.compile("(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})(:(\\d{0,5}))?")
+        private val SCHEME_REGEX = "(https?|rtsp)://".toRegex(RegexOption.IGNORE_CASE)
+        private val DOMAIN_REGEX =
+            "[\\da-z](?:[\\da-z-]*[\\da-z])*(?:\\.[\\da-z](?:[\\da-z-]*[\\da-z])*)*(?::(\\d{0,5}))?"
+                .toRegex(RegexOption.IGNORE_CASE)
+        private val IPv4_REGEX = "(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})(:(\\d{0,5}))?".toRegex()
     }
 }
