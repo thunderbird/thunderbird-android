7d7c198e7341933f33f11eefcd0afac94711c550
cketti <cketti@gmail.com>
2020-10-31 17:57:58 +0100

    Don't show recipient names in Inbox, Archive, Spam and Trash folders
    Some people assign e.g. the 'Sent' role to the Inbox. In that case we need to treat the folder like the Inbox rather than the Sent folder when it comes to deciding whether or not to display the recipient address in the message list.

--- app/ui/legacy/src/main/java/com/fsck/k9/ui/helper/DisplayAddressHelper.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/ui/helper/DisplayAddressHelper.kt
@@ -5,6 +5,10 @@ import com.fsck.k9.Account
 object DisplayAddressHelper {
     fun shouldShowRecipients(account: Account, folderId: Long): Boolean {
         return when (folderId) {
+            account.inboxFolderId -> false
+            account.archiveFolderId -> false
+            account.spamFolderId -> false
+            account.trashFolderId -> false
             account.sentFolderId -> true
             account.draftsFolderId -> true
             account.outboxFolderId -> true
