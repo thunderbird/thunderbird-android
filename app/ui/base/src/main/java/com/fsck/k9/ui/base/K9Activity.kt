26bb8e3bb7e2df42b2c45f6af6d3cd47c2180d33
cketti <cketti@gmail.com>
2021-03-15 16:28:24 +0100

    Merge 'K9ActivityCommon' into 'K9Activity'

--- app/ui/base/src/main/java/com/fsck/k9/ui/base/K9Activity.kt
+++ app/ui/base/src/main/java/com/fsck/k9/ui/base/K9Activity.kt
@@ -1,26 +1,70 @@
 package com.fsck.k9.ui.base
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.fsck.k9.K9
+import java.util.Locale
+import org.koin.android.ext.android.inject
 
-abstract class K9Activity(themeType: ThemeType) : AppCompatActivity() {
+abstract class K9Activity(private val themeType: ThemeType) : AppCompatActivity() {
     constructor() : this(ThemeType.DEFAULT)
 
-    private val base = K9ActivityCommon(this, themeType)
+    protected val themeManager: ThemeManager by inject()
 
-    val themeManager: ThemeManager
-        get() = base.themeManager
+    private lateinit var currentLanguage: String
+    private lateinit var currentTheme: Theme
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        base.preOnCreate()
+        initializeLanguage()
+        initializeTheme()
         super.onCreate(savedInstanceState)
     }
 
     override fun onResume() {
-        base.preOnResume()
+        languageChangeCheck()
         super.onResume()
+    }
+
+    private fun initializeLanguage() {
+        K9.k9Language.let { language ->
+            currentLanguage = language
+            setLanguage(language)
+        }
+    }
+
+    private fun initializeTheme() {
+        currentTheme = themeManager.appTheme
+        val theme = when (themeType) {
+            ThemeType.DEFAULT -> themeManager.appThemeResourceId
+            ThemeType.ACTION_BAR -> themeManager.appActionBarThemeResourceId
+            ThemeType.DIALOG -> themeManager.translucentDialogThemeResourceId
+        }
+        setTheme(theme)
+    }
+
+    private fun languageChangeCheck() {
+        if (currentLanguage != K9.k9Language) {
+            recreate()
+        }
+    }
+
+    private fun setLanguage(language: String) {
+        val locale = if (TextUtils.isEmpty(language)) {
+            Resources.getSystem().configuration.locale
+        } else if (language.length == 5 && language[2] == '_') {
+            // language is in the form: en_US
+            Locale(language.substring(0, 2), language.substring(3))
+        } else {
+            Locale(language)
+        }
+
+        val config = resources.configuration
+        config.locale = locale
+        resources.updateConfiguration(config, resources.displayMetrics)
     }
 
     protected fun setLayout(@LayoutRes layoutResId: Int) {
@@ -30,4 +74,10 @@ abstract class K9Activity(themeType: ThemeType) : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
     }
+}
+
+enum class ThemeType {
+    DEFAULT,
+    ACTION_BAR,
+    DIALOG
 }
