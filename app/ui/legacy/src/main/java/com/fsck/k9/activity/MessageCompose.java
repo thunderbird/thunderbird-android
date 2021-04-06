8672ee93658b6f20d3ad3755c3798a59a9b67532
plan3d <plan3d@tutanota.com>
2021-04-06 12:14:23 +0200

    Issue 2485: Added ability to discard changes and keep original draft

--- app/ui/legacy/src/main/java/com/fsck/k9/activity/MessageCompose.java
+++ app/ui/legacy/src/main/java/com/fsck/k9/activity/MessageCompose.java
@@ -1102,23 +1102,45 @@ public class MessageCompose extends K9Activity implements OnClickListener,
 
     @Override
     public Dialog onCreateDialog(int id) {
+        final Builder builder;
         switch (id) {
             case DIALOG_SAVE_OR_DISCARD_DRAFT_MESSAGE:
-                return new AlertDialog.Builder(this)
-                        .setTitle(R.string.save_or_discard_draft_message_dlg_title)
-                        .setMessage(R.string.save_or_discard_draft_message_instructions_fmt)
+                builder = new AlertDialog.Builder(this)
+                        .setTitle(R.string.save_or_discard_draft_message_dlg_title);
+                if (draftMessageId == null) {
+                    builder
+                            .setMessage(R.string.save_or_discard_draft_message_instructions_fmt)
+                            .setNegativeButton(R.string.discard_action, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    dismissDialog(DIALOG_SAVE_OR_DISCARD_DRAFT_MESSAGE);
+                                    onDiscard();
+                                }
+                            });
+                } else {
+                    builder
+                            .setMessage(R.string.save_or_discard_draft_message_changes_instructions_fmt)
+                            .setNegativeButton(R.string.discard_action, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    dismissDialog(DIALOG_SAVE_OR_DISCARD_DRAFT_MESSAGE);
+                                    changesMadeSinceLastSave = false;
+                                    draftMessageId = null;
+                                    if (navigateUp) {
+                                        openDefaultFolder();
+                                    } else {
+                                        finish();
+                                    }
+                                }
+
+                            });
+                }
+                return builder
                         .setPositiveButton(R.string.save_draft_action, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int whichButton) {
                                 dismissDialog(DIALOG_SAVE_OR_DISCARD_DRAFT_MESSAGE);
                                 checkToSaveDraftAndSave();
-                            }
-                        })
-                        .setNegativeButton(R.string.discard_action, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int whichButton) {
-                                dismissDialog(DIALOG_SAVE_OR_DISCARD_DRAFT_MESSAGE);
-                                onDiscard();
                             }
                         })
                         .create();
@@ -1146,7 +1168,7 @@ public class MessageCompose extends K9Activity implements OnClickListener,
             case DIALOG_CHOOSE_IDENTITY:
                 int dialogThemeResourceId = getThemeManager().getDialogThemeResourceId();
                 Context context = new ContextThemeWrapper(this, dialogThemeResourceId);
-                Builder builder = new AlertDialog.Builder(context);
+                builder = new AlertDialog.Builder(context);
                 builder.setTitle(R.string.send_as);
                 final IdentityAdapter adapter = new IdentityAdapter(context);
                 builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
