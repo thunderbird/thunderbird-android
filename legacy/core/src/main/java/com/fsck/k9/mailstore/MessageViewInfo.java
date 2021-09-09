f063bdc9aae463cfc2eebfcd68b683c6987ef969
tenzap <fabstz-it@yahoo.fr>
2021-09-09 09:52:36 +0200

    Add menu-option to switch between text/plain & text/html
    Fixes https://github.com/thunderbird/thunderbird-android/issues/1397
    
    This was picked from https://github.com/k9mail/k-9/pull/3936 and fixed so that
    the message view is properly updated with the user choice

--- legacy/core/src/main/java/com/fsck/k9/mailstore/MessageViewInfo.java
+++ legacy/core/src/main/java/com/fsck/k9/mailstore/MessageViewInfo.java
@@ -21,6 +21,7 @@ public class MessageViewInfo {
     public final boolean isSubjectEncrypted;
     public final AttachmentResolver attachmentResolver;
     public final String text;
+    public final String textPlainFormatted;
     public final CryptoResultAnnotation cryptoResultAnnotation;
     public final List<AttachmentViewInfo> attachments;
     public final String extraText;
@@ -31,7 +32,7 @@ public class MessageViewInfo {
     public MessageViewInfo(
             Message message, boolean isMessageIncomplete, Part rootPart,
             String subject, boolean isSubjectEncrypted,
-            String text, List<AttachmentViewInfo> attachments,
+            String text, String textPlainFormatted, List<AttachmentViewInfo> attachments,
             CryptoResultAnnotation cryptoResultAnnotation,
             AttachmentResolver attachmentResolver,
             String extraText, List<AttachmentViewInfo> extraAttachments,
@@ -42,6 +43,7 @@ public class MessageViewInfo {
         this.subject = subject;
         this.isSubjectEncrypted = isSubjectEncrypted;
         this.text = text;
+        this.textPlainFormatted = textPlainFormatted;
         this.cryptoResultAnnotation = cryptoResultAnnotation;
         this.attachmentResolver = attachmentResolver;
         this.attachments = attachments;
@@ -51,10 +53,10 @@ public class MessageViewInfo {
     }
 
     static MessageViewInfo createWithExtractedContent(Message message, Part rootPart, boolean isMessageIncomplete,
-            String text, List<AttachmentViewInfo> attachments, AttachmentResolver attachmentResolver,
+            String text, String textPlainFormatted, List<AttachmentViewInfo> attachments, AttachmentResolver attachmentResolver,
             UnsubscribeUri preferredUnsubscribeUri) {
         return new MessageViewInfo(
-                message, isMessageIncomplete, rootPart, null, false, text, attachments, null, attachmentResolver, null,
+                message, isMessageIncomplete, rootPart, null, false, text, textPlainFormatted, attachments, null, attachmentResolver, null,
                 Collections.<AttachmentViewInfo>emptyList(), preferredUnsubscribeUri);
     }
 
@@ -63,7 +65,7 @@ public class MessageViewInfo {
             Body emptyBody = new TextBody("");
             Part emptyPart = new MimeBodyPart(emptyBody, "text/plain");
             String subject = message.getSubject();
-            return new MessageViewInfo(message, isMessageIncomplete, emptyPart, subject, false, null, null, null, null,
+            return new MessageViewInfo(message, isMessageIncomplete, emptyPart, subject, false, null, null, null, null, null,
                     null, null, null);
         } catch (MessagingException e) {
             throw new AssertionError(e);
@@ -72,13 +74,14 @@ public class MessageViewInfo {
 
     public static MessageViewInfo createForMetadataOnly(Message message, boolean isMessageIncomplete) {
         String subject = message.getSubject();
-        return new MessageViewInfo(message, isMessageIncomplete, null, subject, false, null, null, null, null, null, null, null);
+        return new MessageViewInfo(message, isMessageIncomplete, null, subject, false, null, null, null, null, null, null, null, null);
     }
 
     MessageViewInfo withCryptoData(CryptoResultAnnotation rootPartAnnotation, String extraViewableText,
             List<AttachmentViewInfo> extraAttachmentInfos) {
         return new MessageViewInfo(
-                message, isMessageIncomplete, rootPart, subject, isSubjectEncrypted, text, attachments,
+                message, isMessageIncomplete, rootPart, subject, isSubjectEncrypted, text, textPlainFormatted,
+                attachments,
                 rootPartAnnotation,
                 attachmentResolver,
                 extraViewableText, extraAttachmentInfos,
@@ -88,7 +91,7 @@ public class MessageViewInfo {
 
     MessageViewInfo withSubject(String subject, boolean isSubjectEncrypted) {
         return new MessageViewInfo(
-                message, isMessageIncomplete, rootPart, subject, isSubjectEncrypted, text, attachments,
+                message, isMessageIncomplete, rootPart, subject, isSubjectEncrypted, text, textPlainFormatted, attachments,
                 cryptoResultAnnotation, attachmentResolver, extraText, extraAttachments, preferredUnsubscribeUri
         );
     }
