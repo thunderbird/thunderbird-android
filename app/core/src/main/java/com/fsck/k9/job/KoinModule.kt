20dfc9a75bd82f9acdd32b57374718e854d0a625
cketti <cketti@gmail.com>
2021-10-23 03:53:19 +0200

    Convert class 'Clock' to an interface

--- app/core/src/main/java/com/fsck/k9/job/KoinModule.kt
+++ app/core/src/main/java/com/fsck/k9/job/KoinModule.kt
@@ -1,7 +1,6 @@
 package com.fsck.k9.job
 
 import androidx.work.WorkerFactory
-import com.fsck.k9.Clock
 import org.koin.dsl.module
 
 val jobModule = module {
@@ -9,5 +8,5 @@ val jobModule = module {
     single<WorkerFactory> { K9WorkerFactory(get(), get()) }
     single { get<WorkManagerProvider>().getWorkManager() }
     single { K9JobManager(get(), get(), get()) }
-    factory { MailSyncWorkerManager(get(), Clock.INSTANCE) }
+    factory { MailSyncWorkerManager(workManager = get(), clock = get()) }
 }
