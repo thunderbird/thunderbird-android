c9be254f0ae4ad10cffb624627e41607606b1df3
cketti <cketti@gmail.com>
2021-11-26 21:38:49 +0100

    Get rid of Range lint warnings

--- app/core/src/main/java/com/fsck/k9/helper/CursorExtensions.kt
+++ app/core/src/main/java/com/fsck/k9/helper/CursorExtensions.kt
@@ -23,3 +23,15 @@ fun Cursor.getLongOrNull(columnName: String): Long? {
     val columnIndex = getColumnIndex(columnName)
     return if (isNull(columnIndex)) null else getLong(columnIndex)
 }
+
+fun Cursor.getStringOrThrow(columnName: String): String {
+    return getStringOrNull(columnName) ?: error("Column $columnName must not be null")
+}
+
+fun Cursor.getIntOrThrow(columnName: String): Int {
+    return getIntOrNull(columnName) ?: error("Column $columnName must not be null")
+}
+
+fun Cursor.getLongOrThrow(columnName: String): Long {
+    return getLongOrNull(columnName) ?: error("Column $columnName must not be null")
+}
