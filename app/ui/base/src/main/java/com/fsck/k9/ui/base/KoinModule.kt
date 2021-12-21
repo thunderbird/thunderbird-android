056a206836fae6cb4f2026c3903e34cd7aadee85
Bastian Wilhelm <baswil@mailbox.org>
2021-12-21 12:05:39 +0100

    #5802 fix lint

--- app/ui/base/src/main/java/com/fsck/k9/ui/base/KoinModule.kt
+++ app/ui/base/src/main/java/com/fsck/k9/ui/base/KoinModule.kt
@@ -3,6 +3,6 @@ package com.fsck.k9.ui.base
 import org.koin.dsl.module
 
 val uiBaseModule = module {
-    single { ThemeManager(context = get(), themeProvider = get(), generalSettingsManager = get ()) }
+    single { ThemeManager(context = get(), themeProvider = get(), generalSettingsManager = get()) }
     single { AppLanguageManager() }
 }
