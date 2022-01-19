262f97812ff5d7e50104b068f0caaf00969038ba
cketti <cketti@gmail.com>
2022-01-20 00:28:01 +0100

    Remove JMAP app
    The JMAP backend is still part of the repository. Only currently unused.

--- app/k9mail-jmap/src/main/java/com/fsck/k9/MessagingListenerProvider.kt
+++
@@ -1,5 +0,0 @@
-package com.fsck.k9
-
-import com.fsck.k9.controller.MessagingListener
-
-class MessagingListenerProvider(val listeners: List<MessagingListener>)
--- app/k9mail-jmap/src/main/java/com/fsck/k9/ui/addaccount/KoinModule.kt
+++
@@ -1,8 +0,0 @@
-package com.fsck.k9.ui.addaccount
-
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val uiAddAccountModule = module {
-    viewModel { AddAccountViewModel(get(), get(), get()) }
-}
