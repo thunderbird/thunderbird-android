4c67bbd1564a1f37f7731f7b692ef2d70c78a97e
cketti <cketti@gmail.com>
2022-03-01 19:22:19 +0100

    Update kotlinx-coroutines to version 1.6.0

--- app/ui/legacy/src/main/java/com/fsck/k9/ui/FlowExtensions.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/ui/FlowExtensions.kt
@@ -5,13 +5,13 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.launch
 
-fun <T> Flow<T>.observe(lifecycleOwner: LifecycleOwner, action: suspend (T) -> Unit) {
+fun <T> Flow<T>.observe(lifecycleOwner: LifecycleOwner, collector: FlowCollector<T>) {
     lifecycleOwner.lifecycleScope.launch {
         lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-            collect(action)
+            collect(collector)
         }
     }
 }
