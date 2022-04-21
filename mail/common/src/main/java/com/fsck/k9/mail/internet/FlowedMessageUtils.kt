ef8d9abed393418c2549430a9bad376bb1c9b6b2
cketti <cketti@gmail.com>
2022-04-21 23:40:27 +0200

    Rewrite `FlowedMessageUtils.deflow()`
    This new version should use a lot less allocations.

--- mail/common/src/main/java/com/fsck/k9/mail/internet/FlowedMessageUtils.kt
+++ mail/common/src/main/java/com/fsck/k9/mail/internet/FlowedMessageUtils.kt
@@ -1,120 +1,83 @@
-package com.fsck.k9.mail.internet;
-
+package com.fsck.k9.mail.internet
 
 /**
- * Adapted from the Apache James project, see
- * https://james.apache.org/mailet/base/apidocs/org/apache/mailet/base/FlowedMessageUtils.html
- *
- * <p>Manages texts encoded as <code>text/plain; format=flowed</code>.</p>
- * <p>As a reference see:</p>
- * <ul>
- * <li><a href='http://www.rfc-editor.org/rfc/rfc2646.txt'>RFC2646</a></li>
- * <li><a href='http://www.rfc-editor.org/rfc/rfc3676.txt'>RFC3676</a> (new method with DelSP support).
- * </ul>
- * <h4>Note</h4>
- * <ul>
- * <li>In order to decode, the input text must belong to a mail with headers similar to:
- *   Content-Type: text/plain; charset="CHARSET"; [delsp="yes|no"; ]format="flowed"
- *   (the quotes around CHARSET are not mandatory).
- *   Furthermore the header Content-Transfer-Encoding MUST NOT BE Quoted-Printable
- *   (see RFC3676 paragraph 4.2).(In fact this happens often for non 7bit messages).
- * </li>
- * </ul>
+ * Decodes text encoded as `text/plain; format=flowed` (RFC 3676).
  */
-public final class FlowedMessageUtils {
-    private static final char RFC2646_SPACE = ' ';
-    private static final char RFC2646_QUOTE = '>';
-    private static final String RFC2646_SIGNATURE = "-- ";
-    private static final String RFC2646_CRLF = "\r\n";
+object FlowedMessageUtils {
+    private const val QUOTE = '>'
+    private const val SPACE = ' '
+    private const val CR = '\r'
+    private const val LF = '\n'
+    private const val SIGNATURE = "-- "
+    private const val CRLF = "\r\n"
 
-    private FlowedMessageUtils() {
-        // this class cannot be instantiated
-    }
+    @JvmStatic
+    fun deflow(text: String, delSp: Boolean): String {
+        var lineStartIndex = 0
+        var lastLineQuoteDepth = 0
+        var lastLineFlowed = false
 
-    /**
-     * Decodes a text previously wrapped using "format=flowed".
-     */
-    public static String deflow(String text, boolean delSp) {
-        String[] lines = text.split("\r\n|\n", -1);
-        StringBuffer result = null;
-        StringBuffer resultLine = new StringBuffer();
-        int resultLineQuoteDepth = 0;
-        boolean resultLineFlowed = false;
-        // One more cycle, to close the last line
-        for (int i = 0; i <= lines.length; i++) {
-            String line = i < lines.length ? lines[i] : null;
-            int actualQuoteDepth = 0;
+        return buildString {
+            while (lineStartIndex <= text.lastIndex) {
+                var quoteDepth = 0
+                while (lineStartIndex <= text.lastIndex && text[lineStartIndex] == QUOTE) {
+                    quoteDepth++
+                    lineStartIndex++
+                }
 
-            if (line != null) {
-                if (line.equals(RFC2646_SIGNATURE)) {
-                    // signature handling (the previous line is not flowed)
-                    resultLineFlowed = false;
-                } else if (line.length() > 0 && line.charAt(0) == RFC2646_QUOTE) {
-                    // Quote
-                    actualQuoteDepth = 1;
-                    while (actualQuoteDepth < line.length() && line.charAt(actualQuoteDepth) == RFC2646_QUOTE) {
-                        actualQuoteDepth++;
+                // Remove space stuffing
+                if (lineStartIndex <= text.lastIndex && text[lineStartIndex] == SPACE) {
+                    lineStartIndex++
+                }
+
+                // We support both LF and CRLF line endings. To cover both cases we search for LF.
+                val lineFeedIndex = text.indexOf(LF, lineStartIndex)
+                val lineBreakFound = lineFeedIndex != -1
+
+                var lineEndIndex = if (lineBreakFound) lineFeedIndex else text.length
+                if (lineEndIndex > 0 && text[lineEndIndex - 1] == CR) {
+                    lineEndIndex--
+                }
+
+                if (lastLineFlowed && quoteDepth != lastLineQuoteDepth) {
+                    append(CRLF)
+                    lastLineFlowed = false
+                }
+
+                val lineIsSignatureMarker = lineEndIndex - lineStartIndex == SIGNATURE.length &&
+                    text.regionMatches(lineStartIndex, SIGNATURE, 0, SIGNATURE.length)
+
+                var lineFlowed = false
+                if (lineIsSignatureMarker) {
+                    if (lastLineFlowed) {
+                        append(CRLF)
+                        lastLineFlowed = false
                     }
-                    // if quote-depth changes wrt the previous line then this is not flowed
-                    if (resultLineQuoteDepth != actualQuoteDepth) {
-                        resultLineFlowed = false;
-                    }
-                    line = line.substring(actualQuoteDepth);
-
-                } else {
-                    // if quote-depth changes wrt the first line then this is not flowed
-                    if (resultLineQuoteDepth > 0) {
-                        resultLineFlowed = false;
-                    }
-                }
-
-                if (line.length() > 0 && line.charAt(0) == RFC2646_SPACE) {
-                    // Line space-stuffed
-                    line = line.substring(1);
-                }
-
-                // if the previous was the last then it was not flowed
-            } else {
-                resultLineFlowed = false;
-            }
-
-            // Add the PREVIOUS line.
-            // This often will find the flow looking for a space as the last char of the line.
-            // With quote changes or signatures it could be the following line to void the flow.
-            if (!resultLineFlowed && i > 0) {
-                if (resultLineQuoteDepth > 0) {
-                    resultLine.insert(0, RFC2646_SPACE);
-                }
-                for (int j = 0; j < resultLineQuoteDepth; j++) {
-                    resultLine.insert(0, RFC2646_QUOTE);
-                }
-                if (result == null) {
-                    result = new StringBuffer();
-                } else {
-                    result.append(RFC2646_CRLF);
-                }
-                result.append(resultLine.toString());
-                resultLine = new StringBuffer();
-                resultLineFlowed = false;
-            }
-            resultLineQuoteDepth = actualQuoteDepth;
-
-            if (line != null) {
-                if (!line.equals(RFC2646_SIGNATURE) && line.endsWith("" + RFC2646_SPACE) && i < lines.length - 1) {
-                    // Line flowed (NOTE: for the split operation the line having i == lines.length is the last that
-                    // does not end with RFC2646_CRLF)
+                } else if (lineEndIndex > lineStartIndex && text[lineEndIndex - 1] == SPACE) {
+                    lineFlowed = true
                     if (delSp) {
-                        line = line.substring(0, line.length() - 1);
+                        lineEndIndex--
                     }
-                    resultLineFlowed = true;
-                } else {
-                    resultLineFlowed = false;
                 }
 
-                resultLine.append(line);
+                if (!lastLineFlowed && quoteDepth > 0) {
+                    // This is not a continuation line, so prefix the text with quote characters.
+                    repeat(quoteDepth) {
+                        append(QUOTE)
+                    }
+                    append(SPACE)
+                }
+
+                append(text, lineStartIndex, lineEndIndex)
+
+                if (!lineFlowed && lineBreakFound) {
+                    append(CRLF)
+                }
+
+                lineStartIndex = if (lineBreakFound) lineFeedIndex + 1 else text.length
+                lastLineQuoteDepth = quoteDepth
+                lastLineFlowed = lineFlowed
             }
         }
-
-        return result.toString();
     }
 }
