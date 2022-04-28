4b1dc23ebb1e517ca5d7f0cffcd4ee292eea156c
cketti <cketti@gmail.com>
2022-04-28 22:39:39 +0200

    Remove unused code from `OAuth2TokenProvider`

--- mail/common/src/main/java/com/fsck/k9/mail/oauth/OAuth2TokenProvider.java
+++ mail/common/src/main/java/com/fsck/k9/mail/oauth/OAuth2TokenProvider.java
@@ -3,8 +3,6 @@ package com.fsck.k9.mail.oauth;
 
 import java.util.List;
 
-import android.app.Activity;
-
 import com.fsck.k9.mail.AuthenticationFailedException;
 
 
@@ -21,18 +19,6 @@ public interface OAuth2TokenProvider {
     List<String> getAccounts();
 
     /**
-     * Request API authorization. This is a foreground action that may produce a dialog to interact with.
-     *
-     * @param username
-     *         Username
-     * @param activity
-     *         The responsible activity
-     * @param callback
-     *         A callback to process the asynchronous response
-     */
-    void authorizeApi(String username, Activity activity, OAuth2TokenProviderAuthCallback callback);
-
-    /**
      * Fetch a token. No guarantees are provided for validity.
      */
     String getToken(String username, long timeoutMillis) throws AuthenticationFailedException;
@@ -47,14 +33,4 @@ public interface OAuth2TokenProvider {
      * Invalidating a token and then failure with a new token should be treated as a permanent failure.
      */
     void invalidateToken(String username);
-
-
-    /**
-     * Provides an asynchronous response to an
-     * {@link OAuth2TokenProvider#authorizeApi(String, Activity, OAuth2TokenProviderAuthCallback)} request.
-     */
-    interface OAuth2TokenProviderAuthCallback {
-        void success();
-        void failure(AuthorizationException e);
-    }
 }
