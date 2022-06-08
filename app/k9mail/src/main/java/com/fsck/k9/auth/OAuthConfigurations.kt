6290e9b2a6d65003f4f22ca1e1d4356472bb46e6
cketti <cketti@gmail.com>
2022-06-08 20:22:13 +0200

    Add OAuth support for AOL accounts

--- app/k9mail/src/main/java/com/fsck/k9/auth/OAuthConfigurations.kt
+++ app/k9mail/src/main/java/com/fsck/k9/auth/OAuthConfigurations.kt
@@ -26,6 +26,13 @@ fun createOAuthConfigurationProvider(): OAuthConfigurationProvider {
                 tokenEndpoint = "https://api.login.yahoo.com/oauth2/get_token",
                 redirectUri = redirectUriDoubleSlash
             ),
+            listOf("imap.aol.com", "smtp.aol.com") to OAuthConfiguration(
+                clientId = BuildConfig.OAUTH_AOL_CLIENT_ID,
+                scopes = listOf("mail-w"),
+                authorizationEndpoint = "https://api.login.aol.com/oauth2/request_auth",
+                tokenEndpoint = "https://api.login.aol.com/oauth2/get_token",
+                redirectUri = redirectUriDoubleSlash
+            ),
         ),
         googleConfiguration = googleConfig
     )
