2991b4b83ebc1fc0718d9308d1992d67feb7c774
cketti <cketti@gmail.com>
2022-09-20 20:30:17 +0200

    Remove Iconics and FontAwesome

--- app/ui/legacy/src/main/java/com/fsck/k9/ui/account/AccountFallbackImageProvider.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/ui/account/AccountFallbackImageProvider.kt
@@ -2,24 +2,21 @@ package com.fsck.k9.ui.account
 
 import android.content.Context
 import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
 import com.fsck.k9.ui.R
-import com.mikepenz.iconics.IconicsDrawable
-import com.mikepenz.iconics.typeface.library.fontawesome.FontAwesome
-import com.mikepenz.iconics.utils.backgroundColorInt
-import com.mikepenz.iconics.utils.colorRes
-import com.mikepenz.iconics.utils.paddingDp
-import com.mikepenz.iconics.utils.sizeDp
 
 /**
  * Provides a [Drawable] for the account using the account's color as background color.
  */
 class AccountFallbackImageProvider(private val context: Context) {
     fun getDrawable(color: Int): Drawable {
-        return IconicsDrawable(context, FontAwesome.Icon.faw_user_alt).apply {
-            colorRes = R.color.material_drawer_profile_icon
-            backgroundColorInt = color
-            sizeDp = 56
-            paddingDp = 12
+        val drawable = ContextCompat.getDrawable(context, R.drawable.drawer_account_fallback)
+            ?: error("Error loading drawable")
+
+        return drawable.mutate().apply {
+            colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(color, BlendModeCompat.DST_OVER)
         }
     }
 }
