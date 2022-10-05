620f9e35c0b250a68b98445287491e39569f98d2
cketti <cketti@gmail.com>
2022-10-05 14:49:39 +0200

    Make background colors of the contact picture fallback part of the theme

--- app/ui/legacy/src/main/java/com/fsck/k9/contacts/ContactLetterBitmapCreator.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/contacts/ContactLetterBitmapCreator.kt
@@ -5,7 +5,6 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
 import com.fsck.k9.mail.Address
-import com.fsck.k9.ui.helper.MaterialColors
 
 /**
  * Draw a `Bitmap` containing the "contact letter" obtained by [ContactLetterExtractor].
@@ -48,54 +47,12 @@ class ContactLetterBitmapCreator(
         }
 
         val hash = address.hashCode()
-        if (config.useDarkTheme) {
-            val colorIndex = (hash and Integer.MAX_VALUE) % BACKGROUND_COLORS_DARK.size
-            return BACKGROUND_COLORS_DARK[colorIndex]
-        } else {
-            val colorIndex = (hash and Integer.MAX_VALUE) % BACKGROUND_COLORS_LIGHT.size
-            return BACKGROUND_COLORS_LIGHT[colorIndex]
-        }
+        val backgroundColors = config.backgroundColors
+        val colorIndex = (hash and Integer.MAX_VALUE) % backgroundColors.size
+        return backgroundColors[colorIndex]
     }
 
     fun signatureOf(address: Address): String {
         return calcUnknownContactColor(address).toString()
-    }
-
-    companion object {
-        private val BACKGROUND_COLORS_LIGHT = intArrayOf(
-            MaterialColors.RED_300,
-            MaterialColors.DEEP_PURPLE_300,
-            MaterialColors.LIGHT_BLUE_300,
-            MaterialColors.GREEN_300,
-            MaterialColors.DEEP_ORANGE_300,
-            MaterialColors.BLUE_GREY_300,
-            MaterialColors.PINK_300,
-            MaterialColors.INDIGO_300,
-            MaterialColors.CYAN_300,
-            MaterialColors.AMBER_400,
-            MaterialColors.BROWN_300,
-            MaterialColors.PURPLE_300,
-            MaterialColors.BLUE_300,
-            MaterialColors.TEAL_300,
-            MaterialColors.ORANGE_400
-        )
-
-        private val BACKGROUND_COLORS_DARK = intArrayOf(
-            MaterialColors.RED_600,
-            MaterialColors.DEEP_PURPLE_600,
-            MaterialColors.LIGHT_BLUE_600,
-            MaterialColors.GREEN_600,
-            MaterialColors.DEEP_ORANGE_600,
-            MaterialColors.BLUE_GREY_600,
-            MaterialColors.PINK_600,
-            MaterialColors.INDIGO_600,
-            MaterialColors.CYAN_600,
-            MaterialColors.AMBER_600,
-            MaterialColors.BROWN_600,
-            MaterialColors.PURPLE_600,
-            MaterialColors.BLUE_600,
-            MaterialColors.TEAL_600,
-            MaterialColors.ORANGE_600
-        )
     }
 }
