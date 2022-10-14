e75d15d7e640883ad55ace85b6b120000c575ede
cketti <cketti@gmail.com>
2022-10-14 20:15:04 +0200

    Don't use `MessageProvider` for message list widget

--- app/k9mail/src/main/java/com/fsck/k9/widget/list/KoinModule.kt
+++ app/k9mail/src/main/java/com/fsck/k9/widget/list/KoinModule.kt
@@ -3,5 +3,6 @@ package com.fsck.k9.widget.list
 import org.koin.dsl.module
 
 val messageListWidgetModule = module {
-    single { MessageListWidgetUpdateListener(get()) }
+    single { MessageListWidgetUpdateListener(context = get()) }
+    factory { MessageListLoader(preferences = get(), messageListRepository = get(), messageHelper = get()) }
 }
