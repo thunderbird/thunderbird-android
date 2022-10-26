ee608a6201d1283dbc9e641d9a6f3080903d2a75
cketti <cketti@thunderbird.net>
2022-10-26 19:51:39 +0200

    Change `ThunderbirdDiscovery` to support all specified autoconfig URLs

--- app/autodiscovery/thunderbird/src/main/java/com/fsck/k9/autodiscovery/thunderbird/ThunderbirdDiscovery.kt
+++ app/autodiscovery/thunderbird/src/main/java/com/fsck/k9/autodiscovery/thunderbird/ThunderbirdDiscovery.kt
@@ -4,16 +4,24 @@ import com.fsck.k9.autodiscovery.api.ConnectionSettingsDiscovery
 import com.fsck.k9.autodiscovery.api.DiscoveryResults
 
 class ThunderbirdDiscovery(
+    private val urlProvider: ThunderbirdAutoconfigUrlProvider,
     private val fetcher: ThunderbirdAutoconfigFetcher,
     private val parser: ThunderbirdAutoconfigParser
 ) : ConnectionSettingsDiscovery {
 
     override fun discover(email: String): DiscoveryResults? {
-        val autoconfigInputStream = fetcher.fetchAutoconfigFile(email) ?: return null
+        val autoconfigUrls = urlProvider.getAutoconfigUrls(email)
 
-        return autoconfigInputStream.use {
-            parser.parseSettings(it, email)
-        }
+        return autoconfigUrls
+            .asSequence()
+            .mapNotNull { autoconfigUrl ->
+                fetcher.fetchAutoconfigFile(autoconfigUrl)?.use { inputStream ->
+                    parser.parseSettings(inputStream, email)
+                }
+            }
+            .firstOrNull { result ->
+                result.incoming.isNotEmpty() || result.outgoing.isNotEmpty()
+            }
     }
 
     override fun toString(): String = "Thunderbird autoconfig"
---
+++ app/autodiscovery/thunderbird/src/test/java/com/fsck/k9/autodiscovery/thunderbird/ThunderbirdAutoconfigUrlProviderTest.kt
@@ -0,0 +1,20 @@
+package com.fsck.k9.autodiscovery.thunderbird
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class ThunderbirdAutoconfigUrlProviderTest {
+    private val urlProvider = ThunderbirdAutoconfigUrlProvider()
+
+    @Test
+    fun `getAutoconfigUrls with ASCII email address`() {
+        val autoconfigUrls = urlProvider.getAutoconfigUrls("test@domain.example")
+
+        assertThat(autoconfigUrls.map { it.toString() }).containsExactly(
+            "https://autoconfig.domain.example/mail/config-v1.1.xml?emailaddress=test%40domain.example",
+            "https://domain.example/.well-known/autoconfig/mail/config-v1.1.xml",
+            "http://domain.example/.well-known/autoconfig/mail/config-v1.1.xml",
+            "https://autoconfig.thunderbird.net/v1.1/domain.example"
+        )
+    }
+}
