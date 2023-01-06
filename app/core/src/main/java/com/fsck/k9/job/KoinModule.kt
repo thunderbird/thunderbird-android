fe9b6d48624cd4882582be9ff140a922c455de60
cketti <cketti@thunderbird.net>
2023-01-06 12:38:53 +0100

    Switch to a `WorkerFactory` that is using Koin to create workers

--- app/core/src/main/java/com/fsck/k9/job/KoinModule.kt
+++ app/core/src/main/java/com/fsck/k9/job/KoinModule.kt
@@ -3,12 +3,16 @@ package com.fsck.k9.job
 import android.content.Context
 import androidx.work.WorkManager
 import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
 import org.koin.dsl.module
 
 val jobModule = module {
     single { WorkManagerConfigurationProvider(workerFactory = get()) }
-    single<WorkerFactory> { K9WorkerFactory(get(), get()) }
+    single<WorkerFactory> { K9WorkerFactory() }
     single { WorkManager.getInstance(get<Context>()) }
-    single { K9JobManager(get(), get(), get()) }
+    single { K9JobManager(workManager = get(), preferences = get(), mailSyncWorkerManager = get()) }
     factory { MailSyncWorkerManager(workManager = get(), clock = get()) }
+    factory { (parameters: WorkerParameters) ->
+        MailSyncWorker(messagingController = get(), preferences = get(), context = get(), parameters)
+    }
 }
