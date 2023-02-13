2967e933e42be68eeea7eb748100922cc2acc396
Wolf Montwé <wolf@thunderbird.net>
2023-02-13 17:18:33 +0100

    Change Spotless configuration to build-plugin

--- build-plugin/build.gradle.kts
+++ build-plugin/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 
 dependencies {
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+
+    implementation(plugin(libs.plugins.spotless))
 }
 
 fun plugin(provider: Provider<PluginDependency>) = with(provider.get()) {
