948f11378b66bdc6eeae198c48f04518529f0281
Wolf Montwé <wolf@thunderbird.net>
2023-02-15 18:30:31 +0100

    Add default Android app build-plugin

--- build-plugin/build.gradle.kts
+++ build-plugin/build.gradle.kts
@@ -8,6 +8,10 @@ dependencies {
     implementation(plugin(libs.plugins.kotlin.jvm))
 
     implementation(plugin(libs.plugins.spotless))
+
+    implementation(plugin(libs.plugins.kotlin.android))
+
+    implementation(plugin(libs.plugins.android.application))
 }
 
 fun plugin(provider: Provider<PluginDependency>) = with(provider.get()) {
--- build-plugin/src/main/kotlin/ThunderbirdProjectConfig.kt
+++ build-plugin/src/main/kotlin/ThunderbirdProjectConfig.kt
@@ -3,4 +3,8 @@ import org.gradle.api.JavaVersion
 object ThunderbirdProjectConfig {
 
     val javaVersion = JavaVersion.VERSION_11
+
+    const val androidSdkMin = 21
+    const val androidSdkTarget = 31
+    const val androidSdkCompile = 33
 }
