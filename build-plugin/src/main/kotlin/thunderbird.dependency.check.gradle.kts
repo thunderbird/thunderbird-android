0ace871606f7c2deee34bf785975620c28db065a
Wolf Montwé <wolf@thunderbird.net>
2023-02-24 11:01:41 +0100

    Add dependency check build plugin

---
+++ build-plugin/src/main/kotlin/thunderbird.dependency.check.gradle.kts
@@ -0,0 +1,18 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("com.github.ben-manes.versions")
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[\\d,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
