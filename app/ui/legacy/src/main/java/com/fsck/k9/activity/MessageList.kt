61e50a92387bd2c40b1ff80e38d4ccad604fccd3
cketti <cketti@thunderbird.net>
2023-03-03 17:26:18 +0100

    Fix status bar color on pre-API 23 devices
    In the message list/view screens we now use a semi-transparent status bar; in all other screens a black status bar (because that doesn't require any additional code or layout changes). We do this because these devices don't support displaying dark text on a light status bar.

--- app/ui/legacy/src/main/java/com/fsck/k9/activity/MessageList.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/activity/MessageList.kt
@@ -6,12 +6,14 @@ import android.content.Intent
 import android.content.IntentSender
 import android.content.res.Configuration
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.WindowManager
 import android.view.animation.AnimationUtils
 import android.widget.ProgressBar
 import androidx.appcompat.app.ActionBar
@@ -177,6 +179,10 @@ open class MessageList :
         val rootLayout = findViewById<View>(R.id.drawerLayout)
         rootLayout.systemUiVisibility = rootLayout.systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         toolbar.setOnApplyWindowInsetsListener { view, insets ->
