6a89a030820860570988be60bc865b4555dc4826
Jeroen1602 <1492076+jeroen1602@users.noreply.github.com>
2023-03-13 15:27:44 +0100

    Fixed ThunderbirdAutoconfigFetcher throwing on IOExceptions
    If the requested url does not exist then the `fetchAutoconfigFile` would just crash instead of returning `null`. This would mean that the discovery code would also crash instead of trying other URLs.

--- app/autodiscovery/thunderbird/src/main/java/com/fsck/k9/autodiscovery/thunderbird/ThunderbirdAutoconfigFetcher.kt
+++ app/autodiscovery/thunderbird/src/main/java/com/fsck/k9/autodiscovery/thunderbird/ThunderbirdAutoconfigFetcher.kt
@@ -1,5 +1,7 @@
 package com.fsck.k9.autodiscovery.thunderbird
 
+import com.fsck.k9.logging.Timber
+import java.io.IOException
 import java.io.InputStream
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
@@ -8,13 +10,18 @@ import okhttp3.Request
 class ThunderbirdAutoconfigFetcher(private val okHttpClient: OkHttpClient) {
 
     fun fetchAutoconfigFile(url: HttpUrl): InputStream? {
-        val request = Request.Builder().url(url).build()
+        return try {
+            val request = Request.Builder().url(url).build()
 
-        val response = okHttpClient.newCall(request).execute()
+            val response = okHttpClient.newCall(request).execute()
 
-        return if (response.isSuccessful) {
-            response.body?.byteStream()
-        } else {
+            if (response.isSuccessful) {
+                response.body?.byteStream()
+            } else {
+                null
+            }
+        } catch (e: IOException) {
+            Timber.d(e, "Error fetching URL: %s", url)
             null
         }
     }
