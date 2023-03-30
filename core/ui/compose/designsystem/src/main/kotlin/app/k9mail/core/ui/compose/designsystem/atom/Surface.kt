a3b192fca586a9bf6c9948a4662467118f418ad5
Wolf Montwé <wolf@thunderbird.net>
2023-03-30 11:15:43 +0200

    Add ResponsiveContent to design system

--- core/ui/compose/designsystem/src/main/kotlin/app/k9mail/core/ui/compose/designsystem/atom/Surface.kt
+++ core/ui/compose/designsystem/src/main/kotlin/app/k9mail/core/ui/compose/designsystem/atom/Surface.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import app.k9mail.core.ui.compose.theme.MainTheme
 import app.k9mail.core.ui.compose.theme.PreviewWithThemes
 import androidx.compose.material.Surface as MaterialSurface
@@ -13,11 +14,13 @@ import androidx.compose.material.Surface as MaterialSurface
 fun Surface(
     modifier: Modifier = Modifier,
     color: Color = MainTheme.colors.surface,
+    elevation: Dp = MainTheme.elevations.default,
     content: @Composable () -> Unit,
 ) {
     MaterialSurface(
         modifier = modifier,
         content = content,
+        elevation = elevation,
         color = color,
     )
 }
