8fc01ce4861854d623c1efee04aa2e8527c25200
cketti <cketti@thunderbird.net>
2023-04-24 15:50:06 +0200

    Prepare for version 6.701

--- app/k9mail/build.gradle.kts
+++ app/k9mail/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         testApplicationId = "com.fsck.k9.tests"
 
         versionCode = 37000
-        versionName = "6.700"
+        versionName = "6.701-SNAPSHOT"
 
         // Keep in sync with the resource string array "supported_languages"
         resourceConfigurations.addAll(
