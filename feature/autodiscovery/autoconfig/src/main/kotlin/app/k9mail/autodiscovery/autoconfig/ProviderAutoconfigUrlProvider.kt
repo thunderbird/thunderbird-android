630b606f72395a3e06395b14105366ffd8c868fe
cketti <cketti@thunderbird.net>
2023-04-27 17:20:53 +0200

    Add config options to `ProviderAutoconfigUrlProvider`

--- feature/autodiscovery/autoconfig/src/main/kotlin/app/k9mail/autodiscovery/autoconfig/ProviderAutoconfigUrlProvider.kt
+++ feature/autodiscovery/autoconfig/src/main/kotlin/app/k9mail/autodiscovery/autoconfig/ProviderAutoconfigUrlProvider.kt
@@ -3,18 +3,20 @@ package app.k9mail.autodiscovery.autoconfig
 import com.fsck.k9.helper.EmailHelper
 import okhttp3.HttpUrl
 
-class ProviderAutoconfigUrlProvider : AutoconfigUrlProvider {
+class ProviderAutoconfigUrlProvider(private val config: AutoconfigUrlConfig) : AutoconfigUrlProvider {
     override fun getAutoconfigUrls(email: String): List<HttpUrl> {
         val domain = EmailHelper.getDomainFromEmailAddress(email)
         requireNotNull(domain) { "Couldn't extract domain from email address: $email" }
 
-        return listOf(
-            createProviderUrl(domain, email, useHttps = true),
-            createDomainUrl(domain, email, useHttps = true),
+        return buildList {
+            add(createProviderUrl(domain, email, useHttps = true))
+            add(createDomainUrl(domain, email, useHttps = true))
 
-            createProviderUrl(domain, email, useHttps = false),
-            createDomainUrl(domain, email, useHttps = false),
-        )
+            if (!config.httpsOnly) {
+                add(createProviderUrl(domain, email, useHttps = false))
+                add(createDomainUrl(domain, email, useHttps = false))
+            }
+        }
     }
 
     private fun createProviderUrl(domain: String, email: String, useHttps: Boolean): HttpUrl {
@@ -24,7 +26,11 @@ class ProviderAutoconfigUrlProvider : AutoconfigUrlProvider {
             .scheme(if (useHttps) "https" else "http")
             .host("autoconfig.$domain")
             .addEncodedPathSegments("mail/config-v1.1.xml")
-            .addQueryParameter("emailaddress", email)
+            .apply {
+                if (config.includeEmailAddress) {
+                    addQueryParameter("emailaddress", email)
+                }
+            }
             .build()
     }
 
@@ -35,7 +41,16 @@ class ProviderAutoconfigUrlProvider : AutoconfigUrlProvider {
             .scheme(if (useHttps) "https" else "http")
             .host(domain)
             .addEncodedPathSegments(".well-known/autoconfig/mail/config-v1.1.xml")
-            .addQueryParameter("emailaddress", email)
+            .apply {
+                if (config.includeEmailAddress) {
+                    addQueryParameter("emailaddress", email)
+                }
+            }
             .build()
     }
 }
+
+data class AutoconfigUrlConfig(
+    val httpsOnly: Boolean,
+    val includeEmailAddress: Boolean,
+)
--- feature/autodiscovery/autoconfig/src/test/kotlin/app/k9mail/autodiscovery/autoconfig/ProviderAutoconfigUrlProviderTest.kt
+++ feature/autodiscovery/autoconfig/src/test/kotlin/app/k9mail/autodiscovery/autoconfig/ProviderAutoconfigUrlProviderTest.kt
@@ -5,10 +5,11 @@ import assertk.assertions.containsExactly
 import org.junit.Test
 
 class ProviderAutoconfigUrlProviderTest {
-    private val urlProvider = ProviderAutoconfigUrlProvider()
-
     @Test
-    fun `getAutoconfigUrls with ASCII email address`() {
+    fun `getAutoconfigUrls with http allowed and email address included`() {
+        val urlProvider = ProviderAutoconfigUrlProvider(
+            AutoconfigUrlConfig(httpsOnly = false, includeEmailAddress = true),
+        )
         val autoconfigUrls = urlProvider.getAutoconfigUrls("test@domain.example")
 
         assertThat(autoconfigUrls.map { it.toString() }).containsExactly(
@@ -16,6 +17,47 @@ class ProviderAutoconfigUrlProviderTest {
             "https://domain.example/.well-known/autoconfig/mail/config-v1.1.xml?emailaddress=test%40domain.example",
             "http://autoconfig.domain.example/mail/config-v1.1.xml?emailaddress=test%40domain.example",
             "http://domain.example/.well-known/autoconfig/mail/config-v1.1.xml?emailaddress=test%40domain.example",
+        )
+    }
+
+    @Test
+    fun `getAutoconfigUrls with only https and email address included`() {
+        val urlProvider = ProviderAutoconfigUrlProvider(
+            AutoconfigUrlConfig(httpsOnly = true, includeEmailAddress = true),
+        )
+        val autoconfigUrls = urlProvider.getAutoconfigUrls("test@domain.example")
+
+        assertThat(autoconfigUrls.map { it.toString() }).containsExactly(
+            "https://autoconfig.domain.example/mail/config-v1.1.xml?emailaddress=test%40domain.example",
+            "https://domain.example/.well-known/autoconfig/mail/config-v1.1.xml?emailaddress=test%40domain.example",
+        )
+    }
+
+    @Test
+    fun `getAutoconfigUrls with only https and email address not included`() {
+        val urlProvider = ProviderAutoconfigUrlProvider(
+            AutoconfigUrlConfig(httpsOnly = true, includeEmailAddress = false),
+        )
+        val autoconfigUrls = urlProvider.getAutoconfigUrls("test@domain.example")
+
+        assertThat(autoconfigUrls.map { it.toString() }).containsExactly(
+            "https://autoconfig.domain.example/mail/config-v1.1.xml",
+            "https://domain.example/.well-known/autoconfig/mail/config-v1.1.xml",
+        )
+    }
+
+    @Test
+    fun `getAutoconfigUrls with http allowed and email address not included`() {
+        val urlProvider = ProviderAutoconfigUrlProvider(
+            AutoconfigUrlConfig(httpsOnly = false, includeEmailAddress = false),
+        )
+        val autoconfigUrls = urlProvider.getAutoconfigUrls("test@domain.example")
+
+        assertThat(autoconfigUrls.map { it.toString() }).containsExactly(
+            "https://autoconfig.domain.example/mail/config-v1.1.xml",
+            "https://domain.example/.well-known/autoconfig/mail/config-v1.1.xml",
+            "http://autoconfig.domain.example/mail/config-v1.1.xml",
+            "http://domain.example/.well-known/autoconfig/mail/config-v1.1.xml",
         )
     }
 }
