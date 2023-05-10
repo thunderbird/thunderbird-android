2756ceb6945652bfc1c90aedb6d80dd9a71cc30e
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-05-10 16:07:54 +0200

    Add LoadingView molecule to design system

--- app-ui-catalog/src/main/java/app/k9mail/ui/catalog/items/MoleculeItems.kt
+++ app-ui-catalog/src/main/java/app/k9mail/ui/catalog/items/MoleculeItems.kt
@@ -1,21 +1,39 @@
 package app.k9mail.ui.catalog.items
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import app.k9mail.core.ui.compose.designsystem.atom.text.TextSubtitle1
 import app.k9mail.core.ui.compose.designsystem.molecule.ErrorView
+import app.k9mail.core.ui.compose.designsystem.molecule.LoadingView
+import app.k9mail.core.ui.compose.theme.MainTheme
 
 fun LazyGridScope.moleculeItems() {
     sectionHeaderItem(text = "Molecules")
     item {
-        MoleculeWrapper {
+        MoleculeWrapper(title = "ErrorView") {
             ErrorView(
                 title = "Error",
                 message = "Something went wrong",
+            )
+        }
+    }
+
+    item {
+        MoleculeWrapper(title = "LoadingView") {
+            LoadingView()
+        }
+    }
+    item {
+        MoleculeWrapper(title = "LoadingView with message") {
+            LoadingView(
+                message = "Loading...",
             )
         }
     }
@@ -23,11 +41,17 @@ fun LazyGridScope.moleculeItems() {
 
 @Composable
 private fun MoleculeWrapper(
+    title: String,
     content: @Composable () -> Unit,
 ) {
-    Box(
-        modifier = Modifier.border(1.dp, Color.Gray),
+    Column(
+        verticalArrangement = Arrangement.spacedBy(MainTheme.spacings.default),
     ) {
-        content()
+        TextSubtitle1(text = title)
+        Box(
+            modifier = Modifier.border(1.dp, Color.Gray),
+        ) {
+            content()
+        }
     }
 }
---
+++ core/ui/compose/designsystem/src/main/kotlin/app/k9mail/core/ui/compose/designsystem/molecule/LoadingView.kt
@@ -0,0 +1,57 @@
+package app.k9mail.core.ui.compose.designsystem.molecule
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.k9mail.core.ui.compose.designsystem.atom.CircularProgressIndicator
+import app.k9mail.core.ui.compose.designsystem.atom.text.TextSubtitle1
+import app.k9mail.core.ui.compose.theme.MainTheme
+import app.k9mail.core.ui.compose.theme.PreviewWithThemes
+
+@Composable
+fun LoadingView(
+    modifier: Modifier = Modifier,
+    message: String? = null,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(MainTheme.spacings.default)
+            .then(modifier),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        if (message != null) {
+            TextSubtitle1(text = message)
+        }
+        Row(
+            modifier = Modifier.height(MainTheme.sizes.larger),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            CircularProgressIndicator()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+internal fun LoadingViewPreview() {
+    PreviewWithThemes {
+        LoadingView()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+internal fun LoadingViewWithMessagePreview() {
+    PreviewWithThemes {
+        LoadingView(
+            message = "Loading ...",
+        )
+    }
+}
