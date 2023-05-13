a266e0a6fc96d6ba959e8bb029384e92d7315347
cketti <cketti@thunderbird.net>
2023-05-13 23:10:16 +0200

    Convert `FixedLengthInputStream` to Kotlin

--- mail/common/src/main/java/com/fsck/k9/mail/filter/FixedLengthInputStream.kt
+++ mail/common/src/main/java/com/fsck/k9/mail/filter/FixedLengthInputStream.kt
@@ -1,78 +1,81 @@
+package com.fsck.k9.mail.filter
 
-package com.fsck.k9.mail.filter;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Locale;
+import java.io.IOException
+import java.io.InputStream
+import java.util.Locale
 
 /**
  * A filtering InputStream that stops allowing reads after the given length has been read. This
  * is used to allow a client to read directly from an underlying protocol stream without reading
  * past where the protocol handler intended the client to read.
  */
-public class FixedLengthInputStream extends InputStream {
-    private final InputStream mIn;
-    private final int mLength;
-    private int mCount = 0;
+class FixedLengthInputStream(
+    private val inputStream: InputStream,
+    private val length: Int,
+) : InputStream() {
+    private var numberOfBytesRead = 0
 
-    public FixedLengthInputStream(InputStream in, int length) {
-        this.mIn = in;
-        this.mLength = length;
+    // TODO: Call available() on underlying InputStream if remainingBytes() > 0
+    @Throws(IOException::class)
+    override fun available(): Int {
+        return remainingBytes()
     }
 
-    @Override
-    public int available() throws IOException {
-        return mLength - mCount;
-    }
-
-    @Override
-    public int read() throws IOException {
-        if (mCount >= mLength) {
-            return -1;
+    @Throws(IOException::class)
+    override fun read(): Int {
+        if (remainingBytes() == 0) {
+            return -1
         }
 
-        int d = mIn.read();
-        if (d != -1) {
-            mCount++;
-        }
-        return d;
-    }
-
-    @Override
-    public int read(byte[] b, int offset, int length) throws IOException {
-        if (mCount >= mLength) {
-            return -1;
+        val byte = inputStream.read()
+        if (byte != -1) {
+            numberOfBytesRead++
         }
 
-        int d = mIn.read(b, offset, Math.min(mLength - mCount, length));
-        if (d != -1) {
-            mCount += d;
+        return byte
+    }
+
+    @Throws(IOException::class)
+    override fun read(b: ByteArray, offset: Int, length: Int): Int {
+        if (remainingBytes() == 0) {
+            return -1
         }
-        return d;
-    }
 
-    @Override
-    public int read(byte[] b) throws IOException {
-        return read(b, 0, b.length);
-    }
-
-    @Override
-    public long skip(long n) throws IOException {
-        long d = mIn.skip(Math.min(n, available()));
-        if (d > 0) {
-            mCount += d;
+        val byte = inputStream.read(b, offset, length.coerceAtMost(remainingBytes()))
+        if (byte != -1) {
+            numberOfBytesRead += byte
         }
-        return d;
+
+        return byte
     }
 
-    @Override
-    public String toString() {
-        return String.format(Locale.US, "FixedLengthInputStream(in=%s, length=%d)", mIn.toString(), mLength);
+    @Throws(IOException::class)
+    override fun read(b: ByteArray): Int {
+        return read(b, 0, b.size)
     }
 
-    public void skipRemaining() throws IOException {
-        while (available() > 0) {
-            skip(available());
+    @Throws(IOException::class)
+    override fun skip(n: Long): Long {
+        val numberOfSkippedBytes = inputStream.skip(n.coerceAtMost(remainingBytes().toLong()))
+        if (numberOfSkippedBytes > 0) {
+            numberOfBytesRead += numberOfSkippedBytes.toInt()
         }
+
+        return numberOfSkippedBytes
+    }
+
+    @Throws(IOException::class)
+    fun skipRemaining() {
+        while (remainingBytes() > 0) {
+            skip(remainingBytes().toLong())
+        }
+    }
+
+    private fun remainingBytes(): Int {
+        return length - numberOfBytesRead
+    }
+
+    override fun toString(): String {
+        return String.format(Locale.ROOT, "FixedLengthInputStream(in=%s, length=%d)", inputStream.toString(), length)
     }
 }
