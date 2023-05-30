af60c73d62470b14d784ebf67756809a4e72e083
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-05-30 14:47:27 +0200

    Add EmailSettings types

---
+++ feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/domain/entity/EmailCheckFrequency.kt
@@ -0,0 +1,29 @@
+package app.k9mail.feature.account.setup.domain.entity
+
+import kotlinx.collections.immutable.toImmutableList
+
+@Suppress("MagicNumber")
+enum class EmailCheckFrequency(
+    val minutes: Int,
+) {
+    MANUAL(-1),
+    EVERY_15_MINUTES(15),
+    EVERY_30_MINUTES(30),
+    EVERY_HOUR(1.fromHour()),
+    EVERY_2_HOURS(2.fromHour()),
+    EVERY_3_HOURS(3.fromHour()),
+    EVERY_6_HOURS(6.fromHour()),
+    EVERY_12_HOURS(12.fromHour()),
+    EVERY_24_HOURS(24.fromHour()),
+    ;
+
+    companion object {
+        val DEFAULT = EVERY_HOUR
+        fun all() = values().toList().toImmutableList()
+    }
+}
+
+@Suppress("MagicNumber")
+private fun Int.fromHour(): Int {
+    return 60 * this
+}
---
+++ feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/domain/entity/EmailDisplayCount.kt
@@ -0,0 +1,22 @@
+package app.k9mail.feature.account.setup.domain.entity
+
+import kotlinx.collections.immutable.toImmutableList
+
+@Suppress("MagicNumber")
+enum class EmailDisplayCount(
+    val count: Int,
+) {
+    MESSAGES_10(10),
+    MESSAGES_25(25),
+    MESSAGES_50(50),
+    MESSAGES_100(100),
+    MESSAGES_250(250),
+    MESSAGES_500(500),
+    MESSAGES_1000(1000),
+    ;
+
+    companion object {
+        val DEFAULT = MESSAGES_25
+        fun all() = values().toList().toImmutableList()
+    }
+}
