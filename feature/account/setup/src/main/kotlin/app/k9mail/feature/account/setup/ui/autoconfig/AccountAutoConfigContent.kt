54d082c919f68c11554bef61c017eadd323bdacf
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-06-07 11:00:29 +0200

    Add loading and error views with state handling

--- feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/ui/autoconfig/AccountAutoConfigContent.kt
+++ feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/ui/autoconfig/AccountAutoConfigContent.kt
@@ -10,15 +10,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import app.k9mail.core.ui.compose.common.DevicePreviews
 import app.k9mail.core.ui.compose.designsystem.template.ResponsiveWidthContainer
 import app.k9mail.core.ui.compose.theme.K9Theme
 import app.k9mail.core.ui.compose.theme.MainTheme
 import app.k9mail.core.ui.compose.theme.ThunderbirdTheme
+import app.k9mail.feature.account.setup.R
 import app.k9mail.feature.account.setup.ui.autoconfig.AccountAutoConfigContract.Event
 import app.k9mail.feature.account.setup.ui.autoconfig.AccountAutoConfigContract.State
 import app.k9mail.feature.account.setup.ui.autoconfig.item.contentItems
+import app.k9mail.feature.account.setup.ui.common.item.ErrorItem
+import app.k9mail.feature.account.setup.ui.common.item.LoadingItem
 
 @Composable
 internal fun AccountAutoConfigContent(
@@ -34,16 +39,33 @@ internal fun AccountAutoConfigContent(
             .fillMaxWidth()
             .then(modifier),
     ) {
+        val resources = LocalContext.current.resources
+
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .imePadding(),
             verticalArrangement = Arrangement.spacedBy(MainTheme.spacings.double, Alignment.CenterVertically),
         ) {
-            contentItems(
-                state = state,
-                onEvent = onEvent,
-            )
+            if (state.isLoading) {
+                item(key = "loading") {
+                    LoadingItem(
+                        message = stringResource(id = R.string.account_setup_auto_config_loading_message),
+                    )
+                }
+            } else if (state.error != null) {
+                item(key = "error") {
+                    ErrorItem(
+                        title = stringResource(id = R.string.account_setup_auto_config_loading_error),
+                        message = state.error.toResourceString(resources)
+                    )
+                }
+            } else {
+                contentItems(
+                    state = state,
+                    onEvent = onEvent,
+                )
+            }
         }
     }
 }
