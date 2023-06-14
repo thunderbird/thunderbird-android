8aadfe4baeaae1b9b293626cffd3a67763e070c9
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-06-14 11:00:40 +0200

    Cleanup AutoDiscovery entities

--- feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/domain/entity/AutoDiscovery.kt
+++
@@ -1,7 +0,0 @@
-package app.k9mail.feature.account.setup.domain.entity
-
-data class AutoDiscovery(
-    val incomingServerSetting: IncomingServerSetting,
-    val outgoingServerSetting: OutgoingServerSetting,
-    val isTrusted: Boolean,
-)
