fde82fe7b458626a2ce97a45abbf2575b28446df
cketti <cketti@thunderbird.net>
2023-06-17 13:12:50 +0200

    Convert `PeekableInputStream` to Kotlin

--- mail/common/src/main/java/com/fsck/k9/mail/filter/PeekableInputStream.kt
+++ mail/common/src/main/java/com/fsck/k9/mail/filter/PeekableInputStream.kt
@@ -1,68 +1,68 @@
-package com.fsck.k9.mail.filter;
+package com.fsck.k9.mail.filter
 
-
-import java.io.FilterInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Locale;
-
+import java.io.FilterInputStream
+import java.io.IOException
+import java.io.InputStream
+import java.util.Locale
 
 /**
- * A filtering InputStream that allows single byte "peeks" without consuming the byte. The
- * client of this stream can call peek() to see the next available byte in the stream
- * and a subsequent read will still return the peeked byte.
+ * A filtering InputStream that allows single byte "peeks" without consuming the byte.
+ *
+ * The client of this stream can call `peek()` to see the next available byte in the stream and a subsequent read will
+ * still return the peeked byte.
  */
-public class PeekableInputStream extends FilterInputStream {
-    private boolean peeked;
-    private int peekedByte;
+class PeekableInputStream(private val inputStream: InputStream) : FilterInputStream(inputStream) {
+    private var peeked = false
+    private var peekedByte = 0
 
-
-    public PeekableInputStream(InputStream in) {
-        super(in);
-    }
-
-    @Override
-    public int read() throws IOException {
-        if (!peeked) {
-            return in.read();
+    @Throws(IOException::class)
+    override fun read(): Int {
+        return if (!peeked) {
+            inputStream.read()
         } else {
-            peeked = false;
-            return peekedByte;
+            peeked = false
+            peekedByte
         }
     }
 
-    public int peek() throws IOException {
+    @Throws(IOException::class)
+    fun peek(): Int {
         if (!peeked) {
-            peekedByte = in.read();
-            peeked = true;
+            peekedByte = inputStream.read()
+            peeked = true
         }
-        return peekedByte;
+
+        return peekedByte
     }
 
-    @Override
-    public int read(byte[] buffer, int offset, int length) throws IOException {
-        if (!peeked) {
-            return in.read(buffer, offset, length);
+    @Throws(IOException::class)
+    override fun read(buffer: ByteArray, offset: Int, length: Int): Int {
+        return if (!peeked) {
+            inputStream.read(buffer, offset, length)
         } else {
-            buffer[offset] = (byte) peekedByte;
-            peeked = false;
-            int r = in.read(buffer, offset + 1, length - 1);
-            if (r == -1) {
-                return 1;
+            buffer[offset] = peekedByte.toByte()
+            peeked = false
+
+            val numberOfBytesRead = inputStream.read(buffer, offset + 1, length - 1)
+            if (numberOfBytesRead == -1) {
+                1
             } else {
-                return r + 1;
+                numberOfBytesRead + 1
             }
         }
     }
 
-    @Override
-    public int read(byte[] buffer) throws IOException {
-        return read(buffer, 0, buffer.length);
+    @Throws(IOException::class)
+    override fun read(buffer: ByteArray): Int {
+        return read(buffer, 0, buffer.size)
     }
 
-    @Override
-    public String toString() {
-        return String.format(Locale.US, "PeekableInputStream(in=%s, peeked=%b, peekedByte=%d)",
-                in.toString(), peeked, peekedByte);
+    override fun toString(): String {
+        return "PeekableInputStream(in=%s, peeked=%b, peekedByte=%d)".format(
+            Locale.ROOT,
+            inputStream.toString(),
+            peeked,
+            peekedByte,
+        )
     }
 }
