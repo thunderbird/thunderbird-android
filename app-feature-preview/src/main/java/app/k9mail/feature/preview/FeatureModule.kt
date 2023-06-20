8c6c24cfe0ff51ddef14fafb3e5fdc20d9fa3dc6
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-06-20 13:20:17 +0200

    Add OAuthProviderSettings to feature app

--- app-feature-preview/src/main/java/app/k9mail/feature/preview/FeatureModule.kt
+++ app-feature-preview/src/main/java/app/k9mail/feature/preview/FeatureModule.kt
@@ -1,5 +1,7 @@
 package app.k9mail.feature.preview
 
+import app.k9mail.core.common.oauth.OAuthProvider
+import app.k9mail.core.common.oauth.OAuthProviderSettings
 import app.k9mail.feature.account.setup.featureAccountSetupModule
 import okhttp3.OkHttpClient
 import org.koin.core.module.Module
@@ -10,6 +12,21 @@ val featureModule: Module = module {
     // TODO move to network module
     single<OkHttpClient> {
         OkHttpClient()
+    }
+
+    single<OAuthProviderSettings> {
+        OAuthProviderSettings(
+            applicationId = "com.fsck.k9.debug",
+            clientIds = mapOf(
+                OAuthProvider.AOL to BuildConfig.OAUTH_AOL_CLIENT_ID,
+                OAuthProvider.GMAIL to BuildConfig.OAUTH_GMAIL_CLIENT_ID,
+                OAuthProvider.MICROSOFT to BuildConfig.OAUTH_MICROSOFT_CLIENT_ID,
+                OAuthProvider.YAHOO to BuildConfig.OAUTH_YAHOO_CLIENT_ID,
+            ),
+            redirectUriIds = mapOf(
+                OAuthProvider.MICROSOFT to BuildConfig.OAUTH_MICROSOFT_REDIRECT_URI_ID,
+            ),
+        )
     }
 
     includes(featureAccountSetupModule)
