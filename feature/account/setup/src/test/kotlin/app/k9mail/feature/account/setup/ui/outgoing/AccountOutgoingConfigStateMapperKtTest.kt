613c2a3b6b80af155ecef32d5f488a5e0145a79e
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-06-27 12:26:03 +0200

    Add AccountOutgoingConfigStateMapper that maps to server settings

--- feature/account/setup/src/test/kotlin/app/k9mail/feature/account/setup/ui/outgoing/AccountOutgoingConfigStateMapperKtTest.kt
+++ feature/account/setup/src/test/kotlin/app/k9mail/feature/account/setup/ui/outgoing/AccountOutgoingConfigStateMapperKtTest.kt
@@ -1,41 +1,38 @@
-package app.k9mail.feature.account.setup.ui.incoming
+package app.k9mail.feature.account.setup.ui.outgoing
 
 import app.k9mail.feature.account.setup.domain.entity.ConnectionSecurity
-import app.k9mail.feature.account.setup.domain.entity.IncomingProtocolType
 import app.k9mail.feature.account.setup.domain.entity.MailConnectionSecurity
 import app.k9mail.feature.account.setup.domain.input.NumberInputField
 import app.k9mail.feature.account.setup.domain.input.StringInputField
-import app.k9mail.feature.account.setup.ui.incoming.AccountIncomingConfigContract.State
+import app.k9mail.feature.account.setup.ui.outgoing.AccountOutgoingConfigContract.State
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.fsck.k9.mail.AuthType
 import com.fsck.k9.mail.ServerSettings
 import org.junit.Test
 
-class AccountIncomingConfigStateMapperKtTest {
+class AccountOutgoingConfigStateMapperKtTest {
 
     @Test
     fun `should map to server settings`() {
-        val incomingState = State(
-            protocolType = IncomingProtocolType.IMAP,
-            server = StringInputField(value = "imap.example.org"),
-            port = NumberInputField(value = 993),
+        val outgoingState = State(
+            server = StringInputField(value = "smtp.example.org"),
+            port = NumberInputField(value = 587),
             security = ConnectionSecurity.TLS,
             username = StringInputField(value = "user"),
             password = StringInputField(value = "password"),
             clientCertificate = "",
             imapAutodetectNamespaceEnabled = true,
-            imapPrefix = StringInputField(value = "prefix"),
             useCompression = true,
         )
 
-        val result = incomingState.toServerSettings()
+        val result = outgoingState.toServerSettings()
 
         assertThat(result).isEqualTo(
             ServerSettings(
-                type = "imap",
-                host = "imap.example.org",
-                port = 993,
+                type = "smtp",
+                host = "smtp.example.org",
+                port = 587,
                 connectionSecurity = MailConnectionSecurity.SSL_TLS_REQUIRED,
                 authenticationType = AuthType.PLAIN,
                 username = "user",
