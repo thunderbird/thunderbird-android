ece413d9a26f1fb9819dbcbdd6a5342976bc3c99
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-06-27 19:53:22 +0200

    Cleanup account options

--- feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/ui/options/AccountOptionsViewModel.kt
+++ feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/ui/options/AccountOptionsViewModel.kt
@@ -4,14 +4,6 @@ import app.k9mail.core.common.domain.usecase.validation.ValidationResult
 import app.k9mail.core.ui.compose.common.mvi.BaseViewModel
 import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.Effect
 import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.Event
-import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.Event.OnAccountNameChanged
-import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.Event.OnBackClicked
-import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.Event.OnCheckFrequencyChanged
-import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.Event.OnDisplayNameChanged
-import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.Event.OnEmailSignatureChanged
-import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.Event.OnMessageDisplayCountChanged
-import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.Event.OnNextClicked
-import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.Event.OnShowNotificationChanged
 import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.State
 import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.Validator
 import app.k9mail.feature.account.setup.ui.options.AccountOptionsContract.ViewModel
@@ -29,44 +21,44 @@ internal class AccountOptionsViewModel(
 
     override fun event(event: Event) {
         when (event) {
-            is OnAccountNameChanged -> updateState { state ->
+            is Event.OnAccountNameChanged -> updateState { state ->
                 state.copy(
                     accountName = state.accountName.updateValue(event.accountName),
                 )
             }
 
-            is OnDisplayNameChanged -> updateState {
+            is Event.OnDisplayNameChanged -> updateState {
                 it.copy(
                     displayName = it.displayName.updateValue(event.displayName),
                 )
             }
 
-            is OnEmailSignatureChanged -> updateState {
+            is Event.OnEmailSignatureChanged -> updateState {
                 it.copy(
                     emailSignature = it.emailSignature.updateValue(event.emailSignature),
                 )
             }
 
-            is OnCheckFrequencyChanged -> updateState {
+            is Event.OnCheckFrequencyChanged -> updateState {
                 it.copy(
                     checkFrequency = event.checkFrequency,
                 )
             }
 
-            is OnMessageDisplayCountChanged -> updateState { state ->
+            is Event.OnMessageDisplayCountChanged -> updateState { state ->
                 state.copy(
                     messageDisplayCount = event.messageDisplayCount,
                 )
             }
 
-            is OnShowNotificationChanged -> updateState { state ->
+            is Event.OnShowNotificationChanged -> updateState { state ->
                 state.copy(
                     showNotification = event.showNotification,
                 )
             }
 
-            OnNextClicked -> submit()
-            OnBackClicked -> navigateBack()
+            Event.OnNextClicked -> submit()
+            Event.OnBackClicked -> navigateBack()
         }
     }
 
