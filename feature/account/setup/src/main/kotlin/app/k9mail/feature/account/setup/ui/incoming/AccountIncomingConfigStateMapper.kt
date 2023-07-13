e24cbaaddb5c310449c43a2a5913e3b086ff40bf
cketti <cketti@thunderbird.net>
2023-07-13 16:29:32 +0200

    Map IMAP advanced settings when creating `ServerSettings`

--- feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/ui/incoming/AccountIncomingConfigStateMapper.kt
+++ feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/ui/incoming/AccountIncomingConfigStateMapper.kt
@@ -1,10 +1,11 @@
 package app.k9mail.feature.account.setup.ui.incoming
 
+import app.k9mail.feature.account.setup.domain.entity.IncomingProtocolType
 import app.k9mail.feature.account.setup.domain.entity.toAuthType
 import app.k9mail.feature.account.setup.domain.entity.toMailConnectionSecurity
 import com.fsck.k9.mail.ServerSettings
+import com.fsck.k9.mail.store.imap.ImapStoreSettings
 
-// TODO map extras
 // TODO map clientCertificateAlias
 internal fun AccountIncomingConfigContract.State.toServerSettings(): ServerSettings {
     return ServerSettings(
@@ -16,5 +17,19 @@ internal fun AccountIncomingConfigContract.State.toServerSettings(): ServerSetti
         username = username.value,
         password = if (authenticationType.isPasswordRequired) password.value else null,
         clientCertificateAlias = null, // TODO replace by actual client certificate alias
+        extra = createExtras(),
     )
+}
+
+private fun AccountIncomingConfigContract.State.createExtras(): Map<String, String?> {
+    return if (protocolType == IncomingProtocolType.IMAP) {
+        ImapStoreSettings.createExtra(
+            autoDetectNamespace = imapAutodetectNamespaceEnabled,
+            pathPrefix = if (imapAutodetectNamespaceEnabled) null else imapPrefix.value,
+            useCompression = imapUseCompression,
+            sendClientId = imapSendClientId,
+        )
+    } else {
+        emptyMap()
+    }
 }
