92d7682e48cca3cc54c1473daa120919a832b25b
cketti <cketti@thunderbird.net>
2023-07-19 16:20:44 +0200

    Use client certificate alias when creating `ServerSettings`

--- feature/account/setup/src/test/kotlin/app/k9mail/feature/account/setup/ui/outgoing/AccountOutgoingConfigStateMapperKtTest.kt
+++ feature/account/setup/src/test/kotlin/app/k9mail/feature/account/setup/ui/outgoing/AccountOutgoingConfigStateMapperKtTest.kt
@@ -23,7 +23,7 @@ class AccountOutgoingConfigStateMapperKtTest {
             authenticationType = AuthenticationType.PasswordCleartext,
             username = StringInputField(value = "user"),
             password = StringInputField(value = "password"),
-            clientCertificateAlias = "",
+            clientCertificateAlias = null,
         )
 
         val result = outgoingState.toServerSettings()
