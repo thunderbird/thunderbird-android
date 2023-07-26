8af494fa526d9dcc06eed9e9ae89db9456b6ffa1
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-07-26 18:12:38 +0200

    Change WizardNavigationBar to be able to show and hide buttons

--- feature/account/common/src/main/kotlin/app/k9mail/feature/account/common/ui/WizardNavigationBarState.kt
+++ feature/account/common/src/main/kotlin/app/k9mail/feature/account/common/ui/WizardNavigationBarState.kt
@@ -2,5 +2,7 @@ package app.k9mail.feature.account.common.ui
 
 data class WizardNavigationBarState(
     val isNextEnabled: Boolean = true,
+    val showNext: Boolean = true,
     val isBackEnabled: Boolean = true,
+    val showBack: Boolean = true,
 )
