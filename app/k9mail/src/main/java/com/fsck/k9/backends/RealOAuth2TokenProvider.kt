2b3b768eed87ddfbd530500d95aa6317a14c11e3
cketti <cketti@thunderbird.net>
2023-08-16 14:56:54 +0200

    Handle broken `AuthState`
    The authorization state created using the new account setup in K-9 Mail 6.709 is broken and can't be used to refresh the access token. So we clear the authorization state when trying to refresh the token fails with an `IllegalStateException`.

--- app/k9mail/src/main/java/com/fsck/k9/backends/RealOAuth2TokenProvider.kt
+++ app/k9mail/src/main/java/com/fsck/k9/backends/RealOAuth2TokenProvider.kt
@@ -1,6 +1,7 @@
 package com.fsck.k9.backends
 
 import android.content.Context
+import com.fsck.k9.BuildConfig
 import com.fsck.k9.mail.AuthenticationFailedException
 import com.fsck.k9.mail.oauth.AuthStateStorage
 import com.fsck.k9.mail.oauth.OAuth2TokenProvider
@@ -12,6 +13,7 @@ import net.openid.appauth.AuthorizationException
 import net.openid.appauth.AuthorizationException.AuthorizationRequestErrors
 import net.openid.appauth.AuthorizationException.GeneralErrors
 import net.openid.appauth.AuthorizationService
+import timber.log.Timber
 
 class RealOAuth2TokenProvider(
     context: Context,
@@ -20,6 +22,7 @@ class RealOAuth2TokenProvider(
     private val authService = AuthorizationService(context)
     private var requestFreshToken = false
 
+    @Suppress("TooGenericExceptionCaught")
     override fun getToken(timeoutMillis: Long): String {
         val latch = CountDownLatch(1)
         var token: String? = null
@@ -34,14 +37,30 @@ class RealOAuth2TokenProvider(
 
         val oldAccessToken = authState.accessToken
 
-        authState.performActionWithFreshTokens(authService) { accessToken: String?, _, authException: AuthorizationException? ->
-            token = accessToken
-            exception = authException
+        try {
+            authState.performActionWithFreshTokens(authService) { accessToken: String?, _, authException: AuthorizationException? ->
+                token = accessToken
+                exception = authException
 
-            latch.countDown()
+                latch.countDown()
+            }
+
+            latch.await(timeoutMillis, TimeUnit.MILLISECONDS)
+        } catch (e: Exception) {
+            // OAuth errors are communicated via the callback. If we end up here, it's probably a programming error.
+            if (BuildConfig.DEBUG) {
+                throw AssertionError("Wrong usage of AuthState.performActionWithFreshTokens()?", e)
+            }
+
+            Timber.w(e, "Failed to fetch an access token. Clearing authorization state.")
+
+            authStateStorage.updateAuthorizationState(authorizationState = null)
+
+            throw AuthenticationFailedException(
+                message = "Failed to fetch an access token",
+                throwable = e,
+            )
         }
-
-        latch.await(timeoutMillis, TimeUnit.MILLISECONDS)
 
         val authException = exception
         if (authException == GeneralErrors.NETWORK_ERROR ||
