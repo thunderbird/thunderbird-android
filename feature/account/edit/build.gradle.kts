5694e0934bc4454aaf7799a529eb08e4a596c5d1
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-09-06 15:18:33 +0200

    Add `account:edit` module

---
+++ feature/account/edit/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id(ThunderbirdPlugins.Library.androidCompose)
+}
+
+android {
+    namespace = "app.k9mail.feature.account.edit"
+    resourcePrefix = "account_edit_"
+
+    buildTypes {
+        debug {
+            manifestPlaceholders["appAuthRedirectScheme"] = "FIXME: override this in your app project"
+        }
+        release {
+            manifestPlaceholders["appAuthRedirectScheme"] = "FIXME: override this in your app project"
+        }
+    }
+}
+
+dependencies {
+    implementation(projects.core.ui.compose.designsystem)
+    implementation(projects.core.common)
+
+    implementation(projects.feature.account.common)
+    implementation(projects.feature.account.oauth)
+    implementation(projects.feature.account.server.validation)
+
+    testImplementation(projects.core.ui.compose.testing)
+}
