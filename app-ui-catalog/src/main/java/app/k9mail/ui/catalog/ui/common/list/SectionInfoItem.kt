180b6af2f9d94dff116371a76a4f552a45f2ce3c
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-09-19 11:24:07 +0200

    Add ContentLoadingErrorView to design system

---
+++ app-ui-catalog/src/main/java/app/k9mail/ui/catalog/ui/common/list/SectionInfoItem.kt
@@ -0,0 +1,31 @@
+package app.k9mail.ui.catalog.ui.common.list
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import app.k9mail.core.ui.compose.designsystem.atom.text.TextCaption
+import app.k9mail.core.ui.compose.theme.MainTheme
+
+fun LazyGridScope.sectionInfoItem(
+    text: String,
+) {
+    item(span = { GridItemSpan(maxLineSpan) }) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = MainTheme.spacings.double,
+                    end = MainTheme.spacings.double,
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            TextCaption(
+                text = text,
+            )
+        }
+    }
+}
