a8014693d88397d0d77ed9fb2a50ff8849bf5372
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-11-30 11:10:00 +0100

    Remove deprecation from CertificateErrorNotificationController

--- app/core/src/main/java/com/fsck/k9/notification/CertificateErrorNotificationController.kt
+++ app/core/src/main/java/com/fsck/k9/notification/CertificateErrorNotificationController.kt
@@ -6,7 +6,6 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.fsck.k9.Account
 
-@Deprecated("Remove once all usages have been removed for the new account edit feature")
 internal open class CertificateErrorNotificationController(
     private val notificationHelper: NotificationHelper,
     private val actionCreator: NotificationActionCreator,
