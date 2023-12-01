fdbfb06d07e84bdf9aed162fa956242d38809a6a
cketti <cketti@thunderbird.net>
2023-12-02 00:15:48 +0100

    Convert `UidCopyResponse` to Kotlin

--- mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/UidCopyResponse.kt
+++ mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/UidCopyResponse.kt
@@ -1,59 +1,51 @@
-package com.fsck.k9.mail.store.imap;
+package com.fsck.k9.mail.store.imap
 
+internal class UidCopyResponse private constructor(val uidMapping: Map<String, String>) {
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+    companion object {
+        @JvmStatic
+        fun parse(imapResponses: List<ImapResponse>): UidCopyResponse? {
+            val uidMapping = mutableMapOf<String, String>()
+            for (imapResponse in imapResponses) {
+                parseUidCopyResponse(imapResponse, uidMapping)
+            }
 
-import static com.fsck.k9.mail.store.imap.ImapResponseParser.equalsIgnoreCase;
-import static com.fsck.k9.mail.store.imap.ImapUtility.getImapSequenceValues;
-
-
-class UidCopyResponse {
-    private final Map<String, String> uidMapping;
-
-
-    private UidCopyResponse(Map<String, String> uidMapping) {
-        this.uidMapping = uidMapping;
-    }
-
-    public static UidCopyResponse parse(List<ImapResponse> imapResponses) {
-        Map<String, String> uidMapping = new LinkedHashMap<>();
-        for (ImapResponse imapResponse : imapResponses) {
-            parseUidCopyResponse(imapResponse, uidMapping);
+            return if (uidMapping.isNotEmpty()) {
+                UidCopyResponse(uidMapping)
+            } else {
+                null
+            }
         }
 
-        return uidMapping.isEmpty() ? null : new UidCopyResponse(uidMapping);
-    }
+        @Suppress("ReturnCount", "ComplexCondition", "MagicNumber")
+        private fun parseUidCopyResponse(response: ImapResponse, uidMappingOutput: MutableMap<String, String>) {
+            if (!response.isTagged || response.size < 2 ||
+                !ImapResponseParser.equalsIgnoreCase(response[0], Responses.OK) || !response.isList(1)
+            ) {
+                return
+            }
 
-    private static void parseUidCopyResponse(ImapResponse response, Map<String, String> uidMappingOutput) {
-        if (!response.isTagged() || response.size() < 2 || !equalsIgnoreCase(response.get(0), Responses.OK) ||
-                !response.isList(1)) {
-            return;
+            val responseTextList = response.getList(1)
+            if (responseTextList.size < 4 ||
+                !ImapResponseParser.equalsIgnoreCase(responseTextList[0], Responses.COPYUID) ||
+                !responseTextList.isString(1) || !responseTextList.isString(2) || !responseTextList.isString(3)
+            ) {
+                return
+            }
+
+            val sourceUids = ImapUtility.getImapSequenceValues(responseTextList.getString(2))
+            val destinationUids = ImapUtility.getImapSequenceValues(responseTextList.getString(3))
+
+            val size = sourceUids.size
+            if (size == 0 || size != destinationUids.size) {
+                return
+            }
+
+            for (i in 0 until size) {
+                val sourceUid = sourceUids[i]
+                val destinationUid = destinationUids[i]
+                uidMappingOutput[sourceUid] = destinationUid
+            }
         }
-
-        ImapList responseTextList = response.getList(1);
-        if (responseTextList.size() < 4 || !equalsIgnoreCase(responseTextList.get(0), Responses.COPYUID) ||
-                !responseTextList.isString(1) || !responseTextList.isString(2) || !responseTextList.isString(3)) {
-            return;
-        }
-
-        List<String> sourceUids = getImapSequenceValues(responseTextList.getString(2));
-        List<String> destinationUids = getImapSequenceValues(responseTextList.getString(3));
-
-        int size = sourceUids.size();
-        if (size == 0 || size != destinationUids.size()) {
-            return;
-        }
-
-        for (int i = 0; i < size; i++) {
-            String sourceUid = sourceUids.get(i);
-            String destinationUid = destinationUids.get(i);
-            uidMappingOutput.put(sourceUid, destinationUid);
-        }
-    }
-
-    public Map<String, String> getUidMapping() {
-        return uidMapping;
     }
 }
