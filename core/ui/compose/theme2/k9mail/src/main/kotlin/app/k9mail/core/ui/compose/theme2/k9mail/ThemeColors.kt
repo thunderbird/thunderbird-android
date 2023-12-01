5670de0ab20b3c6a840f2a1861e53ea8f11b0af5
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-12-01 14:31:01 +0100

    Add theme2 for K9Mail

---
+++ core/ui/compose/theme2/k9mail/src/main/kotlin/app/k9mail/core/ui/compose/theme2/k9mail/ThemeColors.kt
@@ -0,0 +1,89 @@
+package app.k9mail.core.ui.compose.theme2.k9mail
+
+import app.k9mail.core.ui.compose.theme2.ThemeColorScheme
+
+internal val darkThemeColorScheme = ThemeColorScheme(
+    primary = TonalColorPalette.primary080,
+    onPrimary = TonalColorPalette.primary020,
+    primaryContainer = TonalColorPalette.primary030,
+    onPrimaryContainer = TonalColorPalette.primary090,
+
+    secondary = TonalColorPalette.secondary080,
+    onSecondary = TonalColorPalette.secondary020,
+    secondaryContainer = TonalColorPalette.secondary030,
+    onSecondaryContainer = TonalColorPalette.secondary090,
+
+    tertiary = TonalColorPalette.tertiary080,
+    onTertiary = TonalColorPalette.tertiary020,
+    tertiaryContainer = TonalColorPalette.tertiary030,
+    onTertiaryContainer = TonalColorPalette.tertiary090,
+
+    error = TonalColorPalette.error080,
+    onError = TonalColorPalette.error020,
+    errorContainer = TonalColorPalette.error030,
+    onErrorContainer = TonalColorPalette.error090,
+
+    surface = TonalColorPalette.neutral006,
+    onSurface = TonalColorPalette.neutral090,
+    onSurfaceVariant = TonalColorPalette.neutralVariant080,
+    surfaceContainerLowest = TonalColorPalette.neutral004,
+    surfaceContainerLow = TonalColorPalette.neutral010,
+    surfaceContainer = TonalColorPalette.neutral012,
+    surfaceContainerHigh = TonalColorPalette.neutral017,
+    surfaceContainerHighest = TonalColorPalette.neutral022,
+
+    inverseSurface = TonalColorPalette.neutral090,
+    inverseOnSurface = TonalColorPalette.neutral020,
+    inversePrimary = TonalColorPalette.primary040,
+
+    outline = TonalColorPalette.neutralVariant060,
+    outlineVariant = TonalColorPalette.neutralVariant030,
+
+    surfaceBright = TonalColorPalette.neutral024,
+    surfaceDim = TonalColorPalette.neutral006,
+
+    scrim = TonalColorPalette.neutral000,
+)
+
+internal val lightThemeColorScheme = ThemeColorScheme(
+    primary = TonalColorPalette.primary040,
+    onPrimary = TonalColorPalette.primary100,
+    primaryContainer = TonalColorPalette.primary090,
+    onPrimaryContainer = TonalColorPalette.primary010,
+
+    secondary = TonalColorPalette.secondary040,
+    onSecondary = TonalColorPalette.secondary100,
+    secondaryContainer = TonalColorPalette.secondary090,
+    onSecondaryContainer = TonalColorPalette.secondary010,
+
+    tertiary = TonalColorPalette.tertiary040,
+    onTertiary = TonalColorPalette.tertiary100,
+    tertiaryContainer = TonalColorPalette.tertiary090,
+    onTertiaryContainer = TonalColorPalette.tertiary010,
+
+    error = TonalColorPalette.error040,
+    onError = TonalColorPalette.error100,
+    errorContainer = TonalColorPalette.error090,
+    onErrorContainer = TonalColorPalette.error010,
+
+    surface = TonalColorPalette.neutral098,
+    onSurface = TonalColorPalette.neutral010,
+    onSurfaceVariant = TonalColorPalette.neutralVariant030,
+    surfaceContainerLowest = TonalColorPalette.neutral100,
+    surfaceContainerLow = TonalColorPalette.neutral096,
+    surfaceContainer = TonalColorPalette.neutral094,
+    surfaceContainerHigh = TonalColorPalette.neutral092,
+    surfaceContainerHighest = TonalColorPalette.neutral090,
+
+    inverseSurface = TonalColorPalette.neutral020,
+    inverseOnSurface = TonalColorPalette.neutral095,
+    inversePrimary = TonalColorPalette.primary080,
+
+    outline = TonalColorPalette.neutralVariant050,
+    outlineVariant = TonalColorPalette.neutralVariant080,
+
+    surfaceBright = TonalColorPalette.neutral098,
+    surfaceDim = TonalColorPalette.neutral087,
+
+    scrim = TonalColorPalette.neutral000,
+)
