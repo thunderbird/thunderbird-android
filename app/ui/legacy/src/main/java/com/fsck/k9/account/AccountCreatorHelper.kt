ee3e9c4eb42d7911055dc58941ecaf7c3d7ee6ac
Wolf-Martell Montwé <wolf@thunderbird.net>
2023-12-04 16:56:51 +0100

    Remove unused default port functionality

--- app/ui/legacy/src/main/java/com/fsck/k9/account/AccountCreatorHelper.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/account/AccountCreatorHelper.kt
@@ -2,12 +2,9 @@ package com.fsck.k9.account
 
 import app.k9mail.core.common.mail.Protocols
 import com.fsck.k9.Account.DeletePolicy
-import com.fsck.k9.mail.ConnectionSecurity
 
 /**
  * Deals with logic surrounding account creation.
- *
- * TODO move this close to account creator
  */
 class AccountCreatorHelper {
 
@@ -18,26 +15,5 @@ class AccountCreatorHelper {
             "demo" -> DeletePolicy.ON_DELETE
             else -> throw AssertionError("Unhandled case: $type")
         }
-    }
-
-    fun getDefaultPort(securityType: ConnectionSecurity, serverType: String): Int {
-        return when (serverType) {
-            Protocols.IMAP -> getImapDefaultPort(securityType)
-            Protocols.POP3 -> getPop3DefaultPort(securityType)
-            Protocols.SMTP -> getSmtpDefaultPort(securityType)
-            else -> throw AssertionError("Unhandled case: $serverType")
-        }
-    }
-
-    private fun getImapDefaultPort(connectionSecurity: ConnectionSecurity): Int {
-        return if (connectionSecurity == ConnectionSecurity.SSL_TLS_REQUIRED) 993 else 143
-    }
-
-    private fun getPop3DefaultPort(connectionSecurity: ConnectionSecurity): Int {
-        return if (connectionSecurity == ConnectionSecurity.SSL_TLS_REQUIRED) 995 else 110
-    }
-
-    private fun getSmtpDefaultPort(connectionSecurity: ConnectionSecurity): Int {
-        return if (connectionSecurity == ConnectionSecurity.SSL_TLS_REQUIRED) 465 else 587
     }
 }
--- app/ui/legacy/src/test/java/com/fsck/k9/account/AccountCreatorHelperTest.kt
+++ app/ui/legacy/src/test/java/com/fsck/k9/account/AccountCreatorHelperTest.kt
@@ -5,7 +5,6 @@ import assertk.assertions.isEqualTo
 import assertk.assertions.isInstanceOf
 import com.fsck.k9.Account.DeletePolicy
 import com.fsck.k9.account.AccountCreatorHelper
-import com.fsck.k9.mail.ConnectionSecurity
 import org.junit.Test
 
 class AccountCreatorHelperTest {
@@ -30,26 +29,5 @@ class AccountCreatorHelperTest {
         assertFailure {
             accountCreatorHelper.getDefaultDeletePolicy(Protocols.SMTP)
         }.isInstanceOf<AssertionError>()
-    }
-
-    @Test
-    fun `getDefaultPort with NoConnectionSecurity and IMAP should return default port`() {
-        val result = accountCreatorHelper.getDefaultPort(ConnectionSecurity.NONE, Protocols.IMAP)
-
-        assertThat(result).isEqualTo(143)
-    }
-
-    @Test
-    fun `getDefaultPort with StartTls and IMAP should return default port`() {
-        val result = accountCreatorHelper.getDefaultPort(ConnectionSecurity.STARTTLS_REQUIRED, Protocols.IMAP)
-
-        assertThat(result).isEqualTo(143)
-    }
-
-    @Test
-    fun `getDefaultPort with Tls and IMAP should return default Tls port`() {
-        val result = accountCreatorHelper.getDefaultPort(ConnectionSecurity.SSL_TLS_REQUIRED, Protocols.IMAP)
-
-        assertThat(result).isEqualTo(993)
     }
 }
