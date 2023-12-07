025272bbbb88c847ed1927982403b328b66c1d6d
cketti <cketti@thunderbird.net>
2023-12-07 21:00:49 +0100

    Convert `CertificateChainException` to Kotlin

--- mail/common/src/main/java/com/fsck/k9/mail/CertificateChainException.kt
+++ mail/common/src/main/java/com/fsck/k9/mail/CertificateChainException.kt
@@ -1,27 +1,13 @@
-package com.fsck.k9.mail;
+package com.fsck.k9.mail
 
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 
 /**
- * A {@link CertificateException} extension that provides access to
- * the pertinent certificate chain.
- *
+ * A [CertificateException] extension that provides access to the pertinent certificate chain.
  */
-public class CertificateChainException extends CertificateException {
-
-    private static final long serialVersionUID = 1103894512106650107L;
-    private X509Certificate[] mCertChain;
-
-    public CertificateChainException(String msg, X509Certificate[] chain, Throwable cause) {
-        super(msg, cause);
-        setCertChain(chain);
-    }
-
-    public void setCertChain(X509Certificate[] chain) {
-        mCertChain = chain;
-    }
-    public X509Certificate[] getCertChain() {
-        return mCertChain;
-    }
-}
+class CertificateChainException(
+    message: String?,
+    val certChain: Array<out X509Certificate>?,
+    cause: Throwable?,
+) : CertificateException(message, cause)
