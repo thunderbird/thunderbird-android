b890bb9403910dd28ead4ecef4a894b4a22a20ed
cketti <cketti@thunderbird.net>
2023-12-13 19:54:52 +0100

    Fix string representation of `SpecialFolderOption.None(isAutomatic = true)`

--- feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/ui/specialfolders/SpecialFoldersStringMapper.kt
+++ feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/ui/specialfolders/SpecialFoldersStringMapper.kt
@@ -5,7 +5,14 @@ import app.k9mail.feature.account.common.domain.entity.SpecialFolderOption
 import app.k9mail.feature.account.setup.R
 
 internal fun SpecialFolderOption.toResourceString(resources: Resources) = when (this) {
-    is SpecialFolderOption.None -> resources.getString(R.string.account_setup_special_folders_folder_none)
+    is SpecialFolderOption.None -> {
+        val noneString = resources.getString(R.string.account_setup_special_folders_folder_none)
+        if (isAutomatic) {
+            resources.getString(R.string.account_setup_special_folders_folder_automatic, noneString)
+        } else {
+            noneString
+        }
+    }
     is SpecialFolderOption.Regular -> remoteFolder.displayName
     is SpecialFolderOption.Special -> {
         if (isAutomatic) {
