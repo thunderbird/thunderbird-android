9311f8d4daae8e21a353e67aec57a533a907966e
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-01-15 17:39:17 +0100

    Remove `app-feature-preview`

--- app-feature-preview/src/main/java/app/k9mail/feature/preview/account/AccountOwnerNameProvider.kt
+++
@@ -1,9 +0,0 @@
-package app.k9mail.feature.preview.account
-
-import app.k9mail.feature.account.setup.AccountSetupExternalContract
-
-class AccountOwnerNameProvider : AccountSetupExternalContract.AccountOwnerNameProvider {
-    override suspend fun getOwnerName(): String? {
-        return null
-    }
-}
