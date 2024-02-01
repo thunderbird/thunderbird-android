1097fd3371063a8e2136628af26635732da13154
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-02-01 18:15:34 +0100

    Prepare for version 6.716

--- app-k9mail/build.gradle.kts
+++ app-k9mail/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         testApplicationId = "com.fsck.k9.tests"
 
         versionCode = 37015
-        versionName = "6.715"
+        versionName = "6.716-SNAPSHOT"
 
         // Keep in sync with the resource string array "supported_languages"
         resourceConfigurations.addAll(
