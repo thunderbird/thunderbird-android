e19dae00215c96fe816619b47a7e1d1b97a4f257
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-02-09 12:57:42 +0100

    Add AutoDiscoveryRegistry to easily change the available discovery mechanisms

---
+++ feature/autodiscovery/service/src/main/kotlin/app/k9mail/autodiscovery/service/RealAutoDiscoveryRegistry.kt
@@ -0,0 +1,41 @@
+package app.k9mail.autodiscovery.service
+
+import app.k9mail.autodiscovery.api.AutoDiscovery
+import app.k9mail.autodiscovery.api.AutoDiscoveryRegistry
+import app.k9mail.autodiscovery.autoconfig.AutoconfigUrlConfig
+import app.k9mail.autodiscovery.autoconfig.createIspDbAutoconfigDiscovery
+import app.k9mail.autodiscovery.autoconfig.createMxLookupAutoconfigDiscovery
+import app.k9mail.autodiscovery.autoconfig.createProviderAutoconfigDiscovery
+import okhttp3.OkHttpClient
+
+class RealAutoDiscoveryRegistry(
+    private val autoDiscoveries: List<AutoDiscovery> = emptyList(),
+) : AutoDiscoveryRegistry {
+
+    override fun getAutoDiscoveries(): List<AutoDiscovery> = autoDiscoveries
+
+    companion object {
+        val defaultAutoconfigUrlConfig = AutoconfigUrlConfig(
+            httpsOnly = false,
+            includeEmailAddress = false,
+        )
+
+        fun createDefault(
+            okHttpClient: OkHttpClient,
+            autoconfigUrlConfig: AutoconfigUrlConfig = defaultAutoconfigUrlConfig,
+        ): RealAutoDiscoveryRegistry = RealAutoDiscoveryRegistry(
+            autoDiscoveries = listOf(
+                createProviderAutoconfigDiscovery(
+                    okHttpClient = okHttpClient,
+                    config = autoconfigUrlConfig,
+                ),
+                createMxLookupAutoconfigDiscovery(
+                    okHttpClient = okHttpClient,
+                ),
+                createIspDbAutoconfigDiscovery(
+                    okHttpClient = okHttpClient,
+                ),
+            ),
+        )
+    }
+}
