43818ecb6cc2ed3b8bd99ef77f9939986eb93515
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-02-09 11:27:21 +0100

    Remove unused HtmlToSpanned

--- app/ui/legacy/src/main/java/com/fsck/k9/ui/KoinModule.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/ui/KoinModule.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import com.fsck.k9.ui.base.ThemeProvider
 import com.fsck.k9.ui.helper.DisplayHtmlUiFactory
 import com.fsck.k9.ui.helper.HtmlSettingsProvider
-import com.fsck.k9.ui.helper.HtmlToSpanned
 import com.fsck.k9.ui.helper.SizeFormatter
 import com.fsck.k9.ui.messageview.LinkTextHandler
 import com.fsck.k9.ui.share.ShareIntentBuilder
@@ -12,7 +11,6 @@ import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val uiModule = module {
-    single { HtmlToSpanned() }
     single<ThemeProvider> { K9ThemeProvider() }
     single { HtmlSettingsProvider(get()) }
     single { DisplayHtmlUiFactory(get()) }
