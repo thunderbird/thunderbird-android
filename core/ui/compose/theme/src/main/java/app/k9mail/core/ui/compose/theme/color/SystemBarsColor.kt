177ffc2a2a00de641ed8ef18615fcbe5052c4b1a
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-02-21 14:56:42 +0100

    Replace Accompanist Systemuicontroller with edge-to-edge from AndroidX Activity

--- core/ui/compose/theme/src/main/java/app/k9mail/core/ui/compose/theme/color/SystemBarsColor.kt
+++ core/ui/compose/theme/src/main/java/app/k9mail/core/ui/compose/theme/color/SystemBarsColor.kt
@@ -1,19 +1,24 @@
 package app.k9mail.core.ui.compose.theme.color
 
+import android.app.Activity
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 @Composable
 fun SetSystemBarsColor(
-    color: Color,
+    darkTheme: Boolean,
+    color: Color = Color.Transparent,
 ) {
-    val systemUiController = rememberSystemUiController()
-
-    DisposableEffect(Unit) {
-        systemUiController.setSystemBarsColor(color = color)
-
-        onDispose { }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = color.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
     }
 }
