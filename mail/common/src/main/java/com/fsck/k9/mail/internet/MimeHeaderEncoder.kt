09a155bcfdd06ca2a2700280ed3d5ba9e4c5ae4e
cketti <cketti@thunderbird.net>
2024-03-04 13:05:29 +0100

    Add header folding to `MimeHeaderEncoder`

--- mail/common/src/main/java/com/fsck/k9/mail/internet/MimeHeaderEncoder.kt
+++ mail/common/src/main/java/com/fsck/k9/mail/internet/MimeHeaderEncoder.kt
@@ -1,23 +1,29 @@
 package com.fsck.k9.mail.internet
 
+import org.apache.james.mime4j.util.MimeUtil
+
 object MimeHeaderEncoder {
     @JvmStatic
     fun encode(name: String, value: String): String {
         // TODO: Fold long text that provides enough opportunities for folding and doesn't contain any characters that
         //  need to be encoded.
-        return if (hasToBeEncoded(name, value)) {
-            EncoderUtil.encodeEncodedWord(value)
+
+        // Number of characters already used up on the first line (header field name + colon + space)
+        val usedCharacters = name.length + COLON_PLUS_SPACE_LENGTH
+
+        return if (hasToBeEncoded(value, usedCharacters)) {
+            MimeUtil.fold(EncoderUtil.encodeEncodedWord(value), usedCharacters)
         } else {
             value
         }
     }
 
-    private fun hasToBeEncoded(name: String, value: String): Boolean {
-        return exceedsRecommendedLineLength(name, value) || charactersNeedEncoding(value)
+    private fun hasToBeEncoded(value: String, usedCharacters: Int): Boolean {
+        return exceedsRecommendedLineLength(value, usedCharacters) || charactersNeedEncoding(value)
     }
 
-    private fun exceedsRecommendedLineLength(name: String, value: String): Boolean {
-        return name.length + COLON_PLUS_SPACE_LENGTH + value.length > RECOMMENDED_MAX_LINE_LENGTH
+    private fun exceedsRecommendedLineLength(value: String, usedCharacters: Int): Boolean {
+        return usedCharacters + value.length > RECOMMENDED_MAX_LINE_LENGTH
     }
 
     private fun charactersNeedEncoding(text: String): Boolean {
