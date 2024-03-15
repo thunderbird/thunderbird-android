34396624462b32eaa511aa840eafe0df9e21fa09
cketti <cketti@thunderbird.net>
2024-03-15 16:23:36 +0100

    Add `ImapStoreConfig.isExpungeImmediately()`

--- mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/ImapStoreConfig.kt
+++ mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/ImapStoreConfig.kt
@@ -3,5 +3,6 @@ package com.fsck.k9.mail.store.imap
 interface ImapStoreConfig {
     val logLabel: String
     fun isSubscribedFoldersOnly(): Boolean
+    fun isExpungeImmediately(): Boolean
     fun clientId(): ImapClientId
 }
