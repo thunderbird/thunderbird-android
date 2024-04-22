46907163a05e7bb8f8be92e36df83d3645108a51
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-04-22 14:24:18 +0200

    Remove remaining Material 2 text atoms

--- app-ui-catalog/src/main/java/app/k9mail/ui/catalog/ui/atom/items/TypographyItems.kt
+++ app-ui-catalog/src/main/java/app/k9mail/ui/catalog/ui/atom/items/TypographyItems.kt
@@ -1,16 +1,11 @@
 package app.k9mail.ui.catalog.ui.atom.items
 
 import androidx.compose.foundation.lazy.grid.LazyGridScope
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
-import app.k9mail.core.ui.compose.designsystem.atom.text.TextButton
-import app.k9mail.core.ui.compose.designsystem.atom.text.TextHeadline1
-import app.k9mail.core.ui.compose.designsystem.atom.text.TextHeadline3
-import app.k9mail.ui.catalog.ui.common.list.itemDefaultPadding
 import app.k9mail.ui.catalog.ui.common.list.sectionHeaderItem
 
 fun LazyGridScope.typographyItems() {
@@ -29,27 +24,7 @@ private fun LazyGridScope.textItems(
     isAnnotated: Boolean = false,
     color: Color = Color.Unspecified,
 ) {
-    item {
-        TextHeadline1(
-            text = annotatedString("Headline1", isAnnotated),
-            modifier = Modifier.itemDefaultPadding(),
-            color = color,
-        )
-    }
-    item {
-        TextHeadline3(
-            text = annotatedString("Headline3", isAnnotated),
-            modifier = Modifier.itemDefaultPadding(),
-            color = color,
-        )
-    }
-    item {
-        TextButton(
-            text = annotatedString("Button", isAnnotated),
-            modifier = Modifier.itemDefaultPadding(),
-            color = color,
-        )
-    }
+    // TODO: Add Material3 typography items
 }
 
 private fun annotatedString(
