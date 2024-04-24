6de38fcc9e62eb31101f022546cc50c7ea20641c
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-04-24 17:29:41 +0200

    Change account setup options sync feature to theme 2

---
+++ feature/account/setup/src/debug/kotlin/app/k9mail/feature/account/setup/ui/options/sync/SyncOptionsContentPreview.kt
@@ -0,0 +1,18 @@
+package app.k9mail.feature.account.setup.ui.options.sync
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import app.k9mail.core.ui.compose.designsystem.PreviewWithTheme
+
+@Composable
+@Preview(showBackground = true)
+internal fun SyncOptionsContentPreview() {
+    PreviewWithTheme {
+        SyncOptionsContent(
+            state = SyncOptionsContract.State(),
+            onEvent = {},
+            contentPadding = PaddingValues(),
+        )
+    }
+}
---
+++ feature/account/setup/src/debug/kotlin/app/k9mail/feature/account/setup/ui/options/sync/SyncOptionsScreenPreview.kt
@@ -0,0 +1,20 @@
+package app.k9mail.feature.account.setup.ui.options.sync
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import app.k9mail.core.ui.compose.designsystem.PreviewWithTheme
+import app.k9mail.feature.account.common.ui.fake.FakeAccountStateRepository
+
+@Composable
+@Preview(showBackground = true)
+internal fun SyncOptionsScreenPreview() {
+    PreviewWithTheme {
+        SyncOptionsScreen(
+            onNext = {},
+            onBack = {},
+            viewModel = SyncOptionsViewModel(
+                accountStateRepository = FakeAccountStateRepository(),
+            ),
+        )
+    }
+}
