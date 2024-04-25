d3d3117c3de7f3eac5be123fc6e351e191bedcd0
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-04-25 13:32:50 +0200

    Change to Material 3 library

--- app-ui-catalog/src/main/java/app/k9mail/ui/catalog/ui/common/PagedContent.kt
+++ app-ui-catalog/src/main/java/app/k9mail/ui/catalog/ui/common/PagedContent.kt
@@ -12,9 +12,9 @@ import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.ScrollableTabRow
-import androidx.compose.material.Tab
-import androidx.compose.material.Text
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
