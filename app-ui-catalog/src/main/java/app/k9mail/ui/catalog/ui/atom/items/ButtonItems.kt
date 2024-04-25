6abbc6aedd8721ca531adcd8806cbdedc674d5c8
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-04-25 13:27:43 +0200

    Add missing Material 3 butttons

--- app-ui-catalog/src/main/java/app/k9mail/ui/catalog/ui/atom/items/ButtonItems.kt
+++ app-ui-catalog/src/main/java/app/k9mail/ui/catalog/ui/atom/items/ButtonItems.kt
@@ -6,16 +6,21 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import app.k9mail.core.ui.compose.designsystem.atom.button.ButtonElevated
 import app.k9mail.core.ui.compose.designsystem.atom.button.ButtonFilled
+import app.k9mail.core.ui.compose.designsystem.atom.button.ButtonFilledTonal
+import app.k9mail.core.ui.compose.designsystem.atom.button.ButtonIcon
 import app.k9mail.core.ui.compose.designsystem.atom.button.ButtonOutlined
 import app.k9mail.core.ui.compose.designsystem.atom.button.ButtonText
+import app.k9mail.core.ui.compose.designsystem.atom.icon.Icons
 import app.k9mail.core.ui.compose.theme2.MainTheme
 import app.k9mail.ui.catalog.ui.common.list.itemDefaultPadding
 import app.k9mail.ui.catalog.ui.common.list.sectionHeaderItem
 
+@Suppress("LongMethod")
 @OptIn(ExperimentalLayoutApi::class)
 fun LazyGridScope.buttonItems() {
-    sectionHeaderItem(text = "Buttons - Filled")
+    sectionHeaderItem(text = "Button - Filled")
     item {
         FlowRow(
             horizontalArrangement = Arrangement.spacedBy(MainTheme.spacings.default),
@@ -25,7 +30,27 @@ fun LazyGridScope.buttonItems() {
             ButtonFilled(text = "Disabled", onClick = { }, enabled = false)
         }
     }
-    sectionHeaderItem(text = "Buttons - Outlined")
+    sectionHeaderItem(text = "Button - Filled Tonal")
+    item {
+        FlowRow(
+            horizontalArrangement = Arrangement.spacedBy(MainTheme.spacings.default),
+            modifier = Modifier.itemDefaultPadding(),
+        ) {
+            ButtonFilledTonal(text = "Enabled", onClick = { })
+            ButtonFilledTonal(text = "Disabled", onClick = { }, enabled = false)
+        }
+    }
+    sectionHeaderItem(text = "Button - Elevated")
+    item {
+        FlowRow(
+            horizontalArrangement = Arrangement.spacedBy(MainTheme.spacings.default),
+            modifier = Modifier.itemDefaultPadding(),
+        ) {
+            ButtonElevated(text = "Enabled", onClick = { })
+            ButtonElevated(text = "Disabled", onClick = { }, enabled = false)
+        }
+    }
+    sectionHeaderItem(text = "Button - Outlined")
     item {
         FlowRow(
             horizontalArrangement = Arrangement.spacedBy(MainTheme.spacings.default),
@@ -35,7 +60,7 @@ fun LazyGridScope.buttonItems() {
             ButtonOutlined(text = "Disabled", onClick = { }, enabled = false)
         }
     }
-    sectionHeaderItem(text = "Buttons - Text only")
+    sectionHeaderItem(text = "Button - Text")
     item {
         FlowRow(
             horizontalArrangement = Arrangement.spacedBy(MainTheme.spacings.default),
@@ -44,6 +69,16 @@ fun LazyGridScope.buttonItems() {
             ButtonText(text = "Enabled", onClick = { })
             ButtonText(text = "Colored", onClick = { }, color = Color.Magenta)
             ButtonText(text = "Disabled", onClick = { }, enabled = false)
+        }
+    }
+    sectionHeaderItem(text = "Button - Icon")
+    item {
+        FlowRow(
+            horizontalArrangement = Arrangement.spacedBy(MainTheme.spacings.default),
+            modifier = Modifier.itemDefaultPadding(),
+        ) {
+            ButtonIcon(onClick = { }, imageVector = Icons.Outlined.celebration)
+            ButtonIcon(onClick = { }, imageVector = Icons.Outlined.celebration, enabled = false)
         }
     }
 }
