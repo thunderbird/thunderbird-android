270fb1d2c62a410baa8ec38e04d554696d722e4c
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-05-06 17:57:07 +0200

    Change FolderIconProvider and K9Drawer to reference drawables directly

--- app/ui/legacy/src/main/java/com/fsck/k9/ui/folders/FolderIconProvider.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/ui/folders/FolderIconProvider.kt
@@ -1,39 +1,17 @@
 package com.fsck.k9.ui.folders
 
-import android.content.res.Resources
-import android.util.TypedValue
 import com.fsck.k9.mailstore.FolderType
 import com.fsck.k9.ui.R
 
-class FolderIconProvider(private val theme: Resources.Theme) {
-    private val iconFolderInboxResId: Int
-    private val iconFolderOutboxResId: Int
-    private val iconFolderSentResId: Int
-    private val iconFolderTrashResId: Int
-    private val iconFolderDraftsResId: Int
-    private val iconFolderArchiveResId: Int
-    private val iconFolderSpamResId: Int
-    var iconFolderResId: Int
-
-    init {
-        iconFolderInboxResId = getResId(R.attr.iconFolderInbox)
-        iconFolderOutboxResId = getResId(R.attr.iconFolderOutbox)
-        iconFolderSentResId = getResId(R.attr.iconFolderSent)
-        iconFolderTrashResId = getResId(R.attr.iconFolderTrash)
-        iconFolderDraftsResId = getResId(R.attr.iconFolderDrafts)
-        iconFolderArchiveResId = getResId(R.attr.iconFolderArchive)
-        iconFolderSpamResId = getResId(R.attr.iconFolderSpam)
-        iconFolderResId = getResId(R.attr.iconFolder)
-    }
-
-    private fun getResId(resAttribute: Int): Int {
-        val typedValue = TypedValue()
-        val found = theme.resolveAttribute(resAttribute, typedValue, true)
-        if (!found) {
-            throw AssertionError("Couldn't find resource with attribute $resAttribute")
-        }
-        return typedValue.resourceId
-    }
+class FolderIconProvider {
+    private val iconFolderInboxResId: Int = R.drawable.ic_inbox
+    private val iconFolderOutboxResId: Int = R.drawable.ic_outbox
+    private val iconFolderSentResId: Int = R.drawable.ic_send
+    private val iconFolderTrashResId: Int = R.drawable.ic_trash_can
+    private val iconFolderDraftsResId: Int = R.drawable.ic_drafts_folder
+    private val iconFolderArchiveResId: Int = R.drawable.ic_archive
+    private val iconFolderSpamResId: Int = R.drawable.ic_alert_octagon
+    var iconFolderResId: Int = R.drawable.ic_folder
 
     fun getFolderIcon(type: FolderType): Int = when (type) {
         FolderType.INBOX -> iconFolderInboxResId
