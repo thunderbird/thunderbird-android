d57d1bdb0c632d278837adab826c61ea12c31796
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-05-13 13:47:06 +0200

    Add preview buildType to disable debug slowdowns for composable rendering

--- app-ui-catalog/build.gradle.kts
+++ app-ui-catalog/build.gradle.kts
@@ -10,6 +10,18 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    buildTypes {
+        // Preview build type to render compose without debug features.
+        // This gives a better idea of the real world drawing performance.
+        create("preview") {
+            initWith(getByName("debug"))
+            applicationIdSuffix = ".preview"
+            isDebuggable = false
+            matchingFallbacks += listOf("release")
+            isDefault = true
+        }
+    }
 }
 
 dependencies {
