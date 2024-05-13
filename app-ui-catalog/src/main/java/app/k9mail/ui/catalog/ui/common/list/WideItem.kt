58de17e90db9c4f0b150109cab19b4bebc5fcfb5
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-05-13 13:33:15 +0200

    Add WideItem to draw content about two LazyGrid rows

---
+++ app-ui-catalog/src/main/java/app/k9mail/ui/catalog/ui/common/list/WideItem.kt
@@ -0,0 +1,16 @@
+package app.k9mail.ui.catalog.ui.common.list
+
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridItemScope
+import androidx.compose.foundation.lazy.grid.LazyGridScope
+import androidx.compose.runtime.Composable
+import java.util.UUID
+
+fun LazyGridScope.wideItem(content: @Composable LazyGridItemScope.() -> Unit) {
+    item(
+        key = UUID.randomUUID().toString(),
+        span = { GridItemSpan(2) },
+    ) {
+        content()
+    }
+}
