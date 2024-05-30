e699090a1f725dbff39f0c197d67e25646d250d8
cketti <cketti@thunderbird.net>
2024-05-30 12:41:24 +0200

    Clean up `DefaultStorageMigrationsHelper`

--- app/storage/src/main/java/com/fsck/k9/preferences/migrations/DefaultStorageMigrationsHelper.kt
+++ app/storage/src/main/java/com/fsck/k9/preferences/migrations/DefaultStorageMigrationsHelper.kt
@@ -1,68 +1,58 @@
 package com.fsck.k9.preferences.migrations
 
-import android.content.ContentValues
-import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
-import com.fsck.k9.helper.Utility
+import androidx.core.content.contentValuesOf
+import app.k9mail.core.android.common.database.getStringOrThrow
+import app.k9mail.core.android.common.database.map
 import timber.log.Timber
 
 class DefaultStorageMigrationsHelper : StorageMigrationsHelper {
     override fun readAllValues(db: SQLiteDatabase): Map<String, String> {
-        val loadedValues = HashMap<String, String>()
-        var cursor: Cursor? = null
-        try {
-            cursor = db.rawQuery("SELECT primkey, value FROM preferences_storage", null)
-            while (cursor.moveToNext()) {
-                val key = cursor.getString(0)
-                val value = cursor.getString(1)
+        return db.query(TABLE_NAME, arrayOf(KEY_COLUMN, VALUE_COLUMN), null, null, null, null, null).use {
+            it.map { cursor ->
+                val key = cursor.getStringOrThrow(KEY_COLUMN)
+                val value = cursor.getStringOrThrow(VALUE_COLUMN)
                 Timber.d("Loading key '%s', value = '%s'", key, value)
-                loadedValues[key] = value
-            }
-        } finally {
-            Utility.closeQuietly(cursor)
-        }
 
-        return loadedValues
+                key to value
+            }
+        }.toMap()
     }
 
     override fun readValue(db: SQLiteDatabase, key: String): String? {
-        var cursor: Cursor? = null
-        var value: String? = null
-        try {
-            cursor = db.query(
-                "preferences_storage",
-                arrayOf("value"),
-                "primkey = ?",
-                arrayOf(key),
-                null,
-                null,
-                null
-            )
-
+        return db.query(
+            TABLE_NAME,
+            arrayOf(VALUE_COLUMN),
+            "$KEY_COLUMN = ?",
+            arrayOf(key),
+            null,
+            null,
+            null,
+        ).use { cursor ->
             if (cursor.moveToNext()) {
-                value = cursor.getString(0)
-                Timber.d("Loading key '%s', value = '%s'", key, value)
+                cursor.getStringOrThrow(VALUE_COLUMN).also { value ->
+                    Timber.d("Loading key '%s', value = '%s'", key, value)
+                }
+            } else {
+                null
             }
-        } finally {
-            Utility.closeQuietly(cursor)
         }
-
-        return value
     }
 
     override fun writeValue(db: SQLiteDatabase, key: String, value: String?) {
         if (value == null) {
-            db.delete("preferences_storage", "primkey = ?", arrayOf(key))
+            db.delete(TABLE_NAME, "$KEY_COLUMN = ?", arrayOf(key))
             return
         }
 
-        val cv = ContentValues()
-        cv.put("primkey", key)
-        cv.put("value", value)
+        val values = contentValuesOf(
+            KEY_COLUMN to key,
+            VALUE_COLUMN to value,
+        )
 
-        val result = db.update("preferences_storage", cv, "primkey = ?", arrayOf(key)).toLong()
+        val result = db.update(TABLE_NAME, values, "$KEY_COLUMN = ?", arrayOf(key))
 
-        if (result == -1L) {
+        if (result == -1) {
             Timber.e("Error writing key '%s', value = '%s'", key, value)
         }
     }
@@ -72,14 +62,21 @@ class DefaultStorageMigrationsHelper : StorageMigrationsHelper {
             return
         }
 
-        val cv = ContentValues()
-        cv.put("primkey", key)
-        cv.put("value", value)
+        val values = contentValuesOf(
+            KEY_COLUMN to key,
+            VALUE_COLUMN to value,
+        )
 
-        val result = db.insert("preferences_storage", null, cv)
+        val result = db.insert(TABLE_NAME, null, values)
 
         if (result == -1L) {
             Timber.e("Error writing key '%s', value = '%s'", key, value)
         }
+    }
+
+    companion object {
+        private const val TABLE_NAME = "preferences_storage"
+        private const val KEY_COLUMN = "primkey"
+        private const val VALUE_COLUMN = "value"
     }
 }
