6cbd5f59d43c69c45c638bd79de243d29d44de08
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-05-31 12:05:48 +0200

    Change theme naming scheme to follow the default order

--- app/ui/legacy/src/main/java/com/fsck/k9/ui/K9ThemeProvider.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/ui/K9ThemeProvider.kt
@@ -7,6 +7,6 @@ class K9ThemeProvider : ThemeProvider {
     override val appThemeResourceId = R.style.Theme_K9_DayNight
     override val appLightThemeResourceId = R.style.Theme_K9_Light
     override val appDarkThemeResourceId = R.style.Theme_K9_Dark
-    override val dialogThemeResourceId = R.style.Theme_K9_Dialog_DayNight
-    override val translucentDialogThemeResourceId = R.style.Theme_K9_Dialog_Translucent_DayNight
+    override val dialogThemeResourceId = R.style.Theme_K9_DayNight_Dialog
+    override val translucentDialogThemeResourceId = R.style.Theme_K9_DayNight_Dialog_Translucent
 }
