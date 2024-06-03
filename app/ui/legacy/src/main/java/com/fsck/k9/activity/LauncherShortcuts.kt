1f8b53d6b599a90e874247267158db456ea30dcc
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-06-03 11:40:45 +0200

    Change LauncherShortcuts to Kotlin
    Signed-off-by: Wolf-Martell Montwé <wolf@thunderbird.net>

--- app/ui/legacy/src/main/java/com/fsck/k9/activity/LauncherShortcuts.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/activity/LauncherShortcuts.kt
@@ -1,49 +1,42 @@
-package com.fsck.k9.activity;
+package com.fsck.k9.activity
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Parcelable;
+import android.content.Intent
+import android.os.Bundle
+import android.os.Parcelable
+import com.fsck.k9.BaseAccount
+import com.fsck.k9.activity.MessageList.Companion.shortcutIntent
+import com.fsck.k9.activity.MessageList.Companion.shortcutIntentForAccount
+import com.fsck.k9.search.SearchAccount
+import com.fsck.k9.ui.R
 
-import com.fsck.k9.BaseAccount;
-import com.fsck.k9.ui.R;
-import com.fsck.k9.search.SearchAccount;
-
-public class LauncherShortcuts extends AccountList {
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        setTitle(R.string.shortcuts_title);
+class LauncherShortcuts : AccountList() {
+    override fun onCreate(icicle: Bundle?) {
+        super.onCreate(icicle)
+        setTitle(R.string.shortcuts_title)
 
         // finish() immediately if we aren't supposed to be here
-        if (!Intent.ACTION_CREATE_SHORTCUT.equals(getIntent().getAction())) {
-            finish();
+        if (Intent.ACTION_CREATE_SHORTCUT != intent.action) {
+            finish()
         }
     }
 
-    @Override
-    protected void onAccountSelected(BaseAccount account) {
-        Intent shortcutIntent;
-        if (account instanceof SearchAccount) {
-            SearchAccount searchAccount = (SearchAccount) account;
-            shortcutIntent = MessageList.shortcutIntent(this, searchAccount.getId());
+    override fun onAccountSelected(account: BaseAccount) {
+        val shortcutIntent: Intent
+        if (account is SearchAccount) {
+            shortcutIntent = shortcutIntent(this, account.id)
         } else {
-            shortcutIntent = MessageList.shortcutIntentForAccount(this, account.getUuid());
+            shortcutIntent = shortcutIntentForAccount(this, account.uuid)
         }
 
-        Intent intent = new Intent();
-        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-        String accountName = account.getName();
-        String displayName;
-        if (accountName != null) {
-            displayName = accountName;
-        } else {
-            displayName = account.getEmail();
-        }
-        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, displayName);
-        Parcelable iconResource = Intent.ShortcutIconResource.fromContext(this, R.drawable.ic_launcher);
-        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource);
+        val intent = Intent()
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent)
+        val accountName = account.name
+        val displayName = accountName ?: account.email
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, displayName)
+        val iconResource: Parcelable = Intent.ShortcutIconResource.fromContext(this, R.drawable.ic_launcher)
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource)
 
-        setResult(RESULT_OK, intent);
-        finish();
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
