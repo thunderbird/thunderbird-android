2b1d10d12ede9def969f8cb7514ad85949b22a4e
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-06-14 11:25:39 +0200

    Replace GlobalScope with viewModelScope for SettingsViewModel

--- app/ui/legacy/src/main/java/com/fsck/k9/ui/settings/SettingsViewModel.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/ui/settings/SettingsViewModel.kt
@@ -2,28 +2,30 @@ package com.fsck.k9.ui.settings
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.fsck.k9.Account
 import com.fsck.k9.preferences.AccountManager
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 internal class SettingsViewModel(
     private val accountManager: AccountManager,
-    private val coroutineScope: CoroutineScope = GlobalScope,
     private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : ViewModel() {
     val accounts = accountManager.getAccountsFlow().asLiveData()
 
     fun moveAccount(account: Account, newPosition: Int) {
-        coroutineScope.launch(coroutineDispatcher) {
+        viewModelScope.launch(coroutineDispatcher) {
             // Delay saving the account so the animation is not disturbed
             delay(500)
 
-            accountManager.moveAccount(account, newPosition)
+            withContext(NonCancellable) {
+                accountManager.moveAccount(account, newPosition)
+            }
         }
     }
 }
