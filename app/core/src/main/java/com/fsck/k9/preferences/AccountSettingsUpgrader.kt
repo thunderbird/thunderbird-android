0857d96f7a8de61616c145b63b0c7e965e061643
cketti <cketti@thunderbird.net>
2024-06-17 17:00:53 +0200

    Extract `ServerSettingsUpgrader`

--- app/core/src/main/java/com/fsck/k9/preferences/AccountSettingsUpgrader.kt
+++ app/core/src/main/java/com/fsck/k9/preferences/AccountSettingsUpgrader.kt
@@ -3,6 +3,7 @@ package com.fsck.k9.preferences
 internal class AccountSettingsUpgrader {
     private val identitySettingsUpgrader = IdentitySettingsUpgrader()
     private val folderSettingsUpgrader = FolderSettingsUpgrader()
+    private val serverSettingsUpgrader = ServerSettingsUpgrader()
 
     fun upgrade(contentVersion: Int, account: ValidatedSettings.Account): ValidatedSettings.Account {
         val validatedSettings = account.settings.toMutableMap()
@@ -12,6 +13,8 @@ internal class AccountSettingsUpgrader {
 
         return account.copy(
             settings = validatedSettings.toMap(),
+            incoming = serverSettingsUpgrader.upgrade(contentVersion, account.incoming),
+            outgoing = serverSettingsUpgrader.upgrade(contentVersion, account.outgoing),
             identities = upgradeIdentities(contentVersion, account.identities),
             folders = upgradeFolders(contentVersion, account.folders),
         )
