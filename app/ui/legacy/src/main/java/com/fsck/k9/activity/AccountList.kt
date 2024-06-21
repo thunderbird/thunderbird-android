ef38a71d914e1e5ffb58fc6e5886cc72651c4ef8
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-06-21 14:48:11 +0200

    Change AccountList to Kotlin
    Signed-off-by: Wolf-Martell Montwé <wolf@thunderbird.net>

--- app/ui/legacy/src/main/java/com/fsck/k9/activity/AccountList.kt
+++ app/ui/legacy/src/main/java/com/fsck/k9/activity/AccountList.kt
@@ -1,162 +1,140 @@
-package com.fsck.k9.activity;
+package com.fsck.k9.activity
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import com.fsck.k9.Account;
-import com.fsck.k9.BaseAccount;
-import com.fsck.k9.K9;
-import com.fsck.k9.Preferences;
-import com.fsck.k9.search.SearchAccount;
-import com.fsck.k9.ui.R;
-import com.google.android.material.textview.MaterialTextView;
-
+import android.os.AsyncTask
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import com.fsck.k9.Account
+import com.fsck.k9.BaseAccount
+import com.fsck.k9.K9.isShowUnifiedInbox
+import com.fsck.k9.Preferences.Companion.getPreferences
+import com.fsck.k9.search.SearchAccount.Companion.createUnifiedInboxAccount
+import com.fsck.k9.ui.R
+import com.google.android.material.textview.MaterialTextView
 
 /**
  * Activity displaying the list of accounts.
  *
- * <p>
- * Classes extending this abstract class have to provide an {@link #onAccountSelected(BaseAccount)}
+ *
+ *
+ * Classes extending this abstract class have to provide an [.onAccountSelected]
  * method to perform an action when an account is selected.
- * </p>
+ *
  */
-public abstract class AccountList extends K9ListActivity implements OnItemClickListener {
+abstract class AccountList : K9ListActivity(), OnItemClickListener {
+    public override fun onCreate(icicle: Bundle?) {
+        super.onCreate(icicle)
 
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+        setResult(RESULT_CANCELED)
 
-        setResult(RESULT_CANCELED);
+        setLayout(R.layout.account_list)
 
-        setLayout(R.layout.account_list);
-
-        ListView listView = getListView();
-        listView.setOnItemClickListener(this);
-        listView.setItemsCanFocus(false);
+        val listView = listView
+        listView.onItemClickListener = this
+        listView.itemsCanFocus = false
     }
 
     /**
      * Reload list of accounts when this activity is resumed.
      */
-    @Override
-    public void onResume() {
-        super.onResume();
-        new LoadAccounts().execute();
+    public override fun onResume() {
+        super.onResume()
+        LoadAccounts().execute()
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        BaseAccount account = (BaseAccount) parent.getItemAtPosition(position);
-        onAccountSelected(account);
+    override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+        val account = parent.getItemAtPosition(position) as BaseAccount
+        onAccountSelected(account)
     }
 
     /**
-     * Create a new {@link AccountsAdapter} instance and assign it to the {@link ListView}.
+     * Create a new [AccountsAdapter] instance and assign it to the [ListView].
      *
      * @param realAccounts
-     *         An array of accounts to display.
+     * An array of accounts to display.
      */
-    public void populateListView(List<Account> realAccounts) {
-        List<BaseAccount> accounts = new ArrayList<>();
+    fun populateListView(realAccounts: List<Account>?) {
+        val accounts: MutableList<BaseAccount> = ArrayList()
 
-        if (K9.isShowUnifiedInbox()) {
-            BaseAccount unifiedInboxAccount = SearchAccount.createUnifiedInboxAccount();
-            accounts.add(unifiedInboxAccount);
+        if (isShowUnifiedInbox) {
+            val unifiedInboxAccount: BaseAccount = createUnifiedInboxAccount()
+            accounts.add(unifiedInboxAccount)
         }
 
-        accounts.addAll(realAccounts);
-        AccountsAdapter adapter = new AccountsAdapter(accounts);
-        ListView listView = getListView();
-        listView.setAdapter(adapter);
-        listView.invalidate();
+        accounts.addAll(realAccounts!!)
+        val adapter: AccountsAdapter = AccountsAdapter(accounts)
+        val listView = listView
+        listView.adapter = adapter
+        listView.invalidate()
     }
 
     /**
      * This method will be called when an account was selected.
      *
      * @param account
-     *         The account the user selected.
+     * The account the user selected.
      */
-    protected abstract void onAccountSelected(BaseAccount account);
+    protected abstract fun onAccountSelected(account: BaseAccount?)
 
-    class AccountsAdapter extends ArrayAdapter<BaseAccount> {
-        public AccountsAdapter(List<BaseAccount> accounts) {
-            super(AccountList.this, 0, accounts);
-        }
+    internal inner class AccountsAdapter(accounts: List<BaseAccount?>?) : ArrayAdapter<BaseAccount?>(
+        this@AccountList, 0, accounts!!,
+    ) {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val account = getItem(position)
+            val view = convertView ?: layoutInflater.inflate(R.layout.accounts_item, parent, false)
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            final BaseAccount account = getItem(position);
-
-            final View view;
-            if (convertView != null) {
-                view = convertView;
-            } else {
-                view = getLayoutInflater().inflate(R.layout.accounts_item, parent, false);
-            }
-
-            AccountViewHolder holder = (AccountViewHolder) view.getTag();
+            var holder: AccountViewHolder? = view.tag as AccountViewHolder
             if (holder == null) {
-                holder = new AccountViewHolder();
-                holder.description = view.findViewById(R.id.description);
-                holder.email = view.findViewById(R.id.email);
-                holder.chip = view.findViewById(R.id.chip);
+                holder = AccountViewHolder()
+                holder!!.description = view.findViewById<MaterialTextView>(R.id.description)
+                holder.email = view.findViewById<MaterialTextView>(R.id.email)
+                holder.chip = view.findViewById<View>(R.id.chip)
 
-                view.setTag(holder);
+                view.tag = holder
             }
 
-            String accountName = account.getName();
+            val accountName = account!!.name
             if (accountName != null) {
-                holder.description.setText(accountName);
-                holder.email.setText(account.getEmail());
-                holder.email.setVisibility(View.VISIBLE);
+                holder!!.description!!.text = accountName
+                holder.email!!.text = account.email
+                holder.email!!.visibility = View.VISIBLE
             } else {
-                holder.description.setText(account.getEmail());
-                holder.email.setVisibility(View.GONE);
+                holder!!.description!!.text = account.email
+                holder.email!!.visibility = View.GONE
             }
 
-            if (account instanceof Account) {
-                Account realAccount = (Account) account;
-                holder.chip.setBackgroundColor(realAccount.getChipColor());
+            if (account is Account) {
+                holder.chip!!.setBackgroundColor(account.chipColor)
             } else {
-                holder.chip.setBackgroundColor(0xff999999);
+                holder.chip!!.setBackgroundColor(0xff999999.toInt())
             }
 
-            holder.chip.getBackground().setAlpha(255);
+            holder.chip!!.background.alpha = 255
 
 
-            return view;
+            return view
         }
 
-        class AccountViewHolder {
-            public MaterialTextView description;
-            public MaterialTextView email;
-            public View chip;
+        internal inner class AccountViewHolder {
+            var description: MaterialTextView? = null
+            var email: MaterialTextView? = null
+            var chip: View? = null
         }
     }
 
     /**
      * Load accounts in a background thread
      */
-    class LoadAccounts extends AsyncTask<Void, Void, List<Account>> {
-        @Override
-        protected List<Account> doInBackground(Void... params) {
-            return Preferences.getPreferences().getAccounts();
+    internal inner class LoadAccounts : AsyncTask<Void?, Void?, List<Account>>() {
+        override fun doInBackground(vararg params: Void?): List<Account> {
+            return getPreferences().getAccounts()
         }
 
-        @Override
-        protected void onPostExecute(List<Account> accounts) {
-            populateListView(accounts);
+        override fun onPostExecute(accounts: List<Account>) {
+            populateListView(accounts)
         }
     }
 }
