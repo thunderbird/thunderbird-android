32037eb938052ea9a53bfe70ea036ff26e7e1f0f
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-06-27 17:38:19 +0200

    Version 6.904

--- app-k9mail/build.gradle.kts
+++ app-k9mail/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.fsck.k9"
         testApplicationId = "com.fsck.k9.tests"
 
-        versionCode = 39003
-        versionName = "6.904-SNAPSHOT"
+        versionCode = 39004
+        versionName = "6.904"
 
         // Keep in sync with the resource string array "supported_languages"
         resourceConfigurations.addAll(
