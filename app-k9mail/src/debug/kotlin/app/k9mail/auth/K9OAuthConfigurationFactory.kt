4676efb678d2dafeb7e5e2feb7103db12c15aebd
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-07-29 15:25:51 +0200

    Change K-9 Mail OAuth configuration

--- app-k9mail/src/debug/kotlin/app/k9mail/auth/K9OAuthConfigurationFactory.kt
+++ app-k9mail/src/debug/kotlin/app/k9mail/auth/K9OAuthConfigurationFactory.kt
@@ -4,6 +4,7 @@ import app.k9mail.core.common.oauth.OAuthConfiguration
 import app.k9mail.core.common.oauth.OAuthConfigurationFactory
 import com.fsck.k9.BuildConfig
 
+@Suppress("ktlint:standard:max-line-length")
 class K9OAuthConfigurationFactory : OAuthConfigurationFactory {
     override fun createConfigurations(): Map<List<String>, OAuthConfiguration> {
         return mapOf(
@@ -15,8 +16,11 @@ class K9OAuthConfigurationFactory : OAuthConfigurationFactory {
     }
 
     private fun createAolConfiguration(): Pair<List<String>, OAuthConfiguration> {
-        return listOf("imap.aol.com", "smtp.aol.com") to OAuthConfiguration(
-            clientId = BuildConfig.OAUTH_AOL_CLIENT_ID,
+        return listOf(
+            "imap.aol.com",
+            "smtp.aol.com",
+        ) to OAuthConfiguration(
+            clientId = "dj0yJmk9cHYydkJkTUxHcXlYJmQ9WVdrOWVHZHhVVXN4VVV3bWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTdm",
             scopes = listOf("mail-w"),
             authorizationEndpoint = "https://api.login.aol.com/oauth2/request_auth",
             tokenEndpoint = "https://api.login.aol.com/oauth2/get_token",
@@ -31,7 +35,7 @@ class K9OAuthConfigurationFactory : OAuthConfigurationFactory {
             "smtp.gmail.com",
             "smtp.googlemail.com",
         ) to OAuthConfiguration(
-            clientId = BuildConfig.OAUTH_GMAIL_CLIENT_ID,
+            clientId = "262622259280-5qb3vtj68d5dtudmaif4g9vd3cpar8r3.apps.googleusercontent.com",
             scopes = listOf("https://mail.google.com/"),
             authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth",
             tokenEndpoint = "https://oauth2.googleapis.com/token",
@@ -44,7 +48,7 @@ class K9OAuthConfigurationFactory : OAuthConfigurationFactory {
             "outlook.office365.com",
             "smtp.office365.com",
         ) to OAuthConfiguration(
-            clientId = BuildConfig.OAUTH_MICROSOFT_CLIENT_ID,
+            clientId = "e647013a-ada4-4114-b419-e43d250f99c5",
             scopes = listOf(
                 "https://outlook.office.com/IMAP.AccessAsUser.All",
                 "https://outlook.office.com/SMTP.Send",
@@ -52,7 +56,7 @@ class K9OAuthConfigurationFactory : OAuthConfigurationFactory {
             ),
             authorizationEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
             tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token",
-            redirectUri = BuildConfig.OAUTH_MICROSOFT_REDIRECT_URI,
+            redirectUri = "msauth://com.fsck.k9.debug/VZF2DYuLYAu4TurFd6usQB2JPts%3D",
         )
     }
 
@@ -61,7 +65,7 @@ class K9OAuthConfigurationFactory : OAuthConfigurationFactory {
             "imap.mail.yahoo.com",
             "smtp.mail.yahoo.com",
         ) to OAuthConfiguration(
-            clientId = BuildConfig.OAUTH_YAHOO_CLIENT_ID,
+            clientId = "dj0yJmk9ejRCRU1ybmZjQlVBJmQ9WVdrOVVrZEViak4xYmxZbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTZj",
             scopes = listOf("mail-w"),
             authorizationEndpoint = "https://api.login.yahoo.com/oauth2/request_auth",
             tokenEndpoint = "https://api.login.yahoo.com/oauth2/get_token",
