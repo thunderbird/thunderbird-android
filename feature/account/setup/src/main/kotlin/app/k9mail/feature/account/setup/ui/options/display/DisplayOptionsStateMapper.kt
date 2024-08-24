2362c7ca49eaa80620bea0c62f0b412dc304f12f
SuGotLand <sugotland@outlook.com>
2024-08-25 02:04:01 +0800

    Add displayName and emailSignature when displayOption is null.

--- feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/ui/options/display/DisplayOptionsStateMapper.kt
+++ feature/account/setup/src/main/kotlin/app/k9mail/feature/account/setup/ui/options/display/DisplayOptionsStateMapper.kt
@@ -1,17 +1,20 @@
 package app.k9mail.feature.account.setup.ui.options.display
 
+import android.preference.Preference
 import app.k9mail.feature.account.common.domain.entity.AccountDisplayOptions
 import app.k9mail.feature.account.common.domain.entity.AccountState
 import app.k9mail.feature.account.common.domain.input.StringInputField
 import app.k9mail.feature.account.setup.ui.options.display.DisplayOptionsContract.State
+import app.k9mail.feature.account.setup
 
 internal fun AccountState.toDisplayOptionsState(): State {
     val options = displayOptions
     return if (options == null) {
         State(
             accountName = StringInputField(emailAddress ?: ""),
-            // displayName = StringInputField(""),
             // TODO: get display name from: preferences.defaultAccount?.senderName ?: ""
+            displayName = StringInputField(""),
+            emailSignature = StringInputField(""),
         )
     } else {
         State(
