74653a7bd87bdd2fdaaad81c575ebc4b053cc942
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-09-12 12:49:47 +0200

    Add optional icon to NavigationDrawerItemBadge

--- core/ui/compose/designsystem/src/debug/kotlin/app/k9mail/core/ui/compose/designsystem/organism/drawer/NavigationDrawerItemBadgePreview.kt
+++ core/ui/compose/designsystem/src/debug/kotlin/app/k9mail/core/ui/compose/designsystem/organism/drawer/NavigationDrawerItemBadgePreview.kt
@@ -3,13 +3,25 @@ package app.k9mail.core.ui.compose.designsystem.organism.drawer
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import app.k9mail.core.ui.compose.designsystem.PreviewWithThemes
+import app.k9mail.core.ui.compose.designsystem.atom.icon.Icons
 
 @Composable
 @Preview(showBackground = true)
 internal fun NavigationDrawerItemBadgePreview() {
     PreviewWithThemes {
         NavigationDrawerItemBadge(
-            label = "100+",
+            label = "99+",
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+internal fun NavigationDrawerItemBadgeWithIconPreview() {
+    PreviewWithThemes {
+        NavigationDrawerItemBadge(
+            label = "99+",
+            imageVector = Icons.Outlined.Info,
         )
     }
 }
