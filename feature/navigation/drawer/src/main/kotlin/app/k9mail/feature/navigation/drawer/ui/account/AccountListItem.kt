4a97c52e05afb5986fa0e13848c850d85a008e94
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-09-18 14:54:46 +0200

    Add AccountListItem

---
+++ feature/navigation/drawer/src/main/kotlin/app/k9mail/feature/navigation/drawer/ui/account/AccountListItem.kt
@@ -0,0 +1,28 @@
+package app.k9mail.feature.navigation.drawer.ui.account
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import app.k9mail.core.ui.compose.theme2.MainTheme
+import app.k9mail.feature.navigation.drawer.domain.entity.DisplayAccount
+
+@Composable
+fun AccountListItem(
+    account: DisplayAccount,
+    onClick: (DisplayAccount) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier.width(MainTheme.sizes.large)
+            .padding(vertical = MainTheme.spacings.half),
+        contentAlignment = Alignment.Center,
+    ) {
+        AccountAvatar(
+            account = account,
+            onClick = onClick,
+        )
+    }
+}
