7becd9e6336304dfa89ac3d30f38fdbabd1e33f2
ShellWen <me@shellwen.com>
2024-10-01 03:46:37 +0800

    Replaced deprecated kotlinOptions with compilerOptions

--- build-plugin/src/main/kotlin/KotlinExtension.kt
+++ build-plugin/src/main/kotlin/KotlinExtension.kt
@@ -1,11 +1,12 @@
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun Project.configureKotlinJavaCompatibility() {
     tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = ThunderbirdProjectConfig.javaCompatibilityVersion.toString()
+        compilerOptions {
+            jvmTarget.set(JvmTarget.fromTarget(ThunderbirdProjectConfig.javaCompatibilityVersion.toString()))
         }
     }
 }
