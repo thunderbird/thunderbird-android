e18cdf2566e155a0a91695466dea40dca6858187
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-10-01 16:04:36 +0200

    Add SettingsList entry for starting the support page
    (cherry picked from commit 06971a178762a0f0c1e8cca222d6cfef80869e8c)

--- legacy/ui/legacy/src/main/java/com/fsck/k9/ui/settings/SettingsListFragment.kt
+++ legacy/ui/legacy/src/main/java/com/fsck/k9/ui/settings/SettingsListFragment.kt
@@ -15,7 +15,9 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import app.k9mail.core.common.provider.AppNameProvider
 import app.k9mail.core.ui.legacy.designsystem.atom.icon.Icons
+import app.k9mail.feature.funding.api.FundingManager
 import app.k9mail.feature.launcher.FeatureLauncherActivity
 import app.k9mail.feature.launcher.FeatureLauncherTarget
 import app.k9mail.legacy.account.Account
@@ -29,11 +31,14 @@ import com.mikepenz.fastadapter.adapters.ItemAdapter
 import com.mikepenz.fastadapter.drag.ItemTouchCallback
 import com.mikepenz.fastadapter.drag.SimpleDragCallback
 import com.mikepenz.fastadapter.utils.DragDropUtil
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import app.k9mail.feature.settings.importing.R as SettingsImportR
 
 class SettingsListFragment : Fragment(), ItemTouchCallback {
     private val viewModel: SettingsViewModel by viewModel()
+    private val fundingManager: FundingManager by inject()
+    private val appNameProvider: AppNameProvider by inject()
 
     private lateinit var itemAdapter: ItemAdapter<GenericItem>
 
@@ -131,6 +136,20 @@ class SettingsListFragment : Fragment(), ItemTouchCallback {
                     url = getString(R.string.user_forum_url),
                     icon = Icons.Outlined.Help,
                 )
+
+                if (fundingManager.isFundingFeatureIncluded()) {
+                    addIntent(
+                        text = getString(R.string.settings_list_action_support, appNameProvider.appName),
+                        icon = Icons.Outlined.Favorite,
+                        intent = FeatureLauncherActivity.getIntent(requireActivity(), FeatureLauncherTarget.Funding),
+                    )
+                } else {
+                    addUrlAction(
+                        text = getString(R.string.settings_list_action_support, appNameProvider.appName),
+                        url = getString(R.string.donate_url),
+                        icon = Icons.Outlined.Favorite,
+                    )
+                }
             }
         }
 
@@ -140,8 +159,9 @@ class SettingsListFragment : Fragment(), ItemTouchCallback {
     private fun handleItemClick(item: GenericItem) {
         when (item) {
             is AccountItem -> launchAccountSettings(item.account)
-            is UrlActionItem -> openUrl(item.url)
+            is IntentItem -> startActivity(item.intent)
             is SettingsActionItem -> findNavController().navigate(item.navigationAction)
+            is UrlActionItem -> openUrl(item.url)
         }
     }
 
@@ -184,6 +204,11 @@ class SettingsListFragment : Fragment(), ItemTouchCallback {
 
         fun addAccount(account: Account, isDraggable: Boolean) {
             settingsList.add(AccountItem(account, isDraggable))
+        }
+
+        fun addIntent(text: String, @DrawableRes icon: Int, intent: Intent) {
+            itemId++
+            settingsList.add(IntentItem(itemId, text, icon, intent))
         }
 
         fun addSection(title: String, block: SettingsListBuilder.() -> Unit) {
