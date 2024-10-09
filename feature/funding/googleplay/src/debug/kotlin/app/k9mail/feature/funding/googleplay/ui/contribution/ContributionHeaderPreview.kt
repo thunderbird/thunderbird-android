0473e3da715dd7054867c2a1a2c89b5cc4fa85e9
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-10-09 15:12:44 +0200

    Add ContributionHeader

---
+++ feature/funding/googleplay/src/debug/kotlin/app/k9mail/feature/funding/googleplay/ui/contribution/ContributionHeaderPreview.kt
@@ -0,0 +1,13 @@
+package app.k9mail.feature.funding.googleplay.ui.contribution
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import app.k9mail.core.ui.compose.designsystem.PreviewWithTheme
+
+@Composable
+@Preview(showBackground = true)
+internal fun ContributionHeaderPreview() {
+    PreviewWithTheme {
+        ContributionHeader()
+    }
+}
