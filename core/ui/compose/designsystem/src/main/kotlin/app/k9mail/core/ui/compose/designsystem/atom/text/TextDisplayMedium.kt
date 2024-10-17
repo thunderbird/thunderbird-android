815c219441522d8d25b633da20eec98d4284ee43
shamim-emon <emon.dev.bd@gmail.com>
2024-10-17 07:41:48 +0600

    Fix of "Title wraps within word in "Improve the Experience" screen - Added AutoResizeableTextDisplayMedium"

--- core/ui/compose/designsystem/src/main/kotlin/app/k9mail/core/ui/compose/designsystem/atom/text/TextDisplayMedium.kt
+++ core/ui/compose/designsystem/src/main/kotlin/app/k9mail/core/ui/compose/designsystem/atom/text/TextDisplayMedium.kt
@@ -1,10 +1,17 @@
 package app.k9mail.core.ui.compose.designsystem.atom.text
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.isUnspecified
 import app.k9mail.core.ui.compose.theme2.MainTheme
 import androidx.compose.material3.Text as Material3Text
 
@@ -37,5 +44,45 @@ fun TextDisplayMedium(
         color = color,
         textAlign = textAlign,
         style = MainTheme.typography.displayMedium,
+    )
+}
+
+@Composable
+fun AutoResizeableTextDisplayMedium(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    textAlign: TextAlign? = null,
+    style: TextStyle = MainTheme.typography.displayMedium,
+) {
+    var shouldDraw by remember { mutableStateOf(false) }
+    var resizedTextStyle by remember { mutableStateOf(style) }
+    val defaultFontSize = style.fontSize
+
+    Material3Text(
+        text = text,
+        modifier = modifier.drawWithContent {
+            if (shouldDraw) {
+                drawContent()
+            }
+        },
+        color = color,
+        textAlign = textAlign,
+        softWrap = false,
+        style = resizedTextStyle,
+        onTextLayout = { result ->
+            if (result.didOverflowWidth) {
+                if (style.fontSize.isUnspecified) {
+                    resizedTextStyle = resizedTextStyle.copy(
+                        fontSize = defaultFontSize,
+                    )
+                }
+                resizedTextStyle = resizedTextStyle.copy(
+                    fontSize = resizedTextStyle.fontSize * 0.95,
+                )
+            } else {
+                shouldDraw = true
+            }
+        },
     )
 }
--- feature/account/common/src/main/kotlin/app/k9mail/feature/account/common/ui/AppTitleTopHeader.kt
+++ feature/account/common/src/main/kotlin/app/k9mail/feature/account/common/ui/AppTitleTopHeader.kt
@@ -1,7 +1,8 @@
 package app.k9mail.feature.account.common.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -9,9 +10,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import app.k9mail.core.ui.compose.designsystem.atom.text.TextDisplayMedium
+import app.k9mail.core.ui.compose.designsystem.atom.text.AutoResizeableTextDisplayMedium
 import app.k9mail.core.ui.compose.designsystem.template.ResponsiveWidthContainer
 import app.k9mail.core.ui.compose.theme2.MainTheme
 
@@ -31,15 +31,16 @@ fun AppTitleTopHeader(
             )
             .then(modifier),
     ) {
-        Column(
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(
-                    start = MainTheme.spacings.double,
-                    end = MainTheme.spacings.double,
+                    start = MainTheme.spacings.half,
+                    end = MainTheme.spacings.quadruple,
                 )
                 .then(modifier),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
         ) {
             Image(
                 painter = painterResource(id = MainTheme.images.logo),
@@ -50,7 +51,7 @@ fun AppTitleTopHeader(
                 contentDescription = null,
             )
 
-            TextDisplayMedium(text = title, textAlign = TextAlign.Center)
+            AutoResizeableTextDisplayMedium(text = title)
         }
     }
 }
