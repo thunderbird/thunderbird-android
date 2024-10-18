1d59a29b8582b7147ea7aea2730fd300c876f601
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-10-18 14:01:32 +0200

    Add funding reminder dialog
    (cherry picked from commit 35dd88b2423a5de98525dc40dc95dff149868baa)
    (cherry picked from commit d01b1b05f5c297f5d4cc15558be5c81a4b0cea8c)

--- feature/funding/link/src/main/kotlin/app/k9mail/feature/funding/link/LinkFundingManager.kt
+++ feature/funding/link/src/main/kotlin/app/k9mail/feature/funding/link/LinkFundingManager.kt
@@ -1,5 +1,7 @@
 package app.k9mail.feature.funding.link
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import app.k9mail.feature.funding.api.FundingManager
 import app.k9mail.feature.funding.api.FundingType
 
@@ -7,4 +9,6 @@ class LinkFundingManager : FundingManager {
     override fun getFundingType(): FundingType {
         return FundingType.LINK
     }
+
+    override fun addFundingReminder(activity: AppCompatActivity, launcherBaseIntent: Intent) = Unit
 }
