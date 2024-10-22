77f98217bda728686001418c0803fba7b3d24a69
Shamim Shahrier Emon <emon.dev.bd@gmail.com>
2024-10-23 00:50:36 +0600

    Update legacy/core/src/main/java/com/fsck/k9/preferences/SettingsExporter.kt
    Co-authored-by: cketti <ck@cketti.de>

--- legacy/core/src/main/java/com/fsck/k9/preferences/SettingsExporter.kt
+++ legacy/core/src/main/java/com/fsck/k9/preferences/SettingsExporter.kt
@@ -483,7 +483,7 @@ class SettingsExporter(
     fun generateDatedExportFileName(): String {
         val now = Calendar.getInstance()
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.US)
-        return String.format("%s_%s.%s", filePrefixProvider.filePrefix, dateFormat.format(now.time), EXPORT_FILENAME_SUFFIX)
+        return String.format("%s_%s_%s.%s", EXPORT_FILENAME_MIDDLE_PART, filePrefixProvider.filePrefix, dateFormat.format(now.time), EXPORT_FILENAME_SUFFIX)
     }
 
     companion object {
