dcff2a497e160f34f054f11a964d5f542e8b711b
Wolf-Martell Montwé <wolf@thunderbird.net>
2024-11-13 13:34:42 +0100

    Enable funding for K-9 Mail Full

--- app-thunderbird/src/main/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
+++ app-thunderbird/src/main/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
@@ -2,12 +2,9 @@ package net.thunderbird.android.featureflag
 
 import app.k9mail.core.featureflag.FeatureFlag
 import app.k9mail.core.featureflag.FeatureFlagFactory
-import app.k9mail.core.featureflag.FeatureFlagKey
 
 class TbFeatureFlagFactory : FeatureFlagFactory {
     override fun createFeatureCatalog(): List<FeatureFlag> {
-        return listOf(
-            FeatureFlag(FeatureFlagKey("funding_google_play"), true),
-        )
+        return listOf()
     }
 }
