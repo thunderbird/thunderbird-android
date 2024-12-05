352718c2fcf42decf23b6cab52799463dbda8e88
cketti <cketti@thunderbird.net>
2024-12-05 14:59:25 +0100

    Move to release channel/build type specific feature flags

---
+++ app-thunderbird/src/daily/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
@@ -0,0 +1,13 @@
+package net.thunderbird.android.featureflag
+
+import app.k9mail.core.featureflag.FeatureFlag
+import app.k9mail.core.featureflag.FeatureFlagFactory
+
+/**
+ * Feature flags for Thunderbird Daily
+ */
+class TbFeatureFlagFactory : FeatureFlagFactory {
+    override fun createFeatureCatalog(): List<FeatureFlag> {
+        return listOf()
+    }
+}
--- app-thunderbird/src/debug/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
+++ app-thunderbird/src/debug/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
@@ -3,6 +3,9 @@ package net.thunderbird.android.featureflag
 import app.k9mail.core.featureflag.FeatureFlag
 import app.k9mail.core.featureflag.FeatureFlagFactory
 
+/**
+ * Feature flags for Thunderbird Debug
+ */
 class TbFeatureFlagFactory : FeatureFlagFactory {
     override fun createFeatureCatalog(): List<FeatureFlag> {
         return listOf()
