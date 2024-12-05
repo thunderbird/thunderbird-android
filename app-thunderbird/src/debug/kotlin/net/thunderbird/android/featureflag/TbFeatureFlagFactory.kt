49ce9c065ef3f85412ddf74a1d0dd418b2e1f5bf
cketti <cketti@thunderbird.net>
2024-12-05 15:34:10 +0100

    Add `archive_marks_as_read` feature flag
    For now we enable the functionality only in Thunderbird Beta/Daily/Debug.

--- app-thunderbird/src/debug/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
+++ app-thunderbird/src/debug/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
@@ -2,12 +2,15 @@ package net.thunderbird.android.featureflag
 
 import app.k9mail.core.featureflag.FeatureFlag
 import app.k9mail.core.featureflag.FeatureFlagFactory
+import app.k9mail.core.featureflag.toFeatureFlagKey
 
 /**
  * Feature flags for Thunderbird Debug
  */
 class TbFeatureFlagFactory : FeatureFlagFactory {
     override fun createFeatureCatalog(): List<FeatureFlag> {
-        return listOf()
+        return listOf(
+            FeatureFlag("archive_marks_as_read".toFeatureFlagKey(), enabled = true),
+        )
     }
 }
