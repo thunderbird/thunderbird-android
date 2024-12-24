cc62711b02b800fef713f2b9185363a7cabd4fdd
Clément Rivière <criviere@teicee.com>
2024-12-24 12:22:14 +0100

    feat: adding TreeFolder to manage folders trees

---
+++ feature/navigation/drawer/dropdown/src/main/kotlin/net/thunderbird/feature/navigation/drawer/dropdown/domain/entity/TreeFolder.kt
@@ -0,0 +1,24 @@
+package net.thunderbird.feature.navigation.drawer.dropdown.domain.entity
+
+internal data class TreeFolder(
+    var value: DisplayFolder? = null
+) {
+    val children: ArrayList<TreeFolder> = ArrayList()
+
+    fun getAllUnreadMessageCount(): Int {
+        var allUnreadMessageCount = 0
+        for (child in children) {
+            allUnreadMessageCount += child.getAllUnreadMessageCount()
+        }
+        return allUnreadMessageCount + (value?.unreadMessageCount ?: 0)
+    }
+
+    fun getAllStarredMessageCount(): Int {
+        var allStarredMessageCount = 0
+        for (child in children) {
+            allStarredMessageCount += child.getAllStarredMessageCount()
+        }
+        return allStarredMessageCount + (value?.starredMessageCount ?: 0)
+    }
+}
+
