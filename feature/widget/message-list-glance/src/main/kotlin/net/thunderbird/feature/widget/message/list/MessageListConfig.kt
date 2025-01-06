7659f6952f6572480e486eed17894b7226133629
Vayun <69178052+vayun-mathur@users.noreply.github.com>
2025-01-06 13:46:57 +0800

    Add new list widget

---
+++ feature/widget/message-list-glance/src/main/kotlin/net/thunderbird/feature/widget/message/list/MessageListConfig.kt
@@ -0,0 +1,12 @@
+package net.thunderbird.feature.widget.message.list
+
+import app.k9mail.legacy.account.Account.SortType
+import app.k9mail.legacy.search.LocalSearch
+
+internal data class MessageListConfig(
+    val search: LocalSearch,
+    val showingThreadedList: Boolean,
+    val sortType: SortType,
+    val sortAscending: Boolean,
+    val sortDateAscending: Boolean,
+)
