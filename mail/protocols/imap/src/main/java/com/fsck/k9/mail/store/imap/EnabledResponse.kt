2db6a43e9433a66fc80401df307be1cae0ddcbe3
Arnt Gulbrandsen <arnt@gulbrandsen.priv.no>
2025-01-21 13:38:46 +0100

    Add support for RFCs 5530 and 9755.
    5530 (ENABLE) lets a client tell the server which extensions it would like
    to use, and lets the server tell the client which extensions is has
    enabled. Most IMAP extensions don't need to be enabled, but UTF8=ACCEPT
    does.
    
    9755 (UTF8=ACCEPT) requires four things of clients:
    1. The client uses ENABLE (added with tests)
    2. The client accepts UTF8 strings (worked already, this adds testing)
    3. The client cannot use certain SEARCH syntax (tb already did not)
    4. The client uses UTF8 for folder names instead of mUTF7
    
    This commit contains a couple of unit tests that do nothing right now,
    they merely guard against possible future breakage.

---
+++ mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/EnabledResponse.kt
@@ -0,0 +1,33 @@
+package com.fsck.k9.mail.store.imap
+
+import com.fsck.k9.mail.store.imap.ImapResponseParser.equalsIgnoreCase
+import java.util.Locale
+
+internal class EnabledResponse private constructor(val capabilities: Set<String>) {
+
+    companion object {
+        fun parse(responses: List<ImapResponse>): EnabledResponse? {
+            var result: EnabledResponse? = null
+            for (response in responses) {
+                if (result == null && response.tag == null) {
+                    result = parse(response)
+                }
+            }
+            return result
+        }
+
+        private fun parse(capabilityList: ImapList): EnabledResponse? {
+            if (capabilityList.isEmpty() || !equalsIgnoreCase(capabilityList[0], Responses.ENABLED)) {
+                return null
+            }
+            val capabilities = mutableSetOf<String>()
+            for (i in 1 until capabilityList.size) {
+                if (!capabilityList.isString(i)) {
+                    return null
+                }
+                capabilities.add(capabilityList.getString(i).uppercase(Locale.US))
+            }
+            return EnabledResponse(capabilities)
+        }
+    }
+}
