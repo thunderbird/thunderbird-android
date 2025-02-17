ee0b55ce046853c98d1a45e1adedb8c57a995dc4
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-02-17 19:07:55 +0100

    Change CommandSetFlag to Kotlin

--- backend/pop3/src/main/java/com/fsck/k9/backend/pop3/CommandSetFlag.kt
+++ backend/pop3/src/main/java/com/fsck/k9/backend/pop3/CommandSetFlag.kt
@@ -1,47 +1,41 @@
-package com.fsck.k9.backend.pop3;
+package com.fsck.k9.backend.pop3
 
+import com.fsck.k9.mail.Flag
+import com.fsck.k9.mail.MessagingException
+import com.fsck.k9.mail.store.pop3.Pop3Message
+import com.fsck.k9.mail.store.pop3.Pop3Store
+import java.util.ArrayList
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+internal class CommandSetFlag(pop3Store: Pop3Store) {
+    private val pop3Store: Pop3Store
 
-import com.fsck.k9.mail.Flag;
-import com.fsck.k9.mail.MessagingException;
-import com.fsck.k9.mail.store.pop3.Pop3Folder;
-import com.fsck.k9.mail.store.pop3.Pop3Message;
-import com.fsck.k9.mail.store.pop3.Pop3Store;
-import org.jetbrains.annotations.NotNull;
-
-
-class CommandSetFlag {
-    private final Pop3Store pop3Store;
-
-
-    CommandSetFlag(Pop3Store pop3Store) {
-        this.pop3Store = pop3Store;
+    init {
+        this.pop3Store = pop3Store
     }
 
-    void setFlag(@NotNull String folderServerId, @NotNull List<String> messageServerIds, @NotNull Flag flag,
-            boolean newState) throws MessagingException {
-
-        Pop3Folder remoteFolder = pop3Store.getFolder(folderServerId);
+    @Throws(MessagingException::class)
+    fun setFlag(
+        folderServerId: String, messageServerIds: MutableList<String?>, flag: Flag,
+        newState: Boolean
+    ) {
+        val remoteFolder = pop3Store.getFolder(folderServerId)
         if (!remoteFolder.isFlagSupported(flag)) {
-            return;
+            return
         }
 
         try {
-            remoteFolder.open();
-            List<Pop3Message> messages = new ArrayList<>();
-            for (String uid : messageServerIds) {
-                messages.add(remoteFolder.getMessage(uid));
+            remoteFolder.open()
+            val messages: MutableList<Pop3Message?> = ArrayList<Pop3Message?>()
+            for (uid in messageServerIds) {
+                messages.add(remoteFolder.getMessage(uid))
             }
 
             if (messages.isEmpty()) {
-                return;
+                return
             }
-            remoteFolder.setFlags(messages, Collections.singleton(flag), newState);
+            remoteFolder.setFlags(messages, mutableSetOf<Flag?>(flag), newState)
         } finally {
-            remoteFolder.close();
+            remoteFolder.close()
         }
     }
 }
