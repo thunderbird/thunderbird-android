351bd9b14f510a415640eefba6f669531b49eb99
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-03-11 17:17:59 +0100

    Remove obsolete navigation extensions

--- core/ui/compose/common/src/main/kotlin/app/k9mail/core/ui/compose/common/navigation/NavBackStackEntryExtension.kt
+++
@@ -1,7 +0,0 @@
-package app.k9mail.core.ui.compose.common.navigation
-
-import androidx.navigation.NavBackStackEntry
-
-fun NavBackStackEntry.getStringArgument(key: String): String {
-    return arguments?.getString(key) ?: error("Missing argument: $key")
-}
