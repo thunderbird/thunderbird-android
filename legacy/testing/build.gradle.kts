8724150d5d405f94311608714e3394dd6691a0b4
shamim-emon <emon.dev.bd@gmail.com>
2025-04-14 06:29:37 +0600

    Refactor: Removed dependencies that are no longer needed from :legacy:testing module

--- legacy/testing/build.gradle.kts
+++ legacy/testing/build.gradle.kts
@@ -4,12 +4,6 @@ plugins {
 
 dependencies {
     implementation(projects.legacy.core)
-
-    api(projects.core.android.testing)
-
-    api(libs.koin.core)
-    api(libs.mockito.core)
-    api(libs.mockito.kotlin)
 }
 
 android {
