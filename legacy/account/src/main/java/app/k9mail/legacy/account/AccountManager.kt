526fb56bcdffaa5e5a25363839c296d2bad1c6ba
Rafael Tonholo <rafael@tonholo.dev>
2025-05-08 15:20:00 -0300

    refactor: extract AccountManager interface to `core/account`
    - Mark the old `AccountManager` interface in `legacy/account` as deprecated and make it extend the new `AccountManager` interface.
    - Bind `net.thunderbird.core.account.AccountManager` to the existing `AccountManager` implementation.

--- legacy/account/src/main/java/app/k9mail/legacy/account/AccountManager.kt
+++ legacy/account/src/main/java/app/k9mail/legacy/account/AccountManager.kt
@@ -1,15 +1,24 @@
 package app.k9mail.legacy.account
 
 import kotlinx.coroutines.flow.Flow
+import net.thunderbird.core.account.AccountManager
 
-interface AccountManager {
-    fun getAccounts(): List<LegacyAccount>
-    fun getAccountsFlow(): Flow<List<LegacyAccount>>
-    fun getAccount(accountUuid: String): LegacyAccount?
-    fun getAccountFlow(accountUuid: String): Flow<LegacyAccount?>
+@Deprecated(
+    message = "Use net.thunderbird.core.account.AccountManager<TAccount : BaseAccount> instead",
+    replaceWith = ReplaceWith(
+        expression = "AccountManager<LegacyAccount>",
+        "net.thunderbird.core.account.AccountManager",
+        "app.k9mail.legacy.account.LegacyAccount",
+    ),
+)
+interface AccountManager : AccountManager<LegacyAccount> {
+    override fun getAccounts(): List<LegacyAccount>
+    override fun getAccountsFlow(): Flow<List<LegacyAccount>>
+    override fun getAccount(accountUuid: String): LegacyAccount?
+    override fun getAccountFlow(accountUuid: String): Flow<LegacyAccount?>
     fun addAccountRemovedListener(listener: AccountRemovedListener)
-    fun moveAccount(account: LegacyAccount, newPosition: Int)
+    override fun moveAccount(account: LegacyAccount, newPosition: Int)
     fun addOnAccountsChangeListener(accountsChangeListener: AccountsChangeListener)
     fun removeOnAccountsChangeListener(accountsChangeListener: AccountsChangeListener)
-    fun saveAccount(account: LegacyAccount)
+    override fun saveAccount(account: LegacyAccount)
 }
