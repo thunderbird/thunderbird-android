2fbcfc346e07b2922d2f01205a5b1435a8f631cd
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-05-09 18:14:50 +0200

    feat(account): Add account name to general account settings

--- feature/account/settings/impl/src/main/kotlin/net/thunderbird/feature/account/settings/impl/ui/general/GeneralSettingsViewModel.kt
+++ feature/account/settings/impl/src/main/kotlin/net/thunderbird/feature/account/settings/impl/ui/general/GeneralSettingsViewModel.kt
@@ -7,18 +7,35 @@ import net.thunderbird.core.outcome.handle
 import net.thunderbird.core.ui.compose.preference.api.PreferenceSetting
 import net.thunderbird.feature.account.api.AccountId
 import net.thunderbird.feature.account.settings.impl.domain.AccountSettingsDomainContract.UseCase
+import net.thunderbird.feature.account.settings.impl.domain.usecase.GetAccountName
 import net.thunderbird.feature.account.settings.impl.ui.general.GeneralSettingsContract.Effect
 import net.thunderbird.feature.account.settings.impl.ui.general.GeneralSettingsContract.Event
 import net.thunderbird.feature.account.settings.impl.ui.general.GeneralSettingsContract.State
 
 internal class GeneralSettingsViewModel(
     private val accountId: AccountId,
+    private val getAccountName: UseCase.GetAccountName,
     private val getGeneralPreferences: UseCase.GetGeneralPreferences,
     private val updateGeneralPreferences: UseCase.UpdateGeneralPreferences,
     initialState: State = State(),
 ) : BaseViewModel<State, Event, Effect>(initialState), GeneralSettingsContract.ViewModel {
 
     init {
+        viewModelScope.launch {
+            getAccountName(accountId).collect { outcome ->
+                outcome.handle(
+                    onSuccess = { accountName ->
+                        updateState { state ->
+                            state.copy(
+                                subtitle = accountName,
+                            )
+                        }
+                    },
+                    onFailure = {},
+                )
+            }
+        }
+
         viewModelScope.launch {
             getGeneralPreferences(accountId).collect { outcome ->
                 outcome.handle(
