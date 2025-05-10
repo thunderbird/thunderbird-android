50050c13c74d27c12c640ecb2841c0c74e551c00
shamim-emon <emon.dev.bd@gmail.com>
2025-05-10 16:54:05 +0600

    Added Tests for receiver input validation check

--- legacy/ui/legacy/src/test/java/com/fsck/k9/TestApp.kt
+++ legacy/ui/legacy/src/test/java/com/fsck/k9/TestApp.kt
@@ -7,6 +7,7 @@ import app.k9mail.core.featureflag.InMemoryFeatureFlagProvider
 import app.k9mail.feature.telemetry.telemetryModule
 import app.k9mail.legacy.account.AccountDefaultsProvider
 import app.k9mail.legacy.di.DI
+import com.fsck.k9.contacts.ContactPictureLoader
 import com.fsck.k9.preferences.StoragePersister
 import net.thunderbird.core.android.preferences.InMemoryStoragePersister
 import org.koin.dsl.module
@@ -40,4 +41,6 @@ val testModule = module {
             },
         )
     }
+
+    single<ContactPictureLoader> { mock() }
 }
