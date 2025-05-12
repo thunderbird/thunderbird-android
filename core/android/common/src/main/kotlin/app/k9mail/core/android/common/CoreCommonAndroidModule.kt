6c7f73e215f90d5611a6aaf51e77ad41bbac6e4c
Rafael Tonholo <rafael@tonholo.dev>
2025-05-12 10:57:26 -0300

    feat(resources): add ResourceManager enabling usage of string resources within ViewModels without directly requiring android's context

--- core/android/common/src/main/kotlin/app/k9mail/core/android/common/CoreCommonAndroidModule.kt
+++ core/android/common/src/main/kotlin/app/k9mail/core/android/common/CoreCommonAndroidModule.kt
@@ -2,11 +2,14 @@ package app.k9mail.core.android.common
 
 import app.k9mail.core.android.common.camera.cameraModule
 import app.k9mail.core.android.common.contact.contactModule
+import net.thunderbird.core.android.common.resources.resourcesAndroidModule
 import net.thunderbird.core.common.coreCommonModule
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val coreCommonAndroidModule: Module = module {
+    includes(resourcesAndroidModule)
+
     includes(coreCommonModule)
 
     includes(contactModule)
---
+++ core/common/src/commonMain/kotlin/net/thunderbird/core/common/resources/ResourceNotFoundException.kt
@@ -0,0 +1,8 @@
+package net.thunderbird.core.common.resources
+
+/**
+ * Exception thrown when a requested resource cannot be found.
+ * This can occur, for example, when trying to access a file or data
+ * that does not exist at the specified location.
+ */
+expect class ResourceNotFoundException
