8971ce56b194903aa671f57f8df84fa2b06019d9
Corey Bryant <corey@thunderbird.net>
2025-05-14 10:11:02 -0400

    Revert "Release: Thunderbird Beta 11.0b2"
    This reverts commit bb4c177500243c0683c1d0e8e3cdda9fddc8ae61.

--- app-thunderbird/build.gradle.kts
+++ app-thunderbird/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "net.thunderbird.android"
         testApplicationId = "net.thunderbird.android.tests"
 
-        versionCode = 15
+        versionCode = 14
         versionName = "11.0"
 
         // Keep in sync with the resource string array "supported_languages"
@@ -118,7 +118,7 @@ android {
             signingConfig = signingConfigs.getByType(SigningType.TB_BETA)
 
             applicationIdSuffix = ".beta"
-            versionNameSuffix = "b2"
+            versionNameSuffix = "b1"
 
             isMinifyEnabled = true
             isShrinkResources = true
