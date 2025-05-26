563bcd5b1c47d10b44299652b8d876ad8cf2eb28
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-05-26 10:35:39 +0200

    refactor(search): change SearchCondition to idiomatic Kotlin

--- feature/search/src/main/java/net/thunderbird/feature/search/api/SearchCondition.kt
+++ feature/search/src/main/java/net/thunderbird/feature/search/api/SearchCondition.kt
@@ -1,71 +1,24 @@
 package net.thunderbird.feature.search.api
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 /**
- * This class represents 1 value for a certain search field. One value consists of three things: an attribute: equals,
- * starts with, contains,... a searchfield: date, flags, sender, subject,... a value: "apple", "jesse",..
+ * Represents a search condition describing what to search for in a specific field
+ * and how to match it using an attribute.
  *
- * @author dzan
+ * @param field The field to search in (e.g., subject, sender, date)
+ * @param attribute The attribute to apply to the field (e.g., contains, equals, starts with)
+ * @param value The value to match against the field and attribute
  */
-class SearchCondition : Parcelable {
+@Parcelize
+data class SearchCondition(
     @JvmField
-    val value: String?
-
-    @JvmField
-    val attribute: SearchAttribute
+    val field: SearchField,
 
     @JvmField
-    val field: SearchField
+    val attribute: SearchAttribute,
 
-    constructor(field: SearchField, attribute: SearchAttribute, value: String?) {
-        this.value = value
-        this.attribute = attribute
-        this.field = field
-    }
-
-    private constructor(parcel: Parcel) {
-        this.value = parcel.readString()
-        this.attribute = SearchAttribute.values()[parcel.readInt()]
-        this.field = SearchField.values()[parcel.readInt()]
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (other is SearchCondition) {
-            val tmp = other
-            return tmp.attribute == attribute &&
-                tmp.field == field &&
-                tmp.value == value
-        }
-
-        return false
-    }
-
-    override fun hashCode(): Int {
-        var result = 1
-        result = 31 * result + attribute.hashCode()
-        result = 31 * result + field.hashCode()
-        result = 31 * result + value.hashCode()
-
-        return result
-    }
-
-    override fun describeContents(): Int = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(value)
-        dest.writeInt(attribute.ordinal)
-        dest.writeInt(field.ordinal)
-    }
-
-    companion object CREATOR : Parcelable.Creator<SearchCondition> {
-        override fun createFromParcel(parcel: Parcel): SearchCondition {
-            return SearchCondition(parcel)
-        }
-
-        override fun newArray(size: Int): Array<SearchCondition?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    @JvmField
+    val value: String?,
+) : Parcelable
