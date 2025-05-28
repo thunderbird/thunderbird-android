28f3e431ae22a892d00ec7fb192a952da82b349f
Rafael Tonholo <rafael@tonholo.dev>
2025-05-28 11:21:07 -0300

    chore(archive): move Preview functions to `debug` folder

--- feature/messages/src/main/kotlin/net/thunderbird/feature/messages/ui/dialog/EmailCantBeArchivedDialogButtons.kt
+++ feature/messages/src/main/kotlin/net/thunderbird/feature/messages/ui/dialog/EmailCantBeArchivedDialogButtons.kt
@@ -3,28 +3,17 @@ package net.thunderbird.feature.messages.ui.dialog
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.unit.dp
-import app.k9mail.core.ui.compose.designsystem.PreviewLightDarkLandscape
-import app.k9mail.core.ui.compose.designsystem.PreviewWithThemeLightDark
 import app.k9mail.core.ui.compose.designsystem.atom.Checkbox
-import app.k9mail.core.ui.compose.designsystem.atom.Surface
-import app.k9mail.core.ui.compose.designsystem.atom.text.TextBodyMedium
 import app.k9mail.core.ui.compose.designsystem.atom.text.TextLabelSmall
 import app.k9mail.core.ui.compose.theme2.MainTheme
 import net.thunderbird.feature.messages.R
@@ -70,40 +59,6 @@ internal fun EmailCantBeArchivedDialogButtons(
                 onCheckedChange = null,
             )
             TextLabelSmall(text = "Do not show this message again")
-        }
-    }
-}
-
-@PreviewLightDarkLandscape
-@Composable
-private fun Preview() {
-    PreviewWithThemeLightDark(
-        useRow = true,
-        useScrim = true,
-        scrimPadding = PaddingValues(32.dp),
-        arrangement = Arrangement.spacedBy(24.dp),
-    ) {
-        Surface(
-            shape = MainTheme.shapes.extraLarge,
-            modifier = Modifier.width(300.dp),
-        ) {
-            val state by remember { mutableStateOf(State.EmailCantBeArchived(isDoNotShowDialogAgainChecked = false)) }
-            Column(
-                modifier = Modifier.padding(24.dp),
-            ) {
-                TextBodyMedium(text = stringResource(R.string.setup_archive_folder_dialog_configure_archive_folder))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End,
-                ) {
-                    EmailCantBeArchivedDialogButtons(
-                        state = state,
-                        onSetArchiveFolderClick = {},
-                        onSkipClick = {},
-                        onDoNotShowAgainChange = {},
-                    )
-                }
-            }
         }
     }
 }
