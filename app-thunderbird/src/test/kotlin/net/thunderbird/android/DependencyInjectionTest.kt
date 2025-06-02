985908a0b2cb350386e576e23212a2d9b3d1e7ad
Rafael Tonholo <rafael@tonholo.dev>
2025-06-02 09:38:04 -0300

    fix(unit-test): fix DependencyInjection Tests where Storage class couldn't be instantiated

--- app-thunderbird/src/test/kotlin/net/thunderbird/android/DependencyInjectionTest.kt
+++ app-thunderbird/src/test/kotlin/net/thunderbird/android/DependencyInjectionTest.kt
@@ -11,6 +11,7 @@ import com.fsck.k9.ui.changelog.ChangelogViewModel
 import com.fsck.k9.ui.endtoend.AutocryptKeyTransferActivity
 import com.fsck.k9.ui.endtoend.AutocryptKeyTransferPresenter
 import com.fsck.k9.ui.helper.SizeFormatter
+import net.thunderbird.core.preferences.Storage
 import net.thunderbird.feature.account.api.AccountId
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -53,6 +54,7 @@ class DependencyInjectionTest : AutoCloseKoinTest() {
             withInstance(lifecycleOwner)
             withInstance(mock<WorkerParameters>())
             withInstance(AccountId.create())
+            withInstance(mock<Storage>())
         }
     }
 }
