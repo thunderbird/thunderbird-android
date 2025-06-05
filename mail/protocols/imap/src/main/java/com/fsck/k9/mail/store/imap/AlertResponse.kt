1bce5ae7a21ae0ed3a8b43faaae8128992ba4834
shamim-emon <emon.dev.bd@gmail.com>
2025-06-05 22:08:49 +0600

    refactor:convert alertResponse class from java to kotlin

--- mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/AlertResponse.kt
+++ mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/AlertResponse.kt
@@ -1,26 +1,25 @@
-package com.fsck.k9.mail.store.imap;
+package com.fsck.k9.mail.store.imap
 
+import com.fsck.k9.mail.store.imap.ImapResponseParser.equalsIgnoreCase
 
-import static com.fsck.k9.mail.store.imap.ImapResponseParser.equalsIgnoreCase;
+internal object AlertResponse {
+    private const val ALERT_RESPONSE_CODE = "ALERT"
+    private const val MINIMUM_RESPONSE_SIZE = 3
 
-
-class AlertResponse {
-    private static final String ALERT_RESPONSE_CODE = "ALERT";
-
-
-    private AlertResponse() {
-    }
-
-    public static String getAlertText(ImapResponse response) {
-        if (response.size() < 3 || !response.isList(1)) {
-            return null;
+    @JvmStatic
+    fun getAlertText(response: ImapResponse): String? {
+        return if (
+            response.size >= MINIMUM_RESPONSE_SIZE &&
+            response.isList(1)
+        ) {
+            val responseTextCode = response.getList(1)
+            if (responseTextCode.size == 1 && equalsIgnoreCase(responseTextCode[0], ALERT_RESPONSE_CODE)) {
+                response.getString(2)
+            } else {
+                null
+            }
+        } else {
+            null
         }
-
-        ImapList responseTextCode = response.getList(1);
-        if (responseTextCode.size() != 1 || !equalsIgnoreCase(responseTextCode.get(0), ALERT_RESPONSE_CODE)) {
-            return null;
-        }
-
-        return response.getString(2);
     }
 }
