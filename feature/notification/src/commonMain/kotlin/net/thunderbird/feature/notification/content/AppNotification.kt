d3cdb5e7e8861d64305ef6af77a8f4159a9a7b1b
Rafael Tonholo <rafael@tonholo.dev>
2025-06-05 11:19:31 -0300

    feat(notification): add and categorize the application's notification data types

---
+++ feature/notification/src/commonMain/kotlin/net/thunderbird/feature/notification/content/AppNotification.kt
@@ -0,0 +1,90 @@
+package net.thunderbird.feature.notification.content
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import net.thunderbird.feature.notification.LockscreenNotificationAppearance
+import net.thunderbird.feature.notification.NotificationChannel
+import net.thunderbird.feature.notification.NotificationGroup
+import net.thunderbird.feature.notification.NotificationId
+import net.thunderbird.feature.notification.NotificationSeverity
+import net.thunderbird.feature.notification.ui.action.NotificationAction
+
+/**
+ * Represents a notification that can be displayed to the user.
+ *
+ * This interface defines the common properties that all notifications must have.
+ * Must not be directly implemented. You must extend [AppNotification] instead.
+ *
+ * @property id The unique identifier of the notification.
+ * @property title The title of the notification.
+ * @property accessibilityText The text to be used for accessibility purposes.
+ * @property contentText The main content text of the notification, can be null.
+ * @property severity The severity level of the notification.
+ * @property createdAt The date and time when the notification was created.
+ * @property actions A set of actions that can be performed on the notification.
+ * @property authenticationRequired Indicates whether authentication is required to view the notification.
+ * @property channel The notification channel to which this notification belongs.
+ * @property group The notification group to which this notification belongs, can be null.
+ * @see AppNotification
+ */
+sealed interface Notification {
+    val id: NotificationId
+    val title: String
+    val accessibilityText: String
+    val contentText: String?
+    val severity: NotificationSeverity
+    val createdAt: LocalDateTime
+    val actions: Set<NotificationAction>
+    val authenticationRequired: Boolean
+    val channel: NotificationChannel
+    val group: NotificationGroup?
+}
+
+/**
+ * The abstract implementation of [Notification], representing an app notification.
+ * This abstraction is meant to provide default properties implementation to easy the app notification creation.
+ *
+ * @property accessibilityText The text that will be read by accessibility services.
+ * Defaults to the notification's title.
+ * @property createdAt The timestamp when the notification was created. Defaults to the current UTC time.
+ * @property actions A set of actions that can be performed on the notification. Defaults to an empty set.
+ * @property authenticationRequired Whether authentication is required to interact with the notification.
+ * Defaults to false.
+ * @property group The notification group this notification belongs to, if any. Defaults to null.
+ * @see Notification
+ */
+sealed class AppNotification : Notification {
+    override val accessibilityText: String = title
+    override val createdAt: LocalDateTime = Clock.System.now().toLocalDateTime(timeZone = TimeZone.UTC)
+    override val actions: Set<NotificationAction> = emptySet()
+    override val authenticationRequired: Boolean = false
+    override val group: NotificationGroup? = null
+}
+
+/**
+ * Represents a notification displayed by the system, **requiring user permission**.
+ * This type of notification can appear on the lock screen.
+ *
+ * @property lockscreenNotification The notification to display on the lock screen.
+ * Override if you need to hide any content when showing this notification in the lockscreen.
+ * By default, this is the same as the notification itself.
+ * @property lockscreenNotificationAppearance The appearance of the notification on the lockscreen.
+ * By default, the notification is [LockscreenNotificationAppearance.Public].
+ * @see LockscreenNotificationAppearance
+ */
+sealed interface SystemNotification : Notification {
+    val lockscreenNotification: SystemNotification get() = this
+    val lockscreenNotificationAppearance: LockscreenNotificationAppearance
+        get() = LockscreenNotificationAppearance.Public
+}
+
+/**
+ *
+ * Represents a notification displayed within the application.
+ *
+ * In-app notifications are typically less intrusive than system notifications and **do not require**
+ * system notification permissions to be displayed.
+ */
+sealed interface InAppNotification : Notification
---
+++ feature/notification/src/commonMain/kotlin/net/thunderbird/feature/notification/ui/action/NotificationAction.kt
@@ -0,0 +1,50 @@
+package net.thunderbird.feature.notification.ui.action
+
+/**
+ * Represents the various actions that can be performed on a notification.
+ */
+sealed interface NotificationAction {
+    /**
+     * Action to reply to the email message associated with the notification.
+     */
+    data object Reply : NotificationAction
+
+    /**
+     * Action to mark the email message associated with the notification as read.
+     */
+    data object MarkAsRead : NotificationAction
+
+    /**
+     * Action to delete the email message associated with the notification.
+     */
+    data object Delete : NotificationAction
+
+    /**
+     * Action to mark the email message associated with the notification as spam.
+     */
+    data object MarkAsSpam : NotificationAction
+
+    /**
+     * Action to archive the email message associated with the notification.
+     */
+    data object Archive : NotificationAction
+
+    /**
+     * Action to prompt the user to update server settings, typically when authentication fails.
+     */
+    data object UpdateServerSettings : NotificationAction
+
+    /**
+     * Action to retry a failed operation, such as sending a message or fetching new messages.
+     */
+    data object Retry : NotificationAction
+
+    /**
+     * Represents a custom notification action.
+     *
+     * This can be used for actions that are not predefined and require a specific message.
+     *
+     * @property message The text to be displayed for this custom action.
+     */
+    data class CustomAction(val message: String) : NotificationAction
+}
