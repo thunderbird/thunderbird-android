cd1366b8103e657a83745f2265ad34d531252f16
shamim-emon <emon.dev.bd@gmail.com>
2025-06-07 18:47:49 +0600

    refactor: convert searchresponse class from java to kotlin

--- mail/protocols/imap/src/test/java/com/fsck/k9/mail/store/imap/SearchResponseTest.kt
+++ mail/protocols/imap/src/test/java/com/fsck/k9/mail/store/imap/SearchResponseTest.kt
@@ -1,89 +1,83 @@
-package com.fsck.k9.mail.store.imap;
+package com.fsck.k9.mail.store.imap
 
+import com.fsck.k9.mail.store.imap.SearchResponse.Companion.parse
+import org.junit.Assert
+import kotlin.test.Test
 
-import java.util.Collections;
-import java.util.List;
-
-import org.junit.Test;
-
-import static com.fsck.k9.mail.store.imap.ImapResponseHelper.createImapResponseList;
-import static java.util.Arrays.asList;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-
-public class SearchResponseTest {
+class SearchResponseTest {
     @Test
-    public void parse_withSingleSearchResponse_shouldExtractNumbers() throws Exception {
-        List<ImapResponse> imapResponses = createImapResponseList(
-                "* SEARCH 1 2 3",
-                "* 23 EXISTS",
-                "* SEARCH 4",
-                "1 OK SEARCH completed");
+    fun parse_withSingleSearchResponse_shouldExtractNumbers() {
+        val imapResponses = ImapResponseHelper.createImapResponseList(
+            "* SEARCH 1 2 3",
+            "* 23 EXISTS",
+            "* SEARCH 4",
+            "1 OK SEARCH completed",
+        )
 
-        SearchResponse result = SearchResponse.parse(imapResponses);
+        val result = parse(imapResponses)
 
-        assertNotNull(result);
-        assertEquals(asList(1L, 2L, 3L, 4L), result.getNumbers());
+        Assert.assertNotNull(result)
+        Assert.assertEquals(mutableListOf(1L, 2L, 3L, 4L), result.numbers)
     }
 
     @Test
-    public void parse_withMultipleSearchResponses_shouldExtractNumbers() throws Exception {
-        List<ImapResponse> imapResponses = createImapResponseList(
-                "* SEARCH 1 2 3",
-                "* 23 EXISTS",
-                "* SEARCH 4",
-                "1 OK SEARCH completed",
-                "* SEARCH 5 6",
-                "* 19 EXPUNGED",
-                "* SEARCH 7",
-                "2 OK SEARCH completed",
-                "* SEARCH 8",
-                "3 OK SEARCH completed");
+    fun parse_withMultipleSearchResponses_shouldExtractNumbers() {
+        val imapResponses = ImapResponseHelper.createImapResponseList(
+            "* SEARCH 1 2 3",
+            "* 23 EXISTS",
+            "* SEARCH 4",
+            "1 OK SEARCH completed",
+            "* SEARCH 5 6",
+            "* 19 EXPUNGED",
+            "* SEARCH 7",
+            "2 OK SEARCH completed",
+            "* SEARCH 8",
+            "3 OK SEARCH completed",
+        )
 
-        SearchResponse result = SearchResponse.parse(imapResponses);
+        val result = parse(imapResponses)
 
-        assertNotNull(result);
-        assertEquals(asList(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L), result.getNumbers());
+        Assert.assertNotNull(result)
+        Assert.assertEquals(mutableListOf(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L), result.numbers)
     }
 
     @Test
-    public void parse_withSingleTaggedSearchResponse_shouldReturnEmptyList() throws Exception {
-        List<ImapResponse> imapResponses = createImapResponseList("x SEARCH 7 8 9");
+    fun parse_withSingleTaggedSearchResponse_shouldReturnEmptyList() {
+        val imapResponses = ImapResponseHelper.createImapResponseList("x SEARCH 7 8 9")
 
-        SearchResponse result = SearchResponse.parse(imapResponses);
+        val result = parse(imapResponses)
 
-        assertNotNull(result);
-        assertEquals(Collections.emptyList(), result.getNumbers());
+        Assert.assertNotNull(result)
+        Assert.assertEquals(emptyList<Any>(), result.numbers)
     }
 
     @Test
-    public void parse_withSingleTooShortResponse_shouldReturnEmptyList() throws Exception {
-        List<ImapResponse> imapResponses = createImapResponseList("* SEARCH");
+    fun parse_withSingleTooShortResponse_shouldReturnEmptyList() {
+        val imapResponses = ImapResponseHelper.createImapResponseList("* SEARCH")
 
-        SearchResponse result = SearchResponse.parse(imapResponses);
+        val result = parse(imapResponses)
 
-        assertNotNull(result);
-        assertEquals(Collections.emptyList(), result.getNumbers());
+        Assert.assertNotNull(result)
+        Assert.assertEquals(emptyList<Any>(), result.numbers)
     }
 
     @Test
-    public void parse_withSingleNoSearchResponse_shouldReturnEmptyList() throws Exception {
-        List<ImapResponse> imapResponses = createImapResponseList("* 23 EXPUNGE");
+    fun parse_withSingleNoSearchResponse_shouldReturnEmptyList() {
+        val imapResponses = ImapResponseHelper.createImapResponseList("* 23 EXPUNGE")
 
-        SearchResponse result = SearchResponse.parse(imapResponses);
+        val result = parse(imapResponses)
 
-        assertNotNull(result);
-        assertEquals(Collections.emptyList(), result.getNumbers());
+        Assert.assertNotNull(result)
+        Assert.assertEquals(emptyList<Any>(), result.numbers)
     }
 
     @Test
-    public void parse_withSingleSearchResponseContainingInvalidNumber_shouldReturnEmptyList() throws Exception {
-        List<ImapResponse> imapResponses = createImapResponseList("* SEARCH A");
+    fun parse_withSingleSearchResponseContainingInvalidNumber_shouldReturnEmptyList() {
+        val imapResponses = ImapResponseHelper.createImapResponseList("* SEARCH A")
 
-        SearchResponse result = SearchResponse.parse(imapResponses);
+        val result = parse(imapResponses)
 
-        assertNotNull(result);
-        assertEquals(Collections.emptyList(), result.getNumbers());
+        Assert.assertNotNull(result)
+        Assert.assertEquals(emptyList<Any>(), result.numbers)
     }
 }
