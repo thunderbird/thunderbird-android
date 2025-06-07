91a3839a7a55c07609c6872b176f50c5f7977ca1
shamim-emon <emon.dev.bd@gmail.com>
2025-06-07 17:24:13 +0600

    refactor: convert fetchPartCallback class from java to kotlin

--- mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/FetchPartCallback.kt
+++ mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/FetchPartCallback.kt
@@ -1,34 +1,23 @@
-package com.fsck.k9.mail.store.imap;
+package com.fsck.k9.mail.store.imap
 
+import com.fsck.k9.mail.BodyFactory
+import com.fsck.k9.mail.Part
+import com.fsck.k9.mail.filter.FixedLengthInputStream
+import com.fsck.k9.mail.internet.MimeHeader
+import java.io.IOException
 
-import java.io.IOException;
+internal class FetchPartCallback(private val part: Part, private val bodyFactory: BodyFactory) :
+    ImapResponseCallback {
+    @Throws(IOException::class)
+    override fun foundLiteral(response: ImapResponse, literal: FixedLengthInputStream): Any? {
+        if (response.tag == null && ImapResponseParser.equalsIgnoreCase(response[1], "FETCH")) {
+            // TODO: check for correct UID
 
-import com.fsck.k9.mail.BodyFactory;
-import com.fsck.k9.mail.Part;
-import com.fsck.k9.mail.filter.FixedLengthInputStream;
-import com.fsck.k9.mail.internet.MimeHeader;
+            val contentTransferEncoding = part.getHeader(MimeHeader.HEADER_CONTENT_TRANSFER_ENCODING)[0]
+            val contentType = part.getHeader(MimeHeader.HEADER_CONTENT_TYPE)[0]
 
-
-class FetchPartCallback implements ImapResponseCallback {
-    private final Part part;
-    private final BodyFactory bodyFactory;
-
-
-    FetchPartCallback(Part part, BodyFactory bodyFactory) {
-        this.part = part;
-        this.bodyFactory = bodyFactory;
-    }
-
-    @Override
-    public Object foundLiteral(ImapResponse response, FixedLengthInputStream literal) throws IOException {
-        if (response.getTag() == null && ImapResponseParser.equalsIgnoreCase(response.get(1), "FETCH")) {
-            //TODO: check for correct UID
-
-            String contentTransferEncoding = part.getHeader(MimeHeader.HEADER_CONTENT_TRANSFER_ENCODING)[0];
-            String contentType = part.getHeader(MimeHeader.HEADER_CONTENT_TYPE)[0];
-
-            return bodyFactory.createBody(contentTransferEncoding, contentType, literal);
+            return bodyFactory.createBody(contentTransferEncoding, contentType, literal)
         }
-        return null;
+        return null
     }
 }
