d5896537ef4b978bf62f58f7a76e3be2326a0377
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-06-18 16:31:37 +0200

    feat(daily): add demo backend

--- app-thunderbird/src/daily/kotlin/net/thunderbird/android/dev/DailyConfig.kt
+++ app-thunderbird/src/daily/kotlin/net/thunderbird/android/dev/DailyConfig.kt
@@ -1,15 +1,17 @@
 package net.thunderbird.android.dev
 
 import app.k9mail.autodiscovery.api.AutoDiscovery
+import app.k9mail.autodiscovery.demo.DemoAutoDiscovery
 import com.fsck.k9.backend.BackendFactory
 import org.koin.core.module.Module
 import org.koin.core.qualifier.named
 
 fun Module.developmentModuleAdditions() {
+    single { DemoBackendFactory(backendStorageFactory = get()) }
     single<Map<String, BackendFactory>>(named("developmentBackends")) {
-        emptyMap()
+        mapOf("demo" to get<DemoBackendFactory>())
     }
     single<List<AutoDiscovery>>(named("extraAutoDiscoveries")) {
-        emptyList()
+        listOf(DemoAutoDiscovery())
     }
 }
