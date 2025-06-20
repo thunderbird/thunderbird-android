ae78ffff97f447adf20c7725fea653b63f4fe7b1
shamim-emon <emon.dev.bd@gmail.com>
2025-06-20 19:45:40 +0600

    refactor(test): refactored SearchResponseTest
    Replace junit assertion with AssertK assertion.
    
    Adoption to new test guideline.

--- mail/protocols/imap/src/test/java/com/fsck/k9/mail/store/imap/SearchResponseTest.kt
+++ mail/protocols/imap/src/test/java/com/fsck/k9/mail/store/imap/SearchResponseTest.kt
@@ -1,12 +1,19 @@
 package com.fsck.k9.mail.store.imap
 
+import assertk.all
+import assertk.assertThat
+import assertk.assertions.containsExactly
+import assertk.assertions.isEmpty
+import assertk.assertions.prop
 import com.fsck.k9.mail.store.imap.SearchResponse.Companion.parse
-import org.junit.Assert
+import jdk.dynalink.linker.support.Guards.isNotNull
 import kotlin.test.Test
+import org.junit.Assert
 
 class SearchResponseTest {
     @Test
     fun parse_withSingleSearchResponse_shouldExtractNumbers() {
+        // Arrange
         val imapResponses = ImapResponseHelper.createImapResponseList(
             "* SEARCH 1 2 3",
             "* 23 EXISTS",
@@ -14,14 +21,20 @@ class SearchResponseTest {
             "1 OK SEARCH completed",
         )
 
+        // Act
         val result = parse(imapResponses)
 
-        Assert.assertNotNull(result)
-        Assert.assertEquals(mutableListOf(1L, 2L, 3L, 4L), result.numbers)
+        // Assert
+        assertThat(result).all {
+            isNotNull()
+            prop("numbers") { it.numbers }
+                .containsExactly(1L, 2L, 3L, 4L)
+        }
     }
 
     @Test
     fun parse_withMultipleSearchResponses_shouldExtractNumbers() {
+        // Arrange
         val imapResponses = ImapResponseHelper.createImapResponseList(
             "* SEARCH 1 2 3",
             "* 23 EXISTS",
@@ -35,49 +48,81 @@ class SearchResponseTest {
             "3 OK SEARCH completed",
         )
 
+        // Act
         val result = parse(imapResponses)
 
-        Assert.assertNotNull(result)
-        Assert.assertEquals(mutableListOf(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L), result.numbers)
+        // Assert
+        assertThat(result).all {
+            isNotNull()
+            prop("numbers") { it.numbers }
+                .containsExactly(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L)
+        }
     }
 
     @Test
     fun parse_withSingleTaggedSearchResponse_shouldReturnEmptyList() {
+        // Arrange
         val imapResponses = ImapResponseHelper.createImapResponseList("x SEARCH 7 8 9")
 
+        // Act
         val result = parse(imapResponses)
 
         Assert.assertNotNull(result)
         Assert.assertEquals(emptyList<Any>(), result.numbers)
+
+        // Assert
+        assertThat(result).all {
+            isNotNull()
+            prop("numbers") { it.numbers }
+                .isEmpty()
+        }
     }
 
     @Test
     fun parse_withSingleTooShortResponse_shouldReturnEmptyList() {
+        // Arrange
         val imapResponses = ImapResponseHelper.createImapResponseList("* SEARCH")
 
+        // Act
         val result = parse(imapResponses)
 
-        Assert.assertNotNull(result)
-        Assert.assertEquals(emptyList<Any>(), result.numbers)
+        // Assert
+        assertThat(result).all {
+            isNotNull()
+            prop("numbers") { it.numbers }
+                .isEmpty()
+        }
     }
 
     @Test
     fun parse_withSingleNoSearchResponse_shouldReturnEmptyList() {
+        // Arrange
         val imapResponses = ImapResponseHelper.createImapResponseList("* 23 EXPUNGE")
 
+        // Act
         val result = parse(imapResponses)
 
-        Assert.assertNotNull(result)
-        Assert.assertEquals(emptyList<Any>(), result.numbers)
+        // Assert
+        assertThat(result).all {
+            isNotNull()
+            prop("numbers") { it.numbers }
+                .isEmpty()
+        }
     }
 
     @Test
     fun parse_withSingleSearchResponseContainingInvalidNumber_shouldReturnEmptyList() {
+        // Arrange
         val imapResponses = ImapResponseHelper.createImapResponseList("* SEARCH A")
 
+        // Act
         val result = parse(imapResponses)
 
-        Assert.assertNotNull(result)
-        Assert.assertEquals(emptyList<Any>(), result.numbers)
+        // Assert
+        assertThat(result).all {
+            isNotNull()
+            prop("numbers") { it.numbers }
+                .isEmpty()
+        }
     }
 }
