bb2423356013eef9dd23ae74582971e02f8458f6
thunderbird-botmobile <202943968+thunderbird-botmobile[bot]@users.noreply.github.com>
2025-06-24 22:00:13 +0000

    Release: K-9 Mail 10.0

--- app-k9mail/build.gradle.kts
+++ app-k9mail/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.fsck.k9"
         testApplicationId = "com.fsck.k9.tests"
 
-        versionCode = 39023
+        versionCode = 39024
         versionName = "10.0"
 
         // Keep in sync with the resource string array "supported_languages"
