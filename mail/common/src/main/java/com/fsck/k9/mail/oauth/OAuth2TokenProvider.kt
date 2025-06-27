57ee3c5627cc576e984ae186a9d97d06e3a1be83
Rafael Tonholo <rafael@tonholo.dev>
2025-06-27 12:25:32 -0300

    chore: convert OAuth2TokenProvider to Kotlin

--- mail/common/src/main/java/com/fsck/k9/mail/oauth/OAuth2TokenProvider.kt
+++ mail/common/src/main/java/com/fsck/k9/mail/oauth/OAuth2TokenProvider.kt
@@ -1,29 +1,28 @@
-package com.fsck.k9.mail.oauth;
+package com.fsck.k9.mail.oauth
 
+import com.fsck.k9.mail.AuthenticationFailedException
 
-import com.fsck.k9.mail.AuthenticationFailedException;
-
-
-public interface OAuth2TokenProvider {
-    /**
-     * A default timeout value to use when fetching tokens.
-     */
-    int OAUTH2_TIMEOUT = 30000;
-
+interface OAuth2TokenProvider {
+    companion object {
+        /**
+         * A default timeout value to use when fetching tokens.
+         */
+        const val OAUTH2_TIMEOUT: Int = 30000
+    }
 
     /**
      * Fetch a token. No guarantees are provided for validity.
      */
-    String getToken(long timeoutMillis) throws AuthenticationFailedException;
+    @Throws(AuthenticationFailedException::class)
+    fun getToken(timeoutMillis: Long): String
 
     /**
      * Invalidate the token for this username.
      *
-     * <p>
      * Note that the token should always be invalidated on credential failure. However invalidating a token every
      * single time is not recommended.
-     * <p>
+     *
      * Invalidating a token and then failure with a new token should be treated as a permanent failure.
      */
-    void invalidateToken();
+    fun invalidateToken()
 }
