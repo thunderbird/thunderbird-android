c8ee62d24e46859e585b19195daba9f12d0b1ecd
Rafael Tonholo <rafael@tonholo.dev>
2025-07-02 15:24:03 -0300

    chore(notification): add todo comments with follow up tasks

--- feature/notification/api/src/commonMain/kotlin/net/thunderbird/feature/notification/api/NotificationGroup.kt
+++ feature/notification/api/src/commonMain/kotlin/net/thunderbird/feature/notification/api/NotificationGroup.kt
@@ -3,7 +3,7 @@ package net.thunderbird.feature.notification.api
 import net.thunderbird.core.common.io.KmpParcelable
 import net.thunderbird.core.common.io.KmpParcelize
 
-// TODO: Properly handle notification groups, adding summary, etc.
+// TODO(9419): Properly handle notification groups, adding summary, etc.
 @KmpParcelize
 data class NotificationGroup(
     val key: NotificationGroupKey,
