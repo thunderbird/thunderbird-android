93742151b06cf43c3ab6300cd55fd9f8cdcf4ecf
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-07-11 10:49:47 +0200

    chore: enable email notifications by default for release

--- app-thunderbird/src/release/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
+++ app-thunderbird/src/release/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
@@ -13,7 +13,7 @@ class TbFeatureFlagFactory : FeatureFlagFactory {
             FeatureFlag("archive_marks_as_read".toFeatureFlagKey(), enabled = true),
             FeatureFlag("new_account_settings".toFeatureFlagKey(), enabled = false),
             FeatureFlag("disable_font_size_config".toFeatureFlagKey(), enabled = true),
-            FeatureFlag("email_notification_default".toFeatureFlagKey(), enabled = false),
+            FeatureFlag("email_notification_default".toFeatureFlagKey(), enabled = true),
             FeatureFlag("enable_dropdown_drawer".toFeatureFlagKey(), enabled = true),
         )
     }
