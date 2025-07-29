3848bfcbc2fe22ae6367d61c499187437df3520f
Rafael Tonholo <rafael@tonholo.dev>
2025-07-29 13:10:15 -0300

    feat(notifications): add notification action button composable

---
+++ feature/notification/api/src/androidDebug/kotlin/net/thunderbird/feature/notification/api/ui/component/molecule/NotificationActionButtonPreview.kt
@@ -0,0 +1,28 @@
+package net.thunderbird.feature.notification.api.ui.component.molecule
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import app.k9mail.core.ui.compose.designsystem.PreviewWithThemesLightDark
+import app.k9mail.core.ui.compose.theme2.MainTheme
+
+@PreviewLightDark
+@Composable
+private fun NotificationActionButtonPreview() {
+    PreviewWithThemesLightDark {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(MainTheme.spacings.default),
+        ) {
+            NotificationActionButton(
+                onClick = {},
+                text = "Sign in",
+            )
+            NotificationActionButton(
+                onClick = {},
+                text = "View support article",
+                isExternalLink = true,
+            )
+        }
+    }
+}
---
+++ feature/notification/api/src/androidMain/kotlin/net/thunderbird/feature/notification/api/ui/component/molecule/NotificationActionButton.kt
@@ -0,0 +1,41 @@
+package net.thunderbird.feature.notification.api.ui.component.molecule
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.movableContentOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import app.k9mail.core.ui.compose.designsystem.atom.button.ButtonText
+import app.k9mail.core.ui.compose.designsystem.atom.icon.Icon
+import app.k9mail.core.ui.compose.theme2.MainTheme
+import net.thunderbird.feature.notification.api.ui.icon.atom.OpenInNew
+
+@Composable
+fun NotificationActionButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isExternalLink: Boolean = false,
+) {
+    val leadingIcon = remember(isExternalLink) {
+        if (isExternalLink) {
+            movableContentOf {
+                Icon(
+                    imageVector = OpenInNew,
+                    modifier = Modifier.size(MainTheme.sizes.iconSmall),
+                )
+                Spacer(modifier = Modifier.width(MainTheme.spacings.default))
+            }
+        } else {
+            null
+        }
+    }
+    ButtonText(
+        text = text,
+        onClick = onClick,
+        leadingIcon = leadingIcon,
+        modifier = modifier,
+    )
+}
