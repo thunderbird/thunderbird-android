8f59c9bf32772a130ec09e7d630f787b82857157
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-08-06 11:18:33 +0200

    feat(account-setting): add profile indicator setting

--- feature/account/settings/impl/src/main/kotlin/net/thunderbird/feature/account/settings/impl/domain/usecase/UpdateGeneralSettings.kt
+++ feature/account/settings/impl/src/main/kotlin/net/thunderbird/feature/account/settings/impl/domain/usecase/UpdateGeneralSettings.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.firstOrNull
 import net.thunderbird.core.outcome.Outcome
 import net.thunderbird.core.ui.setting.SettingValue
 import net.thunderbird.feature.account.AccountId
+import net.thunderbird.feature.account.profile.AccountAvatar
 import net.thunderbird.feature.account.profile.AccountProfile
 import net.thunderbird.feature.account.profile.AccountProfileRepository
 import net.thunderbird.feature.account.settings.impl.domain.AccountSettingsDomainContract.SettingsError
@@ -19,6 +20,13 @@ internal class UpdateGeneralSettings(
         setting: SettingValue<*>,
     ): Outcome<Unit, SettingsError> {
         return when (setting.id) {
+            GeneralPreference.PROFILE_INDICATOR.generateId(accountId) -> {
+                val avatar = setting.value as AccountAvatar
+                updateAccountProfile(accountId) {
+                    copy(avatar = avatar)
+                }
+            }
+
             GeneralPreference.NAME.generateId(accountId) -> {
                 updateAccountProfile(accountId) {
                     copy(name = setting.value as String)
