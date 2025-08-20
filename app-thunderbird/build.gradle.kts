a7f10b00f63b4479b0d97dea0b9a8f94ff9c9e56
Daniel Darnell <daniel@thunderbird.net>
2025-08-20 11:48:28 -0700

    Revert "Release: Thunderbird 11.0"
    This reverts commit 562df5365713a349c4cb33ec538bdc50bc0c7931.

--- app-thunderbird/build.gradle.kts
+++ app-thunderbird/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "net.thunderbird.android"
         testApplicationId = "net.thunderbird.android.tests"
 
-        versionCode = 13
+        versionCode = 12
         versionName = "11.0"
 
         // Keep in sync with the resource string array "supported_languages"
