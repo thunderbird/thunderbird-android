855dcb1063bd30f1b0ec8de462370b8f27f33e4c
Rafael Tonholo <rafael@tonholo.dev>
2025-08-21 15:24:40 -0300

    chore(notifications): filter notifications by account uuid

--- legacy/ui/legacy/src/main/java/com/fsck/k9/ui/messagelist/item/MessageListInAppNotificationViewHolder.kt
+++ legacy/ui/legacy/src/main/java/com/fsck/k9/ui/messagelist/item/MessageListInAppNotificationViewHolder.kt
@@ -1,5 +1,6 @@
 package com.fsck.k9.ui.messagelist.item
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -7,6 +8,7 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import kotlinx.collections.immutable.persistentSetOf
 import net.thunderbird.core.ui.theme.api.FeatureThemeProvider
+import net.thunderbird.feature.notification.api.receiver.InAppNotificationEvent
 import net.thunderbird.feature.notification.api.ui.InAppNotificationHost
 import net.thunderbird.feature.notification.api.ui.host.DisplayInAppNotificationFlag
 import org.koin.compose.koinInject
@@ -31,13 +33,17 @@ abstract class MessageListInAppNotificationViewHolder(protected val view: Compos
     abstract fun Content()
 }
 
-class BannerInlineListInAppNotificationViewHolder(view: ComposeView) : MessageListInAppNotificationViewHolder(view) {
+class BannerInlineListInAppNotificationViewHolder(
+    private val eventFilter: (InAppNotificationEvent) -> Boolean,
+    view: ComposeView,
+) : MessageListInAppNotificationViewHolder(view) {
     @Composable
     override fun Content() {
         InAppNotificationHost(
             onActionClick = { },
             enabled = persistentSetOf(DisplayInAppNotificationFlag.BannerInlineNotifications),
-            modifier = Modifier.fillMaxWidth(),
+            eventFilter = eventFilter,
+            modifier = Modifier.animateContentSize(),
         )
     }
 }
