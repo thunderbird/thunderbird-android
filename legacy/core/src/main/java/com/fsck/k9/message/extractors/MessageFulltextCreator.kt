43b55b594c92d5e3fbf340b3f255c0cd3f73dabe
shamim-emon <emon.dev.bd@gmail.com>
2025-08-30 17:44:58 +0600

    refactor : MessageFulltextCreator to Kotlin and ensure the implementation follows idiomatic Kotlin conventions.

--- legacy/core/src/main/java/com/fsck/k9/message/extractors/MessageFulltextCreator.kt
+++ legacy/core/src/main/java/com/fsck/k9/message/extractors/MessageFulltextCreator.kt
@@ -1,48 +1,32 @@
-package com.fsck.k9.message.extractors;
+package com.fsck.k9.message.extractors
 
+import com.fsck.k9.mail.Message
+import com.fsck.k9.mail.Part
+import com.fsck.k9.mail.internet.MessageExtractor
+import com.fsck.k9.mail.internet.MimeUtility
+import com.fsck.k9.message.html.HtmlConverter
 
-import androidx.annotation.NonNull;
-
-import com.fsck.k9.message.html.HtmlConverter;
-import com.fsck.k9.mail.Message;
-import com.fsck.k9.mail.Part;
-import com.fsck.k9.mail.internet.MessageExtractor;
-import com.fsck.k9.mail.internet.MimeUtility;
-
-
-public class MessageFulltextCreator {
-    private static final int MAX_CHARACTERS_CHECKED_FOR_FTS = 200*1024;
-
-
-    private final TextPartFinder textPartFinder;
-
-
-    MessageFulltextCreator(TextPartFinder textPartFinder) {
-        this.textPartFinder = textPartFinder;
-    }
-
-    public static MessageFulltextCreator newInstance() {
-        TextPartFinder textPartFinder = new TextPartFinder();
-        return new MessageFulltextCreator(textPartFinder);
-    }
-
-    public String createFulltext(@NonNull Message message) {
-        Part textPart = textPartFinder.findFirstTextPart(message);
+class MessageFulltextCreator private constructor(private val textPartFinder: TextPartFinder) {
+    fun createFulltext(message: Message): String? {
+        val textPart = textPartFinder.findFirstTextPart(message)
         if (textPart == null || hasEmptyBody(textPart)) {
-            return null;
+            return null
         }
 
-        String text = MessageExtractor.getTextFromPart(textPart, MAX_CHARACTERS_CHECKED_FOR_FTS);
-        String mimeType = textPart.getMimeType();
-        if (!MimeUtility.isSameMimeType(mimeType, "text/html")) {
-            return text;
+        val text = MessageExtractor.getTextFromPart(textPart, MAX_CHARACTERS_CHECKED_FOR_FTS)
+        val mimeType = textPart.mimeType
+        return if (!MimeUtility.isSameMimeType(mimeType, "text/html")) {
+            text
+        } else {
+            HtmlConverter.htmlToText(text)
         }
-
-        return HtmlConverter.htmlToText(text);
     }
 
-    private boolean hasEmptyBody(Part textPart) {
-        return textPart.getBody() == null;
-    }
+    private fun hasEmptyBody(textPart: Part) = textPart.getBody() == null
 
+    companion object {
+        private const val MAX_CHARACTERS_CHECKED_FOR_FTS = 200 * 1024L
+
+        fun newInstance() = MessageFulltextCreator(TextPartFinder())
+    }
 }
