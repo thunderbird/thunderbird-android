a64c07f171ba374a35a9c1d51b1e824fddf7f3dd
shamim-emon <emon.dev.bd@gmail.com>
2025-08-31 09:45:20 +0600

    refactor : ComposePgpEnableByDefaultDecider  to Kotlin and ensure the implementation follows idiomatic Kotlin conventions.

--- legacy/core/src/main/java/com/fsck/k9/message/ComposePgpEnableByDefaultDecider.kt
+++ legacy/core/src/main/java/com/fsck/k9/message/ComposePgpEnableByDefaultDecider.kt
@@ -1,20 +1,13 @@
-package com.fsck.k9.message;
+package com.fsck.k9.message
 
+import com.fsck.k9.crypto.MessageCryptoStructureDetector
+import com.fsck.k9.mail.Message
 
-import java.util.List;
+class ComposePgpEnableByDefaultDecider {
+    fun shouldEncryptByDefault(localMessage: Message?) = messageIsEncrypted(localMessage)
 
-import com.fsck.k9.crypto.MessageCryptoStructureDetector;
-import com.fsck.k9.mail.Message;
-import com.fsck.k9.mail.Part;
-
-
-public class ComposePgpEnableByDefaultDecider {
-    public boolean shouldEncryptByDefault(Message localMessage) {
-        return messageIsEncrypted(localMessage);
-    }
-
-    private boolean messageIsEncrypted(Message localMessage) {
-        List<Part> encryptedParts = MessageCryptoStructureDetector.findMultipartEncryptedParts(localMessage);
-        return !encryptedParts.isEmpty();
-    }
+    private fun messageIsEncrypted(localMessage: Message?) =
+        localMessage?.let {
+            MessageCryptoStructureDetector.findMultipartEncryptedParts(it).isNotEmpty()
+        } ?: false
 }
