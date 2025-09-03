875e119249279a75e08e036d5d89bab4150fb286
thunderbird-botmobile <202943968+thunderbird-botmobile[bot]@users.noreply.github.com>
2025-09-03 17:49:17 +0000

    Release: Thunderbird 12.1

--- app-thunderbird/build.gradle.kts
+++ app-thunderbird/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "net.thunderbird.android"
         testApplicationId = "net.thunderbird.android.tests"
 
-        versionCode = 14
+        versionCode = 15
         versionName = "12.1"
 
         buildConfigField("String", "CLIENT_INFO_APP_NAME", "\"Thunderbird for Android\"")
