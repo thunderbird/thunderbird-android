efb198e2c85dc3b4e11de8930b7d5dd3464b6037
Stephen King <sking@thunderbird.net>
2025-09-03 10:22:11 -0700

    Revert "Release: Thunderbird 12.0"
    This reverts commit 1b82bbbf644ba5062bc684dc38dc462111d83f5b.

--- app-thunderbird/build.gradle.kts
+++ app-thunderbird/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "net.thunderbird.android"
         testApplicationId = "net.thunderbird.android.tests"
 
-        versionCode = 15
+        versionCode = 14
         versionName = "12.0"
 
         buildConfigField("String", "CLIENT_INFO_APP_NAME", "\"Thunderbird for Android\"")
