8094a29fdf845d8e539b1b98afc90ef595107f0f
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-09-30 11:41:54 +0200

    fix: KoinPreview needs to use KoinApplicationPreview

--- core/ui/compose/common/src/main/kotlin/app/k9mail/core/ui/compose/common/koin/KoinPreview.kt
+++ core/ui/compose/common/src/main/kotlin/app/k9mail/core/ui/compose/common/koin/KoinPreview.kt
@@ -1,26 +1,38 @@
 package app.k9mail.core.ui.compose.common.koin
 
 import androidx.compose.runtime.Composable
-import org.koin.compose.KoinContext
-import org.koin.core.Koin
+import org.koin.compose.KoinApplicationPreview
+import org.koin.core.module.Module
 import org.koin.dsl.ModuleDeclaration
 import org.koin.dsl.module
 
 /**
  * Helper to make Compose previews work when some dependencies are injected via Koin.
+ *
+ * Usage:
+ * ```kotlin
+ * @Composable
+ * @Preview()
+ * fun SomeComposablePreview() {
+ *     koinPreview {
+ *        // Declare Koin modules here
+ *        factory { SomeDependency() }
+ *     } WithContent {
+ *     // Your preview content here
+ *     }
+ * }
+ * ```
  */
 fun koinPreview(moduleDeclaration: ModuleDeclaration): KoinPreview {
-    val koin = Koin().apply {
-        loadModules(listOf(module(moduleDeclaration = moduleDeclaration)))
-    }
+    val modules = listOf(module(moduleDeclaration = moduleDeclaration))
 
-    return KoinPreview(koin)
+    return KoinPreview(modules)
 }
 
-class KoinPreview internal constructor(private val koin: Koin) {
+class KoinPreview internal constructor(private val modulesList: List<Module>) {
     @Composable
     infix fun WithContent(content: @Composable () -> Unit) {
-        KoinContext(context = koin) {
+        KoinApplicationPreview(application = { modules(modulesList) }) {
             content()
         }
     }
