55c4a615de0258bf80b3cb3d052258838925300d
Rafael Tonholo <rafael@tonholo.dev>
2025-10-01 10:44:57 -0300

    feat(notifications): add sent folder not found notification type

---
+++ feature/notification/api/src/androidMain/kotlin/net/thunderbird/feature/notification/api/content/SentFolderNotFoundNotification.android.kt
@@ -0,0 +1,9 @@
+package net.thunderbird.feature.notification.api.content
+
+import app.k9mail.core.ui.compose.designsystem.atom.icon.Icons
+import app.k9mail.core.ui.compose.designsystem.atom.icon.outlined.Warning
+import net.thunderbird.feature.notification.api.ui.icon.NotificationIcon
+import net.thunderbird.feature.notification.api.ui.icon.NotificationIcons
+
+internal actual val NotificationIcons.SentFolderNotFound: NotificationIcon
+    get() = NotificationIcon(inAppNotificationIcon = Icons.Outlined.Warning)
