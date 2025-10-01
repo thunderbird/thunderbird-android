ece66649e085c0e14c755fd42055ed6ad4d0037e
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-10-01 15:45:45 +0200

    fix: add missing foojay-resolver and limit it to JvmVendorSpec.ADOPTIUM

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
