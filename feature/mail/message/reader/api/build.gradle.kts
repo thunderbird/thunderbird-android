beb26a3513b332e496f3f7fca0a4c7429df42b19
Rafael Tonholo <rafael@tonholo.dev>
2025-10-28 09:17:28 -0300

    chore(message-reader): add :feature:mail:message:reader module

---
+++ feature/mail/message/reader/api/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id(ThunderbirdPlugins.Library.kmp)
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+        }
+    }
+}
+
+android {
+    namespace = "net.thunderbird.feature.mail.message.reader.api"
+}
