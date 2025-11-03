d46f8a02e7004cf27103f308dbd63c827a9adfd5
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-11-03 12:33:20 +0100

    remove(account-setting): remove old account general setting

--- app-thunderbird/src/debug/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
+++ app-thunderbird/src/debug/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
@@ -15,7 +15,6 @@ class TbFeatureFlagFactory : FeatureFlagFactory {
     override fun createFeatureCatalog(): List<FeatureFlag> {
         return listOf(
             FeatureFlag("archive_marks_as_read".toFeatureFlagKey(), enabled = true),
-            FeatureFlag("new_account_settings".toFeatureFlagKey(), enabled = true),
             FeatureFlag("disable_font_size_config".toFeatureFlagKey(), enabled = true),
             FeatureFlag("email_notification_default".toFeatureFlagKey(), enabled = true),
             FeatureFlag("enable_dropdown_drawer".toFeatureFlagKey(), enabled = true),
