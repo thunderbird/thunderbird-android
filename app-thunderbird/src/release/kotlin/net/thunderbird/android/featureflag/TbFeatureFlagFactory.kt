742d36f6e6baf3aacfa0a5656d67b1c1f99e53c1
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-11-06 12:39:06 +0100

    chore(feature-flag): enable dropdown drawer for release

--- app-thunderbird/src/release/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
+++ app-thunderbird/src/release/kotlin/net/thunderbird/android/featureflag/TbFeatureFlagFactory.kt
@@ -15,11 +15,11 @@ class TbFeatureFlagFactory : FeatureFlagFactory {
     override fun createFeatureCatalog(): List<FeatureFlag> {
         return listOf(
             FeatureFlag("archive_marks_as_read".toFeatureFlagKey(), enabled = true),
-            FeatureFlag("new_account_settings".toFeatureFlagKey(), enabled = false),
+            FeatureFlag("new_account_settings".toFeatureFlagKey(), enabled = true),
             FeatureFlag("disable_font_size_config".toFeatureFlagKey(), enabled = true),
             FeatureFlag("email_notification_default".toFeatureFlagKey(), enabled = true),
             FeatureFlag("enable_dropdown_drawer".toFeatureFlagKey(), enabled = true),
-            FeatureFlag("enable_dropdown_drawer_ui".toFeatureFlagKey(), enabled = false),
+            FeatureFlag("enable_dropdown_drawer_ui".toFeatureFlagKey(), enabled = true),
             FeatureFlag(FeatureFlagKey.DisplayInAppNotifications, enabled = false),
             FeatureFlag(FeatureFlagKey.UseNotificationSenderForSystemNotifications, enabled = false),
             FeatureFlag(MessageListFeatureFlags.UseComposeForMessageListItems, enabled = false),
