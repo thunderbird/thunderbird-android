ba55e9d51dde5fc8e506dfa1f5518be7a8e3f0c3
Rafael Tonholo <rafael@tonholo.dev>
2025-11-12 10:17:43 -0400

    refactor(debug-settings): change SecretDebugSettingsRoute from sealed interface to data class with enum for selected tab

--- feature/debug-settings/src/main/kotlin/net/thunderbird/feature/debug/settings/navigation/SecretDebugSettingsRoute.kt
+++ feature/debug-settings/src/main/kotlin/net/thunderbird/feature/debug/settings/navigation/SecretDebugSettingsRoute.kt
@@ -1,17 +1,19 @@
 package net.thunderbird.feature.debug.settings.navigation
 
+import androidx.annotation.StringRes
 import app.k9mail.core.ui.compose.navigation.Route
 import kotlinx.serialization.Serializable
+import net.thunderbird.feature.debug.settings.R
 
-sealed interface SecretDebugSettingsRoute : Route {
-    @Serializable
-    data object Notification : SecretDebugSettingsRoute {
-        override val basePath: String = "$SECRET_DEBUG_SETTINGS/notification"
+private const val SECRET_DEBUG_SETTINGS = "app://secret_debug_settings"
 
-        override fun route(): String = basePath
-    }
-
-    companion object {
-        const val SECRET_DEBUG_SETTINGS = "app://secret_debug_settings"
+@Serializable
+data class SecretDebugSettingsRoute(val tab: Tab = Tab.Notification) : Route {
+    override val basePath: String = "$SECRET_DEBUG_SETTINGS/${tab.name}"
+    override fun route(): String = basePath
+    enum class Tab(@param:StringRes val titleRes: Int) {
+        Notification(titleRes = R.string.debug_settings_notifications_title),
+        FeatureFlag(titleRes = R.string.debug_settings_feature_flag_title),
+        ;
     }
 }
