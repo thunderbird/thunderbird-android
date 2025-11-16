c4153b54662609beda09497f571a4c728e82b975
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2025-11-16 18:28:49 +0100

    refactor: extract thin interfaces for ArchiveFolderResolver
    Applied Interface Segregation Principle by extracting two thin
    interfaces from thick dependencies:
    - FolderIdResolver (2 methods) replaces MessageStoreManager (60+ methods)
    - ArchiveFolderCreator (1 method) replaces BackendStorageFactory
    
    Added adapter implementations for production use and created simple
    fakes for testing. Tests now use 15-line fakes instead of 200+ line
    fakes that implemented entire thick interfaces.

---
+++ legacy/core/src/main/java/com/fsck/k9/controller/BackendStorageArchiveFolderCreator.kt
@@ -0,0 +1,26 @@
+package com.fsck.k9.controller
+
+import com.fsck.k9.backend.api.FolderInfo
+import com.fsck.k9.backend.api.createFolder
+import com.fsck.k9.backend.api.updateFolders
+import com.fsck.k9.mailstore.LegacyAccountDtoBackendStorageFactory
+import net.thunderbird.core.android.account.LegacyAccountDto
+import net.thunderbird.core.logging.legacy.Log
+
+internal class BackendStorageArchiveFolderCreator(
+    private val backendStorageFactory: LegacyAccountDtoBackendStorageFactory,
+) : ArchiveFolderCreator {
+    override fun createFolder(account: LegacyAccountDto, folderInfo: FolderInfo): Long? {
+        return try {
+            val backendStorage = backendStorageFactory.createBackendStorage(account)
+            backendStorage.updateFolders {
+                createFolder(folderInfo)
+            }
+        } catch (e: Exception) {
+            Log.e(e, "Failed to create archive subfolder: ${folderInfo.serverId}")
+            // TODO: Inform the user that archive folder creation failed. Currently returns null which
+            //  will skip archiving the message. Consider showing a notification or error message.
+            null
+        }
+    }
+}
