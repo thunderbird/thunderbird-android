23b25f31604a85059be913fd94ef7326c2ddf564
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-11-19 14:38:16 +0100

    feat(account-settings): add link to RFC for account name validation

--- feature/account/settings/impl/src/main/kotlin/net/thunderbird/feature/account/settings/impl/domain/usecase/ValidateAccountName.kt
+++ feature/account/settings/impl/src/main/kotlin/net/thunderbird/feature/account/settings/impl/domain/usecase/ValidateAccountName.kt
@@ -18,8 +18,10 @@ internal class ValidateAccountName : UseCase.ValidateAccountName {
         /**
          * Maximum length for an account name.
          *
-         * See RFC 5321, 4.5.3.1.3.
-         * The maximum length of 'Path' limits the length of the whole email address.
+         * See [RFC 5321, 4.5.3.1.3](https://datatracker.ietf.org/doc/html/rfc5321#section-4.5.3.1.3).
+         *
+         * The account name could be an email address. So the maximum length of 'Path' limits the length of the
+         * whole email address and thus the account name.
          */
         const val MAX_NAME_LENGTH = 254
     }
