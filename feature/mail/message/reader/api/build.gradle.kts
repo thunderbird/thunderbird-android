b734e223a178436a6b17957b1c60b641cf43a1c0
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-11-20 13:08:35 +0100

    chore(build): update new modules

--- feature/mail/message/reader/api/build.gradle.kts
+++ feature/mail/message/reader/api/build.gradle.kts
@@ -3,14 +3,13 @@ plugins {
 }
 
 kotlin {
+    androidLibrary {
+        namespace = "net.thunderbird.feature.mail.message.reader.api"
+    }
     sourceSets {
         commonMain.dependencies {
             implementation(projects.core.common)
             implementation(projects.core.featureflag)
         }
     }
-}
-
-android {
-    namespace = "net.thunderbird.feature.mail.message.reader.api"
 }
