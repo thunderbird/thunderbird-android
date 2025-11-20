3c916f9c9ea062b4100c17d887760bafe568e76e
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-11-20 16:39:59 +0100

    feat(account-settigs): add icons for the avatar

---
+++ core/ui/compose/designsystem/src/main/kotlin/net/thunderbird/core/ui/compose/designsystem/atom/icon/outlined/OutlinedPetsIcon.kt
@@ -0,0 +1,296 @@
+package net.thunderbird.core.ui.compose.designsystem.atom.icon.outlined
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import net.thunderbird.core.ui.compose.designsystem.atom.icon.Icon
+import net.thunderbird.core.ui.compose.designsystem.atom.icon.icon
+import net.thunderbird.core.ui.compose.designsystem.atom.icon.iconPath
+
+@Suppress("MagicNumber")
+internal val OutlinedPetsIcon: ImageVector by lazy {
+    icon(
+        name = "OutlinedPetsIcon",
+        viewportWidth = 960.0f,
+        viewportHeight = 960.0f,
+    ) {
+        iconPath {
+            moveTo(x = 180.0f, y = 485.0f)
+            quadToRelative(
+                dx1 = -42.0f,
+                dy1 = 0.0f,
+                dx2 = -71.0f,
+                dy2 = -29.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = -29.0f,
+                dy1 = -71.0f,
+            )
+            quadToRelative(
+                dx1 = 0.0f,
+                dy1 = -42.0f,
+                dx2 = 29.0f,
+                dy2 = -71.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 71.0f,
+                dy1 = -29.0f,
+            )
+            quadToRelative(
+                dx1 = 42.0f,
+                dy1 = 0.0f,
+                dx2 = 71.0f,
+                dy2 = 29.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 29.0f,
+                dy1 = 71.0f,
+            )
+            quadToRelative(
+                dx1 = 0.0f,
+                dy1 = 42.0f,
+                dx2 = -29.0f,
+                dy2 = 71.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = -71.0f,
+                dy1 = 29.0f,
+            )
+            close()
+            moveToRelative(dx = 180.0f, dy = -160.0f)
+            quadToRelative(
+                dx1 = -42.0f,
+                dy1 = 0.0f,
+                dx2 = -71.0f,
+                dy2 = -29.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = -29.0f,
+                dy1 = -71.0f,
+            )
+            quadToRelative(
+                dx1 = 0.0f,
+                dy1 = -42.0f,
+                dx2 = 29.0f,
+                dy2 = -71.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 71.0f,
+                dy1 = -29.0f,
+            )
+            quadToRelative(
+                dx1 = 42.0f,
+                dy1 = 0.0f,
+                dx2 = 71.0f,
+                dy2 = 29.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 29.0f,
+                dy1 = 71.0f,
+            )
+            quadToRelative(
+                dx1 = 0.0f,
+                dy1 = 42.0f,
+                dx2 = -29.0f,
+                dy2 = 71.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = -71.0f,
+                dy1 = 29.0f,
+            )
+            close()
+            moveToRelative(dx = 240.0f, dy = 0.0f)
+            quadToRelative(
+                dx1 = -42.0f,
+                dy1 = 0.0f,
+                dx2 = -71.0f,
+                dy2 = -29.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = -29.0f,
+                dy1 = -71.0f,
+            )
+            quadToRelative(
+                dx1 = 0.0f,
+                dy1 = -42.0f,
+                dx2 = 29.0f,
+                dy2 = -71.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 71.0f,
+                dy1 = -29.0f,
+            )
+            quadToRelative(
+                dx1 = 42.0f,
+                dy1 = 0.0f,
+                dx2 = 71.0f,
+                dy2 = 29.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 29.0f,
+                dy1 = 71.0f,
+            )
+            quadToRelative(
+                dx1 = 0.0f,
+                dy1 = 42.0f,
+                dx2 = -29.0f,
+                dy2 = 71.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = -71.0f,
+                dy1 = 29.0f,
+            )
+            close()
+            moveToRelative(dx = 180.0f, dy = 160.0f)
+            quadToRelative(
+                dx1 = -42.0f,
+                dy1 = 0.0f,
+                dx2 = -71.0f,
+                dy2 = -29.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = -29.0f,
+                dy1 = -71.0f,
+            )
+            quadToRelative(
+                dx1 = 0.0f,
+                dy1 = -42.0f,
+                dx2 = 29.0f,
+                dy2 = -71.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 71.0f,
+                dy1 = -29.0f,
+            )
+            quadToRelative(
+                dx1 = 42.0f,
+                dy1 = 0.0f,
+                dx2 = 71.0f,
+                dy2 = 29.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 29.0f,
+                dy1 = 71.0f,
+            )
+            quadToRelative(
+                dx1 = 0.0f,
+                dy1 = 42.0f,
+                dx2 = -29.0f,
+                dy2 = 71.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = -71.0f,
+                dy1 = 29.0f,
+            )
+            close()
+            moveTo(x = 266.0f, y = 885.0f)
+            quadToRelative(
+                dx1 = -45.0f,
+                dy1 = 0.0f,
+                dx2 = -75.5f,
+                dy2 = -34.5f,
+            )
+            reflectiveQuadTo(
+                x1 = 160.0f,
+                y1 = 769.0f,
+            )
+            quadToRelative(
+                dx1 = 0.0f,
+                dy1 = -52.0f,
+                dx2 = 35.5f,
+                dy2 = -91.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 70.5f,
+                dy1 = -77.0f,
+            )
+            quadToRelative(
+                dx1 = 29.0f,
+                dy1 = -31.0f,
+                dx2 = 50.0f,
+                dy2 = -67.5f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 50.0f,
+                dy1 = -68.5f,
+            )
+            quadToRelative(
+                dx1 = 22.0f,
+                dy1 = -26.0f,
+                dx2 = 51.0f,
+                dy2 = -43.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 63.0f,
+                dy1 = -17.0f,
+            )
+            quadToRelative(
+                dx1 = 34.0f,
+                dy1 = 0.0f,
+                dx2 = 63.0f,
+                dy2 = 16.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 51.0f,
+                dy1 = 42.0f,
+            )
+            quadToRelative(
+                dx1 = 28.0f,
+                dy1 = 32.0f,
+                dx2 = 49.5f,
+                dy2 = 69.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 50.5f,
+                dy1 = 69.0f,
+            )
+            quadToRelative(
+                dx1 = 35.0f,
+                dy1 = 38.0f,
+                dx2 = 70.5f,
+                dy2 = 77.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = 35.5f,
+                dy1 = 91.0f,
+            )
+            quadToRelative(
+                dx1 = 0.0f,
+                dy1 = 47.0f,
+                dx2 = -30.5f,
+                dy2 = 81.5f,
+            )
+            reflectiveQuadTo(
+                x1 = 694.0f,
+                y1 = 885.0f,
+            )
+            quadToRelative(
+                dx1 = -54.0f,
+                dy1 = 0.0f,
+                dx2 = -107.0f,
+                dy2 = -9.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = -107.0f,
+                dy1 = -9.0f,
+            )
+            quadToRelative(
+                dx1 = -54.0f,
+                dy1 = 0.0f,
+                dx2 = -107.0f,
+                dy2 = 9.0f,
+            )
+            reflectiveQuadToRelative(
+                dx1 = -107.0f,
+                dy1 = 9.0f,
+            )
+            close()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    Icon(imageVector = OutlinedPetsIcon, contentDescription = null)
+}
