27f65ed3daecfe4878b2cc41a498a6f7c902ddf6
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-11-21 13:04:28 +0100

    refactor(core-file): add MimeTypeResolver and MimeType

--- app-common/src/main/kotlin/net/thunderbird/app/common/core/AppCommonCoreModule.kt
+++ app-common/src/main/kotlin/net/thunderbird/app/common/core/AppCommonCoreModule.kt
@@ -1,15 +1,19 @@
 package net.thunderbird.app.common.core
 
 import android.content.Context
+import com.eygraber.uri.toAndroidUri
 import net.thunderbird.app.common.core.configstore.appCommonCoreConfigStoreModule
 import net.thunderbird.app.common.core.logging.appCommonCoreLogger
 import net.thunderbird.app.common.core.ui.appCommonCoreUiModule
 import net.thunderbird.core.file.AndroidDirectoryProvider
 import net.thunderbird.core.file.AndroidFileSystemManager
+import net.thunderbird.core.file.AndroidMimeTypeProvider
+import net.thunderbird.core.file.AndroidMimeTypeResolver
 import net.thunderbird.core.file.DefaultFileManager
 import net.thunderbird.core.file.DirectoryProvider
 import net.thunderbird.core.file.FileManager
 import net.thunderbird.core.file.FileSystemManager
+import net.thunderbird.core.file.MimeTypeResolver
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -35,6 +39,20 @@ val appCommonCoreModule: Module = module {
     single<DirectoryProvider> {
         AndroidDirectoryProvider(
             context = get(),
+        )
+    }
+
+    single<AndroidMimeTypeProvider> {
+        val contentResolver = get<Context>().contentResolver
+
+        AndroidMimeTypeProvider { uri ->
+            contentResolver.getType(uri.toAndroidUri())
+        }
+    }
+
+    single<MimeTypeResolver> {
+        AndroidMimeTypeResolver(
+            mimeTypeProvider = get(),
         )
     }
 }
