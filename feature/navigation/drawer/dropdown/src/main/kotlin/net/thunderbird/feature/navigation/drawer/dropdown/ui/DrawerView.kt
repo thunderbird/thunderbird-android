e1adbe18da25dd1e329efc31a86bdc29a31b854e
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-11-27 15:00:21 +0100

    remove(drawer): remove remaining siderail ui
    # Conflicts:
    #	feature/navigation/drawer/dropdown/src/main/kotlin/net/thunderbird/feature/navigation/drawer/dropdown/ui/DrawerView.kt
    #	feature/navigation/drawer/dropdown/src/main/kotlin/net/thunderbird/feature/navigation/drawer/siderail/ui/SideRailDrawerContent.kt
    #	feature/navigation/drawer/dropdown/src/main/kotlin/net/thunderbird/feature/navigation/drawer/siderail/ui/account/SideRailAccountList.kt
    #	feature/navigation/drawer/dropdown/src/main/kotlin/net/thunderbird/feature/navigation/drawer/siderail/ui/account/SideRailAccountView.kt
    #	feature/navigation/drawer/dropdown/src/main/kotlin/net/thunderbird/feature/navigation/drawer/siderail/ui/setting/SideRailSettingList.kt
    #	feature/navigation/drawer/dropdown/src/test/kotlin/net/thunderbird/feature/navigation/drawer/dropdown/ui/DrawerViewKtTest.kt

--- feature/navigation/drawer/dropdown/src/main/kotlin/net/thunderbird/feature/navigation/drawer/dropdown/ui/DrawerView.kt
+++ feature/navigation/drawer/dropdown/src/main/kotlin/net/thunderbird/feature/navigation/drawer/dropdown/ui/DrawerView.kt
@@ -2,16 +2,11 @@ package net.thunderbird.feature.navigation.drawer.dropdown.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import app.k9mail.core.ui.compose.common.mvi.observe
-import net.thunderbird.core.featureflag.FeatureFlagKey
-import net.thunderbird.core.featureflag.FeatureFlagProvider
-import net.thunderbird.core.featureflag.FeatureFlagResult
 import net.thunderbird.feature.navigation.drawer.dropdown.FolderDrawerState
 import net.thunderbird.feature.navigation.drawer.dropdown.ui.DrawerContract.Effect
 import net.thunderbird.feature.navigation.drawer.dropdown.ui.DrawerContract.Event
 import net.thunderbird.feature.navigation.drawer.dropdown.ui.DrawerContract.ViewModel
-import net.thunderbird.feature.navigation.drawer.siderail.ui.SideRailDrawerContent
 import org.koin.androidx.compose.koinViewModel
 
 @Suppress("LongParameterList")
@@ -25,7 +20,6 @@ internal fun DrawerView(
     openSettings: () -> Unit,
     openAddAccount: () -> Unit,
     closeDrawer: () -> Unit,
-    featureFlagProvider: FeatureFlagProvider,
     viewModel: ViewModel = koinViewModel<DrawerViewModel>(),
 ) {
     val (state, dispatch) = viewModel.observe { effect ->
@@ -44,10 +38,6 @@ internal fun DrawerView(
         }
     }
 
-    val isDropdownDrawerEnabled = remember {
-        featureFlagProvider.provide(FeatureFlagKey("enable_dropdown_drawer_ui")) == FeatureFlagResult.Enabled
-    }
-
     LaunchedEffect(drawerState.selectedAccountUuid) {
         dispatch(Event.SelectAccount(drawerState.selectedAccountUuid))
     }
@@ -56,15 +46,8 @@ internal fun DrawerView(
         dispatch(Event.SelectFolder(drawerState.selectedFolderId))
     }
 
-    if (isDropdownDrawerEnabled) {
-        DrawerContent(
-            state = state.value,
-            onEvent = { dispatch(it) },
-        )
-    } else {
-        SideRailDrawerContent(
-            state = state.value,
-            onEvent = { dispatch(it) },
-        )
-    }
+    DrawerContent(
+        state = state.value,
+        onEvent = { dispatch(it) },
+    )
 }
