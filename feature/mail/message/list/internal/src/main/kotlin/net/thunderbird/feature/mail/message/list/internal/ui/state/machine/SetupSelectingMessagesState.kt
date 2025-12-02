10c14f371fb2069d76f8c0c81921dcec7647629c
Rafael Tonholo <rafael@tonholo.dev>
2025-12-02 16:58:41 -0400

    refactor(message-list): add message list state machine

---
+++ feature/mail/message/list/internal/src/main/kotlin/net/thunderbird/feature/mail/message/list/internal/ui/state/machine/SetupSelectingMessagesState.kt
@@ -0,0 +1,40 @@
+package net.thunderbird.feature.mail.message.list.internal.ui.state.machine
+
+import kotlinx.collections.immutable.toPersistentList
+import net.thunderbird.core.common.state.builder.StateMachineBuilder
+import net.thunderbird.feature.mail.message.list.ui.event.MessageItemEvent
+import net.thunderbird.feature.mail.message.list.ui.event.MessageListEvent
+import net.thunderbird.feature.mail.message.list.ui.state.MessageListState
+
+/**
+ * Defines the state transitions for when the user is actively selecting messages.
+ *
+ * This state is entered when the user long-presses a message, initiating selection mode.
+ *
+ * It handles the following events:
+ *  - [MessageItemEvent.ToggleSelectMessages]: Toggles the selection status of one or more messages.
+ *  - [MessageListEvent.ExitSelectionMode]: Exits selection mode, deselecting all messages and returning
+ *   to the [MessageListState.LoadedMessages] state.
+ */
+internal fun StateMachineBuilder<MessageListState, MessageListEvent>.selectingMessagesState() {
+    state<MessageListState.SelectingMessages> {
+        transition<MessageItemEvent.ToggleSelectMessages> { state, event ->
+            state.copy(
+                messages = state.messages.map { message ->
+                    if (message in event.messages) message.copy(selected = !message.selected) else message
+                }.toPersistentList(),
+            )
+        }
+        transition<MessageListEvent.ExitSelectionMode> { state, _ ->
+            MessageListState.LoadedMessages(
+                folder = null,
+                messages = state.messages.map { message -> message.copy(selected = false) }.toPersistentList(),
+                preferences = state.preferences,
+                swipeActions = state.swipeActions,
+                selectedSortTypes = state.selectedSortTypes,
+                activeMessage = state.activeMessage,
+                isActive = state.isActive,
+            )
+        }
+    }
+}
