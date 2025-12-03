045d98d248e6c0d58be27e288ba3044547b1821f
Rafael Tonholo <rafael@tonholo.dev>
2025-12-03 08:19:02 -0400

    feat(message-list): add new message list view model

--- feature/mail/message/list/internal/src/main/kotlin/net/thunderbird/feature/mail/message/list/internal/FeatureMessageListModule.kt
+++ feature/mail/message/list/internal/src/main/kotlin/net/thunderbird/feature/mail/message/list/internal/FeatureMessageListModule.kt
@@ -1,6 +1,7 @@
 package net.thunderbird.feature.mail.message.list.internal
 
 import net.thunderbird.core.common.inject.factoryListOf
+import net.thunderbird.core.common.inject.getList
 import net.thunderbird.feature.mail.message.list.domain.DomainContract
 import net.thunderbird.feature.mail.message.list.internal.domain.usecase.BuildSwipeActions
 import net.thunderbird.feature.mail.message.list.internal.domain.usecase.CreateArchiveFolder
@@ -8,11 +9,14 @@ import net.thunderbird.feature.mail.message.list.internal.domain.usecase.GetAcco
 import net.thunderbird.feature.mail.message.list.internal.domain.usecase.GetMessageListPreferences
 import net.thunderbird.feature.mail.message.list.internal.domain.usecase.GetSortTypes
 import net.thunderbird.feature.mail.message.list.internal.domain.usecase.SetArchiveFolder
+import net.thunderbird.feature.mail.message.list.internal.ui.MessageListViewModel
 import net.thunderbird.feature.mail.message.list.internal.ui.dialog.SetupArchiveFolderDialogFragment
 import net.thunderbird.feature.mail.message.list.internal.ui.dialog.SetupArchiveFolderDialogViewModel
+import net.thunderbird.feature.mail.message.list.internal.ui.state.machine.MessageListStateMachine
 import net.thunderbird.feature.mail.message.list.internal.ui.state.sideeffect.LoadSortTypeStateSideEffectHandler
 import net.thunderbird.feature.mail.message.list.internal.ui.state.sideeffect.LoadSwipeActionsStateSideEffectHandler
 import net.thunderbird.feature.mail.message.list.internal.ui.state.sideeffect.StateSideEffectHandler
+import net.thunderbird.feature.mail.message.list.ui.MessageListContract
 import net.thunderbird.feature.mail.message.list.ui.dialog.SetupArchiveFolderDialogContract
 import net.thunderbird.feature.mail.message.list.ui.dialog.SetupArchiveFolderDialogFragmentFactory
 import org.koin.core.module.dsl.viewModel
@@ -80,4 +84,17 @@ val featureMessageListModule = module {
             )
         },
     )
+    factory { parameters ->
+        MessageListStateMachine.Factory(
+            logger = get(),
+            stateSideEffectHandlersFactories = getList(parameters = { parameters }),
+        )
+    }
+    viewModel<MessageListContract.ViewModel> { parameters ->
+        MessageListViewModel(
+            logger = get(),
+            messageListStateMachineFactory = get { parameters },
+            getMessageListPreferences = get(),
+        )
+    }
 }
