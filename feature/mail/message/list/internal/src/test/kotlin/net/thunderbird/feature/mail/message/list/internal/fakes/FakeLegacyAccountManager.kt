66fe047f3eff4b4d2ed3c7fa7ef81f260f86f171
Rafael Tonholo <rafael@tonholo.dev>
2025-12-08 14:11:35 -0400

    refactor(message-list): migrate swipe actions to a reactive approach

---
+++ feature/mail/message/list/internal/src/test/kotlin/net/thunderbird/feature/mail/message/list/internal/fakes/FakeLegacyAccountManager.kt
@@ -0,0 +1,31 @@
+package net.thunderbird.feature.mail.message.list.internal.fakes
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import net.thunderbird.core.android.account.LegacyAccount
+import net.thunderbird.core.android.account.LegacyAccountManager
+import net.thunderbird.feature.account.AccountId
+
+internal open class FakeLegacyAccountManager(
+    private val accounts: List<LegacyAccount>,
+) : LegacyAccountManager {
+    override fun getAccounts(): List<LegacyAccount> = accounts
+
+    override fun getAccountsFlow(): Flow<List<LegacyAccount>> = flowOf(accounts)
+
+    override fun getAccount(accountUuid: String): LegacyAccount? = accounts.firstOrNull { it.uuid == accountUuid }
+
+    override fun getAccountFlow(accountUuid: String): Flow<LegacyAccount?> = flowOf(getAccount(accountUuid))
+
+    override fun moveAccount(
+        account: LegacyAccount,
+        newPosition: Int,
+    ) = error("not implemented.")
+
+    override fun saveAccount(account: LegacyAccount) = Unit
+    override fun getAll(): Flow<List<LegacyAccount>> = flowOf(getAccounts())
+
+    override fun getById(id: AccountId): Flow<LegacyAccount?> = flowOf(getAccount(id.asRaw()))
+
+    override suspend fun update(account: LegacyAccount) = saveAccount(account)
+}
