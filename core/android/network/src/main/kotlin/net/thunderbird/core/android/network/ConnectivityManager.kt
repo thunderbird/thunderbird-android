646498f57bcef90e1b2869d0c1bd101fb3de505b
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-12-17 10:13:33 +0100

    refactor: change ConnectivityManager to compat

--- core/android/network/src/main/kotlin/net/thunderbird/core/android/network/ConnectivityManager.kt
+++ core/android/network/src/main/kotlin/net/thunderbird/core/android/network/ConnectivityManager.kt
@@ -1,8 +1,5 @@
 package net.thunderbird.core.android.network
 
-import android.os.Build
-import android.net.ConnectivityManager as SystemConnectivityManager
-
 interface ConnectivityManager {
     fun start()
     fun stop()
@@ -14,11 +11,4 @@ interface ConnectivityManager {
 interface ConnectivityChangeListener {
     fun onConnectivityChanged()
     fun onConnectivityLost()
-}
-
-internal fun ConnectivityManager(systemConnectivityManager: SystemConnectivityManager): ConnectivityManager {
-    return when {
-        Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> ConnectivityManagerApi24(systemConnectivityManager)
-        else -> ConnectivityManagerApi23(systemConnectivityManager)
-    }
 }
