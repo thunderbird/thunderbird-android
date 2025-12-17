69d48106dd005c73ce03e18d9e268f480bd6a31a
Wolf-Martell Montwé <wolf@thunderbird.net>
2025-12-17 14:01:20 +0100

    remove: outdated lint rule

--- feature/account/server/settings/src/debug/kotlin/app/k9mail/feature/account/server/settings/ui/incoming/IncomingServerSettingsScreenPreview.kt
+++ feature/account/server/settings/src/debug/kotlin/app/k9mail/feature/account/server/settings/ui/incoming/IncomingServerSettingsScreenPreview.kt
@@ -1,6 +1,7 @@
 package app.k9mail.feature.account.server.settings.ui.incoming
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import app.k9mail.core.ui.compose.common.annotation.PreviewDevices
 import app.k9mail.core.ui.compose.designsystem.PreviewWithTheme
 import app.k9mail.feature.account.common.domain.entity.InteractionMode
@@ -13,11 +14,13 @@ internal fun IncomingServerSettingsScreenPreview() {
         IncomingServerSettingsScreen(
             onNext = {},
             onBack = {},
-            viewModel = IncomingServerSettingsViewModel(
-                mode = InteractionMode.Create,
-                validator = IncomingServerSettingsValidator(),
-                accountStateRepository = FakeAccountStateRepository(),
-            ),
+            viewModel = viewModel {
+                IncomingServerSettingsViewModel(
+                    mode = InteractionMode.Create,
+                    validator = IncomingServerSettingsValidator(),
+                    accountStateRepository = FakeAccountStateRepository(),
+                )
+            },
         )
     }
 }
--- feature/onboarding/permissions/src/debug/kotlin/app/k9mail/feature/onboarding/permissions/ui/PermissionScreenPreview.kt
+++ feature/onboarding/permissions/src/debug/kotlin/app/k9mail/feature/onboarding/permissions/ui/PermissionScreenPreview.kt
@@ -1,6 +1,7 @@
 package app.k9mail.feature.onboarding.permissions.ui
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import app.k9mail.core.android.permissions.PermissionState
 import app.k9mail.core.ui.compose.common.annotation.PreviewDevices
 import app.k9mail.core.ui.compose.designsystem.PreviewWithTheme
@@ -12,10 +13,12 @@ import net.thunderbird.core.common.provider.BrandNameProvider
 internal fun PermissionScreenPreview() {
     PreviewWithTheme {
         PermissionsScreen(
-            viewModel = PermissionsViewModel(
-                checkPermission = { PermissionState.Denied },
-                backgroundDispatcher = Dispatchers.Main.immediate,
-            ),
+            viewModel = viewModel {
+                PermissionsViewModel(
+                    checkPermission = { PermissionState.Denied },
+                    backgroundDispatcher = Dispatchers.Main.immediate,
+                )
+            },
             brandNameProvider = object : BrandNameProvider {
                 override val brandName: String = "BrandName"
             },
