1e4510a4616e277fdfd2acbd8dab157117b9d6b2
Stephen King <sking@thunderbird.net>
2026-01-20 21:17:37 -0700

    Bump Version Code

--- app-thunderbird/build.gradle.kts
+++ app-thunderbird/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "net.thunderbird.android"
         testApplicationId = "net.thunderbird.android.tests"
 
-        versionCode = 40
+        versionCode = 41
         versionName = "16.0"
 
         buildConfigField("String", "CLIENT_INFO_APP_NAME", "\"Thunderbird for Android\"")
