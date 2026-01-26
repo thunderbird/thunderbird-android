7f24f6ccd4e41429084a6aba41827fc29734fa55
Wolf-Martell Montwé <wolf@thunderbird.net>
2026-01-26 18:34:31 +0100

    refactor: inline shared config and compose setup into module build files

--- build-plugin/src/main/kotlin/thunderbird.library.android.compose.gradle.kts
+++ build-plugin/src/main/kotlin/thunderbird.library.android.compose.gradle.kts
@@ -10,7 +10,9 @@ plugins {
 }
 
 android {
-    configureSharedComposeConfig(libs)
+    buildFeatures {
+        compose = true
+    }
 }
 
 androidComponents {
@@ -21,5 +23,15 @@ androidComponents {
 }
 
 dependencies {
-    configureSharedComposeDependencies(libs)
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    implementation(libs.bundles.shared.jvm.android.compose)
+
+    debugImplementation(libs.bundles.shared.jvm.android.compose.debug)
+
+    testImplementation(libs.bundles.shared.jvm.test.compose)
+
+    androidTestImplementation(libs.bundles.shared.jvm.androidtest.compose)
 }
