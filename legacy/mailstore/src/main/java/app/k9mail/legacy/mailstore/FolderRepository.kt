b178ef89a4805d2d80b0e98b26bf7855bf9d5119
Wolf-Martell Montwé <wolf@thunderbird.net>
2026-02-03 13:55:45 +0100

    refactor(mailstore): replace FolderRepository class with interface and implement DefaultFolderRepository

--- legacy/mailstore/src/main/java/app/k9mail/legacy/mailstore/FolderRepository.kt
+++ legacy/mailstore/src/main/java/app/k9mail/legacy/mailstore/FolderRepository.kt
@@ -1,218 +1,149 @@
 package app.k9mail.legacy.mailstore
 
-import app.k9mail.legacy.mailstore.FolderTypeMapper.folderTypeOf
-import app.k9mail.legacy.mailstore.RemoteFolderTypeMapper.toFolderType
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.buffer
-import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.flowOn
-import net.thunderbird.core.android.account.LegacyAccount
-import net.thunderbird.core.android.account.LegacyAccountManager
 import net.thunderbird.core.common.exception.MessagingException
 import net.thunderbird.feature.account.AccountId
 import net.thunderbird.feature.mail.folder.api.Folder
 import net.thunderbird.feature.mail.folder.api.FolderDetails
-import net.thunderbird.feature.mail.folder.api.FolderType
-import net.thunderbird.feature.mail.folder.api.OutboxFolderManager
 import net.thunderbird.feature.mail.folder.api.RemoteFolder
 
+/**
+ * Provides access to local and remote folder information and management.
+ */
 @Suppress("TooManyFunctions")
-class FolderRepository(
-    private val accountManager: LegacyAccountManager,
-    private val messageStoreManager: MessageStoreManager,
-    private val outboxFolderManager: OutboxFolderManager,
-    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
-) {
-    suspend fun getFolder(accountId: AccountId, folderId: Long): Folder? {
-        val account = getAccountById(accountId)
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        val outboxFolderId = outboxFolderManager.getOutboxFolderId(accountId)
-        return messageStore.getFolder(folderId) { folder ->
-            Folder(
-                id = folder.id,
-                name = folder.name,
-                type = folder.getFolderType(account, outboxFolderId),
-                isLocalOnly = folder.isLocalOnly,
-            )
-        }
-    }
+interface FolderRepository {
+    /**
+     * Returns a [Folder] for the given [accountId] and [folderId].
+     *
+     * @param accountId The account identifier.
+     * @param folderId The folder identifier.
+     */
+    suspend fun getFolder(accountId: AccountId, folderId: Long): Folder?
 
-    suspend fun getFolderDetails(accountId: AccountId, folderId: Long): FolderDetails? {
-        val account = getAccountById(accountId)
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        val outboxFolderId = outboxFolderManager.getOutboxFolderId(accountId)
-        return messageStore.getFolder(folderId) { folder ->
-            FolderDetails(
-                folder = Folder(
-                    id = folder.id,
-                    name = folder.name,
-                    type = folder.getFolderType(account, outboxFolderId),
-                    isLocalOnly = folder.isLocalOnly,
-                ),
-                isInTopGroup = folder.isInTopGroup,
-                isIntegrate = folder.isIntegrate,
-                isSyncEnabled = folder.isSyncEnabled,
-                isVisible = folder.isVisible,
-                isNotificationsEnabled = folder.isNotificationsEnabled,
-                isPushEnabled = folder.isPushEnabled,
-            )
-        }
-    }
+    /**
+     * Returns a [FolderDetails] for the given [accountId] and [folderId].
+     *
+     * @param accountId The account identifier.
+     * @param folderId The folder identifier.
+     */
+    suspend fun getFolderDetails(accountId: AccountId, folderId: Long): FolderDetails?
 
+    /**
+     * Returns a list of [RemoteFolder]s for the given [accountId].
+     *
+     * @param accountId The account identifier.
+     * @throws MessagingException if there's a problem accessing the folders.
+     */
     @Throws(MessagingException::class)
-    fun getRemoteFolders(accountId: AccountId): List<RemoteFolder> {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        return messageStore.getFolders(excludeLocalOnly = true) { folder ->
-            RemoteFolder(
-                id = folder.id,
-                serverId = folder.serverIdOrThrow(),
-                name = folder.name,
-                type = folder.type.toFolderType(),
-            )
-        }
-    }
+    fun getRemoteFolders(accountId: AccountId): List<RemoteFolder>
 
-    fun getRemoteFolderDetails(accountId: AccountId): List<RemoteFolderDetails> {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        return messageStore.getFolders(excludeLocalOnly = true) { folder ->
-            RemoteFolderDetails(
-                folder = RemoteFolder(
-                    id = folder.id,
-                    serverId = folder.serverIdOrThrow(),
-                    name = folder.name,
-                    type = folder.type.toFolderType(),
-                ),
-                isInTopGroup = folder.isInTopGroup,
-                isIntegrate = folder.isIntegrate,
-                isSyncEnabled = folder.isSyncEnabled,
-                isVisible = folder.isVisible,
-                isNotificationsEnabled = folder.isNotificationsEnabled,
-                isPushEnabled = folder.isPushEnabled,
-            )
-        }
-    }
+    /**
+     * Returns a list of [RemoteFolderDetails] for the given [accountId].
+     *
+     * @param accountId The account identifier.
+     */
+    fun getRemoteFolderDetails(accountId: AccountId): List<RemoteFolderDetails>
 
-    fun getPushFoldersFlow(accountId: AccountId): Flow<List<RemoteFolder>> {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        return callbackFlow {
-            send(getPushFolders(accountId))
+    /**
+     * Returns a [Flow] of [RemoteFolder]s for the given [accountId] that should be used for push.
+     *
+     * @param accountId The account identifier.
+     */
+    fun getPushFoldersFlow(accountId: AccountId): Flow<List<RemoteFolder>>
 
-            val listener = FolderSettingsChangedListener {
-                trySendBlocking(getPushFolders(accountId))
-            }
-            messageStore.addFolderSettingsChangedListener(listener)
+    /**
+     * Returns a list of [RemoteFolder]s for the given [accountId] that should be used for push.
+     *
+     * @param accountId The account identifier.
+     */
+    fun getPushFolders(accountId: AccountId): List<RemoteFolder>
 
-            awaitClose {
-                messageStore.removeFolderSettingsChangedListener(listener)
-            }
-        }.buffer(capacity = Channel.CONFLATED)
-            .distinctUntilChanged()
-            .flowOn(ioDispatcher)
-    }
+    /**
+     * Returns the server ID for the given [accountId] and [folderId].
+     *
+     * @param accountId The account identifier.
+     * @param folderId The folder identifier.
+     */
+    fun getFolderServerId(accountId: AccountId, folderId: Long): String?
 
-    private fun getPushFolders(accountId: AccountId): List<RemoteFolder> {
-        return getRemoteFolderDetails(accountId)
-            .asSequence()
-            .filter { folderDetails -> folderDetails.isPushEnabled }
-            .map { folderDetails -> folderDetails.folder }
-            .toList()
-    }
+    /**
+     * Returns the folder ID for the given [accountId] and [folderServerId].
+     *
+     * @param accountId The account identifier.
+     * @param folderServerId The folder server identifier.
+     */
+    fun getFolderId(accountId: AccountId, folderServerId: String): Long?
 
-    fun getFolderServerId(accountId: AccountId, folderId: Long): String? {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        return messageStore.getFolder(folderId) { folder ->
-            folder.serverId
-        }
-    }
+    /**
+     * Returns `true` if the folder with [folderId] is present for the given [accountId].
+     *
+     * @param accountId The account identifier.
+     * @param folderId The folder identifier.
+     */
+    fun isFolderPresent(accountId: AccountId, folderId: Long): Boolean
 
-    fun getFolderId(accountId: AccountId, folderServerId: String): Long? {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        return messageStore.getFolderId(folderServerId)
-    }
+    /**
+     * Updates the folder details for the given [accountId].
+     *
+     * @param accountId The account identifier.
+     * @param folderDetails The folder details to update.
+     */
+    fun updateFolderDetails(accountId: AccountId, folderDetails: FolderDetails)
 
-    fun isFolderPresent(accountId: AccountId, folderId: Long): Boolean {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        return messageStore.getFolder(folderId) { true } ?: false
-    }
+    /**
+     * Sets whether the folder with [folderId] should be included in the unified inbox.
+     *
+     * @param accountId The account identifier.
+     * @param folderId The folder identifier.
+     * @param includeInUnifiedInbox Whether to include the folder in the unified inbox.
+     */
+    fun setIncludeInUnifiedInbox(accountId: AccountId, folderId: Long, includeInUnifiedInbox: Boolean)
 
-    fun updateFolderDetails(accountId: AccountId, folderDetails: FolderDetails) {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        messageStore.updateFolderSettings(folderDetails)
-    }
+    /**
+     * Sets whether the folder with [folderId] is visible.
+     *
+     * @param accountId The account identifier.
+     * @param folderId The folder identifier.
+     * @param visible Whether the folder is visible.
+     */
+    fun setVisible(accountId: AccountId, folderId: Long, visible: Boolean)
 
-    fun setIncludeInUnifiedInbox(accountId: AccountId, folderId: Long, includeInUnifiedInbox: Boolean) {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        messageStore.setIncludeInUnifiedInbox(folderId, includeInUnifiedInbox)
-    }
+    /**
+     * Sets whether synchronization is enabled for the folder with [folderId].
+     *
+     * @param accountId The account identifier.
+     * @param folderId The folder identifier.
+     * @param enable Whether synchronization is enabled.
+     */
+    fun setSyncEnabled(accountId: AccountId, folderId: Long, enable: Boolean)
 
-    fun setVisible(accountId: AccountId, folderId: Long, visible: Boolean) {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        messageStore.setVisible(folderId, visible)
-    }
+    /**
+     * Sets whether notifications are enabled for the folder with [folderId].
+     *
+     * @param accountId The account identifier.
+     * @param folderId The folder identifier.
+     * @param enable Whether notifications are enabled.
+     */
+    fun setNotificationsEnabled(accountId: AccountId, folderId: Long, enable: Boolean)
 
-    fun setSyncEnabled(accountId: AccountId, folderId: Long, enable: Boolean) {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        messageStore.setSyncEnabled(folderId, enable)
-    }
+    /**
+     * Disables push for the given [accountId].
+     *
+     * @param accountId The account identifier.
+     */
+    fun setPushDisabled(accountId: AccountId)
 
-    fun setNotificationsEnabled(accountId: AccountId, folderId: Long, enable: Boolean) {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        messageStore.setNotificationsEnabled(folderId, enable)
-    }
+    /**
+     * Returns `true` if there's at least one folder with push enabled for the given [accountId].
+     *
+     * @param accountId The account identifier.
+     */
+    fun hasPushEnabledFolder(accountId: AccountId): Boolean
 
-    fun setPushDisabled(accountId: AccountId) {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        messageStore.setPushDisabled()
-    }
-
-    fun hasPushEnabledFolder(accountId: AccountId): Boolean {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        return messageStore.hasPushEnabledFolder()
-    }
-
-    fun hasPushEnabledFolderFlow(accountId: AccountId): Flow<Boolean> {
-        val messageStore = messageStoreManager.getMessageStore(accountId)
-        return callbackFlow {
-            send(hasPushEnabledFolder(accountId))
-
-            val listener = FolderSettingsChangedListener {
-                trySendBlocking(hasPushEnabledFolder(accountId))
-            }
-            messageStore.addFolderSettingsChangedListener(listener)
-
-            awaitClose {
-                messageStore.removeFolderSettingsChangedListener(listener)
-            }
-        }.buffer(capacity = Channel.CONFLATED)
-            .distinctUntilChanged()
-            .flowOn(ioDispatcher)
-    }
-
-    private fun FolderDetailsAccessor.getFolderType(account: LegacyAccount, outboxFolderId: Long): FolderType =
-        if (id == outboxFolderId) {
-            FolderType.OUTBOX
-        } else {
-            folderTypeOf(account, id)
-        }
-
-    private suspend fun getAccountById(accountId: AccountId): LegacyAccount =
-        accountManager.getById(accountId).firstOrNull()
-            ?: error("Account not found: $accountId")
+    /**
+     * Returns a [Flow] that emits `true` if there's at least one folder with push enabled for the given [accountId].
+     *
+     * @param accountId The account identifier.
+     */
+    fun hasPushEnabledFolderFlow(accountId: AccountId): Flow<Boolean>
 }
-
-data class RemoteFolderDetails(
-    val folder: RemoteFolder,
-    val isInTopGroup: Boolean,
-    val isIntegrate: Boolean,
-    val isSyncEnabled: Boolean,
-    val isVisible: Boolean,
-    val isNotificationsEnabled: Boolean,
-    val isPushEnabled: Boolean,
-)
