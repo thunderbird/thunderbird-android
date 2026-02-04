d6950ca4f0fa7c5306fd6f175e9cd1134897e11b
thunderbird-botmobile <202943968+thunderbird-botmobile[bot]@users.noreply.github.com>
2026-02-04 21:08:48 +0000

    Release: Thunderbird Beta 17.0b2

--- app-thunderbird/build.gradle.kts
+++ app-thunderbird/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "net.thunderbird.android"
         testApplicationId = "net.thunderbird.android.tests"
 
-        versionCode = 42
+        versionCode = 43
         versionName = "17.0"
 
         buildConfigField("String", "CLIENT_INFO_APP_NAME", "\"Thunderbird for Android\"")
@@ -121,7 +121,7 @@ android {
             signingConfig = signingConfigs.getByType(SigningType.TB_BETA)
 
             applicationIdSuffix = ".beta"
-            versionNameSuffix = "b1"
+            versionNameSuffix = "b2"
 
             isMinifyEnabled = true
             isShrinkResources = true
