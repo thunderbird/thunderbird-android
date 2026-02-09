713ec1f23ba0b81e7fba5cb18fd5d902ca35a866
Corey Bryant <corey@thunderbird.net>
2026-02-09 15:27:52 -0500

    Bump version to 16.1

--- app-k9mail/build.gradle.kts
+++ app-k9mail/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         testApplicationId = "com.fsck.k9.tests"
 
         versionCode = 39032
-        versionName = "16.0"
+        versionName = "16.1"
 
         buildConfigField("String", "CLIENT_INFO_APP_NAME", "\"K-9 Mail\"")
     }
