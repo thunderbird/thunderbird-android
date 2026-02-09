c3cb4e1fe7161354ea5556beb2cac6a8da5c1a58
Wolf-Martell Montwé <wolf@thunderbird.net>
2026-02-09 13:53:35 +0100

    fix(build): clean up dependencies for account profile

--- feature/account/api/build.gradle.kts
+++ feature/account/api/build.gradle.kts
@@ -11,11 +11,6 @@ kotlin {
         commonMain.dependencies {
             api(projects.core.architecture.api)
         }
-
-        androidMain.dependencies {
-            // ensure Android target can consume the module if it's platform-specific
-            api(projects.core.architecture.api)
-        }
     }
 }
 
