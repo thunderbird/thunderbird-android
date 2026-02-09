0bd88403bb57979bb57f949a66e34539a6e072ad
Rafael Tonholo <rafael@tonholo.dev>
2026-02-09 10:30:03 -0400

    chore(feature-flags): enable new message reader CSS styles and mark legacy code for cleanup for release build (#10499)

--- feature/mail/message/reader/api/src/commonMain/kotlin/net/thunderbird/feature/mail/message/reader/api/MessageReaderFeatureFlags.kt
+++ feature/mail/message/reader/api/src/commonMain/kotlin/net/thunderbird/feature/mail/message/reader/api/MessageReaderFeatureFlags.kt
@@ -3,5 +3,6 @@ package net.thunderbird.feature.mail.message.reader.api
 import net.thunderbird.core.featureflag.FeatureFlagKey
 
 object MessageReaderFeatureFlags {
+    // TODO(#10498): Remove when UseNewMessageReaderCssStyles is no longer required
     val UseNewMessageReaderCssStyles = FeatureFlagKey("use_new_message_reader_css_styles")
 }
