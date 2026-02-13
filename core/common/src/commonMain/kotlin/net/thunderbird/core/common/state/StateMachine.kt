e549b91b8ed7d3dee332c59f45cdddbbc2c15c7a
Rafael Tonholo <rafael@tonholo.dev>
2026-02-13 14:48:40 -0400

    chore(state-machine): record each transaction of the state machine within a history stack

--- core/common/src/commonMain/kotlin/net/thunderbird/core/common/state/StateMachine.kt
+++ core/common/src/commonMain/kotlin/net/thunderbird/core/common/state/StateMachine.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import net.thunderbird.core.common.state.StateMachine.Transition
 
 internal typealias TransactionKey<TState, TEvent> = Pair<KClass<out TState>, KClass<out TEvent>>
 
@@ -33,6 +34,23 @@ interface StateMachine<TState : Any, TEvent : Any> {
     val currentStateSnapshot: TState get() = currentState.value
 
     /**
+     * Represents the history of state transitions in the state machine.
+     *
+     * This list contains the sequence of transitions that have occurred, in the order
+     * they were executed. Each transition encapsulates the logic used to determine
+     * if a state change should occur and the function to generate the new state.
+     * Observing or analyzing this history can provide insights into the state machine's
+     * behavior and processing flow.
+     *
+     * The history stack is immutable and is updated atomically within the state machine
+     * during each valid transition, ensuring thread safety and consistency.
+     *
+     * @param TState The type representing the states managed by the state machine.
+     * @param TEvent The type representing the events processed by the state machine.
+     */
+    val historyStack: List<StateTransitionRecord<TState, TEvent>>
+
+    /**
      * Processes an incoming event against the current state.
      *
      * This function looks for a defined transition that matches the current state's type and the
@@ -49,6 +67,44 @@ interface StateMachine<TState : Any, TEvent : Any> {
      * @return The new state if a transition occurred, or the current state if no transition was made.
      */
     suspend fun process(event: TEvent): TState
+
+    /**
+     * Represents a state transition in a state machine.
+     *
+     * A transition defines the conditions under which a state machine moves from one state to another
+     * based on the current state and an incoming event. Each transition consists of a [guard] function that
+     * determines whether the transition is valid and a state creation function that generates the new state.
+     *
+     * @param TState The type representing the states managed by the state machine.
+     * @param TEvent The type representing the events triggering transitions.
+     * @property guard A condition function that determines if the transition is valid. The function takes
+     *  the current state and the event as input and returns `true` if the transition can occur, or `false`
+     *  otherwise.
+     * @property createNewState A function responsible for generating the new state after a transition. The
+     *  function takes the current state and the event as input and returns the new state.
+     */
+    data class Transition<TState : Any, in TEvent : Any>(
+        val guard: (TState, TEvent) -> Boolean,
+        val createNewState: (TState, TEvent) -> TState,
+    )
+
+    /**
+     * Represents a record of a state transition in a state machine.
+     *
+     * This data class encapsulates the details of a state transition, including the specific transition
+     * object that governs the change, the state before the transition, and the state after the transition.
+     *
+     * @param TState The type representing the states managed by the state machine.
+     * @param TEvent The type representing the events that trigger state transitions.
+     * @property transition The transition object that defines the conditions and rules of the state change.
+     * @property previousState The state of the state machine before the transition occurred.
+     * @property newState The state of the state machine after the transition occurred.
+     */
+    data class StateTransitionRecord<TState : Any, in TEvent : Any>(
+        val transition: Transition<TState, TEvent>,
+        val previousState: TState,
+        val newState: TState,
+    )
 }
 
 internal class DefaultStateMachine<TState : Any, TEvent : Any>(
@@ -64,11 +120,6 @@ internal class DefaultStateMachine<TState : Any, TEvent : Any>(
         val transitions: Map<TransactionKey<out TState, out TEvent>, Transition<TState, TEvent>>,
     )
 
-    data class Transition<TState : Any, in TEvent : Any>(
-        val guard: (TState, TEvent) -> Boolean,
-        val createNewState: (TState, TEvent) -> TState,
-    )
-
     data class StateListeners<in TCurrentState : TState, TState : Any, in TEvent : Any>(
         val onEnter: (TState?.(event: TEvent?, newState: TCurrentState) -> Unit)?,
         val onExit: (TCurrentState.(event: TEvent?) -> Unit)?,
@@ -77,6 +128,7 @@ internal class DefaultStateMachine<TState : Any, TEvent : Any>(
     private val mutex = Mutex()
     private val _currentState = MutableStateFlow(initialState)
     override val currentState: StateFlow<TState> = _currentState.asStateFlow()
+    override val historyStack = ArrayDeque<StateMachine.StateTransitionRecord<TState, TEvent>>()
 
     init {
         scope.launch {
@@ -88,43 +140,48 @@ internal class DefaultStateMachine<TState : Any, TEvent : Any>(
 
     override suspend fun process(event: TEvent): TState {
         mutex.withLock {
-            val currentStateRegistry = stateRegistrar[_currentState.value::class]
-            if (currentStateRegistry?.isFinalState == true) return currentState.value
+            val currentState = _currentState.value
+            val currentStateRegistry = stateRegistrar[currentState::class]
+            if (currentStateRegistry?.isFinalState == true) return currentState
 
-            val key = _currentState.value::class to event::class
+            val key = currentState::class to event::class
             val transition = transitions[key]
                 ?: transitions
                     .filterKeys { (stateClass, eventClass) ->
-                        stateClass.isInstance(_currentState.value) && eventClass == event::class
+                        stateClass.isInstance(currentState) && eventClass == event::class
                     }
                     .firstNotNullOfOrNull { it.value }
 
             return when {
-                transition != null && transition.guard(_currentState.value, event) -> {
-                    transition.createNewState(_currentState.value, event).also { newState ->
-                        _currentState.update { currentState ->
-                            val newStateRegistry = stateRegistrar[newState::class]
-                            when {
-                                newStateRegistry?.isFinalState == true -> {
-                                    currentStateRegistry?.listeners?.onExit?.invoke(currentState, event)
-                                    newStateRegistry.listeners.onEnter?.invoke(currentState, event, newState)
-                                    newStateRegistry.listeners.onExit?.invoke(newState, null)
-                                }
+                transition != null && transition.guard(currentState, event) -> {
+                    val newState = transition.createNewState(currentState, event)
+                    val newStateRegistry = stateRegistrar[newState::class]
+                    when {
+                        newStateRegistry?.isFinalState == true -> {
+                            currentStateRegistry?.listeners?.onExit?.invoke(currentState, event)
+                            newStateRegistry.listeners.onEnter?.invoke(currentState, event, newState)
+                            newStateRegistry.listeners.onExit?.invoke(newState, null)
+                        }
 
-                                currentState::class != newState::class -> {
-                                    currentStateRegistry?.listeners?.onExit?.invoke(currentState, event)
-                                    newStateRegistry?.listeners?.onEnter?.invoke(currentState, event, newState)
-                                }
-                            }
-
-                            newState
+                        currentState::class != newState::class -> {
+                            currentStateRegistry?.listeners?.onExit?.invoke(currentState, event)
+                            newStateRegistry?.listeners?.onEnter?.invoke(currentState, event, newState)
                         }
                     }
+                    _currentState.update { newState }
+                    historyStack.addLast(
+                        StateMachine.StateTransitionRecord(
+                            transition = transition,
+                            previousState = currentState,
+                            newState = newState,
+                        ),
+                    )
+                    newState
                 }
 
                 else -> {
-                    // No transition defined or guard failed -> Ignore event
-                    _currentState.value
+                    // No transition defined, or guard failed -> Ignore event
+                    currentState
                 }
             }
         }
