8da419a09e13082d62346fd398f5c1f85119970d
Rafael Tonholo <rafael@tonholo.dev>
2026-03-02 15:23:53 -0400

    feat(message-list): add messageReference to MessageItemUi

--- feature/mail/message/list/internal/src/test/kotlin/net/thunderbird/feature/mail/message/list/internal/ui/state/machine/MessageListStateMachineTest.kt
+++ feature/mail/message/list/internal/src/test/kotlin/net/thunderbird/feature/mail/message/list/internal/ui/state/machine/MessageListStateMachineTest.kt
@@ -720,6 +720,7 @@ private fun createMessageUiItemList(
 private fun createMessageUiItem(
     state: State,
     id: String,
+    messageReference: String = "message_reference",
     accountId: AccountId = AccountIdFactory.create(),
     senders: ComposedAddressUi = ComposedAddressUi(displayName = "sender"),
     subject: String = "mock subject",
@@ -735,6 +736,7 @@ private fun createMessageUiItem(
 ): MessageItemUi = MessageItemUi(
     state = state,
     id = id,
+    messageReference = messageReference,
     account = Account(id = accountId, color = Color.Unspecified),
     senders = senders,
     subject = subject,
