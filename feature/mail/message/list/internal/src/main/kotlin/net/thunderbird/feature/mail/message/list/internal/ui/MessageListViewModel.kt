492fcdf109256db3e881614d729d44befc82cd20
Rafael Tonholo <rafael@tonholo.dev>
2026-03-02 16:00:45 -0400

    refactor(message-list): remove manual refresh on sort criteria change in MessageListViewModel

--- feature/mail/message/list/internal/src/main/kotlin/net/thunderbird/feature/mail/message/list/internal/ui/MessageListViewModel.kt
+++ feature/mail/message/list/internal/src/main/kotlin/net/thunderbird/feature/mail/message/list/internal/ui/MessageListViewModel.kt
@@ -7,7 +7,6 @@ import net.thunderbird.core.common.state.StateMachine
 import net.thunderbird.core.logging.Logger
 import net.thunderbird.feature.mail.message.list.internal.ui.state.machine.MessageListStateMachine
 import net.thunderbird.feature.mail.message.list.ui.MessageListContract
-import net.thunderbird.feature.mail.message.list.ui.effect.MessageListEffect
 import net.thunderbird.feature.mail.message.list.ui.event.MessageListEvent
 import net.thunderbird.feature.mail.message.list.ui.state.MessageListState
 import net.thunderbird.feature.mail.message.list.ui.state.sideeffect.MessageListStateSideEffectHandlerFactory
@@ -21,26 +20,12 @@ class MessageListViewModel(
 ) : MessageListContract.ViewModel(logger, stateSideEffectHandlersFactories) {
     override val stateMachine: StateMachine<MessageListState, MessageListEvent> = messageListStateMachineFactory
         .create(scope = viewModelScope, dispatch = ::event, dispatchUiEffect = ::emitEffect)
-    private var previousState: MessageListState? = null
 
     init {
         logger.verbose(TAG) { "init() called" }
         state
             .onEach { state ->
                 logger.verbose(TAG) { "state.onEach called with: state = $state" }
-                val previousState = previousState
-                if (previousState != null &&
-                    previousState.metadata.sortCriteriaPerAccount != state.metadata.sortCriteriaPerAccount
-                ) {
-                    // TODO(#10251): Required as the current implementation of sortType and sortAscending
-                    //  returns null before we load the sort type. That should be removed when
-                    //  the message list item's load is switched to the new state.
-                    emitEffect(MessageListEffect.RefreshMessageList(state))
-                }
-
-                if (previousState != state) {
-                    this.previousState = state
-                }
             }
             .launchIn(viewModelScope)
     }
