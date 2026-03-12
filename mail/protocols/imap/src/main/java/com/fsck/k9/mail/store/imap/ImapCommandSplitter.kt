07490ae3e29307d6638ca2ed511a8f4a692945b5
Wolf-Martell Montwé <wolf@thunderbird.net>
2026-03-12 19:14:09 +0100

    refactor: convert ImapCommandSplitter from Java to Kotlin

--- mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/ImapCommandSplitter.kt
+++ mail/protocols/imap/src/main/java/com/fsck/k9/mail/store/imap/ImapCommandSplitter.kt
@@ -1,64 +1,60 @@
-package com.fsck.k9.mail.store.imap;
+package com.fsck.k9.mail.store.imap
 
+import java.util.ArrayList
+import java.util.TreeSet
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+internal object ImapCommandSplitter {
+    fun splitCommand(prefix: String, suffix: String, groupedIds: GroupedIds, lengthLimit: Int): List<String> {
+        val commands: MutableList<String> = ArrayList()
+        val workingIdSet: MutableSet<Long> = TreeSet(groupedIds.ids)
+        val workingIdGroups: MutableList<ContiguousIdGroup> = ArrayList(groupedIds.idGroups)
 
-
-class ImapCommandSplitter {
-    static List<String> splitCommand(String prefix, String suffix, GroupedIds groupedIds, int lengthLimit) {
-        List<String> commands = new ArrayList<>();
-        Set<Long> workingIdSet = new TreeSet<>(groupedIds.ids);
-        List<ContiguousIdGroup> workingIdGroups = new ArrayList<>(groupedIds.idGroups);
-
-        int suffixLength = suffix.length();
-        int staticCommandLength = prefix.length() + suffixLength + 2;
-        while (!workingIdSet.isEmpty() || !workingIdGroups.isEmpty()) {
-            StringBuilder commandBuilder = new StringBuilder(prefix).append(' ');
-            int length = staticCommandLength;
+        val suffixLength = suffix.length
+        val staticCommandLength = prefix.length + suffixLength + 2
+        while (workingIdSet.isNotEmpty() || workingIdGroups.isNotEmpty()) {
+            val commandBuilder = StringBuilder(prefix).append(' ')
+            var length = staticCommandLength
             while (length < lengthLimit) {
-                if (!workingIdSet.isEmpty()) {
-                    Long id = workingIdSet.iterator().next();
-                    String idString = Long.toString(id);
+                if (workingIdSet.isNotEmpty()) {
+                    val id = workingIdSet.iterator().next()
+                    val idString = id.toString()
 
-                    length += idString.length() + 1;
+                    length += idString.length + 1
                     if (length >= lengthLimit) {
-                        break;
+                        break
                     }
 
-                    commandBuilder.append(idString).append(',');
-                    workingIdSet.remove(id);
-                } else if (!workingIdGroups.isEmpty()) {
-                    ContiguousIdGroup idGroup = workingIdGroups.iterator().next();
-                    String idGroupString = idGroup.toString();
+                    commandBuilder.append(idString).append(',')
+                    workingIdSet.remove(id)
+                } else if (workingIdGroups.isNotEmpty()) {
+                    val idGroup = workingIdGroups.iterator().next()
+                    val idGroupString = idGroup.toString()
 
-                    length += idGroupString.length() + 1;
+                    length += idGroupString.length + 1
                     if (length >= lengthLimit) {
-                        break;
+                        break
                     }
 
-                    commandBuilder.append(idGroupString).append(',');
-                    workingIdGroups.remove(idGroup);
+                    commandBuilder.append(idGroupString).append(',')
+                    workingIdGroups.remove(idGroup)
                 } else {
-                    break;
+                    break
                 }
             }
 
             if (suffixLength != 0) {
                 // Replace the last comma with a space
-                commandBuilder.setCharAt(commandBuilder.length() - 1, ' ');
-                commandBuilder.append(suffix);
+                commandBuilder.setCharAt(commandBuilder.length - 1, ' ')
+                commandBuilder.append(suffix)
             } else {
                 // Remove last comma
-                commandBuilder.setLength(commandBuilder.length() - 1);
+                commandBuilder.setLength(commandBuilder.length - 1)
             }
 
-            String command = commandBuilder.toString();
-            commands.add(command);
+            val command = commandBuilder.toString()
+            commands.add(command)
         }
 
-        return commands;
+        return commands
     }
 }
