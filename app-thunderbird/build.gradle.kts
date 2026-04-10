71881c140372bb6dc194ede4a6973e2fdd2cb130
Daniel Darnell <daniel@thunderbird.net>
2026-04-10 00:03:54 -0700

    Bump version number post-merge

--- app-thunderbird/build.gradle.kts
+++ app-thunderbird/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         testApplicationId = "net.thunderbird.android.tests"
 
         versionCode = 4
-        versionName = "19.0"
+        versionName = "20.0"
 
         buildConfigField("String", "CLIENT_INFO_APP_NAME", "\"Thunderbird for Android\"")
     }
