563819e49ab1adcbd57008a73a6878d5a5465708
Rafael Tonholo <rafael@tonholo.dev>
2026-04-15 09:26:35 -0300

    feat(thundermail): add brand background provider with Thunderbird visual gradient

--- feature/thundermail/thunderbird/src/main/kotlin/net/thunderbird/feature/thundermail/thunderbird/inject/FeatureThundermailModule.kt
+++ feature/thundermail/thunderbird/src/main/kotlin/net/thunderbird/feature/thundermail/thunderbird/inject/FeatureThundermailModule.kt
@@ -1,6 +1,10 @@
 package net.thunderbird.feature.thundermail.thunderbird.inject
 
+import androidx.compose.ui.Modifier
+import net.thunderbird.feature.thundermail.thunderbird.ui.thunderbirdBrandBackgroundProvider
+import net.thunderbird.feature.thundermail.ui.BrandBackgroundModifierProvider
 import org.koin.dsl.module
 
 val featureThundermailModule = module {
+    single<BrandBackgroundModifierProvider> { Modifier.thunderbirdBrandBackgroundProvider() }
 }
