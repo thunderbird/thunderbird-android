1f97cf98a11cb1eeabfe5dcfff6453416adbfb3f
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2026-04-15 12:26:32 +0200

    Fix build for pre-O Android

--- core/ui/animation/manager/src/main/kotlin/net/thunderbird/core/ui/animation/manager/AnimationManager.kt
+++ core/ui/animation/manager/src/main/kotlin/net/thunderbird/core/ui/animation/manager/AnimationManager.kt
@@ -1,6 +1,7 @@
 package net.thunderbird.core.ui.animation.manager
 
 import android.animation.ValueAnimator
+import android.os.Build
 import net.thunderbird.core.preference.AnimationPreference
 import net.thunderbird.core.preference.display.visualSettings.DisplayVisualSettingsPreferenceManager
 
@@ -15,7 +16,11 @@ class DefaultAnimationManager(
         return when (visualSettingsPreferenceManager.getConfig().animationPreference) {
             AnimationPreference.ON -> true
             AnimationPreference.OFF -> false
-            AnimationPreference.FOLLOW_SYSTEM -> ValueAnimator.areAnimatorsEnabled()
+            AnimationPreference.FOLLOW_SYSTEM -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                ValueAnimator.areAnimatorsEnabled()
+            } else {
+                true
+            }
         }
     }
 }
