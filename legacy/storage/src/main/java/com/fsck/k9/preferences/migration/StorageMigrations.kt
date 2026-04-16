77b13989b61e3fb458b445ca66c083ae7578658c
Rafael Tonholo <rafael@tonholo.dev>
2026-04-16 08:19:14 -0300

    chore: revert unwanted changes

--- legacy/storage/src/main/java/com/fsck/k9/preferences/migration/StorageMigrations.kt
+++ legacy/storage/src/main/java/com/fsck/k9/preferences/migration/StorageMigrations.kt
@@ -15,6 +15,7 @@ internal object StorageMigrations {
         if (oldVersion < 6) StorageMigrationTo6(db, migrationsHelper).performLegacyMigrations()
         if (oldVersion < 7) StorageMigrationTo7(db, migrationsHelper).rewriteEnumOrdinalsToNames()
         if (oldVersion < 8) StorageMigrationTo8(db, migrationsHelper).rewriteTheme()
+        // 9: "Temporarily disable Push" is no longer necessary
         if (oldVersion < 10) StorageMigrationTo10(db, migrationsHelper).removeSavedFolderSettings()
         if (oldVersion < 11) StorageMigrationTo11(db, migrationsHelper).upgradeMessageViewContentFontSize()
         if (oldVersion < 12) StorageMigrationTo12(db, migrationsHelper).removeStoreAndTransportUri()
