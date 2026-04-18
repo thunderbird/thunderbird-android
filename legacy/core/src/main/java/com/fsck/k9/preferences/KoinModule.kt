1598cb2e5b465d4257071c3de5c664dfa6f56f83
shamim-emon <emon.dev.bd@gmail.com>
2026-04-19 05:29:47 +0600

    feat: integrate preferenceChangeBroker to DefaultNetworkSettingsPreferenceManager

--- legacy/core/src/main/java/com/fsck/k9/preferences/KoinModule.kt
+++ legacy/core/src/main/java/com/fsck/k9/preferences/KoinModule.kt
@@ -127,6 +127,7 @@ val preferencesModule = module {
             logger = get(),
             storagePersister = get(),
             storageEditor = get<Preferences>().createStorageEditor(),
+            preferenceChangeBroker = get(),
         )
     }
     single<DebuggingSettingsPreferenceManager> {
