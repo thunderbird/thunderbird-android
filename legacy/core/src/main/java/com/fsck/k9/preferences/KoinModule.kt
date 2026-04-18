89507eb6a3bea81442e372ef1367d614edb3cb57
shamim-emon <emon.dev.bd@gmail.com>
2026-04-19 05:23:08 +0600

    feat: integrate preferenceChangeBroker to DefaultDebuggingSettingsPreferenceManager

--- legacy/core/src/main/java/com/fsck/k9/preferences/KoinModule.kt
+++ legacy/core/src/main/java/com/fsck/k9/preferences/KoinModule.kt
@@ -130,6 +130,7 @@ val preferencesModule = module {
             storageEditor = get<Preferences>().createStorageEditor(),
             logLevelManager = get(),
             platformConfigProvider = get(),
+            preferenceChangeBroker = get(),
         )
     }
     single<PlatformInitializer> {
