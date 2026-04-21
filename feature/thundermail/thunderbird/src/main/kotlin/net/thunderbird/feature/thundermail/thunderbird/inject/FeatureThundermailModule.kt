a61593182fbcb4d1c43180e0c263f2890051f1b6
Rafael Tonholo <rafael@tonholo.dev>
2026-04-21 08:29:33 -0300

    feat(thundermail): add brand background provider for k-9 mail

--- feature/thundermail/thunderbird/src/main/kotlin/net/thunderbird/feature/thundermail/thunderbird/inject/FeatureThundermailModule.kt
+++ feature/thundermail/thunderbird/src/main/kotlin/net/thunderbird/feature/thundermail/thunderbird/inject/FeatureThundermailModule.kt
@@ -1,10 +1,9 @@
 package net.thunderbird.feature.thundermail.thunderbird.inject
 
-import androidx.compose.ui.Modifier
-import net.thunderbird.feature.thundermail.thunderbird.ui.thunderbirdBrandBackgroundProvider
+import net.thunderbird.feature.thundermail.thunderbird.ui.ThunderbirdBrandBackgroundProvider
 import net.thunderbird.feature.thundermail.ui.BrandBackgroundModifierProvider
 import org.koin.dsl.module
 
 val featureThundermailModule = module {
-    single<BrandBackgroundModifierProvider> { Modifier.thunderbirdBrandBackgroundProvider() }
+    single<BrandBackgroundModifierProvider> { ThunderbirdBrandBackgroundProvider }
 }
