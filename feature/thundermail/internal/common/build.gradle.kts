bf62965e24aeb585c4201779c35d01feaad3de70
Rafael Tonholo <rafael@tonholo.dev>
2026-04-22 16:09:01 -0300

    chore(thundermail): add :feature:thundermail:internal:common module

---
+++ feature/thundermail/internal/common/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id(ThunderbirdPlugins.Library.androidCompose)
+}
+
+android {
+    namespace = "net.thunderbird.feature.thundermail.internal.common"
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
+
+dependencies {
+    implementation(projects.core.logging.api)
+    implementation(projects.core.ui.compose.designsystem)
+    implementation(projects.core.ui.compose.theme2.common)
+    implementation(projects.feature.account.common)
+    implementation(projects.feature.account.oauth)
+    implementation(projects.feature.account.setup)
+    implementation(projects.feature.autodiscovery.api)
+    implementation(projects.feature.thundermail.api)
+    implementation(libs.appauth)
+}
+
+codeCoverage {
+    branchCoverage = 0
+    lineCoverage = 0
+}
