bf5a552c70677944893890a79e324689a6532aa7
shamim-emon <emon.dev.bd@gmail.com>
2026-04-30 20:49:59 +0600

    refactor: add emission scope support to PreferenceChangePublisher.

--- core/preference/api/src/commonMain/kotlin/net/thunderbird/core/preference/PreferenceChangePublisher.kt
+++ core/preference/api/src/commonMain/kotlin/net/thunderbird/core/preference/PreferenceChangePublisher.kt
@@ -2,11 +2,15 @@ package net.thunderbird.core.preference
 
 /**
  * Publishes changes of preferences to all subscribers.
+ * The change can be scoped to a specific category of preferences or applied globally.
+ * @see PreferenceScope for available scopes of preference changes.
  */
 interface PreferenceChangePublisher {
 
     /**
      * Publish a change in the preferences.
+     * @param scope Defines which category of preferences has changed.
+     * By default, [PreferenceScope.ALL] is used, indicating a global change affecting all preferences.
      */
-    fun publish()
+    fun publish(scope: PreferenceScope = PreferenceScope.ALL)
 }
