9603de71a121958725955469f4827e6e3d84d8f4
shamim-emon <emon.dev.bd@gmail.com>
2026-04-30 23:38:12 +0600

    feat: support scoped preference change handling in DefaultNetworkSettingsPreferenceManager

--- core/preference/api/src/commonMain/kotlin/net/thunderbird/core/preference/network/NetworkSettingsPreferenceManager.kt
+++ core/preference/api/src/commonMain/kotlin/net/thunderbird/core/preference/network/NetworkSettingsPreferenceManager.kt
@@ -2,6 +2,17 @@ package net.thunderbird.core.preference.network
 
 import net.thunderbird.core.preference.PreferenceManager
 
-const val KEY_BG_OPS = "backgroundOperations"
+enum class NetworkSettingKey(val value: String) {
 
+    BackgroundOperations("backgroundOperations"),
+    ;
+}
+
+    companion object {
+
+        fun isValid(value: String): Boolean {
+            return entries.any { it.value == value }
+        }
+    }
+}
 interface NetworkSettingsPreferenceManager : PreferenceManager<NetworkSettings>
--- legacy/core/src/main/java/com/fsck/k9/preferences/KoinModule.kt
+++ legacy/core/src/main/java/com/fsck/k9/preferences/KoinModule.kt
@@ -126,6 +126,7 @@ val preferencesModule = module {
             logger = get(),
             storagePersister = get(),
             storageEditor = get<Preferences>().createStorageEditor(),
+            preferenceChangeBroker = get(),
         )
     }
     single<DebuggingSettingsPreferenceManager> {
