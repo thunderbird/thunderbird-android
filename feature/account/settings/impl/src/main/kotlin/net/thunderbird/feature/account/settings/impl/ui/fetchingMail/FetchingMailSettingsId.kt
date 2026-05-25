2017f9ba54153926b9ee287ea28534908aa0b2b8
shamim-emon <emon.dev.bd@gmail.com>
2026-05-25 21:38:14 +0600

    feat: add FetchingMailSettingsId

---
+++ feature/account/settings/impl/src/main/kotlin/net/thunderbird/feature/account/settings/impl/ui/fetchingMail/FetchingMailSettingsId.kt
@@ -0,0 +1,16 @@
+package net.thunderbird.feature.account.settings.impl.ui.fetchingMail
+
+object FetchingMailSettingsId {
+    const val LOCAL_FOLDER_SIZE = "local_folder_size"
+    const val SYNC_MESSAGE_FROM = "sync_message_from"
+    const val FETCH_MESSAGE_UP_TO = "fetch_message_up_to"
+    const val FOLDER_POLL_FREQUENCY = "folder_poll_frequency"
+    const val SYNC_SERVER_DELETIONS = "sync_server_deletions"
+    const val MARK_AS_READ_WHEN_DELETED = "mark_as_read_when_deleted"
+    const val WHEN_I_DELETE_A_MESSAGE = "when_i_delete_a_message"
+    const val ERASE_DELETED_MESSAGE_ON_SERVER = "erase_deleted_message_on_server"
+    const val IN_COMING_SERVER = "in_coming_server"
+    const val ADVANCE = "advance"
+    const val MAX_FOLDER_TO_CHECK_WITH_PUSH = "max_folder_to_check_with_push"
+    const val REFRESH_IDLE_CONNECTION = "refresh_idle_connection"
+}
