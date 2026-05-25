a37a595b2efce1a90be5b771f22a87da403e50e8
shamim-emon <emon.dev.bd@gmail.com>
2026-05-25 22:33:02 +0600

    feat: add AdvancedFetchingMailSettingsScreen

---
+++ feature/account/settings/impl/src/main/kotlin/net/thunderbird/feature/account/settings/impl/ui/fetchingMail/advanced/AdvancedFetchingMailSettingsScreen.kt
@@ -0,0 +1,49 @@
+package net.thunderbird.feature.account.settings.impl.ui.fetchingMail.advanced
+
+import android.annotation.SuppressLint
+import androidx.activity.compose.BackHandler
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.NoOpUpdate
+import kotlin.uuid.ExperimentalUuidApi
+import net.thunderbird.core.ui.contract.mvi.observe
+import net.thunderbird.core.ui.setting.SettingViewProvider
+import net.thunderbird.feature.account.AccountId
+import net.thunderbird.feature.account.settings.impl.ui.fetchingMail.FetchingMailSettingsContract
+import net.thunderbird.feature.account.settings.impl.ui.fetchingMail.FetchingMailSettingsContract.Effect
+import net.thunderbird.feature.account.settings.impl.ui.fetchingMail.FetchingMailSettingsViewModel
+import org.koin.compose.koinInject
+import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.parameter.parametersOf
+
+@OptIn(ExperimentalUuidApi::class)
+@SuppressLint("ContextCastToActivity")
+@Composable
+internal fun AdvancedFetchingMailSettingsScreen(
+    accountId: AccountId,
+    onBack: () -> Unit,
+    viewModel: FetchingMailSettingsContract.ViewModel = koinViewModel<FetchingMailSettingsViewModel> {
+        parametersOf(accountId)
+    },
+    provider: SettingViewProvider = koinInject(),
+    builder: FetchingMailSettingsContract.SettingsBuilder = koinInject(),
+) {
+    val (state, dispatch) = viewModel.observe { effect ->
+        when (effect) {
+            is Effect.NavigateBack -> {
+                onBack()
+            }
+
+            else -> {
+                NoOpUpdate
+            }
+        }
+    }
+    BackHandler(onBack = onBack)
+
+    AdvancedFetchingMailSettingsContent(
+        state = state.value,
+        onEvent = { dispatch(it) },
+        provider = provider,
+        builder = builder,
+    )
+}
