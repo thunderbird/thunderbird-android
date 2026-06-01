e18592c6a6c7f908205e65c1545a3f7c5bd690b5
Rafael Tonholo <rafael@tonholo.dev>
2026-06-01 08:19:24 -0300

    chore: convert AttachmentViewInfo.java to Kotlin

--- legacy/core/src/main/java/com/fsck/k9/mailstore/AttachmentViewInfo.kt
+++ legacy/core/src/main/java/com/fsck/k9/mailstore/AttachmentViewInfo.kt
@@ -1,55 +1,40 @@
-package com.fsck.k9.mailstore;
+package com.fsck.k9.mailstore
 
+import android.net.Uri
+import com.fsck.k9.helper.MimeTypeUtil
+import com.fsck.k9.mail.Part
 
-import android.net.Uri;
-
-import com.fsck.k9.helper.MimeTypeUtil;
-import com.fsck.k9.mail.Part;
-
-
-public class AttachmentViewInfo {
-    public static final long UNKNOWN_SIZE = -1;
-
-    public final String mimeType;
-    public final String displayName;
-    public final long size;
-
-    /**
-     * A content provider URI that can be used to retrieve the decoded attachment.
-     * <p/>
-     * Note: All content providers must support an alternative MIME type appended as last URI segment.
-     */
-    public final Uri internalUri;
-    public final boolean inlineAttachment;
-    public final Part part;
-    private boolean contentAvailable;
-
-    public AttachmentViewInfo(String mimeType, String displayName, long size, Uri internalUri, boolean inlineAttachment,
-            Part part, boolean contentAvailable) {
-        this.mimeType = mimeType;
-        this.displayName = displayName;
-        this.size = size;
-        this.internalUri = internalUri;
-        this.inlineAttachment = inlineAttachment;
-        this.part = part;
-        this.contentAvailable = contentAvailable;
+/**
+ * @property internalUri A content provider URI that can be used to retrieve the decoded attachment.
+ * Note: All content providers must support an alternative MIME type appended as last URI segment.
+ */
+class AttachmentViewInfo(
+    @JvmField val mimeType: String?,
+    @JvmField val displayName: String,
+    @JvmField val size: Long,
+    @JvmField val internalUri: Uri,
+    @JvmField val inlineAttachment: Boolean,
+    @JvmField val part: Part?,
+    var isContentAvailable: Boolean,
+) {
+    fun setContentAvailable() {
+        this.isContentAvailable = true
     }
 
-    public boolean isContentAvailable() {
-        return contentAvailable;
-    }
+    val isSupportedImage: Boolean
+        get() {
+            if (mimeType == null) {
+                return false
+            }
 
-    public void setContentAvailable() {
-        this.contentAvailable = true;
-    }
-
-    public boolean isSupportedImage() {
-        if (mimeType == null) {
-            return false;
+            return MimeTypeUtil.isSupportedImageType(mimeType) || (MimeTypeUtil.isSameMimeType(
+                MimeTypeUtil.DEFAULT_ATTACHMENT_MIME_TYPE,
+                mimeType,
+            ) &&
+                MimeTypeUtil.isSupportedImageExtension(displayName))
         }
 
-        return MimeTypeUtil.isSupportedImageType(mimeType) || (
-                MimeTypeUtil.isSameMimeType(MimeTypeUtil.DEFAULT_ATTACHMENT_MIME_TYPE, mimeType) &&
-                MimeTypeUtil.isSupportedImageExtension(displayName));
+    companion object {
+        const val UNKNOWN_SIZE: Long = -1
     }
 }
