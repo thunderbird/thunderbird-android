07dee96deb7c5d01b038dde8ac19b4bd7d1d19cb
thunderbird-botmobile <202943968+thunderbird-botmobile[bot]@users.noreply.github.com>
2026-06-03 20:35:16 +0000

    Release: K-9 Mail 19.1

--- app-k9mail/build.gradle.kts
+++ app-k9mail/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.fsck.k9"
         testApplicationId = "com.fsck.k9.tests"
 
-        versionCode = 39036
+        versionCode = 39037
         versionName = "19.1"
 
         buildConfigField("String", "CLIENT_INFO_APP_NAME", "\"K-9 Mail\"")
