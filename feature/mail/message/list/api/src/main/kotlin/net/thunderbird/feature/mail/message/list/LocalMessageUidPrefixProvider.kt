6db787df2aaa1475a25bf9e8e12b8d79f02cd7c4
Rafael Tonholo <rafael@tonholo.dev>
2026-06-03 11:33:02 -0300

    docs: add documentation to LocalMessageUidPrefixProvider interface

--- feature/mail/message/list/api/src/main/kotlin/net/thunderbird/feature/mail/message/list/LocalMessageUidPrefixProvider.kt
+++ feature/mail/message/list/api/src/main/kotlin/net/thunderbird/feature/mail/message/list/LocalMessageUidPrefixProvider.kt
@@ -1,5 +1,16 @@
 package net.thunderbird.feature.mail.message.list
 
+/**
+ * Provides a unique prefix string for generating local message UIDs.
+ *
+ * Implementations should guarantee that the returned prefix is consistent and unique
+ * within the context where it will be used, typically per account or per application instance.
+ */
 interface LocalMessageUidPrefixProvider {
+    /**
+     * Returns the unique prefix string used for generating local message UIDs.
+     *
+     * @return The prefix string to be used when creating unique identifiers for locally stored messages.
+     */
     fun get(): String
 }
