1f808251bb7486912a6109032cf628ee56298edc
Rafael Tonholo <rafael@tonholo.dev>
2026-06-04 08:57:37 -0300

    refactor(migration): replace NoOpMigrationManager with K9MigrationManager using QrCodeScannerActivityContract (#11099)

--- feature/migration/launcher/noop/src/main/kotlin/app/k9mail/feature/migration/launcher/FeatureMigrationModule.kt
+++ feature/migration/launcher/noop/src/main/kotlin/app/k9mail/feature/migration/launcher/FeatureMigrationModule.kt
@@ -1,9 +1,11 @@
 package app.k9mail.feature.migration.launcher
 
 import app.k9mail.feature.migration.launcher.api.MigrationManager
-import app.k9mail.feature.migration.launcher.noop.NoOpMigrationManager
+import app.k9mail.feature.migration.launcher.k9.K9MigrationManager
+import app.k9mail.feature.migration.qrcode.qrCodeModule
 import org.koin.dsl.module
 
 val featureMigrationModule = module {
-    single<MigrationManager> { NoOpMigrationManager() }
+    includes(qrCodeModule)
+    single<MigrationManager> { K9MigrationManager() }
 }
