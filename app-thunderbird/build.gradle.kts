0a5ef202d063f8a71f59a264f17de8f227e79790
Stephen King <sking@thunderbird.net>
2026-06-18 11:54:03 -0700

    Bump versionName to 22.0

--- app-thunderbird/build.gradle.kts
+++ app-thunderbird/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         testApplicationId = "net.thunderbird.android.tests"
 
         versionCode = 4
-        versionName = "21.0"
+        versionName = "22.0"
 
         buildConfigField("String", "CLIENT_INFO_APP_NAME", "\"Thunderbird for Android\"")
     }
