694f74aac4cb66045ab4230eaac7995d2a092168
thunderbird-botmobile <202943968+thunderbird-botmobile[bot]@users.noreply.github.com>
2026-07-10 12:31:00 +0000

    Deploy docs update from [a92a325]

--- docs/latest/searcher-09f2665d.js
+++ docs/latest/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e5066a5c.js',
+                path_to_root + 'searchindex-3b0a0c45.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
