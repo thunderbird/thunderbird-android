d4ea8dfdeedd022ca4532b8e658d57ec9b8f4e78
Rafael Tonholo <rafael@tonholo.dev>
2026-08-18 08:51:16 -0300

    feat(webkit): add webkit api and internal modules

---
+++ core/android/webkit/internal/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id(ThunderbirdPlugins.Library.android)
+}
+
+android {
+    namespace = "net.thunderbird.core.android.webkit.internal"
+}
+
+codeCoverage {
+    branchCoverage = 66
+    lineCoverage = 71
+}
