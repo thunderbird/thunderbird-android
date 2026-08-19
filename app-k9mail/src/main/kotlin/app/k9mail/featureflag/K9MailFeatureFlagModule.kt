3ec7a7f488ea477ca9f5bf9422cb1456bedde61e
Rafael Tonholo <rafael@tonholo.dev>
2026-08-19 15:34:54 -0300

    refactor(feature-flag): rewire injections so koin is happy with concrete types injections

--- app-k9mail/src/main/kotlin/app/k9mail/featureflag/K9MailFeatureFlagModule.kt
+++ app-k9mail/src/main/kotlin/app/k9mail/featureflag/K9MailFeatureFlagModule.kt
@@ -1,13 +1,26 @@
 package app.k9mail.featureflag
 
 import net.thunderbird.core.featureflag.inject.featureFlagModule
+import net.thunderbird.core.featureflag.inject.qualifier.InjectQualifier
 import net.thunderbird.core.featureflag.model.AppVariantOverrides
 import net.thunderbird.core.featureflag.model.EmptyAppVariantOverride
+import net.thunderbird.core.featureflag.provider.CatalogFeatureFlagProvider
+import net.thunderbird.core.featureflag.provider.RuntimeDebugOverrideFeatureFlagProvider
 import net.thunderbird.core.featureflag.serialization.FlagRegistryOverrideSerializer
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val k9MailFeatureFlagModule = module {
     includes(featureFlagModule)
     factory<AppVariantOverrides.Factory> { K9MailOverrides.Factory }
     factory { FlagRegistryOverrideSerializer(k9Factory = get(), thunderbirdFactory = EmptyAppVariantOverride) }
+    single {
+        RuntimeDebugOverrideFeatureFlagProvider(
+            configStore = get(),
+            logger = get(),
+        )
+    }
+    single<CatalogFeatureFlagProvider>(named(InjectQualifier.InMemory)) {
+        get<RuntimeDebugOverrideFeatureFlagProvider>()
+    }
 }
