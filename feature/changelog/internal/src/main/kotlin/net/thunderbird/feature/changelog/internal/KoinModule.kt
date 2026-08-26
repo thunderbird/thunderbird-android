60516f2bcc17a84704f93b4a1d264eb75a75c6e7
shamim-emon <emon.dev.bd@gmail.com>
2026-08-26 15:39:24 +0600

    refactor(changelog): updated koin modules to comply with added changes.

--- feature/changelog/internal/src/main/kotlin/net/thunderbird/feature/changelog/internal/KoinModule.kt
+++ feature/changelog/internal/src/main/kotlin/net/thunderbird/feature/changelog/internal/KoinModule.kt
@@ -7,10 +7,21 @@ import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val changelogUiModule = module {
-    single { ChangeLogManager(context = get(), appCoroutineScope = get(named("AppCoroutineScope"))) }
+    single<ChangelogProvider> { ChangelogProvider(context = get(), provider = get()) }
+    single<ChangelogVersionHistory> {
+        ChangelogVersionHistory(
+            context = get(),
+            changeLogProvider = get(),
+            logger = get(),
+            storagePersister = get(),
+            storageEditor = get(),
+        )
+    }
+    single { ChangeLogManager(changelogVersionHistory = get(), appCoroutineScope = get(named("AppCoroutineScope"))) }
     viewModel { (mode: ChangeLogMode) ->
         ChangelogViewModel(generalSettingsManager = get(), changeLogManager = get(), mode = mode)
     }
     viewModel { RecentChangesViewModel(generalSettingsManager = get(), changeLogManager = get()) }
     single<ChangelogNavigation> { DefaultChangelogNavigation() }
+    single<ChangelogProvider> { ChangelogProvider(context = get(), provider = get()) }
 }
