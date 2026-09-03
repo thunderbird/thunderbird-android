937b4e5cf5f997c8766fe3dd6f971c9e189bd507
kryoseu <me@kryoseu.com>
2026-09-03 12:44:46 -0700

    Bump versionName to 25.0

--- app-k9mail/build.gradle.kts
+++ app-k9mail/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         testApplicationId = "com.fsck.k9.tests"
 
         versionCode = 39004
-        versionName = "24.0"
+        versionName = "25.0"
         versionNameSuffix = "a1"
 
         buildConfigField("String", "CLIENT_INFO_APP_NAME", "\"K-9 Mail\"")
