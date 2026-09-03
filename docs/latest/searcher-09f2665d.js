de82da2262b41ccf0a32aecc1f37fe3bdaf79fde
thunderbird-botmobile <202943968+thunderbird-botmobile[bot]@users.noreply.github.com>
2026-09-03 07:45:59 +0000

    Deploy docs update from [d5afc89]

--- docs/latest/searcher-09f2665d.js
+++ docs/latest/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d3defe69.js',
+                path_to_root + 'searchindex-3d6f2427.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
