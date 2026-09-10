8ffb8c5b590ddc92b575a0ed780d52cb81d4b3a2
itsSiide <moritz.lober@gmx.de>
2026-09-10 11:39:45 +0200

    fix: changing XML export and import to transfer K-9 avatar to tb -Added a function to correctly serialize the image instead of just setting the image uri -added test to confirm the generated xml output -added test to confirm image output is null when no image is selected

--- legacy/core/src/main/java/com/fsck/k9/preferences/SettingsExporter.kt
+++ legacy/core/src/main/java/com/fsck/k9/preferences/SettingsExporter.kt
@@ -19,8 +19,11 @@ import net.thunderbird.feature.account.storage.legacy.LegacyAccountStorageHandle
 import net.thunderbird.feature.account.storage.legacy.LegacyAccountStorageHandler.Companion.IDENTITY_EMAIL_KEY
 import net.thunderbird.feature.account.storage.legacy.LegacyAccountStorageHandler.Companion.IDENTITY_NAME_KEY
 import net.thunderbird.feature.mail.folder.api.data.repository.FolderQueryRepository
+import net.thunderbird.feature.account.storage.profile.AvatarTypeDto
 import net.thunderbird.legacy.logging.Log
 import org.xmlpull.v1.XmlSerializer
+import androidx.core.net.toUri
+import android.util.Base64
 
 class SettingsExporter(
     private val contentResolver: ContentResolver,
@@ -246,6 +249,7 @@ class SettingsExporter(
         writeFolderNameSettings(account, folderQueryRepository, serializer)
 
         serializer.endTag(null, SETTINGS_ELEMENT)
+        writeAvatarImage(account,serializer)
 
         if (identities.isNotEmpty()) {
             serializer.startTag(null, IDENTITIES_ELEMENT)
@@ -298,7 +302,24 @@ class SettingsExporter(
         }
     }
 
-    private suspend fun writeFolderNameSettings(
+    private fun writeAvatarImage(
+        account: LegacyAccountDto,
+        serializer: XmlSerializer
+    ){
+        val avatar = account.avatar
+        val uriString = avatar.avatarImageUri
+        require(avatar.avatarType == AvatarTypeDto.IMAGE && uriString != null){ return }
+
+        val uri = uriString.toUri()
+        val bytes: ByteArray? = contentResolver.openInputStream(uri)?.use { input ->
+            input.readBytes()
+        }
+        val encoded = Base64.encodeToString(bytes, Base64.NO_WRAP)
+        serializer.startTag(null, AVATAR_IMAGE_ELEMENT)
+        serializer.text(encoded)
+        serializer.endTag(null, AVATAR_IMAGE_ELEMENT)
+    }
+    private fun writeFolderNameSettings(
         account: LegacyAccountDto,
         folderQueryRepository: FolderQueryRepository,
         serializer: XmlSerializer,
@@ -547,6 +568,8 @@ class SettingsExporter(
         const val NAME_ELEMENT = "name"
         const val EMAIL_ELEMENT = "email"
         const val DESCRIPTION_ELEMENT = "description"
+
+        const val AVATAR_IMAGE_ELEMENT = "avatar-image"
 
         private val FOLDER_NAME_KEYS = setOf(
             "autoExpandFolderName",
